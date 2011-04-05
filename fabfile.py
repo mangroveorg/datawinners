d0802e776be25dda195f40e69a4a68ed50601ebf
asifmomin <asifmom@thoughtworks.com>
2011-04-05 16:57:27 +0530

    Adding the fabric file for deployment on linode<Asif,Shweta>

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import run
+from fabric.context_managers import cd, settings
+
+def deploy(branch,commit_sha):
+    code_dir = '/home/mangrover/mangrove'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone git://github.com/mangroveorg/mangrove.git %s" % code_dir)
+    with cd(code_dir):
+        run("git checkout -b %s %s"% branch,commit_sha )
