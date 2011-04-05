ae53e92032a9b0bf2e7903ccade6ab1e6c56a9f6
asifmomin <asifmom@thoughtworks.com>
2011-04-05 18:07:04 +0530

    Fabfile for deployment <shweta/asif>

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,6 @@ def deploy(branch,commit_sha):
     code_dir = '/home/mangrover/mangrove'
     with settings(warn_only=True):
         if run("test -d %s" % code_dir).failed:
-            run("git clone git://github.com/mangroveorg/mangrove.git %s" % code_dir)
+           run("git clone git://github.com/mangroveorg/mangrove.git %s" % code_dir)
     with cd(code_dir):
-        run("git checkout -b %s %s"% branch,commit_sha )
+        run("git checkout -b %s %s" % (branch,commit_sha) )
