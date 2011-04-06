a8d891f4f892fc4e1a34c52b3aac7d5dff76827c
Shweta <shwetags@thoughtworks.com>
2011-04-06 15:58:57 +0530

    Fixed a fabfile settings error

--- fabfile.py
+++ fabfile.py
@@ -7,15 +7,15 @@ def deploy(branch,commit_sha):
     with settings(warn_only=True):
         if run("test -d %s" % code_dir).failed:
            run("git clone git://github.com/mangroveorg/mangrove.git %s" % home_dir)
-    with cd(code_dir):
-        run("git pull origin develop")
-        if run("git branch -a|grep %s" % branch).failed:
-            run("git checkout -b %s %s" % (branch,commit_sha) )
-        else:
-            run("git branch -D %s" % branch)
-            run("git checkout -b %s %s" % (branch,commit_sha) )
-            
-        run("source %s/awe_ve/bin/activate" % home_dir )
-        run("pip install -r requirements.pip")
-    with cd(code_dir+'/src/datawinners'):
-        run("python manage.py syncdb")
+        with cd(code_dir):
+            run("git checkout develop")
+            run("git pull origin develop")
+            if run("git branch -a|grep %s" % branch).failed:
+                run("git checkout -b %s %s" % (branch,commit_sha) )
+            else:
+                run("git branch -D %s" % branch)
+                run("git checkout -b %s %s" % (branch,commit_sha) )
+            run("source %s/awe_ve/bin/activate" % home_dir )
+            run("pip install -r requirements.pip")
+        with cd(code_dir+'/src/datawinners'):
+            run("python manage.py syncdb")
