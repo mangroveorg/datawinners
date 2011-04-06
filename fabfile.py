fddf1a14259bd1884ee93d25f374f38fc862eafc
asifmomin <asifmom@thoughtworks.com>
2011-04-06 15:14:33 +0530

    fixing bugs on activating the virtual env

--- fabfile.py
+++ fabfile.py
@@ -2,14 +2,15 @@ from fabric.api import run
 from fabric.context_managers import cd, settings
 
 def deploy(branch,commit_sha):
-    code_dir = '/home/mangrover/mangrove'
+    home_dir = '/home/mangrover/'
+    code_dir = home_dir+'mangrove'
     with settings(warn_only=True):
         if run("test -d %s" % code_dir).failed:
-           run("git clone git://github.com/mangroveorg/mangrove.git %s" % code_dir)
+           run("git clone git://github.com/mangroveorg/mangrove.git %s" % home_dir)
     with cd(code_dir):
         run("git pull origin develop")
         run("git checkout -b %s %s" % (branch,commit_sha) )
-        run("source %s/awe_ve/bin/activate" % code_dir )
+        run("source %s/awe_ve/bin/activate" % home_dir )
         run("pip install -r requirements.pip")
     with cd(code_dir+'/src/datawinners'):
         run("python manage.py syncdb")
