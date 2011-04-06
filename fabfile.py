a2b2d66054516ee10b50be6fee4f23d173dc7614
asifmomin <asifmom@thoughtworks.com>
2011-04-06 13:21:33 +0530

    modification in fab file to do git pull

--- fabfile.py
+++ fabfile.py
@@ -7,4 +7,5 @@ def deploy(branch,commit_sha):
         if run("test -d %s" % code_dir).failed:
            run("git clone git://github.com/mangroveorg/mangrove.git %s" % code_dir)
     with cd(code_dir):
+        run("git pull origin develop")
         run("git checkout -b %s %s" % (branch,commit_sha) )
