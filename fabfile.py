49a5646a3f5bde0bae03fcf53a5a4ad110503818
Shweta <shwetags@thoughtworks.com>
2011-04-07 14:29:45 +0530

    Modified the fab file to deploy on any machine

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,29 @@
 from fabric.api import run
 from fabric.context_managers import cd, settings
 
-def deploy(branch,commit_sha):
-    home_dir = '/home/mangrover/'
-    code_dir = home_dir+'mangrove'
+def deploy(build_number,home_dir,virtual_env):
+    """build_number : hudson build number to be deployed
+       home_dir: directory where you want to deploy the source code
+       virtual_env : path to your virtual_env folder
+    """
+    run("export COMMIT_SHA=`curl http://hudson.mvpafrica.org:8080/job/Mangrove-develop/%s/artifact/last_successful_commit_sha`" % (build_number,))
+
+    code_dir = home_dir+'/mangrove'
     with settings(warn_only=True):
         if run("test -d %s" % code_dir).failed:
-           run("git clone git://github.com/mangroveorg/mangrove.git %s" % home_dir)
+           run("git clone git://github.com/mangroveorg/mangrove.git %s" % code_dir)
         with cd(code_dir):
             run("git checkout develop")
             run("git pull origin develop")
-            if run("git branch -a|grep %s" % branch).failed:
-                run("git checkout -b %s %s" % (branch,commit_sha) )
+            if run("git branch -a|grep %s" % build_number).failed:
+                run("git checkout -b %s $COMMIT_SHA" % (build_number,) )
             else:
-                run("git branch -D %s" % branch)
-                run("git checkout -b %s %s" % (branch,commit_sha) )
+                run("git branch -D %s" % build_number)
+                run("git checkout -b %s $COMMIT_SHA" % (build_number,) )
             
-            run('source %sawe_ve/bin/activate && ' % home_dir + "pip install -r requirements.pip" )
+            activate_and_run(virtual_env,"pip install -r requirements.pip")
         with cd(code_dir+'/src/datawinners'):
-            run("python manage.py syncdb")
+            activate_and_run(virtual_env,"python manage.py syncdb")
+
+def activate_and_run(virtual_env,command):
+    run('source %s/bin/activate && ' % virtual_env + command)
