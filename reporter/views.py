ebfcbda18416e9c9e15c13bd3ef0e91e2d27f88b
Aroj George <apgeorge@thoughtworks.com>
2011-04-14 00:48:02 +0530

    Query API: returns aggregation results with one row per entity.

--- reporter/views.py
+++ reporter/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from datastore.database import get_db_manager
 from datawinners.accountmanagement.models import Organization
 from datawinners.reporter.forms import ReporterRegistrationForm
 
@@ -20,7 +21,7 @@ def register(request):
         form_data = form.cleaned_data
         print form_data
         data =[(k,v) for k,v in form_data.items()]
-        registered_reporter = datarecord.register(entity_type=[organization.name, "Reporter"], data=data, location=[],
+        registered_reporter = datarecord.register(manager=get_db_manager(),entity_type=[organization.name, "Reporter"], data=data, location=[],
                                                   source="Web")
         form = ReporterRegistrationForm()
         message = "The reporter was successfully registered with id %s" % registered_reporter.id
