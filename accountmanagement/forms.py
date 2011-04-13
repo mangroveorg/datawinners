bccdb405136cf706d119037cbe303a6d5933ccd6
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-04-13 19:04:16 +0530

    "Fixing showcase environment"

--- accountmanagement/forms.py
+++ accountmanagement/forms.py
@@ -30,8 +30,7 @@ class RegistrationForm(RegistrationFormUniqueEmail):
 
 
     def clean(self):
-        email = self.cleaned_data.get('email')
-        self.cleaned_data['email'] = email.lower()
+        self.cleaned_data['email'] = self.cleaned_data.get('email').lower()
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 msg = "The two password fields didn't match."
