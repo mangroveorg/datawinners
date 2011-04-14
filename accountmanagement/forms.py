bff307e3aaa4dee658befb30d357983973906056
asifmomin <asifmom@thoughtworks.com>
2011-04-14 12:14:06 +0530

    Fixing the error for duplication

--- accountmanagement/forms.py
+++ accountmanagement/forms.py
@@ -30,12 +30,17 @@ class RegistrationForm(RegistrationFormUniqueEmail):
 
 
     def clean(self):
-        self.cleaned_data['email'] = self.cleaned_data.get('email').lower()
+        self.convert_email_to_lowercase()
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 msg = "The two password fields didn't match."
                 self._errors['password1'] = self.error_class([msg])
         return self.cleaned_data
+
+    def convert_email_to_lowercase(self):
+        email = self.cleaned_data.get('email')
+        if(email is not None):
+            self.cleaned_data['email'] = email.lower()
 
 
 class LoginForm(AuthenticationForm):
