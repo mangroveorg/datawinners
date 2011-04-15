c4fcfc67856fc87c55f06335dca2e5987ed54bf4
Mahesh Lal <maheshkl@mahesh.(none)>
2011-04-15 14:27:20 +0530

    Fixing register reporter issue|<Akshay/Mahesh>

--- reporter/forms.py
+++ reporter/forms.py
@@ -6,8 +6,8 @@ class ReporterRegistrationForm(Form):
     error_css_class = 'error'
     required_css_class = 'required'
 
-    first_name = RegexField(regex="^[a-zA-Z]$", max_length=30, error_message="Please enter a valid value containing only letters a-z or A-Z.", required=True)
-    last_name = RegexField(regex="^[a-zA-Z]^", max_length=30, error_message="Please enter a valid value containing only letters a-z or A-Z.", required=True)
+    first_name = RegexField(regex="^[a-zA-Z]*$", max_length=30, error_message="Please enter a valid value containing only letters a-z or A-Z.", required=True)
+    last_name = RegexField(regex="^[a-zA-Z]*$", max_length=30, error_message="Please enter a valid value containing only letters a-z or A-Z.", required=True)
     telephone_number = USPhoneNumberField(required=True)
     commune = CharField(max_length=30, required=True)
     clinical_affiliation = CharField(max_length=30, required=False)
