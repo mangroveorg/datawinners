c9a08340f513b801f8bd4f7d16ee84974f026c98
Mahesh Lal <maheshkl@mahesh.(none)>
2011-04-15 15:32:47 +0530

    Reporting - Creating the page for location and non location based reporting on entity|<Mahesh/Akshay>

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from mangrove.datastore.database import get_db_manager
@@ -5,22 +6,29 @@ from mangrove.datastore import data
 from reports.forms import Report
 
 def report(request):
-    form = Report()
-    form.entity_type = "Clinic"
-    report_data = data.fetch(get_db_manager(),entity_type=["Health_Facility","Clinic"],
-                            aggregates = {  "director" : "latest" ,
-                                             "beds" : "latest" ,
-                                             "patients" : "sum"  },
-                            aggregate_on = { 'type' : 'entity'},
-                            filter = { 'location' : ['India','MH','Pune']}
-                            )
-    for id, record in report_data.items():
-        record["Clinic_id"] = id
-    id, info= report_data.items()[0]
-    form.column_headers = info.keys()
-    form.column_headers.sort()
-    information = [v for k, v in report_data.items()]
-    form.values = []
-    for row in information:
-        form.values.append([row[h] for h in form.column_headers])
+    if request.method == 'POST':
+        form = Report(request.POST)
+        if form.is_valid():
+            entity_type = form.cleaned_data['entity_type'].split(",")
+            filter_criteria = form.cleaned_data['filter']
+            filter = { 'location' : filter_criteria.split(",")} if filter_criteria else None
+            report_data = data.fetch(get_db_manager(),entity_type=entity_type,
+                                aggregates = {  "director" : "latest" ,
+                                                 "beds" : "latest" ,
+                                                 "patients" : "sum"  },
+                                filter = filter
+                                )
+            print report_data
+            for id, record in report_data.items():
+                record["Clinic_id"] = id
+            id, info= report_data.items()[0] if report_data.items() else ("",{})
+            form.column_headers = info.keys()
+            form.column_headers.sort()
+            information = [v for k, v in report_data.items()]
+            form.values = []
+            for row in information:
+                form.values.append([row[h] for h in form.column_headers])
+    else:
+        form = Report()
     return render_to_response('reports/reportperlocation.html', {'form' : form},context_instance=RequestContext(request))
+
