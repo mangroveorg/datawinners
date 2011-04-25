12f7159ee7373f2bca87cea81e762c1bf9471212
Shweta <shwetags@thoughtworks.com>
2011-04-25 16:58:58 +0530

    Completed the front end for creating a questionnaire.:Shweta/Ashish/Aroj

--- media/javascript/questionnaire.js
+++ media/javascript/questionnaire.js
@@ -1,23 +1,43 @@
 // vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 var viewModel = null;
 $(document).ready(function(){
+    function Question(){
+                        this.title=ko.observable('default');
+                        this.code=ko.observable('default');
+                        this.description=ko.observable('');
+                        this.type=ko.observable('text');
+                        this.choices= ko.observableArray([]);
+                        }
     viewModel =
     {
         questions : ko.observableArray([]),
         addQuestion : function(){
-            question = {title:ko.observable(''), code:ko.observable(''), description: ko.observable(''), type:ko.observable('text'), choices: ko.observableArray([])};
+            var question = new Question();
+            question.display = ko.dependentObservable(function(){
+                                        return this.title() + ' ' + this.code();
+                                       }, question);
             viewModel.questions.push(question);
             viewModel.selectedQuestion(question);
             viewModel.selectedQuestion.valueHasMutated();
             viewModel.questions.valueHasMutated();
         },
+
         removeQuestion: function(question){
             viewModel.questions.remove(question);
             viewModel.changeSelectedQuestion(viewModel.questions()[0]);
         },
-        addOptionToQuestion: function(question){
-            question.choices.push('');
-            question.choices.valueHasMutated();
+        showAddChoice:function(){
+            return viewModel.selectedQuestion().type() == 'choice';
+        },
+        addOptionToQuestion: function(){
+            viewModel.selectedQuestion().choices.push({value:''});
+            viewModel.selectedQuestion().choices.valueHasMutated();
+            viewModel.selectedQuestion.valueHasMutated();
+            viewModel.questions.valueHasMutated();
+        },
+        removeOptionFromQuestion:function(choice){
+            viewModel.selectedQuestion().choices.remove(choice);
+            viewModel.selectedQuestion.valueHasMutated();
         },
         selectedQuestion: ko.observable({}),
         changeSelectedQuestion: function(question){
@@ -25,12 +45,8 @@ $(document).ready(function(){
             viewModel.selectedQuestion.valueHasMutated();
             viewModel.questions.valueHasMutated();
         }
+
     };
     viewModel.addQuestion();
-
-    viewModel.displayQuestion =ko.dependentObservable(function(){
-            return this.selectedQuestion().title();
-    },viewModel);
-
     ko.applyBindings(viewModel);
 })
