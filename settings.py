a6cd002de2bef4b922ad2a580e626796785947f8
Shweta <shwetags@thoughtworks.com>
2011-04-26 11:52:12 +0530

    Adding 960 grid CSS files and SCSS implementation : Prabhu

--- settings.py
+++ settings.py
@@ -136,9 +136,22 @@ INSTALLED_APPS = (
     'registration',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'compressor'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+COMPILER_FORMATS = {
+    '.sass': {
+        'binary_path':'sass',
+        'arguments': '*.sass *.css'
+    },
+    '.scss': {
+        'binary_path':'sass',
+        'arguments': '*.scss *.css'
+    }
+}
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
