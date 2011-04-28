1a91ff6ce95a2ac86a5e97666192b002f4ad7012
Mahesh Lal <maheshkl@mahesh.(none)>
2011-04-28 12:01:03 +0530

    Fixing build|<Mahesh>

--- project/tests/test_helper.py
+++ project/tests/test_helper.py
@@ -11,9 +11,9 @@ from mangrove.datastore.form_model import FormModel, get
 class TestHelper(unittest.TestCase):
 
     def test_creates_questions_from_dict(self):
-        post = [{ "title":"q1", "code":"qc1", "description":"desc1", "type":"text", "choices":[] },
-                { "title":"q2", "code":"qc2", "description":"desc2", "type":"integer", "choices":[] },
-                { "title":"q3", "code":"qc3", "description":"desc3", "type":"choice", "choices":[{ "value":"c1" }, { "value":"c2" } ]}
+        post = [{ "title":"q1", "code":"qc1", "description":"desc1", "type":"text", "choices":[] , "is_entity_question":True},
+                { "title":"q2", "code":"qc2", "description":"desc2", "type":"integer", "choices":[], "is_entity_question":False},
+                { "title":"q3", "code":"qc3", "description":"desc3", "type":"choice", "choices":[{ "value":"c1" }, { "value":"c2" } ], "is_entity_question":False}
                ]
         q1=helper.create_question(post[0])
         q2=helper.create_question(post[1])
@@ -23,12 +23,12 @@ class TestHelper(unittest.TestCase):
         self.assertIsInstance(q3,SelectField)
 
     def test_should_save_questionnaire_from_post(self):
-        post = [{ "title":"q1", "code":"qc1", "description":"desc1", "type":"text", "choices":[] },
-                { "title":"q2", "code":"qc2", "description":"desc2", "type":"integer", "choices":[] },
-                { "title":"q3", "code":"qc3", "description":"desc3", "type":"choice", "choices":[{ "value":"c1" }, { "value":"c2" } ]}
+        post = [{ "title":"q1", "code":"qc1", "description":"desc1", "type":"text", "choices":[], "is_entity_question":True},
+                { "title":"q2", "code":"qc2", "description":"desc2", "type":"integer", "choices":[], "is_entity_question":False },
+                { "title":"q3", "code":"qc3", "description":"desc3", "type":"choice", "choices":[{ "value":"c1" }, { "value":"c2" } ], "is_entity_question":False}
                ]
         q1=helper.create_question(post[0])
         form_model=FormModel(get_db_manager(),"test","test","test",[q1],"test","test")
         questionnaire = helper.save_questionnaire(form_model,post)
-        self.assertEqual(4,len(questionnaire.fields))
+        self.assertEqual(3,len(questionnaire.fields))
 
