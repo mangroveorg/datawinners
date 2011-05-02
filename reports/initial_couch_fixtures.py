9bfa10f8b6a9a16145c84f0d0f12474c963e66e2
Mark Johnston <mejymejy@gmail.com>
2011-05-02 19:14:05 -0400

    Round 4 of datadict-refactor.
    	modified:   datawinners/reports/initial_couch_fixtures.py
    	modified:   mangrove/datastore/documents.py
    	modified:   mangrove/datastore/entity.py
    	modified:   mangrove/datastore/tests/test_data_store_api.py
    	modified:   mangrove/datastore/tests/test_datarecord.py
    	modified:   mangrove/datastore/tests/test_form_model.py
    	modified:   mangrove/datastore/tests/test_query_api.py
    	modified:   mangrove/datastore/views/map_by_values.js
    	modified:   mangrove/datastore/views/map_by_values_time.js
    	modified:   mangrove/datastore/views/map_entity_datatypes.js
    	modified:   mangrove/transport/smsplayer/test_smsplayer.py
    	modified:   nmis/main/management/commands/loadnmisdata.py

--- reports/initial_couch_fixtures.py
+++ reports/initial_couch_fixtures.py
@@ -6,13 +6,22 @@ from pytz import UTC
 from mangrove.datastore.field import TextField, IntegerField, SelectField, SelectField
 from mangrove.datastore.form_model import FormModel
 from mangrove.errors.MangroveException import EntityTypeAlreadyDefined
+from mangrove.datastore.datadict import DataDictType
 
 def load_data():
     manager = get_db_manager()
     ENTITY_TYPE = ["Health_Facility", "Clinic"]
     FEB = datetime.datetime(2011, 02, 01, tzinfo=UTC)
     MARCH = datetime.datetime(2011, 03, 01, tzinfo=UTC)
-
+    dd_types = {
+        'beds': DataDictType(manager, name='beds', slug='beds', primitive_type='number'),
+        'meds': DataDictType(manager, name='meds', slug='meds', primitive_type='number'),
+        'patients': DataDictType(manager, name='patients', slug='patients', primitive_type='number'),
+        'doctors': DataDictType(manager, name='doctors', slug='doctors', primitive_type='number'),
+        'director': DataDictType(manager, name='director', slug='director', primitive_type='string')
+    }
+    for label, dd_type in dd_types.items():
+        dd_type.save()
     define_type(manager, ["Health_Facility", "Clinic"])
     #  Default Entity Types
     try:
@@ -25,41 +34,51 @@ def load_data():
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Surgeon"])
     e.save()
 
-    e.add_data(data=[("beds", 300), ("meds", 20), ("director", "Dr. A"), ("patients", 10)],
+    e.add_data(data=[("beds", 300, dd_types['beds']), ("meds", 20, dd_types['meds']),
+                     ("director", "Dr. A", dd_types['director']), ("patients", 10, dd_types['patients'])],
                event_time=FEB)
-    e.add_data(data=[("beds", 500), ("meds", 20), ("patients", 20)],
+    e.add_data(data=[("beds", 500, dd_types['beds']), ("meds", 20, dd_types['meds']),
+                     ("patients", 20, dd_types['patients'])],
                event_time=MARCH)
 
     e = Entity(manager, entity_type=ENTITY_TYPE, location=['India', 'MH', 'Pune'], id="200")
     e.set_aggregation_path("governance", ["Director", "Med_Supervisor", "Surgeon"])
     e.save()
-    e.add_data(data=[("beds", 100), ("meds", 10), ("director", "Dr. AA"), ("patients", 50)],
+    e.add_data(data=[("beds", 100, dd_types['beds']), ("meds", 10, dd_types['meds']),
+                     ("director", "Dr. AA", dd_types['director']), ("patients", 50, dd_types['patients'])],
                event_time=FEB)
-    e.add_data(data=[("beds", 200), ("meds", 20), ("patients", 20)],
+    e.add_data(data=[("beds", 200, dd_types['beds']), ("meds", 20, dd_types['meds']),
+                     ("patients", 20, dd_types['patients'])],
                event_time=MARCH)
 
     e = Entity(manager, entity_type=ENTITY_TYPE, location=['India', 'MH', 'Mumbai'], id="300")
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Doctor"])
     e.save()
-    e.add_data(data=[("beds", 100), ("meds", 10), ("director", "Dr. AAA"), ("patients", 50)],
+    e.add_data(data=[("beds", 100, dd_types['beds']), ("meds", 10, dd_types['meds']),
+                     ("director", "Dr. AAA", dd_types['director']), ("patients", 50, dd_types['patients'])],
                event_time=FEB)
-    e.add_data(data=[("beds", 200), ("meds", 20), ("patients", 50)],
+    e.add_data(data=[("beds", 200, dd_types['beds']), ("meds", 20, dd_types['meds']),
+                     ("patients", 50, dd_types['patients'])],
                event_time=MARCH)
 
     # Entities for State 2: karnataka
     e = Entity(manager, entity_type=ENTITY_TYPE, location=['India', 'Karnataka', 'Bangalore'], id="400")
     e.set_aggregation_path("governance", ["Director", "Med_Supervisor", "Nurse"])
     e.save()
-    e.add_data(data=[("beds", 100), ("meds", 250), ("director", "Dr. B1"), ("patients", 50)],
+    e.add_data(data=[("beds", 100, dd_types['beds']), ("meds", 250, dd_types['meds']),
+                     ("director", "Dr. B1", dd_types['director']), ("patients", 50, dd_types['patients'])],
                event_time=FEB)
-    e.add_data(data=[("beds", 200), ("meds", 400), ("director", "Dr. B2"), ("patients", 20)],
+    e.add_data(data=[("beds", 200, dd_types['beds']), ("meds", 400, dd_types['meds']),
+                     ("director", "Dr. B2", dd_types['director']), ("patients", 20, dd_types['patients'])],
                event_time=MARCH)
     e = Entity(manager, entity_type=ENTITY_TYPE, location=['India', 'Karnataka', 'Hubli'])
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Surgeon"])
     e.save()
-    e.add_data(data=[("beds", 100), ("meds", 250), ("director", "Dr. B1"), ("patients", 50)],
+    e.add_data(data=[("beds", 100, dd_types['beds']), ("meds", 250, dd_types['meds']),
+                     ("director", "Dr. B1", dd_types['director']), ("patients", 50, dd_types['patients'])],
                event_time=FEB)
-    e.add_data(data=[("beds", 200), ("meds", 400), ("director", "Dr. B2"), ("patients", 20)],
+    e.add_data(data=[("beds", 200, dd_types['beds']), ("meds", 400, dd_types['meds']),
+                     ("director", "Dr. B2", dd_types['director']), ("patients", 20, dd_types['patients'])],
                event_time=MARCH)
 
 
@@ -67,7 +86,8 @@ def load_data():
     e = Entity(manager, entity_type=ENTITY_TYPE, location=['India', 'Kerala', 'Kochi'], id="500")
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Nurse"])
     e.save()
-    e.add_data(data=[("beds", 200), ("meds", 50), ("director", "Dr. C"), ("patients", 12)],
+    e.add_data(data=[("beds", 200, dd_types['beds']), ("meds", 50, dd_types['meds']),
+                     ("director", "Dr. C", dd_types['director']), ("patients", 12, dd_types['patients'])],
                event_time=MARCH)
 
 
