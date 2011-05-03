3f2f265d9b935cd943d6bc7fbaa7c4b9d04dd5c2
Shweta <shwetags@thoughtworks.com>
2011-05-03 10:08:26 +0530

    Integrating the questionnarrie visuals and adding slider functionality - Prabhu

---
+++ media/javascript/step_wizard.js
@@ -0,0 +1,13 @@
+function set_current_tab(){
+      var current_element = $("#tab_items .current");
+      var current_tab = $("#tab_items li").index(current_element)+1;
+      var wizard_slider = $("#step_wizard").slider({
+               range: "max",
+               min: 1,
+               max: 5,
+               value: current_tab,
+               disabled:true
+      });
+
+}
+
