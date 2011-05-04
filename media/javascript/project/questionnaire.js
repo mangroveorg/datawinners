fed1afba6ac9cabbd46b182d53486225e1c3dfc5
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-05-04 14:18:44 +0530

    Shweta|Diptanu Added error handling for questionnaire code

--- media/javascript/project/questionnaire.js
+++ media/javascript/project/questionnaire.js
@@ -86,7 +86,12 @@ $(document).ready(function(){
 
     $("#submit-button").click(function(){
         var data = JSON.stringify(ko.toJS(viewModel.questions()), null,2);
+        if($.trim($("#questionnaire-code").val()) == ""){
+            $("#questionnaire-code-error").html("The Questionnaire code is required.");
+            return;
+        }
         var post_data = {'questionnaire-code':$('#questionnaire-code').val(),'question-set':data,'pid':$('#project-id').val()}
+
         $.post('/project/questionnaire/save', post_data,function(response){$("#message-label").html("<label>"+response+"</label>")});
     });
 })
