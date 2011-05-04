6d91663429667a6cebcc035feda8936497c6d3f5
Aroj George <apgeorge@thoughtworks.com>
2011-05-04 12:42:13 +0530

    Fixing fixtures afer addition of data dictionary type.

--- main/initial_couch_fixtures.py
+++ main/initial_couch_fixtures.py
@@ -1,10 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 import datetime
+from mangrove.datastore.datadict import DataDictType
 from mangrove.datastore.datarecord import register
 from mangrove.datastore.entity import Entity, define_type
 from mangrove.datastore.database import get_db_manager
 from pytz import UTC
-from mangrove.datastore.field import TextField, IntegerField, SelectField, SelectField
+from mangrove.datastore.field import TextField, IntegerField
 from mangrove.errors.MangroveException import EntityTypeAlreadyDefined
 from mangrove.form_model.form_model import FormModel
 
@@ -24,6 +25,22 @@ def load_data():
     except EntityTypeAlreadyDefined:
         pass
 
+
+
+    try:
+        meds_type = DataDictType(manager, name='Medicines', slug='meds', primitive_type='number', description='Number of medications')
+        beds_type = DataDictType(manager, name='Beds', slug='beds', primitive_type='number', description='Number of beds')
+        director_type = DataDictType(manager, name='Director', slug='dir', primitive_type='string', description='Name of director')
+        facility_type = DataDictType(manager, name='Facility', slug='facility', primitive_type='string', description='Name of facility')
+        patients_type = DataDictType(manager, name='Patients', slug='patients', primitive_type='number', description='Patient Count')
+        meds_type.save()
+        beds_type.save()
+        director_type.save()
+        facility_type.save()
+        patients_type.save()
+    except Exception:
+        pass
+
     e = define_entity_instance(manager, ENTITY_TYPE, ['India', 'MH', 'Pune'], "CID001")
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Surgeon"])
     try:
@@ -31,9 +48,10 @@ def load_data():
     except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 300), ("meds", 20), ("director", "Dr. Donald Duck"), ("patients", 10)],
+        e.add_data(data=[("beds", 300,beds_type), ("meds", 20,meds_type), ("director", "Dr. Donald Duck",director_type),
+                         ("patients", 10,patients_type)],
                    event_time=FEB)
-        e.add_data(data=[("beds", 500), ("meds", 20), ("patients", 20)],
+        e.add_data(data=[("beds", 500,beds_type), ("meds", 20,meds_type), ("patients", 20,patients_type)],
                    event_time=MARCH)
 
     e = define_entity_instance(manager, ENTITY_TYPE, ['India', 'MH', 'Pune'], "CID002")
@@ -43,9 +61,9 @@ def load_data():
     except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 100), ("meds", 10), ("director", "Dr. Scrooge"), ("patients", 50)],
-                   event_time=FEB)
-        e.add_data(data=[("beds", 200), ("meds", 20), ("patients", 20)],
+        e.add_data(data=[("beds", 100,beds_type), ("meds", 10,meds_type), ("director", "Dr. Scrooge",director_type),
+        ("patients", 50,patients_type)],event_time=FEB)
+        e.add_data(data=[("beds", 200,beds_type), ("meds", 20,meds_type), ("patients", 20,patients_type)],
                    event_time=MARCH)
 
     e = define_entity_instance(manager, ENTITY_TYPE, ['India', 'MH', 'Mumbai'], "CID003")
@@ -55,9 +73,9 @@ def load_data():
     except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 100), ("meds", 10), ("director", "Dr. Huey"), ("patients", 50)],
+        e.add_data(data=[("beds", 100,beds_type), ("meds", 10,meds_type), ("director","Dr. Huey",director_type), ("patients", 50,patients_type)],
                event_time=FEB)
-        e.add_data(data=[("beds", 200), ("meds", 20), ("patients", 50)],
+        e.add_data(data=[("beds", 200,beds_type), ("meds", 20,meds_type), ("patients", 50,patients_type)],
                event_time=MARCH)
 
     e = define_entity_instance(manager, ENTITY_TYPE, ['India', 'Karnataka', 'Bangalore'], "CID004")
@@ -67,19 +85,19 @@ def load_data():
     except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 100), ("meds", 250), ("director", "Dr. Dewey"), ("patients", 50)],
+        e.add_data(data=[("beds", 100,beds_type), ("meds", 250,meds_type), ("director","Dr. Dewey",director_type), ("patients", 50,patients_type)],
                event_time=FEB)
-        e.add_data(data=[("beds", 200), ("meds", 400), ("director", "Dr. Louie"), ("patients", 20)],
+        e.add_data(data=[("beds", 200,beds_type), ("meds", 400,meds_type), ("director","Dr. Louie",director_type), ("patients", 20,patients_type)],
                event_time=MARCH)
 
     e = define_entity_instance(manager, ENTITY_TYPE, ['India', 'Kerala', 'Kochi'], "CID005")
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Nurse"])
     try:
         e.save()
-    except:
+    except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 200), ("meds", 50), ("director", "Dr. Glomgold"), ("patients", 12)],
+        e.add_data(data=[("beds", 200,beds_type), ("meds", 50,meds_type), ("director","Dr. Glomgold",director_type), ("patients", 12,patients_type)],
                event_time=MARCH)
 
 
@@ -87,10 +105,10 @@ def load_data():
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Nurse"])
     try:
         e.save()
-    except:
+    except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 200), ("meds", 50), ("director", "Dr. Flintheart"), ("patients", 12)],
+        e.add_data(data=[("beds", 200,beds_type), ("meds", 50,meds_type), ("director","Dr. Flintheart",director_type), ("patients", 12,patients_type)],
                event_time=MARCH)
 
 
@@ -98,10 +116,10 @@ def load_data():
     e.set_aggregation_path("governance", ["Director", "Med_Officer", "Nurse"])
     try:
         e.save()
-    except:
+    except Exception:
         pass
     else:
-        e.add_data(data=[("beds", 200), ("meds", 50), ("director", "Dr. Duck"), ("patients", 12)],
+        e.add_data(data=[("beds", 200,beds_type), ("meds", 50,meds_type), ("director","Dr. Duck",director_type), ("patients", 12,patients_type)],
                event_time=MARCH)
 
         
@@ -117,8 +135,13 @@ def load_data():
                            form_code="QRID01", type='survey', fields=[
                     question1, question2, question3])
     form_model.save()
+    
     #Register Reporter
-    register(manager, entity_type=["Reporter"], data=[("telephone_number", "1234567890"),("first_name","Shweta")], location=[],
+    phone_number_type = DataDictType(manager, name='Telephone Number', slug='telephone_number', primitive_type='string')
+    first_name_type = DataDictType(manager, name='First Name', slug='first_name', primitive_type='string')
+    phone_number_type.save()
+    first_name_type.save()
+    register(manager, entity_type=["Reporter"], data=[("telephone_number", "1234567890",phone_number_type),("first_name","Shweta",first_name_type)], location=[],
                         source="sms")
 
 
--- reporter/views.py
+++ reporter/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from mangrove.datastore.database import get_db_manager
-from datawinners.accountmanagement.models import Organization
 from datawinners.reporter.forms import ReporterRegistrationForm
 
 from mangrove.datastore import datarecord
+from mangrove.datastore.datadict import DataDictType
 
 
 @login_required(login_url='/login')
@@ -17,8 +17,10 @@ def register(request):
     message = None
     if form.is_valid():
         form_data = form.cleaned_data
-        data =form_data.items()
-        registered_reporter = datarecord.register(manager=get_db_manager(),entity_type=["Reporter"], data=data, location=[],
+        manager = get_db_manager()
+        dummy_type = DataDictType(manager, name='Dummy Type', slug='dummy_type', primitive_type='string')
+        data = [ (k,v,dummy_type) for (k,v) in form_data.items() ]
+        registered_reporter = datarecord.register(manager,entity_type=["Reporter"], data=data, location=[],
                                                   source="Web")
         form = ReporterRegistrationForm()
         message = "The reporter was successfully registered with id %s" % registered_reporter.id
