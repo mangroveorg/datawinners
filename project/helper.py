6d7667cf2d89280ca10ddc4a92393f1fc183320f
Shweta <shwetags@thoughtworks.com>
2011-05-04 16:17:22 +0530

    Minor refactoring in project/helper

--- project/helper.py
+++ project/helper.py
@@ -6,16 +6,16 @@ from mangrove.form_model.form_model import FormModel, get
 
 def create_question(post_dict):
     if post_dict["type"]=="text":
-        return TextField(name=post_dict["title"],question_code=post_dict["code"],label=post_dict["description"],entity_question_flag=post_dict.get("is_entity_question"))
+        return TextField(name=post_dict["title"],question_code=post_dict["code"],label="default",entity_question_flag=post_dict.get("is_entity_question"))
     if post_dict["type"]=="integer":
         range = {"min":post_dict["range_min"], "max":post_dict["range_max"]}
-        return IntegerField(post_dict["title"],post_dict["code"],post_dict["description"], range)
+        return IntegerField(post_dict["title"],post_dict["code"],"default", range)
     if post_dict["type"]=="select":
         options = [choice["value"] for choice in post_dict["choices"]]
-        return SelectField(post_dict["title"],post_dict["code"],post_dict["description"],options, single_select_flag=True)
+        return SelectField(post_dict["title"],post_dict["code"],"default",options, single_select_flag=True)
     if post_dict["type"]=="select1":
         options = [choice["value"] for choice in post_dict["choices"]]
-        return SelectField(post_dict["title"],post_dict["code"],post_dict["description"],options, single_select_flag=False)
+        return SelectField(post_dict["title"],post_dict["code"],"default",options, single_select_flag=False)
 
 def create_questionnaire(post,dbm=get_db_manager()):
     entity_id_question = TextField(name="What are you reporting on?", question_code="eid", label="Entity being reported on",entity_question_flag=True)
--- project/tests/test_helper.py
+++ project/tests/test_helper.py
@@ -1,3 +1,4 @@
+
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 import unittest
@@ -22,9 +23,9 @@ class TestHelper(unittest.TestCase):
         self.assertEquals(q2._to_json()["range"], {"min":0, "max":100})
 
     def test_should_save_questionnaire_from_post(self):
-        post = [{ "title":"q1", "code":"qc1", "description":"desc1", "type":"text", "choices":[], "is_entity_question":True},
-                { "title":"q2", "code":"qc2", "description":"desc2", "type":"integer", "choices":[], "is_entity_question":False,"range_min":0,"range_max":100  },
-                { "title":"q3", "code":"qc3", "description":"desc3", "type":"select", "choices":[{ "value":"c1" }, { "value":"c2" } ], "is_entity_question":False,"answers_permitted":"single"}
+        post = [{ "title":"q1", "code":"qc1", "type":"text", "choices":[], "is_entity_question":True},
+                { "title":"q2", "code":"qc2", "type":"integer", "choices":[], "is_entity_question":False,"range_min":0,"range_max":100  },
+                { "title":"q3", "code":"qc3",  "type":"select", "choices":[{ "value":"c1" }, { "value":"c2" } ], "is_entity_question":False,"answers_permitted":"single"}
                ]
         q1=helper.create_question(post[0])
         form_model=FormModel(get_db_manager(),"test","test","test",[q1],"test","test")
