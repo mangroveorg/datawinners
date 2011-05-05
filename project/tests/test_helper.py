c29b302c0ff761adf9219146e742708349daff50
Asif Momin <asifmom@thoughtworks.com>
2011-05-05 17:04:32 +0530

    Implementation of Min and Max length constraint for text field

--- project/tests/test_helper.py
+++ project/tests/test_helper.py
@@ -9,7 +9,7 @@ from mangrove.form_model.form_model import FormModel
 class TestHelper(unittest.TestCase):
     def test_creates_questions_from_dict(self):
         post = [{"title": "q1", "code": "qc1", "description": "desc1", "type": "text", "choices": [],
-                 "is_entity_question": True},
+                 "is_entity_question": True, "min_length": 1, "max_length": 15},
                 {"title": "q2", "code": "qc2", "description": "desc2", "type": "integer", "choices": [],
                  "is_entity_question": False, "range_min": 0, "range_max": 100},
                 {"title": "q3", "code": "qc3", "description": "desc3", "type": "select",
@@ -23,9 +23,11 @@ class TestHelper(unittest.TestCase):
         self.assertIsInstance(q2, IntegerField)
         self.assertIsInstance(q3, SelectField)
         self.assertEquals(q2._to_json()["range"], {"min": 0, "max": 100})
+        self.assertEquals(q1._to_json()["length"], {"min": 1, "max": 15})
 
     def test_should_save_questionnaire_from_post(self):
-        post = [{"title": "q1", "code": "qc1", "type": "text", "choices": [], "is_entity_question": True},
+        post = [{"title": "q1", "code": "qc1", "type": "text", "choices": [], "is_entity_question": True,
+                 "min_length": 1, "max_length": ""},
                 {"title": "q2", "code": "qc2", "type": "integer", "choices": [], "is_entity_question": False,
                  "range_min": 0, "range_max": 100},
                 {"title": "q3", "code": "qc3", "type": "select", "choices": [{"value": "c1"}, {"value": "c2"}],
@@ -36,11 +38,31 @@ class TestHelper(unittest.TestCase):
         questionnaire = helper.update_questionnaire_with_questions(form_model, post)
         self.assertEqual(3, len(questionnaire.fields))
 
-    def test_should_save_questionnaire_from_post(self):
-        post = [{ "title":"q1", "code":"qc1", "type":"text", "choices":[], "is_entity_question":True},
-                { "title":"q2", "code":"qc2", "type":"integer", "choices":[], "is_entity_question":False,"range_min":0,"range_max":""  },
-                { "title":"q3", "code":"qc3",  "type":"select", "choices":[{ "value":"c1" }, { "value":"c2" } ], "is_entity_question":False,"answers_permitted":"single"}
-               ]
-        q1=helper.create_question(post[1])
-        self.assertEqual(q1.constraint.max,None)
+    def test_should_create_integer_question_with_no_max_constraint(self):
+        post = [{"title": "q2", "code": "qc2", "type": "integer", "choices": [], "is_entity_question": False,
+                 "range_min": 0, "range_max": ""}]
+        q1 = helper.create_question(post[0])
+        self.assertEqual(q1.constraint.max, None)
 
+    def test_should_create_text_question_with_no_max_length(self):
+        post = [{"title": "q1", "code": "qc1", "type": "text", "choices": [], "is_entity_question": True,
+                 "min_length": 1, "max_length": ""},
+                {"title": "q2", "code": "qc2", "type": "integer", "choices": [], "is_entity_question": False,
+                 "range_min": 0, "range_max": 100},
+                {"title": "q3", "code": "qc3", "type": "select", "choices": [{"value": "c1"}, {"value": "c2"}],
+                 "is_entity_question": False, "answers_permitted": "single"}
+        ]
+        q1 = helper.create_question(post[0])
+        self.assertEqual(q1.constraint.max, None)
+
+    def test_should_create_text_question_with_no_max_lengt_and_min_length(self):
+        post = [{"title": "q1", "code": "qc1", "type": "text", "choices": [], "is_entity_question": True,
+                 },
+                {"title": "q2", "code": "qc2", "type": "integer", "choices": [], "is_entity_question": False,
+                 "range_min": 0, "range_max": 100},
+                {"title": "q3", "code": "qc3", "type": "select", "choices": [{"value": "c1"}, {"value": "c2"}],
+                 "is_entity_question": False, "answers_permitted": "single"}
+        ]
+        q1 = helper.create_question(post[0])
+        self.assertEqual(q1.constraint.max, None)
+        self.assertEqual(q1.constraint.min, None)
