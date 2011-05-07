7e02a497d682bef9cbee8b11eb37fb004de56bcf
Jeff Wishnie <jeff@wishnie.org>
2011-05-07 16:51:52 -0700

    Basic aggregation tree skeleton and Pyflakes/PEP8 fixes

--- project/models.py
+++ project/models.py
@@ -2,7 +2,6 @@ from couchdb.mapping import  TextField, ListField
 from mangrove.datastore.database import get_db_manager, DatabaseManager
 from mangrove.datastore.documents import DocumentBase
 
-
 class Project(DocumentBase):
     name = TextField()
     goals = TextField()
