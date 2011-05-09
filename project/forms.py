6c97c9c2fa2cd26ea753ec5440d22e112e018290
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-05-09 12:07:33 +0530

    making public information radio button disabled.

--- project/forms.py
+++ project/forms.py
@@ -1,20 +1,38 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.forms.fields import CharField, ChoiceField, MultipleChoiceField
 from django.forms.forms import Form
+from django.forms.widgets import RadioFieldRenderer, RadioInput
 from mangrove.datastore.database import get_db_manager
 from mangrove.datastore.entity import load_all_entity_types
 from django import forms
 
 
+class MyRadioFieldRenderer(RadioFieldRenderer):
+    def __init__(self, name, value, attrs, choices):
+        self.name, self.value, self.attrs = name, value, attrs
+        self.choices = choices
+
+    def __iter__(self):
+        for i, choice in enumerate(self.choices):
+            attrs = self.attrs.copy()
+            if choice[0] == 'public information':
+                attrs['disabled'] = 'disabled'
+            yield RadioInput(self.name, self.value, attrs, choice, i)
+
+class MyRadioSelect(forms.RadioSelect):
+    renderer = MyRadioFieldRenderer
+
 class ProjectProfile(Form):
-    PROJECT_TYPE_CHOICES = (('survey', 'Survey project: I want to collect data from the field'), ('public information', 'Public information: I want to send information'))
+    PROJECT_TYPE_CHOICES = (('survey', 'Survey project: I want to collect data from the field'),
+                            ('public information', 'Public information: I want to send information'))
     DEVICE_CHOICES = (('sms', 'SMS'), ('smartphone', 'Smart Phone'), ('web', 'Web'))
     id = CharField(required=False)
     name = CharField(required=True)
     goals = CharField(max_length=300, widget=forms.Textarea, label='Project Background And Goals', required=False)
-    project_type = ChoiceField(label='Project Type', required=True, widget=forms.RadioSelect, choices=PROJECT_TYPE_CHOICES)
+    project_type = ChoiceField(label='Project Type', required=True, widget=MyRadioSelect, choices=PROJECT_TYPE_CHOICES)
     entity_type = ChoiceField(label="Subjects", required=True)
-    devices = MultipleChoiceField(label='Device', widget=forms.CheckboxSelectMultiple, choices=DEVICE_CHOICES, initial=DEVICE_CHOICES[2], required=False)
+    devices = MultipleChoiceField(label='Device', widget=forms.CheckboxSelectMultiple, choices=DEVICE_CHOICES,
+                                  initial=DEVICE_CHOICES[2], required=False)
 
     def get_entity_types(self):
         manager = get_db_manager()
@@ -25,5 +43,6 @@ class ProjectProfile(Form):
     def __init__(self, *args, **kwargs):
         super(ProjectProfile, self).__init__(*args, **kwargs)
         type_list = self.get_entity_types()
-#        type_list.extend([('new','new')])
         self.fields['entity_type']._set_choices(type_list)
+
+
