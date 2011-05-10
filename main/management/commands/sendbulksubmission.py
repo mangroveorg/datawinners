18c3be16a98a9752ac13b9522417324028c7724a
Prabhu <prabhur@thoughtworks.com>
2011-05-10 17:53:17 +0530

    Updated bulk submissions to delete all submissions and then save new ones

--- main/management/commands/sendbulksubmission.py
+++ main/management/commands/sendbulksubmission.py
@@ -1,29 +1,37 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.core.management.base import BaseCommand
 from mangrove.datastore.database import get_db_manager
-from datawinners.main.management.commands.recreateviews import create_views
-from mangrove.transport.submissions import SubmissionHandler, Request
+from mangrove.datastore.documents import SubmissionLogDocument
 
 
 def load_sms_data():
-    s = SubmissionHandler(dbm=get_db_manager())
-    for i in range(1,20):
-        message = "QRID01 +EID CID001 +Q1 prabhu +Q2 20"
+    dbm = get_db_manager()
+    for i in range(1,200):
         sender = "1234567890"
         receiver = 12345
-        response = s.accept(Request(transport="sms", message=message, source=sender, destination=receiver))
-        message = "QRID01 +EID CID002 +Q1 shweta +Q2 30"
-        response = s.accept(Request(transport="sms", message=message, source=sender, destination=receiver))
-        message = "QRID01 +EID CID003 +Q1 prabhu +Q2 20"
-        response = s.accept(Request(transport="sms", message=message, source=sender, destination=receiver))
+        submission_id = dbm.save(SubmissionLogDocument(channel="sms", source=sender,
+                                                                destination=receiver, form_code="QRID01", values={"EID": "CID001", "Q1": "prabhu", "Q2": 20},
+                                                                status=True, error_message="")).id
+
+
+def delete_sms_data():
+    dbm = get_db_manager()
+    rows = dbm.load_all_rows_in_view('mangrove_views/submissionlog', reduce=False)
+    for document in [each.value for each in rows]:
+        dbm.delete(document)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         manager = get_db_manager()
+        try:
+            print 'Deleting earlier submissions'
+            delete_sms_data()
+        except Exception as e:
+            print e.message
         print "Storing multiple submissions....."
         try:
             load_sms_data()
-        except:
-            print "Exception!!!"
+        except Exception as e:
+            print e.message
         print "Done."
