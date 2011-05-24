ece87e8afebf710f135b42e6c32c8fc805adea8a
Asif Momin <asifmom@thoughtworks.com>
2011-05-24 16:47:49 +0530

    Adding Geocode for Reporter registration via web

--- reporter/forms.py
+++ reporter/forms.py
@@ -1,12 +1,30 @@
+from django.core.exceptions import ValidationError
 from django.forms.fields import CharField, RegexField
 from django.forms.forms import Form
+from mangrove.utils.types import is_empty
 
 
 class ReporterRegistrationForm(Form):
     error_css_class = 'error'
     required_css_class = 'required'
 
-    first_name = RegexField(regex="[^0-9.,\s@#$%&*~]*", max_length=30, error_message="Please enter a valid value containing only letters a-z or A-Z or symbols '`- ", required=True, label="* First Name")
-    last_name = RegexField(regex="[^0-9.,\s@#$%&*~]*", max_length=30, error_message="Please enter a valid value containing only letters a-z or A-Z or symbols '`- ", required=True, label="* Last Name")
+    first_name = RegexField(regex="[^0-9.,\s@#$%&*~]*", max_length=30,
+                            error_message="Please enter a valid value containing only letters a-z or A-Z or symbols '`- "
+                            , required=True, label="* First Name")
+    last_name = RegexField(regex="[^0-9.,\s@#$%&*~]*", max_length=30,
+                           error_message="Please enter a valid value containing only letters a-z or A-Z or symbols '`- "
+                           , required=True, label="* Last Name")
     telephone_number = CharField(required=True, label="* Telephone Number")
     commune = CharField(max_length=30, required=True, label="* Commune")
+    geo_code = CharField(max_length=30, required=False, label="Geo Code")
+
+
+    def clean_geo_code(self):
+        geo_code_string = self.cleaned_data['geo_code']
+        if is_empty(geo_code_string):
+            return geo_code_string
+        lat_long_string = geo_code_string.split(' ')
+        if len(lat_long_string) < 2:
+            raise ValidationError("GPS coordinates must be in the format 'lat long'.")
+        return geo_code_string
+
--- reporter/views.py
+++ reporter/views.py
@@ -4,24 +4,54 @@ from django.template.context import RequestContext
 from datawinners.main.utils import get_database_manager
 from datawinners.reporter.forms import ReporterRegistrationForm
 
-from mangrove.datastore import datarecord
-from mangrove.datastore.datadict import DataDictType
+from mangrove.errors.MangroveException import MangroveException
+from mangrove.transport.submissions import SubmissionHandler, Request
+from mangrove.utils.types import is_empty
 
 
 @login_required(login_url='/login')
 def register(request):
-    manager = get_database_manager(request)
     if request.method == 'GET':
         form = ReporterRegistrationForm()
         return render_to_response('reporter/register.html', {'form': form}, context_instance=RequestContext(request))
     form = ReporterRegistrationForm(request.POST)
     message = None
+    success = True
+
     if form.is_valid():
-        form_data = form.cleaned_data
-        dummy_type = DataDictType(manager, name='Dummy Type', slug='dummy_type', primitive_type='string')
-        data = [(k, v, dummy_type) for (k, v) in form_data.items()]
-        registered_reporter = datarecord.register(manager, entity_type=["Reporter"], data=data, location=[],
-                                                  source="Web")
-        form = ReporterRegistrationForm()
-        message = "The reporter was successfully registered with id %s" % registered_reporter.id
-    return render_to_response('reporter/register.html', {'form': form, 'message': message}, context_instance=RequestContext(request))
+        form_data = {k: v for (k, v) in form.cleaned_data.items() if not is_empty(v)}
+        try:
+            s = SubmissionHandler(dbm=get_database_manager(request))
+            response = s.accept(
+                Request(transport='web', message=_get_data(form_data), source='web', destination='mangrove'))
+            message = response.message
+        except MangroveException as exception:
+            message = exception.message
+            success = False
+
+    return render_to_response('reporter/register.html', {'form': form, 'message': message, 'success': success},
+                              context_instance=RequestContext(request))
+
+
+def _get_data(form_data):
+
+    #TODO need to refactor this code. The master dictionary should be maintained by the registration form  model
+    mapper = {'telephone_number': 'M', 'geo_code': 'G', 'Name': 'N', 'commune': 'L'}
+    data = dict()
+    telephone_number = form_data.get('telephone_number')
+    geo_code = form_data.get('geo_code')
+    commune = form_data.get('commune')
+
+    if telephone_number is not None:
+        data[mapper['telephone_number']] = telephone_number
+    if geo_code is not None:
+        data[mapper['geo_code']] = geo_code
+    if commune is  not None:
+        data[mapper['commune']] = commune
+
+    data[mapper['Name']] = " ".join([form_data.get('first_name'),form_data.get('last_name')])
+    data['form_code'] = 'REG'
+    data['T'] = 'Reporter'
+    return data
+
+
