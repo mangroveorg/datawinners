2bdf8f1628ca20431f4d82de28015b78ecc5976c
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-05-31 11:05:34 +0530

    creating database in couchdb only after user is activated : Akshay

---
+++ accountmanagement/post_activation_events.py
@@ -0,0 +1,22 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+import couchdb
+import datawinners
+from datawinners.accountmanagement.models import Organization, OrganizationSettings
+from mangrove.datastore.database import get_db_manager
+
+
+def create_org_database(sender, user, request, **kwargs):
+    from datawinners.initializer import run
+
+    profile = user.get_profile()
+    org = Organization.objects.get(org_id=profile.org_id)
+    if org is None:
+        return None
+    org_settings = OrganizationSettings.objects.get(organization=org)
+    db_name = org_settings.document_store
+    #    Explicitly create the new database. Should fail it db already exists.
+    server = couchdb.client.Server(datawinners.settings.COUCH_DB_SERVER)
+    server.create(db_name)
+    manager = get_db_manager(server=datawinners.settings.COUCH_DB_SERVER, database=db_name)
+    assert manager, "Could not create database manager for %s " % (db_name,)
+    run(manager)
--- accountmanagement/post_registration_events.py
+++ accountmanagement/post_registration_events.py
@@ -1,8 +1,6 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-import couchdb
-import datawinners
-from datawinners.accountmanagement.models import NGOUserProfile, OrganizationSettings
-from mangrove.datastore.database import get_db_manager
+
+from datawinners.accountmanagement.models import NGOUserProfile
 
 
 def ngo_user_created(sender, user, request, **kwargs):
@@ -12,19 +10,3 @@ def ngo_user_created(sender, user, request, **kwargs):
     data.user = user
     print 'saving the user'
     data.save()
-
-
-def create_org_database(sender, user, request, **kwargs):
-    from datawinners.initializer import run
-
-    org = kwargs.get('organization')
-    if org is None:
-        return None
-    org_settings = OrganizationSettings.objects.get(organization=org)
-    db_name = org_settings.document_store
-    #    Explicitly create the new database. Should fail it db already exists.
-    server = couchdb.client.Server(datawinners.settings.COUCH_DB_SERVER)
-    server.create(db_name)
-    manager = get_db_manager(server=datawinners.settings.COUCH_DB_SERVER, database=db_name)
-    assert manager, "Could not create database manager for %s " % (db_name,)
-    run(manager)
