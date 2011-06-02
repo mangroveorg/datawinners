d3d288db6348ae770c1308bf0c15a734071165d0
Prabhu <prabhur@thoughtworks.com>
2011-06-02 17:05:37 +0530

    Questionnaire page JS bug - Akshay/Prabhu

--- media/javascript/project/questionnaire.js
+++ media/javascript/project/questionnaire.js
@@ -140,19 +140,26 @@ $(document).ready(function() {
     $('#question_form').live("click", DW.charCount);
     $('.delete').live("click", DW.charCount);
 
-    //The Questionnarie ;eft navigation toggle functions
+    //The Questionnarie left navigation toggle functions
     $("#questions-panel .add_question .add_link").click(function(){
         $('.question_list ol > div:last').toggleClass("question_selected");
         $('.question_list ol > div:last').find(".selected_question_arrow").show();
     });
     $('.question_list ol > div:first').toggleClass("question_selected");
     $('.question_list ol > div:first').find(".selected_question_arrow").show();
-    $('.question_list ol div').live("click", function(){
+
+    $('.question_list ol div .delete_link').live("click", function(event){
+        event.stopPropagation();
+        $('.question_list ol > div:first').toggleClass("question_selected");
+
+    })
+    $('.question_list ol > div').live("click", function(){
         var selected = $(this).index()+1;
         var selected_div = $('.question_list ol').find("div:nth-child("+selected+")");
             selected_div.toggleClass("question_selected");
             selected_div.find(".selected_question_arrow").show();
     });
+
 
     $.validator.addMethod('spacerule', function(value, element, params) {
         var list = $.trim($('#' + element.id).val()).split(" ");
