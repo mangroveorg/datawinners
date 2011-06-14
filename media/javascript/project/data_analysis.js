7a7eee06a257b8a6661cc8c8da1724b43bdfbafa
Prabhu <prabhur@thoughtworks.com>
2011-06-14 17:37:51 +0530

    Introduced calendar on analysis page:Prabhu/Shweta

--- media/javascript/project/data_analysis.js
+++ media/javascript/project/data_analysis.js
@@ -1,28 +1,24 @@
 $(document).ready(function() {
      $('.dataTables_scrollHead select').customStyle();
-    
-    $('#data_analysis').dataTable( {
-        "bRetrieve" : true,
-        "sPaginationType": "full_numbers",
-        "sScrollX": "100%",
-        "sScrollXInner": "110%",
-        "bScrollCollapse": true,
-		"aaData": initial_data
-
-
-
-	} );
-    var newDataTable;
-    function dataBinding(data){
-            newDataTable = {
-            "bDestroy":true,
+    $("#dateRangePicker").daterangepicker( { presetRanges: [
+    {text: 'Past 7 days', dateStart: 'last week', dateEnd: 'Today' },
+    {text: 'Past 30 days', dateStart: 'last month', dateEnd: 'Today' },
+    {text: 'Past year', dateStart: 'last year', dateEnd: 'Today'}],
+    earliestDate:'1/1/2011', latestDate:'12/21/2012'
+  }
+);
+    function dataBinding(data, destroy, retrive){
+         $('#data_analysis').dataTable( {
+             "bDestroy":destroy,
+            "bRetrieve": retrive,
             "sPaginationType": "full_numbers",
             "sScrollX": "100%",
             "sScrollXInner": "110%",
             "bScrollCollapse": true,
             "aaData": data
-        };
+        });
     }
+    dataBinding(initial_data, false, true);
     $(".aggregation_type").live("change", function(){
         var aggregation_selectBox_Array = $(".aggregation_type"), aggregationArray = new Array();
          aggregation_selectBox_Array.each(function(){
@@ -35,8 +31,7 @@ $(document).ready(function() {
           success:function(response) {
                        console.log("response data"+response)
                        var response_data = JSON.parse(response);
-                       dataBinding(response_data);
-                       $('#data_analysis').dataTable( newDataTable );
+                       dataBinding(response_data, true, false);
 
          }});
     });
