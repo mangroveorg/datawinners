34a6079e914002899bc3ca73ec35c7329985b309
Diptanu Choudhury <diptanuc@gmail.com>
2011-06-20 15:41:03 +0530

    Fixing the build

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,10 @@
 from django.forms import ModelForm
-from accountmanagement.models import Organization
+from datawinners.accountmanagement.models import Organization
 
 class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
+
+    def update(self, posted_form, org_id):
+        organization = Organization.objects.get(org_id = org_id)
+        
--- account/views.py
+++ account/views.py
@@ -3,12 +3,16 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from datawinners.accountmanagement.models import Organization
-from forms import OrganizationForm
+from datawinners.account.forms import OrganizationForm
 
 @login_required(login_url='/login')
 def settings(request):
-    profile = request.user.get_profile()
-    organization = Organization.objects.get(org_id=profile.org_id)
-    organization_form = OrganizationForm(instance = organization)
-    return render_to_response("account/settings.html", {'organization_form' : organization_form}, context_instance=RequestContext(request))
+    if request.method == 'GET':
+        profile = request.user.get_profile()
+        organization = Organization.objects.get(org_id=profile.org_id)
+        organization_form = OrganizationForm(instance = organization)
+        return render_to_response("account/settings.html", {'organization_form' : organization_form}, context_instance=RequestContext(request))
+
+    if request.method == 'POST':
+        OrganizationForm.update(request.form, org_id)
 
