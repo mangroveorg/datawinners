2008949b8bd679e50163b20e86034d5468c7af5d
Diptanu Choudhury <diptanuc@gmail.com>
2011-06-21 10:53:14 +0530

    Deleting unwanted code

--- account/forms.py
+++ account/forms.py
@@ -7,20 +7,8 @@ class OrganizationForm(ModelForm):
 
     def update(self):
         if self.is_valid():
-            organization = Organization.objects.get(org_id = self.cleaned_data['org_id'])
-            organization.name = self.cleaned_data['name']
-            organization.sector = self.cleaned_data['sector']
-            organization.addressline1 = self.cleaned_data['addressline1']
-            organization.addressline2 = self.cleaned_data['addressline2']
-            organization.city = self.cleaned_data['city']
-            organization.state = self.cleaned_data['state']
-            organization.country = self.cleaned_data['country']
-            organization.zipcode = self.cleaned_data['zipcode']
-            organization.office_phone = self.cleaned_data['office_phone']
-            organization.website = self.cleaned_data['website']
-            organization.org_id = self.cleaned_data['org_id']
-
-            organization.save()
-            return True
+            self.save()
+        
+        return self
             
         
