6c6c7e241bbd73ab5cb4e5b2fa2ac436b047b29d
Diptanu Choudhury <diptanuc@gmail.com>
2011-06-23 14:25:30 +0530

    Making the page look nicer

--- media/javascript/add_users.js
+++ media/javascript/add_users.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-   $('#user_form').hide();
+  $('#user_form').hide();
+  $('#return_to_users_list').hide();
+  
   $('#add_users').click(function(){
     $('#add_users').hide();
     $('#users_list').hide();
     $('#user_form').show();
+    $('#return_to_users_list').show();
   });
+  
+  $('#return_to_users_list').click(function(){
+    $('#add_users').show();
+    $('#users_list').show();
+    $('#user_form').hide();
+    $('#return_to_users_list').hide();
+  });
+  
 });
