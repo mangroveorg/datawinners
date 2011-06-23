d520bc0ef5f6c5b4c3d5a065c547803e584f55ed
Prabhu <prabhur@thoughtworks.com>
2011-06-23 11:46:10 +0530

    UI chnages on data analysis page - Prabhu

--- media/javascript/project/data_analysis.js
+++ media/javascript/project/data_analysis.js
@@ -1,107 +1,83 @@
 $(document).ready(function() {
-    DW.submit_data = function(){
-       var aggregation_selectBox_Array = $(".aggregation_type"), aggregationArray = new Array();
-         aggregation_selectBox_Array.each(function(){
-         aggregationArray.push($(this).val())
+    DW.submit_data = function() {
+        var aggregation_selectBox_Array = $(".aggregation_type");
+        aggregationArray = new Array();
+        aggregation_selectBox_Array.each(function() {
+            aggregationArray.push($(this).val())
         });
         var time_range = $("#dateRangePicker").val().split("/");
-        if (time_range[0]!="Click to select a date range" && Date.parse(time_range[0]) == null){
-            $("#dateErrorDiv").html('<label class=error>'+"Enter a correct date. No filtering applied"+'</label>')
+        if (time_range[0] != "Click to select a date range" && Date.parse(time_range[0]) == null) {
+            $("#dateErrorDiv").html('<label class=error>' + "Enter a correct date. No filtering applied" + '</label>')
             hide_message();
-            time_range[0]="";
-            time_range[1]="";
-        }
-        var start_time = time_range[0] || "";
-        var end_time = time_range[1] || start_time;
-        $.ajax({
-          type: 'POST',
-          url: window.location.pathname,
-          data: {'aggregation-types':JSON.stringify(aggregationArray), 'start_time':start_time, 'end_time': end_time},
-          success:function(response) {
-                       var response_data = JSON.parse(response);
-                       DW.dataBinding(response_data, true, false);
-                       DW.wrap_table();
-         }});
-    }
-    DW.wrap_table = function(){
-         $("#data_analysis").wrap("<div class='data_table'/>")
-    }
-  $("#dateRangePicker").daterangepicker( {
-    presetRanges: [
-    {text: 'Current month', dateStart: function(){ return Date.parse('today').moveToFirstDayOfMonth();  }, dateEnd: 'today' },
-    {text: 'Last Month', dateStart: 'last month', dateEnd: 'today' },
-    {text: 'Year to date', dateStart: function(){ var x= Date.parse('today'); x.setMonth(0); x.setDate(1); return x; }, dateEnd: 'today' }
-    ],
-    presets: {dateRange: 'Date Range'},
-    earliestDate:'1/1/2011', latestDate:'21/12/2012', dateFormat:'dd-mm-yy', rangeSplitter:'/',
-    onClose:function(){submit_data()}
-  });
-  DW.dataBinding = function(data, destroy, retrive){
-     $('#data_analysis').dataTable( {
-         "bDestroy":destroy,
-        "bRetrieve": retrive,
-        "sPaginationType": "full_numbers",
-//        "sScrollX": "100%",
-//        "sScrollXInner": "100%",
-//        "bScrollCollapse": true,
-        "aaData": data
-    });
-  }
-<<<<<<< HEAD
-  DW.dataBinding (initial_data, false, true);
-  DW.wrap_table();
-  $('#data_analysis select').customStyle();
-
-  $(".aggregation_type").live("change", function(){
-        DW.submit_data();
-  });
-  function hide_message() {
-=======
-  dataBinding(initial_data, false, true);
-  var aggregationArray = new Array();
-    $('#data_analysis select').customStyle();
-   function submit_data(){
-       var aggregation_selectBox_Array = $(".aggregation_type");
-         aggregationArray = new Array();
-         aggregation_selectBox_Array.each(function(){
-         aggregationArray.push($(this).val())
-        });
-        var time_range = $("#dateRangePicker").val().split("/");
-        if (time_range[0]!="Click to select a date range" && Date.parse(time_range[0]) == null){
-            $("#dateErrorDiv").html('<label class=error>'+"Enter a correct date. No filtering applied"+'</label>')
-            hide_message();
-            time_range[0]="";
-            time_range[1]="";
+            time_range[0] = "";
+            time_range[1] = "";
         }
         var start_time = time_range[0] || "";
         var end_time = time_range[1] || start_time;
         return [start_time , end_time]
-   }
-    $("#data_analysis").wrap("<div class='data_table'/>")
-    $(".aggregation_type").live("change", function(){
-        var time_list = submit_data();
+    }
+    DW.wrap_table = function() {
+        $("#data_analysis").wrap("<div class='data_table'/>")
+    }
+    $("#dateRangePicker").daterangepicker({
+                presetRanges: [
+                    {text: 'Current month', dateStart: function() {
+                        return Date.parse('today').moveToFirstDayOfMonth();
+                    }, dateEnd: 'today' },
+                    {text: 'Last Month', dateStart: 'last month', dateEnd: 'today' },
+                    {text: 'Year to date', dateStart: function() {
+                        var x = Date.parse('today');
+                        x.setMonth(0);
+                        x.setDate(1);
+                        return x;
+                    }, dateEnd: 'today' }
+                ],
+                presets: {dateRange: 'Date Range'},
+                earliestDate:'1/1/2011', latestDate:'21/12/2012', dateFormat:'dd-mm-yy', rangeSplitter:'/',
+                onClose:function() {
+                    DW.submit_data()
+                }
+            });
+    DW.dataBinding = function(data, destroy, retrive) {
+        $('#data_analysis').dataTable({
+                    "bDestroy":destroy,
+                    "bRetrieve": retrive,
+                    "sPaginationType": "full_numbers",
+//        "sScrollX": "100%",
+//        "sScrollXInner": "100%",
+//        "bScrollCollapse": true,
+                    "aaData": data
+                });
+    }
+
+    DW.dataBinding(initial_data, false, true);
+    DW.wrap_table();
+    $('#data_analysis select').customStyle();
+
+    $(".aggregation_type").live("change", function() {
+        var time_list = DW.submit_data();
         $.ajax({
-          type: 'POST',
-          url: window.location.pathname,
-          data: {'aggregation-types':JSON.stringify(aggregationArray), 'start_time':time_list[0], 'end_time': time_list[1]},
-          success:function(response) {
-                       var response_data = JSON.parse(response);
-                       dataBinding(response_data, true, false);
-         }});
-  });
-    $('#export_link').click(function(){
+                    type: 'POST',
+                    url: window.location.pathname,
+                    data: {'aggregation-types':JSON.stringify(aggregationArray), 'start_time':time_list[0], 'end_time': time_list[1]},
+                    success:function(response) {
+                        var response_data = JSON.parse(response);
+                        DW.dataBinding(response_data, true, false);
+                        DW.wrap_table();
+                    }});
+    });
+    function hide_message() {
+        $('#dateErrorDiv label').delay(5000).fadeOut();
+    }
+
+    $('#export_link').click(function() {
         var time_list = submit_data();
         var path = window.location.pathname;
         var element_list = path.split("/");
         $("#aggregation-types").attr("value", JSON.stringify(aggregationArray));
-        $("#questionnaire_code").attr("value", element_list[element_list.length-2]);
+        $("#questionnaire_code").attr("value", element_list[element_list.length - 2]);
         $("#start_time").attr("value", time_list[0]);
         $("#end_time").attr("value", time_list[0]);
         $('#export_form').submit();
     });
-    function hide_message() {
->>>>>>> 9669195a6ae0374a860cf6691b296565f03a1d45
-        $('#dateErrorDiv label').delay(5000).fadeOut();
-  }
-
-} );
+});
