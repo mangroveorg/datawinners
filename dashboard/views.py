fa9419c0a427b2adb40941d32b78c866f531adf0
Rova Ratovo <rova.ratovo@gmail.com>
2011-06-27 14:42:58 +0300

    Updates on dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,9 +10,10 @@ from django.template.context import RequestContext
 from datawinners.main.utils import get_database_manager
 from mangrove.datastore import data
 from mangrove.form_model.form_model import FormModel
+from mangrove.form_model.field import field_to_json
 from mangrove.transport.reporter import find_reporter
 
-def get_submissions(dbm, form_code):
+def get_submissions(dbm, form_code, questions_num):
     rows = dbm.load_all_rows_in_view('submissionlog', reduce=False, descending=True, startkey=[form_code, {}],
                                          endkey=[form_code], limit=7)
 
@@ -20,7 +21,14 @@ def get_submissions(dbm, form_code):
     for row in rows:
         reporter = find_reporter(dbm, row.value["source"])
         reporter = reporter[0]["name"]
-        submission = dict(error_message=row.value["error_message"], created=row.value["submitted_on"], reporter=reporter)
+        if row.value["status"]:
+            if len(row.value["values"]) < questions_num:
+                message = "Error : Partial data received"
+            else:
+                message = " ".join(["%s: %s" % (k, v) for k, v in row.value["values"].items()])
+        else:
+            message = row.value["error_message"]
+        submission = dict(message=message, created=row.value["submitted_on"], reporter=reporter)
         submission_list.append(submission)
     return submission_list
 
@@ -35,7 +43,8 @@ def dashboard(request):
 
         form_model = manager.get(row['value']['qid'], FormModel)
         questionnaire_code = form_model.form_code
-        submissions = get_submissions(manager, questionnaire_code)
+        questions_num = len(form_model.fields)
+        submissions = get_submissions(manager, questionnaire_code, questions_num)
 
         project = dict(name=row['value']['name'], link=link, submissions=submissions)
         project_list.append(project)
