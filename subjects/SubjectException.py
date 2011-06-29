707589bb696ea0975ba6531ef96019f4789d1687
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-06-29 13:06:08 +0530

    xls import subject in project wizard : Akshay

---
+++ subjects/SubjectException.py
@@ -0,0 +1,8 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
+
+
+class InvalidFileFormatException(Exception):
+
+    def __str__(self):
+        return self.message
--- subjects/views.py
+++ subjects/views.py
@@ -1,5 +1,6 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from _csv import Error
+import os
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 import json
@@ -8,16 +9,31 @@ from django.template.context import RequestContext
 from django.views.decorators.csrf import csrf_view_exempt, csrf_response_exempt
 from django.views.decorators.http import require_http_methods
 from datawinners.main.utils import get_database_manager
+from datawinners.subjects.SubjectException import InvalidFileFormatException
 from datawinners.subjects.forms import SubjectUploadForm
 from django.contrib import messages
 from mangrove.datastore.entity import get_all_entities, get_by_short_code
-from mangrove.errors.MangroveException import CSVParserInvalidHeaderFormatException
+from mangrove.errors.MangroveException import CSVParserInvalidHeaderFormatException, XlsParserInvalidHeaderFormatException
 from mangrove.form_model.form_model import NAME_FIELD, MOBILE_NUMBER_FIELD, DESCRIPTION_FIELD
-from mangrove.transport.player.player import CsvPlayer, CsvParser
+from mangrove.transport.player.player import CsvPlayer, CsvParser, XlsPlayer, XlsParser
 from mangrove.transport.submissions import SubmissionHandler
 
 
-def _handle_uploaded_file(request, file=None):
+def _handle_uploaded_file(request, file, extension):
+    manager = get_database_manager(request)
+    if extension == '.csv':
+        file = file.splitlines()
+        csv_player = CsvPlayer(dbm=manager, submission_handler=SubmissionHandler(manager), parser=CsvParser())
+        response = csv_player.accept(file)
+    elif extension == '.xls':
+        xls_player = XlsPlayer(dbm=manager, submission_handler=SubmissionHandler(manager), parser=XlsParser())
+        response = xls_player.accept(file)
+    else:
+        raise InvalidFileFormatException()
+    return response
+
+
+def _handle_uploaded_file1(request, file=None):
     file = request.FILES['file'] if file is None else file.splitlines()
     manager = get_database_manager(request)
     csv_player = CsvPlayer(dbm=manager, submission_handler=SubmissionHandler(manager), parser=CsvParser())
@@ -53,7 +69,7 @@ def _tabulate_failures(rows):
         row[1].errors['row_num'] = row[0] + 2
         if type(row[1].errors['error']) is list:
             for error in row[1].errors['error']:
-                errors = errors+' '+error
+                errors = errors + ' ' + error
             row[1].errors['error'] = errors
         tabulated_data.append(row[1].errors)
     return tabulated_data
@@ -66,7 +82,7 @@ def index(request):
         form = SubjectUploadForm(request.POST, request.FILES)
         if form.is_valid():
             try:
-                response = _handle_uploaded_file(request)
+                response = _handle_uploaded_file1(request)
                 success = len([index for index in response if index.success])
                 total = len(response)
                 failure = [index for index in enumerate(response) if not index[1].success]
@@ -78,7 +94,7 @@ def index(request):
                 messages.error(request, 'We could not import your list of Subjects. ! \
                    You are using a document format we canʼt import. Please use a Comma Separated Values (.csv) ﬁle!')
             except Exception:
-                messages.error(request,'Some unexpected error happened. Please check the CSV file and import again.')
+                messages.error(request, 'Some unexpected error happened. Please check the CSV file and import again.')
     else:
         form = SubjectUploadForm()
     all_subjects = _laod_all_subjects(request)
@@ -97,7 +113,9 @@ def import_subjects_from_project_wizard(request):
     error_message = None
     failure_imports = None
     try:
-        response = _handle_uploaded_file(request=request, file=request.raw_post_data)
+        file_name = request.GET.get('qqfile')
+        base_name, extension = os.path.splitext(file_name)
+        response = _handle_uploaded_file(request=request, file=request.raw_post_data, extension=extension)
         successful_imports = len([index for index in response if index.success])
         total = len(response)
         failure = [index for index in enumerate(response) if not index[1].success]
@@ -107,9 +125,11 @@ def import_subjects_from_project_wizard(request):
         success_message = '%s of %s records uploaded' % (successful_imports, total)
     except CSVParserInvalidHeaderFormatException as e:
         error_message = e.message
-    except Error:
+    except XlsParserInvalidHeaderFormatException as e:
+        error_message = e.message
+    except InvalidFileFormatException:
         error_message = 'We could not import your list of Subjects. ! \
-                    You are using a document format we canʼt import. Please use a Comma Separated Values (.csv) ﬁle!'
+                    You are using a document format we canʼt import. Please use a Comma Separated Values (.csv) ﬁle or a Excel (.xls) file!'
     except Exception:
         error_message = 'Some unexpected error happened. Please check the CSV file and import again.'
     return HttpResponse(json.dumps({'success': success, 'message': success_message, 'error_message': error_message,
