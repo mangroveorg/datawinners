90e1b98adb94cf77c3cf6a8a37d65166b9d96d80
Aroj George <apgeorge@thoughtworks.com>
2011-07-14 19:31:32 +0530

    Fixing build <Aroj>

--- dashboard/tests/test_dashboard.py
+++ dashboard/tests/test_dashboard.py
@@ -1,8 +1,8 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from unittest2 import TestCase
+import unittest
 from django.test import Client
 
-class TestDashboard(TestCase):
+class TestDashboard(unittest.TestCase):
     fixtures = ['initial_data.json']
 
     def test_should_redirect_if_not_logged_in(self):
