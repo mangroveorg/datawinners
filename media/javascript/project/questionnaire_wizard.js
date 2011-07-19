879ffbe62c64d6331faff643b8155664fe5382dd
Mahesh Lal <mahesh.2910@gmail.com>
2011-07-19 14:27:59 +0530

    fixing preview not working <Mahesh>

--- media/javascript/project/questionnaire_wizard.js
+++ media/javascript/project/questionnaire_wizard.js
@@ -140,7 +140,6 @@ $(document).ready(function() {
             }
     )
 
-    $(document).ready(function(){
         $(".questionnaire_preview").dialog({
                 title: "Questionnaire Preview",
                 modal: true,
@@ -154,8 +153,39 @@ $(document).ready(function() {
                 }
               }
           );
+        $(".subject_registration_preview").dialog({
+                title: "Subject Registration Preview",
+                modal: true,
+                autoOpen: false,
+                height: 700,
+                width: 800,
+                closeText: 'hide',
+                open: function() {
+                  // Here I load the content. This is the content of your link.
+                  $(".subject_registration_preview").load(subject_registration_form_preview_link, function() {});
+                }
+              }
+          );
+        $(".sender_registration_preview").dialog({
+                title: "Sender Registration Preview",
+                modal: true,
+                autoOpen: false,
+                height: 700,
+                width: 800,
+                closeText: 'hide',
+                open: function() {
+                  // Here I load the content. This is the content of your link.
+                  $(".sender_registration_preview").load(sender_registration_form_preview_link, function() {});
+                }
+              }
+          );
         $(".preview").bind("click", function(){
            $(".questionnaire_preview").dialog("open");
         })
-    })
+        $(".preview_subject_registration_form").bind("click", function(){
+           $(".subject_registration_preview").dialog("open");
+        })
+        $(".preview_sender_registration_form").bind("click", function(){
+           $(".sender_registration_preview").dialog("open");
+        })
 });
