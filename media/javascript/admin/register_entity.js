9fcc7b0b21e79c12e519871224e632f3c9998428
Mahesh Lal <mahesh.2910@gmail.com>
2011-07-22 16:00:17 +0530

    fixing tests and couch fixtures to support new location handling<Mahesh>

--- media/javascript/admin/register_entity.js
+++ media/javascript/admin/register_entity.js
@@ -42,6 +42,23 @@ $(document).ready(function() {
         }
     }
     );
+    $.widget("custom.catcomplete", $.ui.autocomplete, {
+        _renderMenu: function(ul, items) {
+            var self = this,
+                    currentCategory = "";
+            $.each(items, function(index, item) {
+                if (item.category != currentCategory) {
+                    ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
+                    currentCategory = item.category;
+                }
+                self._renderItem(ul, item);
+            });
+        }
+    });
+
+    $("#id_location").catcomplete({
+        source: "/places"
+    });
 
     DW.viewModel = {
         'message': {
