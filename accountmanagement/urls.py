e7401c17a361371c2edd0dd8acafc8cc9d6b6a50
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-07-25 15:29:19 +0530

    adding the default page as login : Akshay

--- accountmanagement/urls.py
+++ accountmanagement/urls.py
@@ -12,6 +12,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = patterns('',
                        (r'^register/$', 'registration.views.register', {'form_class': RegistrationForm, 'template_name': 'registration/register.html', 'backend': 'datawinners.accountmanagement.registration_backend.RegistrationBackend'}),
                        url(r'^login/$', custom_login, {'template_name': 'registration/login.html', 'authentication_form': LoginForm}, name='auth_login'),
+                       url(r'^$', custom_login, {'template_name': 'registration/login.html', 'authentication_form': LoginForm}, name='auth_login'),
                        url(r'^password/reset/$', auth_views.password_reset, {'password_reset_form': ResetPasswordForm}, name='auth_password_reset'),
                        url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'set_password_form': PasswordSetForm}, name='auth_password_reset_confirm'),
                        ('', include('registration.backends.default.urls')),
