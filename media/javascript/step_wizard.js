df569457f903903cfeb131a6aac6d2f4ee2a7e1f
Prabhu <prabhur@thoughtworks.com>
2011-07-26 15:53:21 +0530

    Creating the cancel wizard for create project pages - Prabhu

--- media/javascript/step_wizard.js
+++ media/javascript/step_wizard.js
@@ -1,22 +1,36 @@
-  function set_current_tab(){
-      var current_element = $("#tab_items .current");
-      var current_tab = $("#tab_items li").index(current_element)+1;
-      var wizard_slider = $("#step_wizard").slider({
-               range: "max",
-               min: 1,
-               max: 5,
-               value: current_tab,
-               disabled:true
-      });
-  }
+function set_current_tab(){
+  var current_element = $("#tab_items .current");
+  var current_tab = $("#tab_items li").index(current_element)+1;
+  var wizard_slider = $("#step_wizard").slider({
+           range: "max",
+           min: 1,
+           max: 5,
+           value: current_tab,
+           disabled:true
+  });
+}
 $(document).ready(function(){
+   $("#cancel_project_block").dialog({
+        title: "Cancel this Project?",
+        modal: true,
+        autoOpen: false,
+        height: 200,
+        width: 300,
+        closeText: 'hide'
+      }
+   );
    $(".cancel").bind("click", function(){
-       var redirect = confirm("Please confirm to continue cancel" +
-               "\n(your previous steps are saved)");
-       if(redirect == true){
-            window.location.href = "/project/";
-       }
+//       var redirect = confirm("Please confirm to continue cancel" +
+//               "\n(your previous steps are saved)");
+//       if(redirect == true){
+//            window.location.href = "/project/";
+//       }
+
+       $("#cancel_project_block").dialog("open");
    })
+    $(".cancel_link").bind("click", function(){
+         $("#cancel_project_block").dialog("close");
+    })
 })
 
 
