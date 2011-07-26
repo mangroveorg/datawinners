ad7bcba79cd5bf3b260752bb312524266d6a3f20
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-07-26 18:34:25 +0530

    fixing When error is displayed on project profile page, SMS device selection disappears: Akshay

--- media/javascript/project/project_profile.js
+++ media/javascript/project/project_profile.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
     set_current_tab();
     $("#id_devices_0").attr('disabled', true);
 
+    $('#submit-button').click(function(){
+       $('#id_devices_0').attr('disabled',false);
+       $('#profile_form').submit();
+    });
+
+
     if ($('#id_activity_report_0').attr('checked')==true) {
              $('#id_entity_type').attr('disabled', true);
     }
