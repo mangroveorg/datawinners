a796f815134f05e94f7e54ecd9e4b11a45512c93
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-07-27 18:03:29 +0530

    fixed the scroll bar issue with subjects accordian on finish and test page : Akshay

--- media/javascript/project/finish_and_test.js
+++ media/javascript/project/finish_and_test.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $("#project_profile").accordion({   alwaysOpen: false, collapsible: true,autoHeight:false});
-$("#subjects").accordion({   alwaysOpen: false, collapsible: true,autoHeight:true, active: false});
+$("#subjects").accordion({   alwaysOpen: false, collapsible: true,autoHeight:false, active: false});
 $("#questionnaire").accordion({   alwaysOpen: false, active: false,collapsible: true,autoHeight:false});
 $("#data_senders").accordion({   alwaysOpen: false, active: false,collapsible: true,autoHeight:false});
 
