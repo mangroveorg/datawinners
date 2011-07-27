4ad8eebf81dbbfb687fb930f51ea20285b6dbab8
Prabhu <prabhur@thoughtworks.com>
2011-07-27 10:46:25 +0530

    Resolving the conflict - Prabhu

--- media/javascript/entity/create_type.js
+++ media/javascript/entity/create_type.js
@@ -17,11 +17,13 @@ $(document).ready(function() {
                         $('#id_entity_type').trigger('change');
                         $("#add_subject_type").accordion({collapsible: true,autoHeight:false, active:2});
                         $("#type_message").html('');
+                        $("#type_message").removeClass('mesage-box');
                     }
                     else{
                         $('#id_entity_type').val(new_type.toLowerCase());
                         $("#add_subject_type").accordion({collapsible: false,autoHeight:false, active:2});
                         $("#type_message").html(data.message);
+                        $("#type_message").addClass('mesage-box');
                     }
                 });
     });
