60214c79cdc2951d963b807bacdb1c0e166f07e5
Rova Ratovo <rova.ratovo@gmail.com>
2011-08-25 09:08:39 +0300

    Datawinners secondary page layout

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.conf.urls.defaults import patterns
 
-from datawinners.home.views import index
+from datawinners.home.views import index, secondary
 
 urlpatterns = patterns('',
         (r'^home/$', index),
+        (r'^home/secondary/$', secondary),
     )
