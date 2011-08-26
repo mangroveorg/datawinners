4e2e40bb856a77da7f589881f92d0c5bc3298182
Ritesh Yadav <yadavr@thoughtworks.com>
2011-08-26 17:53:21 +0530

    Added function tests for mobile number verification on registration page, Fixed bugs of Review and Test page : Ritesh

--- media/javascript/project/finish_and_test.js
+++ media/javascript/project/finish_and_test.js
@@ -4,5 +4,6 @@ $("#reminders").accordion({   alwaysOpen: false, collapsible: true,autoHeight:fa
 $("#subjects").accordion({   alwaysOpen: false, collapsible: true,autoHeight:false, active: false});
 $("#questionnaire").accordion({   alwaysOpen: false, active: false,collapsible: true,autoHeight:false});
 $("#data_senders").accordion({   alwaysOpen: false, active: false,collapsible: true,autoHeight:false});
+$("#reminders").accordion({   alwaysOpen: false, active: false,collapsible: true,autoHeight:false});
 
 });
