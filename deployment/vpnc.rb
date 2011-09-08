b403c290ed6350cdb97691d1599d0be97c0a3c7f
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-09-08 14:36:03 +0530

    Added the vpnc.rb

---
+++ deployment/vpnc.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'bluepill'
+require 'logger'
+
+Bluepill.application("vpnc") do |app|
+  app.process("vpnc-connect") do |process|
+    process.pid_file = "/var/run/vpnc/pid"
+    process.start_command = "/etc/init.d/vpnc start"
+    process.stop_command = "/etc/init.d/vpnc stop"
+    process.restart_command = "/etc/init.d/vpnc restart"
+    process.start_grace_time = 5.seconds
+    process.stop_grace_time = 5.seconds
+    process.restart_grace_time = 10 .seconds
+
+  end
+end
+
