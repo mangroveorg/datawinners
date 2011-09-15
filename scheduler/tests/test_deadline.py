bee06fd9b45034b46d57b7e38baf563686210308
Aroj George <apgeorge@thoughtworks.com>
2011-09-15 18:31:32 +0300

    Implementing following month for deadline. <Aroj>

--- scheduler/tests/test_deadline.py
+++ scheduler/tests/test_deadline.py
@@ -1,5 +1,5 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from unittest2 import TestCase
 
 class Deadline(object):
@@ -8,20 +8,24 @@ class Deadline(object):
         self.mode = mode
 
     def next(self, as_of):
-        return self.frequency.next_date(as_of)
+        return self.frequency.next_date(as_of,self._get_offset())
+
+#      Deadline class converts the modes "Following" or "That" to the currect offsets.
+    def _get_offset(self):
+        if self.mode == "Following":
+            return 1
+        return 0
 
 
 class Month(object):
     def __init__(self,day):
         self.day = day
 
-    def next_date(self, as_of):
+#    Offset is any valid offset > 0. Month knows that offset means months.
+    def next_date(self, as_of,offset):
         if as_of.day > self.day:
             return None
-        return date(as_of.year, as_of.month, self.day)
-#        assert day in range 1-31
-#        test case for feb or day = 31
-
+        return date(as_of.year, as_of.month + offset, self.day)
 
 class TestDeadline(TestCase):
     def test_should_return_next_deadline_date_for_current_month(self):
@@ -36,4 +40,7 @@ class TestDeadline(TestCase):
         deadline = Deadline(frequency=Month(24),mode="That")
         self.assertEqual(None, deadline.next(date(2011,9,30)))
 
-    
+    def test_should_return_next_deadline_date_for_following_month(self):
+        deadline = Deadline(frequency=Month(24),mode="Following")
+        self.assertEqual(date(2011,10,24), deadline.next(date(2011,9,15)))
+
