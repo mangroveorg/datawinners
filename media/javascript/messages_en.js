fd827ad4226799f153c69ce57191d6caa2e40825
herihaja <herihaja@herihaja-MacBookPro.(none)>
2011-09-26 19:40:54 +0300

    Adding translated messages for jquery.validate <herihaja>

---
+++ media/javascript/messages_en.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Language: EN
+ */
+jQuery.extend(jQuery.validator.messages, {
+        required: "This field is required @ teny anglisy.",
+        remote: "Please fill this field.",
+        email: "Please insert a valid email adress.",
+        url: "Please insert a valid URL.",
+        date: "Please insert a valid date.",
+        dateISO: "Please insert a valid date (ISO).",
+        number: "Please insert a valid number.",
+        digits: "Please insert only a numeric value.",
+        creditcard: "Please insert a valid credit card number.",
+        equalTo: "Please re-insert the same value.",
+        accept: "Please insert a value with a valid extension.",
+        maxlength: jQuery.format("Please do not enter more than {0} characters."),
+        minlength: jQuery.format("Please enter at least {0} characters."),
+        rangelength: jQuery.format("Please enter between {0} and {1} characters."),
+        range: jQuery.format("Please enter a value between {0} and {1}."),
+        max: jQuery.format("Please enter a value less than or equal to {0}."),
+        min: jQuery.format("Please enter a value greater than or equal to {0}.")
+});
