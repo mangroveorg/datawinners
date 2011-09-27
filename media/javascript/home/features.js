aa8d450028ea862ab6e87ad308c707a63dba681d
Rova <Rova@Mb-Imi.local>
2011-09-27 18:39:23 +0300

    Updating <What is Datawinners - Features> content

---
+++ media/javascript/home/features.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('#popbox').dialog({
+        autoOpen: false,
+        modal: true,
+        width: 640
+    });
+
+    function showDialog(src, title) {
+        $('#popbox img').attr('src', src);
+        $('#popbox').dialog({
+            title: title
+        });
+        $('#popbox').dialog('open');
+    }
+
+    $('.popbox').click(function(){
+        showDialog($(this).href, $(this).attr('title'));
+        return false;
+    });
+});
