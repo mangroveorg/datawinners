1b756caec0353e8f673cd206c653fa730491691f
Aroj George <apgeorge@thoughtworks.com>
2011-09-28 23:40:58 +0530

    Added ping monitoring of the vpn to bluepill config. <Aroj>

--- deployment/vpnc.rb
+++ deployment/vpnc.rb
@@ -2,15 +2,33 @@ require 'rubygems'
 require 'bluepill'
 require 'logger'
 
-Bluepill.application("vpnc") do |app|
+module Bluepill
+  module ProcessConditions
+      class VpnPing < ProcessCondition
+          def initialize(options = {})
+              @options = options
+          end
+          def run(pid)
+              system("ping -c2 192.168.0.151")
+          end
+          def check(value)
+              value
+          end
+      end
+  end
+end
+
+Bluepill.application("vpnc-connect",:log_file => "/var/log/bluepill.log") do |app|
   app.process("vpnc-connect") do |process|
     process.pid_file = "/var/run/vpnc/pid"
     process.start_command = "/etc/init.d/vpnc start"
     process.stop_command = "/etc/init.d/vpnc stop"
     process.restart_command = "/etc/init.d/vpnc restart"
-    process.start_grace_time = 5.seconds
-    process.stop_grace_time = 5.seconds
-    process.restart_grace_time = 10 .seconds
+    process.start_grace_time = 45.seconds
+    process.stop_grace_time = 15.seconds
+    process.restart_grace_time = 45.seconds
+    
+    process.checks :vpn_ping, :every => 60.seconds, :times => 3
 
   end
 end
