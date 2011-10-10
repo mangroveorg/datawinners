aa3a70aaa45f4f63af18ec4feb5de9c348390eb8
Rova <Rova@Mb-Imi.local>
2011-10-10 17:48:30 +0300

    Updating Datawinners website contents

--- home/urls.py
+++ home/urls.py
@@ -1,20 +1,45 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.conf.urls.defaults import patterns
 from django.views.generic.base import TemplateView
-from datawinners.home.views import index, switch_language
+from datawinners.home.views import index
 
 urlpatterns = patterns('',
         (r'^home/$', index),
-        (r'^switch/(?P<language>.{2}?)/$', switch_language),
-        (r'^what-is-datawinners/$', TemplateView.as_view(template_name='home/what-is-datawinners.html')),
-        (r'^what-is-datawinners/features/$', TemplateView.as_view(template_name='home/datawinners_features.html')),
-        (r'^what-is-datawinners/how-it-works/$', TemplateView.as_view(template_name='home/datawinners_how_it_works.html')),
-        (r'^what-is-datawinners/benefits/$', TemplateView.as_view(template_name='home/datawinners_benefits.html')),
-        (r'^success-stories/$', TemplateView.as_view(template_name='home/success_stories.html')),
-        (r'^success-story/bngrc/$', TemplateView.as_view(template_name='home/success_story_bngrc.html')),
-        (r'^success-story/msi/$', TemplateView.as_view(template_name='home/success_story_msi.html')),
-        (r'^success-story/santenet2/$', TemplateView.as_view(template_name='home/success_story_santenet2.html')),
-        (r'^pricing/$', TemplateView.as_view(template_name='home/pricing.html')),
-        (r'^support/$', index),
-        (r'^about-us/$', TemplateView.as_view(template_name='home/about_us.html')),
+        (r'^home/en/$', TemplateView.as_view(template_name='home/index_en.html')),
+        (r'^home/fr/$', TemplateView.as_view(template_name='home/index_fr.html')),
+
+        (r'^en/what-is-datawinners/$', TemplateView.as_view(template_name='home/what-is-datawinners_en.html')),
+        (r'^en/what-is-datawinners/features/$', TemplateView.as_view(template_name='home/datawinners_features_en.html')),
+        (r'^en/what-is-datawinners/how-it-works/$', TemplateView.as_view(template_name='home/datawinners_how_it_works_en.html')),
+        (r'^en/what-is-datawinners/benefits/$', TemplateView.as_view(template_name='home/datawinners_benefits_en.html')),
+        (r'^fr/what-is-datawinners/$', TemplateView.as_view(template_name='home/what-is-datawinners_fr.html')),
+        (r'^fr/what-is-datawinners/features/$', TemplateView.as_view(template_name='home/datawinners_features_fr.html')),
+        (r'^fr/what-is-datawinners/how-it-works/$', TemplateView.as_view(template_name='home/datawinners_how_it_works_fr.html')),
+        (r'^fr/what-is-datawinners/benefits/$', TemplateView.as_view(template_name='home/datawinners_benefits_fr.html')),
+
+        (r'^en/success-stories/$', TemplateView.as_view(template_name='home/success_stories_en.html')),
+        (r'^en/success-story/bngrc/$', TemplateView.as_view(template_name='home/success_story_bngrc_en.html')),
+        (r'^en/success-story/msi/$', TemplateView.as_view(template_name='home/success_story_msi_en.html')),
+        (r'^en/success-story/santenet2/$', TemplateView.as_view(template_name='home/success_story_santenet2_en.html')),
+        (r'^fr/success-stories/$', TemplateView.as_view(template_name='home/success_stories_fr.html')),
+        (r'^fr/success-story/bngrc/$', TemplateView.as_view(template_name='home/success_story_bngrc_fr.html')),
+        (r'^fr/success-story/msi/$', TemplateView.as_view(template_name='home/success_story_msi_fr.html')),
+        (r'^fr/success-story/santenet2/$', TemplateView.as_view(template_name='home/success_story_santenet2_fr.html')),
+
+        (r'^en/pricing/$', TemplateView.as_view(template_name='home/pricing_en.html')),
+        (r'^fr/pricing/$', TemplateView.as_view(template_name='home/pricing_fr.html')),
+
+        (r'^en/support/$', TemplateView.as_view(template_name='home/support_en.html')),
+        (r'^fr/support/$', TemplateView.as_view(template_name='home/support_fr.html')),
+
+        (r'^en/about-us/$', TemplateView.as_view(template_name='home/about_us_en.html')),
+        (r'^en/about-us/blog/$', TemplateView.as_view(template_name='home/about_blog_en.html')),
+        (r'^en/about-us/partners/$', TemplateView.as_view(template_name='home/about_partners_en.html')),
+        (r'^en/about-us/clients/$', TemplateView.as_view(template_name='home/about_clients_en.html')),
+        (r'^en/about-us/contact-us/$', TemplateView.as_view(template_name='home/about_contact_en.html')),
+        (r'^fr/about-us/$', TemplateView.as_view(template_name='home/about_us_fr.html')),
+        (r'^fr/about-us/blog/$', TemplateView.as_view(template_name='home/about_blog_fr.html')),
+        (r'^fr/about-us/partners/$', TemplateView.as_view(template_name='home/about_partners_fr.html')),
+        (r'^fr/about-us/clients/$', TemplateView.as_view(template_name='home/about_clients_fr.html')),
+        (r'^fr/about-us/contact-us/$', TemplateView.as_view(template_name='home/about_contact_fr.html')),
     )
--- home/views.py
+++ home/views.py
@@ -1,16 +1,7 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
-from django.shortcuts import render_to_response, redirect, HttpResponse
+from django.shortcuts import render_to_response, redirect
 
 
 def index(request):
-    return render_to_response('home/index.html')
-
-
-def switch_language(request, language):
-    request.session['django_language'] = language
-    if request.META.has_key('HTTP_REFERER'):
-        referer= '/' + '/'.join(request.META['HTTP_REFERER'].split('/')[3:])
-    else:
-        referer= '/'
-    return redirect(referer)
+    return render_to_response('home/index_en.html')
