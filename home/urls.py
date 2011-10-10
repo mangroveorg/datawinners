5f5ca77b8bf0090d5b8cad166a707b1c3afc78cf
roov7 <rova.ratovo@gmail.com>
2011-10-10 18:12:12 +0300

    Fix the build

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.conf.urls.defaults import patterns
 from django.views.generic.base import TemplateView
-from datawinners.home.views import index
+from datawinners.home.views import index, switch_language
 
 urlpatterns = patterns('',
         (r'^home/$', index),
+        (r'^switch/(?P<language>.{2}?)/$', switch_language),
         (r'^home/en/$', TemplateView.as_view(template_name='home/index_en.html')),
         (r'^home/fr/$', TemplateView.as_view(template_name='home/index_fr.html')),
 
--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render_to_response, redirect
 
 def index(request):
     return render_to_response('home/index_en.html')
+
+def switch_language(request, language):
+    request.session['django_language'] = language
+    if request.META.has_key('HTTP_REFERER'):
+        referer= '/' + '/'.join(request.META['HTTP_REFERER'].split('/')[3:])
+    else:
+        referer= '/'
+    return redirect(referer)
