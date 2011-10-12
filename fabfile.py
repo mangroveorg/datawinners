5b4d9e4de6a541572f03a0ff4d75d5596145c78e
Ritesh Yadav <yadavr@thoughtworks.com>
2011-10-12 12:06:32 +0530

    Added compilemessages command in Fabfile : Ritesh

--- fabfile.py
+++ fabfile.py
@@ -83,6 +83,7 @@ def deploy(build_number, home_dir, virtual_env, environment="test", branch="deve
             activate_and_run(virtual_env, "python manage.py syncdb --noinput")
             activate_and_run(virtual_env, "python manage.py migrate")
             activate_and_run(virtual_env, "python manage.py recreatedb")
+            activate_and_run(virtual_env, "python manage.py compilemessages")
             restart_gunicorn(virtual_env)
 
 
