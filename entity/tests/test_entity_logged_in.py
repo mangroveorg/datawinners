12da5d13aa2f1a31b7e15284456d64107c37b745
Geet Chandratre <geet_316@hotmail.com>
2011-10-12 14:47:09 +0530

    Geet | Modified entity log in client stub to client

--- entity/tests/test_entity_logged_in.py
+++ entity/tests/test_entity_logged_in.py
@@ -6,27 +6,26 @@ import json
 class TestEntityLoggedIn(unittest.TestCase):
 
     def setUp(self):
-        self.client_stub = Client()
-        self.client_stub.post('/login/', {'username': 'tester150411@gmail.com', 'password': 'tester150411'})
+        self.client = Client()
+        self.client.login(username = 'tester150411@gmail.com', password = 'tester150411')
 
     def test_should_render_datasender_create_view_if_logged_in(self):
-        response = self.client_stub.get('/entity/datasender/create')
+        response = self.client.get('/entity/datasender/create')
         self.assertEquals(response.status_code,200)
 
     def test_should_render_subject_create_view_if_logged_in(self):
-        response = self.client_stub.get('/entity/subject/create')
+        response = self.client.get('/entity/subject/create')
         self.assertEquals(response.status_code,200)
 
     def test_should_render_entity_datasenders_view_if_logged_in(self):
-        response = self.client_stub.get('/entity/datasenders/')
+        response = self.client.get('/entity/datasenders/')
         self.assertEquals(response.status_code,200)
 
-#TODO Refactor this code and apply django tests to it (currently using nose tests)
     def test_should_add_data_sender_with_appropriate_email(self):
-        response = self.client_stub.post('/entity/webuser/create',{'post_data' : json.dumps([{"email":"a1@a1.com","reporter_id":"rep4"},{"email":"a2@a2.com","reporter_id":"test"}])})
+        response = self.client.post('/entity/webuser/create',{'post_data' : json.dumps([{"email":"a1@a1.com","reporter_id":"rep4"},{"email":"a2@a2.com","reporter_id":"test"}])})
         self.assertEquals(response.status_code,200)
 
-        response = self.client_stub.post('/entity/webuser/create',{'post_data' : json.dumps([{"email":"a1@a1.com.com","reporter_id":"rep9"}])})
+        response = self.client.post('/entity/webuser/create',{'post_data' : json.dumps([{"email":"a1@a1.com.com","reporter_id":"rep9"}])})
         self.assertEquals(response.status_code,400)
 
         users = User.objects.filter(email='a1@a1.com')
@@ -37,7 +36,7 @@ class TestEntityLoggedIn(unittest.TestCase):
             user.delete()
 
     def test_should_render_all_subjects_view_if_logged_in(self):
-        response = self.client_stub.get('/entity/subjects/')
+        response = self.client.get('/entity/subjects/')
         self.assertEquals(response.status_code,200)
 
 
