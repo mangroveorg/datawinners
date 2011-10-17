21a6bdc1fdd21b5d3d25882c505aa20677274581
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-10-17 16:18:29 +0530

    Fixing some tests

--- location/LocationTree.py
+++ location/LocationTree.py
@@ -52,17 +52,17 @@ def get_location_hierarchy(lowest_level):
     cursor = connection.cursor()
     sql = """(select distinct 'LEVEL4' as LEVEL, name_4||','||name_3||','||name_2||','||name_1||','||name_0 as NAME from location_locationlevel l where name_4=UPPER('%s') limit 10)
     union
-    (select distinct 'LEVEL3' as LEVEL,  name_3||','||name_2||','||name_1 as NAME from location_locationlevel l where name_3=UPPER('%s') limit 10)
+    (select distinct 'LEVEL3' as LEVEL,  name_3||','||name_2||','||name_1||','||name_0 as NAME from location_locationlevel l where name_3=UPPER('%s') limit 10)
     union
-    (select distinct 'LEVEL2' as LEVEL,  name_2||','||name_1 as NAME from location_locationlevel l where name_2=UPPER('%s') limit 5)
-    union (select distinct 'LEVEL1' as LEVEL,  name_1 as NAME from location_locationlevel l where name_1=UPPER('%s') limit 5)
+    (select distinct 'LEVEL2' as LEVEL,  name_2||','||name_1||','||name_0 as NAME from location_locationlevel l where name_2=UPPER('%s') limit 5)
+    union (select distinct 'LEVEL1' as LEVEL,  name_1||','||name_0 as NAME from location_locationlevel l where name_1=UPPER('%s') limit 5)
     union (select distinct 'LEVEL0' as LEVEL,  name_0 as NAME from location_locationlevel l where name_0=UPPER('%s') limit 5) order by LEVEL;""" % (lowest_level, lowest_level, lowest_level, lowest_level, lowest_level)
     cursor.execute(sql)
     rows = cursor.fetchall()
     location_hierarchy = []
     for level, location in rows:
         location_hierarchy.append(location.split(','))
-    return location_hierarchy[0] if len(location_hierarchy) > 0 else []
+    return location_hierarchy[0] if len(location_hierarchy) > 0 else [lowest_level]
 
 _tree = None
 _tree_lock = Lock()
--- location/tests/test_location_tree.py
+++ location/tests/test_location_tree.py
@@ -2,7 +2,7 @@
 from _collections import defaultdict
 from django.test import TestCase
 from nose.plugins.skip import SkipTest
-from datawinners.location.LocationTree import get_location_groups_for_country, LocationTree
+from datawinners.location.LocationTree import get_location_groups_for_country, LocationTree, get_location_hierarchy
 
 class TestLocationTree(TestCase):
     fixtures = ['test_location_level.json']
@@ -13,23 +13,16 @@ class TestLocationTree(TestCase):
         pass
 
     def test_get_hierarchy_from_location(self):
-        self.assertEqual(self.tree.get_hierarchy_path('Amboanjo'),
-            ['madagascar', 'vatovavy fitovinany', 'manakara atsimo', 'amboanjo'])
+        self.assertEqual([u'AMBOANJO',u'MANAKARA ATSIMO',u'VATOVAVY FITOVINANY',u'Madagascar'], get_location_hierarchy('Amboanjo'))
 
     def test_should_return_same_value_if_not_in_path(self):
-        self.assertEqual(['pune'], self.tree.get_hierarchy_path('pune'))
-
-    def test_is_valid_location(self):
-        self.assertTrue(self.tree._exists("amboanjo"))
-        self.assertFalse(self.tree._exists("XYZ"))
+        self.assertEqual(['pune'], get_location_hierarchy('pune'))
 
     def test_should_get_location_hierarchy_for_geocode(self):
         self.assertEqual([u'madagascar', u'itasy', u'miarinarivo', u'ambatomanjaka', u'fkt ambaribe'],
                                                                                                      self.tree.get_location_hierarchy_for_geocode(
                                                                                                          lat=-18.777180,
                                                                                                          long=46.854321))
-        self.assertFalse(self.tree._exists("XYZ"))
-
     def test_should_get_filtered_list_group_by_levels(self):
         expected_location_group = defaultdict(list)
         expected_location_group[u'LEVEL3'] = [u'AMBOANJO,MANAKARA ATSIMO,VATOVAVY FITOVINANY', u'AMBATOMANJAKA,MIARINARIVO,ITASY']
@@ -40,8 +33,6 @@ class TestLocationTree(TestCase):
     def test_should_get_centroid_for_location_based_on_level_given(self):
         self.assertEqual((46.8558925459, -18.762105675278256), self.tree.get_centroid(location='Ambatomanjaka',
                                                                                       level='3'))
-        self.assertFalse(self.tree._exists("XYZ"))
-
     def test_should_return_none_for_location_unknown(self):
         self.assertEqual(None, self.tree.get_centroid(location='pune', level='3'))
 
