e3ff9f4824d6584758127a17d78d524d2af81448
David Reed <dreed@thoughtworks.com>
2011-10-18 15:46:44 +0800

    Fixing mistake that resulted in dropping all CouchDB managers during runtests.sh (it should only drop the 'test' ones)

--- main/management/commands/recreateviews.py
+++ main/management/commands/recreateviews.py
@@ -1,17 +1,15 @@
 from django.core.management.base import BaseCommand
-from datawinners.main.initial_couch_fixtures import load_manager_for_default_test_account, load_all_managers
+from datawinners.main.initial_couch_fixtures import load_test_managers, load_all_managers
 from datawinners.main.utils import  sync_views
 import mangrove
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        managers = []
         if "syncall" in args:
             managers = load_all_managers()
         else:
-            manager = load_manager_for_default_test_account()
-            managers.append(manager)
+            managers = load_test_managers()
 
         for manager in managers:
             print ("Database %s") % (manager.database_name,)
