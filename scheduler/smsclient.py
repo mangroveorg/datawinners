568891c0a61f667229afe9de7d535bdd5ed7a1e6
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-10-20 12:23:46 +0530

    taking the vumi smsc username and password from database : Akshay

--- scheduler/smsclient.py
+++ scheduler/smsclient.py
@@ -1,11 +1,17 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from datawinners import settings
+from datawinners.accountmanagement.models import OrganizationSetting
 from datawinners.scheduler.vumiclient import VumiClient, Connection
+from mangrove.utils.types import is_not_empty
 
 class SMSClient(object):
-    def __init__(self):
-        self.client = VumiClient(None, None, connection=Connection(settings.VUMI_USER, settings.VUMI_PASSWORD, base_url=settings.VUMI_API_URL))
 
     def send_sms(self,from_tel,to_tel, message):
-        #The encoding of the message to utf-8 is temporary and the fix should ideally be in vumi client.
-        self.client.send_sms(to_msisdn=to_tel,from_msisdn=from_tel, message=message.encode('utf-8'))
+        if is_not_empty(from_tel):
+            smsc = OrganizationSetting.objects.filter(sms_tel_number = from_tel)[0].smsc
+            if smsc is not None:
+                client = VumiClient(None, None, connection=Connection(smsc.vumi_username, smsc.vumi_username, base_url=settings.VUMI_API_URL))
+#        The encoding of the message to utf-8 is temporary and the fix should ideally be in vumi client.
+                client.send_sms(to_msisdn=to_tel,from_msisdn=from_tel, message=message.encode('utf-8'))
+                return True
+        return False
