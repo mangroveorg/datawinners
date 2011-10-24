e85ecaf84ee418194d0142d9c3ba2cd589045692
yongpeishi <pjong@thoughtworks.com>
2011-10-24 22:22:51 +0800

    [yongpeishi & huan] -m 'create broadcast sms page and limit sms content to 160 characters, create functional tests for sms characters limitation'

---
+++ media/javascript/project/broadcast_sms.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var count = function () {
+        var len = $(this).val().length
+        var limitation = 160;
+        if (len > limitation)
+            $(this).val($(this).val().substring(0, limitation))
+        $('#counter').html($(this).val().length)
+    }
+    $('#sms_content').bind('keyup keydown', count);
+});
+
+function clear_sms_content() {
+    $("#sms_content").val('');
+    $('#counter').html('0');
+}
