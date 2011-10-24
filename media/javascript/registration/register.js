cb1b01dcd2c93abd84322d0aef33268442d6995e
herihaja Rabe <herihaja@hni.org>
2011-10-24 13:18:03 +0300

    Terms and conditions

--- media/javascript/registration/register.js
+++ media/javascript/registration/register.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
+
+
+
+    $("#user_should_agree_terms_block").dialog({
+        autoOpen: false,
+        modal: true,
+        title: gettext('Agree terms and conditions'),
+        zIndex:200,
+        width: 500,
+        beforeClose: function() {
+            $('#web_user_error').hide();
+        }
+    });
+
+    $("#user_should_agree_terms_block .agree_link").bind("click", function() {
+        $("#agree-terms").attr("checked", true);
+        $("#user_should_agree_terms_block").dialog("close");
+    });
+
     $('#submit_registration_form').bind("click", function() {
+        if(!$("#agree-terms").attr("checked")){
+            $("#user_should_agree_terms_block").dialog("open");
+            return false;
+        }
+
         $('#submit_registration_form').attr("disabled", true);
         $('#registration_form').submit();
     });
