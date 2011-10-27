2ae0aca6a9e54d0ee41dea23ac1e3700f36bee42
herihaja Rabe <herihaja@hni.org>
2011-10-27 12:14:07 +0530

    Adding Redirection when accessing the homepage of the DW website

--- home/views.py
+++ home/views.py
@@ -11,7 +11,8 @@ class FeatureAwareTemplateView(TemplateView):
 
 
 def index(request):
-    return render_to_response('home/index_en.html', RequestContext(request))
+    language = request.session.get("django_language", "en")
+    return redirect("/%s/home/" % language)
 
 def switch_language(request, language):
     request.session['django_language'] = language
@@ -19,4 +20,8 @@ def switch_language(request, language):
         referer= '/' + '/'.join(request.META['HTTP_REFERER'].split('/')[3:])
     else:
         referer= '/'
+
+    if referer[:4] in ["/en/", "/fr/"]:
+        referer = "/%s/%s" % (language, referer[4:])
+
     return redirect(referer)
