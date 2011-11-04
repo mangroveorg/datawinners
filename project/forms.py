848b3a82e1252a247222abd92e9c950834f5969b
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-11-04 11:48:12 +0530

    Adding the watermark

--- project/forms.py
+++ project/forms.py
@@ -5,7 +5,7 @@ from django.db.models.fields import TextField
 from django.forms.fields import CharField, ChoiceField, MultipleChoiceField
 from django.core.exceptions import ValidationError
 from django.forms.forms import Form
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ugettext_lazy
 from django.forms.widgets import RadioFieldRenderer, RadioInput
 from django import forms
 from datawinners.utils import convert_to_ordinal
@@ -17,6 +17,11 @@ class BroadcastMessageForm(forms.Form):
     to = ChoiceField(label="To:",choices=(("Associated", "Data Senders from my project"), ("All", "All Data Senders")),
                      widget=forms.RadioSelect, initial=("Associated"))
     others = CharField(label="Other People:", max_length=160, widget=forms.Textarea, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(BroadcastMessageForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs['watermark'] = ugettext_lazy('Enter your SMS text')
+        self.fields['others'].widget.attrs['watermark'] = ugettext_lazy('Enter your recipient(s) telephone number. Use a comma (,) to separate the numbers.')
 
 
 class MyRadioFieldRenderer(RadioFieldRenderer):
