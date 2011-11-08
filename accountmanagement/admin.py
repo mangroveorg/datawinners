7b61b5ba517125632d9e4aec38197176dcb4e8f0
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-11-08 17:43:45 +0530

    Showing total sms in admin page

--- accountmanagement/admin.py
+++ accountmanagement/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group
 from datawinners.accountmanagement.models import OrganizationSetting, SMSC
 
 class OrganizationSettingAdmin(admin.ModelAdmin):
-    list_display = ('organization_name', 'organization_id')
+    list_display = ('organization_name', 'organization_id', 'inbound_sms', 'outbound_sms', 'total_sms')
     fields = ('sms_tel_number', 'smsc')
 
     def organization_name(self, obj):
@@ -12,6 +12,15 @@ class OrganizationSettingAdmin(admin.ModelAdmin):
 
     def organization_id(self, obj):
         return obj.organization.org_id
+
+    def inbound_sms(self, obj):
+        return obj.incoming_sms_count
+
+    def outbound_sms(self, obj):
+        return obj.outgoing_sms_count
+
+    def total_sms(self, obj):
+        return obj.incoming_sms_count + obj.outgoing_sms_count
 
 
 admin.site.register(OrganizationSetting, OrganizationSettingAdmin)
