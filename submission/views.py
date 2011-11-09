6a7f01c8d7cea42b97c83fd9d1d92ed43be93d5a
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-11-09 17:06:28 +0530

    Initial stab at refactoring the sms view

--- submission/views.py
+++ submission/views.py
@@ -4,17 +4,18 @@ from django.http import HttpResponse
 from django.utils import translation
 from django.views.decorators.csrf import csrf_view_exempt, csrf_response_exempt
 from django.views.decorators.http import require_http_methods
-from django.utils.translation import gettext as _
+
+from mangrove.errors.MangroveException import MangroveException, SubmissionParseException, FormModelDoesNotExistsException, NumberNotRegisteredException, DataObjectNotFound, SMSParserInvalidFormatException, MultipleSubmissionsForSameCodeException, UnknownOrganization
+from mangrove.form_model.form_model import get_form_model_by_code
+from mangrove.transport.player.parser import SMSParser
+from mangrove.transport.player.player import SMSPlayer, TransportInfo
+
 from datawinners.accountmanagement.models import OrganizationSetting, Organization, TEST_REPORTER_MOBILE_NUMBER
 from datawinners.location.LocationTree import get_location_tree
-from datawinners.main.utils import get_db_manager_for, get_organization_settings_for, get_database_manager
+from datawinners.main.utils import get_database_manager, get_db_manager_for
 from datawinners.messageprovider.messages import exception_messages, SMS
 from datawinners.ordersmsparser.order_sms_parser import OrderSMSParser
 from datawinners.submission.models import DatawinnerLog, SMSResponse
-from mangrove.errors.MangroveException import MangroveException, SubmissionParseException, FormModelDoesNotExistsException, NumberNotRegisteredException, DataObjectNotFound, UnknownOrganization, SMSParserInvalidFormatException, MultipleSubmissionsForSameCodeException
-from mangrove.form_model.form_model import get_form_model_by_code
-from mangrove.transport.player.parser import SMSParser
-from mangrove.transport.player.player import SMSPlayer, Request, TransportInfo
 from datawinners.messageprovider.message_handler import get_exception_message_for
 
 import logging
@@ -37,78 +38,90 @@ def _get_from_and_to_numbers(request):
     _to = request.POST["to_msisdn"]
     return _from, _to
 
-
-@csrf_view_exempt
-@csrf_response_exempt
-@require_http_methods(['POST'])
-#TODO This needs some major refactoring. It is very clear that I want to do things which the accept method of the player outside the accept method and hence I am duplicating here.
-def sms(request):
-    _message = request.POST["message"]
-    _from, _to = _get_from_and_to_numbers(request)
-    if _to is None:
-        return HttpResponse(_("Your organization does not have a telephone number assigned. Please contact DataWinners Support."))
+def submit_to_player(incoming_request):
     try:
-        org_settings = get_organization_settings_for(_from, _to, request.user)
-    except UnknownOrganization as exception:
-        message = get_exception_message_for(exception=exception, channel=SMS)
-        log = DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message)
-        log.save()
-        return HttpResponse(message)
-
-    if _from == TEST_REPORTER_MOBILE_NUMBER:
-        get_database_manager(request.user)
-    else:
-        org_settings.increment_incoming_message_count()
-        if not org_settings.should_handle_message():
-            return HttpResponse(_("You have used up your 100 SMS for the trial account. Please upgrade to a monthly subscription to continue sending in data to your projects."))
-        dbm = get_db_manager_for(_from, _to)
-
-    try:
-        if settings.USE_ORDERED_SMS_PARSER:
-            sms_parser = OrderSMSParser(dbm)
-        else:
-            sms_parser = SMSParser()
-        form_code, token = sms_parser.form_code(_message)
-        form_model = get_form_model_by_code(dbm, form_code)
-        translation.activate(form_model.activeLanguages[0])
-    except (SubmissionParseException,SMSParserInvalidFormatException,MultipleSubmissionsForSameCodeException) as exception:
-        message = get_exception_message_for(exception=exception, channel=SMS)
-        log = DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message)
-        log.save()
-        org_settings.increment_outgoing_message_count()
-        return HttpResponse(message)
-    except FormModelDoesNotExistsException as exception:
-        message = get_exception_message_for(exception=exception, channel=SMS)
-        log = DatawinnerLog(message=_message, from_number=_from, to_number=_to, form_code=exception.data[0],
-                            error=message)
-        log.save()
-        org_settings.increment_outgoing_message_count()
-        return HttpResponse(message)
-
-    try:
-        sms_player = SMSPlayer(dbm, get_location_tree(), sms_parser)
-        transportInfo = TransportInfo(transport=SMS, source=_from, destination=_to)
-        response = sms_player.accept(Request(transportInfo=transportInfo, message=_message))
+        sms_player = SMSPlayer(incoming_request['dbm'], get_location_tree())
+        response = sms_player.accept(incoming_request['transport_info'], incoming_request['form_model'].form_code, incoming_request['values'])
         message = SMSResponse(response).text()
     except (SubmissionParseException, FormModelDoesNotExistsException,) as exception:
         message = get_exception_message_for(exception=exception, channel=SMS)
-        log = DatawinnerLog(message=_message, from_number=_from, to_number=_to, form_code=exception.data[0],
-                            error=message)
-        log.save()
+        DatawinnerLog(message=_message, from_number=_from, to_number=_to, form_code=exception.data[0],error=message).save()
     except NumberNotRegisteredException as exception:
         message = get_exception_message_for(exception=exception, channel=SMS)
-        log = DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message)
-        log.save()
+        DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message).save()
     except DataObjectNotFound as exception:
         message = exception_messages.get(DataObjectNotFound).get(SMS)
-        message = message % (form_model.entity_type[0], exception.data[1], form_model.entity_type[0])
+        message = message % (incoming_request['form_model'].entity_type[0], exception.data[1], incoming_request['form_model'].entity_type[0])
     except MangroveException as exception:
         message = get_exception_message_for(exception=exception, channel=SMS)
     except Exception as exception:
         logger.exception('SMS Processing failure: message')
         message = get_exception_message_for(exception=exception, channel=SMS)
 
-    org_settings.increment_outgoing_message_count()
-    return HttpResponse(message)
+    incoming_request['outgoing_message'] = message
+    return incoming_request
 
+def activate_language(incoming_request):
+    translation.activate(incoming_request['form_model'].activeLanguages[0])
+    incoming_request['next_state'] = submit_to_player
+    return incoming_request
+
+def find_parser(incoming_request):
+    sms_parser = SMSParser() if settings.USE_ORDERED_SMS_PARSER else OrderSMSParser(incoming_request['dbm'])
+    try:
+        form_code, values = sms_parser.parse(incoming_request['incoming_message'])
+        form_model = get_form_model_by_code(incoming_request['dbm'], form_code)
+        incoming_request['form_model'] = form_model
+        incoming_request['submission_values'] = values
+
+    except (SubmissionParseException,SMSParserInvalidFormatException,MultipleSubmissionsForSameCodeException) as exception:
+        message = get_exception_message_for(exception=exception, channel=SMS)
+        DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message).save()
+        incoming_request['outgoing_message'] = message
+    except FormModelDoesNotExistsException as exception:
+        message = get_exception_message_for(exception=exception, channel=SMS)
+        DatawinnerLog(message=_message, from_number=_from, to_number=_to, form_code=exception.data[0], error=message).save()
+        incoming_request['outgoing_message'] = message
+
+    incoming_request['next_state'] = activate_language
+    return incoming_request
+
+def find_dbm(request):
+    _from, _to = _get_from_and_to_numbers(request)
+    transport_info = TransportInfo(transport=SMS, source=_from, destination=_to)
+    incoming_request = {'incoming_message': request.POST["message"], 'transport_info': transport_info}
+    if _to is None:
+        incoming_request['outgoing_message'] = "Your organization does not have a telephone number assigned. Please contact DataWinners Support."
+        return incoming_request
+
+    try:
+        incoming_request['dbm'] = get_database_manager(request.user) if _from == TEST_REPORTER_MOBILE_NUMBER else get_db_manager_for(_from, _to)
+    except UnknownOrganization as exception:
+        message = get_exception_message_for(exception=exception, channel=SMS)
+        incoming_request['outgoing_message'] = message
+        DatawinnerLog(message=incoming_request['incoming_message'], from_number=_from, to_number=_to, error=message).save()
+        return incoming_request
+    incoming_request['next_state'] = find_parser
+    return incoming_request
+
+
+class Responder(object):
+
+    def __init__(self):
+        self.next_state_processor = find_dbm
+
+    def respond(self, incoming_request):
+        request = self.next_state_processor(incoming_request)
+        if request.get('outgoing_message'):
+            return request['outgoing_message']
+
+        self.next_state_processor = request['next_state']
+        return self.respond(request)
+
+@csrf_view_exempt
+@csrf_response_exempt
+@require_http_methods(['POST'])
+def sms(request):
+    message = Responder().respond(request)
+    return HttpResponse(message)
 
