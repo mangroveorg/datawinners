460578f88fe3689755cb980ff93027380b690aec
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-11-10 11:11:41 +0530

    Fixing DataWinners Log

--- submission/views.py
+++ submission/views.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils import translation
+from django.utils.translation import ugettext
 from django.views.decorators.csrf import csrf_view_exempt, csrf_response_exempt
 from django.views.decorators.http import require_http_methods
 
@@ -41,14 +42,14 @@ def _get_from_and_to_numbers(request):
 def submit_to_player(incoming_request):
     try:
         sms_player = SMSPlayer(incoming_request['dbm'], get_location_tree())
-        response = sms_player.accept(incoming_request['transport_info'], incoming_request['form_model'].form_code, incoming_request['values'])
+        response = sms_player.accept(incoming_request['transport_info'], incoming_request['form_model'].form_code, incoming_request['submission_values'])
         message = SMSResponse(response).text()
     except (SubmissionParseException, FormModelDoesNotExistsException,) as exception:
-        message = get_exception_message_for(exception=exception, channel=SMS)
-        DatawinnerLog(message=_message, from_number=_from, to_number=_to, form_code=exception.data[0],error=message).save()
+        message = incoming_request['datawinner_log'].error = get_exception_message_for(exception=exception, channel=SMS)
+        incoming_request['datawinner_log'].save()
     except NumberNotRegisteredException as exception:
-        message = get_exception_message_for(exception=exception, channel=SMS)
-        DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message).save()
+        message = incoming_request['datawinner_log'].error = get_exception_message_for(exception=exception, channel=SMS)
+        incoming_request['datawinner_log'].save()
     except DataObjectNotFound as exception:
         message = exception_messages.get(DataObjectNotFound).get(SMS)
         message = message % (incoming_request['form_model'].entity_type[0], exception.data[1], incoming_request['form_model'].entity_type[0])
@@ -67,39 +68,41 @@ def activate_language(incoming_request):
     return incoming_request
 
 def find_parser(incoming_request):
-    sms_parser = SMSParser() if settings.USE_ORDERED_SMS_PARSER else OrderSMSParser(incoming_request['dbm'])
+    sms_parser = OrderSMSParser(incoming_request['dbm']) if settings.USE_ORDERED_SMS_PARSER else SMSParser()
     try:
         form_code, values = sms_parser.parse(incoming_request['incoming_message'])
         form_model = get_form_model_by_code(incoming_request['dbm'], form_code)
         incoming_request['form_model'] = form_model
         incoming_request['submission_values'] = values
-
+        incoming_request['datawinner_log'].form_code = form_code
     except (SubmissionParseException,SMSParserInvalidFormatException,MultipleSubmissionsForSameCodeException) as exception:
         message = get_exception_message_for(exception=exception, channel=SMS)
-        DatawinnerLog(message=_message, from_number=_from, to_number=_to, error=message).save()
-        incoming_request['outgoing_message'] = message
+        incoming_request['outgoing_message'] = incoming_request['datawinner_log'].error = message
+        incoming_request['datawinner_log'].save()
     except FormModelDoesNotExistsException as exception:
         message = get_exception_message_for(exception=exception, channel=SMS)
-        DatawinnerLog(message=_message, from_number=_from, to_number=_to, form_code=exception.data[0], error=message).save()
-        incoming_request['outgoing_message'] = message
+        incoming_request['outgoing_message'] = incoming_request['datawinner_log'].error = message
+        incoming_request['datawinner_log'].save()
 
     incoming_request['next_state'] = activate_language
     return incoming_request
 
 def find_dbm(request):
-    _from, _to = _get_from_and_to_numbers(request)
+    _from, _to = _get_from_and_to_numbers(request) #This is the http post request. After this state, the request being sent is a python dictionary
     transport_info = TransportInfo(transport=SMS, source=_from, destination=_to)
-    incoming_request = {'incoming_message': request.POST["message"], 'transport_info': transport_info}
+    incoming_request = {'incoming_message': request.POST["message"], 'transport_info': transport_info,
+                        'datawinner_log': DatawinnerLog(message=request.POST["message"], from_number=_from,
+                                                        to_number=_to)}
     if _to is None:
-        incoming_request['outgoing_message'] = "Your organization does not have a telephone number assigned. Please contact DataWinners Support."
+        incoming_request['outgoing_message'] = ugettext("Your organization does not have a telephone number assigned. Please contact DataWinners Support.")
         return incoming_request
 
     try:
         incoming_request['dbm'] = get_database_manager(request.user) if _from == TEST_REPORTER_MOBILE_NUMBER else get_db_manager_for(_from, _to)
     except UnknownOrganization as exception:
         message = get_exception_message_for(exception=exception, channel=SMS)
-        incoming_request['outgoing_message'] = message
-        DatawinnerLog(message=incoming_request['incoming_message'], from_number=_from, to_number=_to, error=message).save()
+        incoming_request['outgoing_message'] = incoming_request['datawinner_log'].error = message
+        incoming_request['datawinner_log'].save()
         return incoming_request
     incoming_request['next_state'] = find_parser
     return incoming_request
