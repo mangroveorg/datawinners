6f74664672f110230a9f67886204e019b3975367
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-11-10 11:51:10 +0530

    Fixed a test: test_should_load_all_subjects

--- entity/tests/test_import_data.py
+++ entity/tests/test_import_data.py
@@ -9,6 +9,7 @@ from mangrove.datastore.datadict import DataDictType
 from mangrove.datastore.entity import create_entity
 from mangrove.datastore.entity_type import define_type
 from mangrove.form_model.form_model import MOBILE_NUMBER_FIELD, NAME_FIELD
+from mangrove.transport.player.parser import SMSParser
 from mangrove.transport.player.player import SMSPlayer, TransportInfo, Request
 
 class TestImportData(TestCase):
@@ -59,7 +60,8 @@ class TestImportData(TestCase):
 
 
     def _register_entity(self, text):
-        self.player.accept(Request(transportInfo=self.transport, message=text))
+        form_code, values = SMSParser().parse(text)
+        self.player.accept(self.transport, form_code, values)
 
     def _register_entities(self):
         self._register_entity('reg .t clinic .s 1 .g 1 1 .n clinic0')
