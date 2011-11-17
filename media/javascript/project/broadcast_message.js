0b02e0055afe1b73aaac33c83e9805f572a9a2a4
Asif Momin <asifmom@thoughtworks.com>
2011-11-17 14:54:12 +0530

    Refactoring of broadcastsms.js

--- media/javascript/project/broadcast_message.js
+++ media/javascript/project/broadcast_message.js
@@ -1,66 +1,123 @@
-$(document).ready(function() {
-    $.validator.addMethod('regexrule', function(value, element) {
-        var text = $('#' + element.id).val();
-        var re = new RegExp('^[0-9 ,]+$');
-        var string_has_non_numeric_char = re.test(text);
-        if (!string_has_non_numeric_char)
-            return false;
-        var telephone_numbers = text.split(',');
-        for (var each in telephone_numbers){
-            if (telephone_numbers[each].length>10){
-                return false;
-            }
-        }
-        return true;
-    }, gettext("Only 10 digit letters are valid"));
+DW.broadcast_sms=function(){
+    this.clearElementId='#clear_broadcast_sms_form';
+    this.formElementId="#broadcast_sms_form";
+    this.idToElement="#id_to";
+    this.messageCountElement="#messageCount";
+    this.smsContentElement="#sms_content";
+    this.maxSMSChar=160;
+    this.additional_column=new DW.additional_column();
 
-    var defaults = $("#broadcast_sms_form").validate({
-        rules: {
-            text:{
-                required:true
-            }
-        },
-        wrapper: "div",
-        errorPlacement: function(error, element) {
-            var offset = element.offset();
-            error.insertAfter(element);
-            error.addClass('error_arrow');  // add a class to the wrapper
-        }
-    });
+}
 
-    DW.additional_column = function() {
-        if ($('#id_to').val() == "Additional") {
-            $('#additional_people_column').removeClass('none');
-            $('#id_others').rules("add", "regexrule");
-            $('#id_others').rules("add", "required");
+DW.broadcast_sms.prototype={
+    init:function(){
+        this.createSMSContentValidationRule();
+        this.createAdditionalTelephoneValidationRule();
+        this.processAddtionalColumnValidation();
+        this.setMessageCount()
+    },
+
+    createAdditionalTelephoneValidationRule:function(){
+        this.additional_column.createAdditionalTelephoneValidationRule()
+    },
+    createSMSContentValidationRule:function(){
+        var defaults = $(this.formElementId).validate({
+            rules: {
+                text:{
+                    required:true
+                }
+            },
+            wrapper: "div",
+            errorPlacement: function(error, element) {
+                var offset = element.offset();
+                error.insertAfter(element);
+                error.addClass('error_arrow');  // add a class to the wrapper
+            }
+        });
+
+    },
+
+    processAddtionalColumnValidation:function(){
+        if (this.isAdditionalSelected()) {
+            this.additional_column.addRule();
         }
         else {
-            $('#additional_people_column').addClass('none');
-            $('#id_others').rules("remove", "regexrule");
-            $('#id_others').rules("remove", "required");
+            this.additional_column.removeRule();
         }
-    };
 
-    //every time page loads do these 2lines
-    DW.additional_column();
-    $('#messageCount').html($('#sms_content').val().length + "/160");
+    },
+
+    isAdditionalSelected:function(){
+        return $(this.idToElement).val() == "Additional"
+    },
+    setMessageCount:function(){
+        $(this.messageCountElement).html(this.getSMSLength() + "/"+this.maxSMSChar);
+    },
+    getSMSLength:function(){
+        return $(this.smsContentElement).val().length;
+    },
+    clearContent:function(){
+        $(this.formElementId)[0].reset();
+        this.setMessageCount();
+
+    }
+
+}
+
+
+DW.additional_column=function(){
+    this.additionalPeopleId="#additional_people_column";
+    this.telephoneNumbersElementId='#id_others';
+    this.telephone_number_rule="regexrule";
+}
+
+DW.additional_column.prototype={
+    createAdditionalTelephoneValidationRule:function(){
+        $.validator.addMethod(this.telephone_number_rule, function(value, element) {
+            var text = $('#' + element.id).val();
+            var re = new RegExp('^[0-9 ,]+$');
+            var string_has_non_numeric_char = re.test(text);
+            if (!string_has_non_numeric_char)
+                return false;
+            var telephone_numbers = text.split(',');
+            for (var each in telephone_numbers){
+                if (telephone_numbers[each].length>10){
+                    return false;
+                }
+            }
+            return true;
+        }, gettext("Only 10 digit letters are valid"));
+
+    },
+
+    addRule:function(){
+        $(this.additionalPeopleId).removeClass('none');
+        $(this.telephoneNumbersElementId).rules("add", this.telephone_number_rule);
+        $(this.telephoneNumbersElementId).rules("add", "required");
+
+    },
+    removeRule:function(){
+        $(this.additionalPeopleId).addClass('none');
+        $(this.telephoneNumbersElementId).rules("remove", this.telephone_number_rule);
+        $(this.telephoneNumbersElementId).rules("remove", "required");
+
+    }
+
+}
+
+$(document).ready(function() {
+    var broadcast_sms=new DW.broadcast_sms();
+    broadcast_sms.init()
 
     $('#sms_content').keyup(function() {
-        var message_length = $('#sms_content').val().length;
-        if (message_length > 160) {
-            $('#messageCount').css('color', 'red');
-        } else {
-            $('#messageCount').css('color', 'green');
-        }
-        $('#messageCount').html(message_length + "/160 ");
+        broadcast_sms.setMessageCount();
     });
 
     $('#id_to').change(function() {
-        DW.additional_column();
+        broadcast_sms.processAddtionalColumnValidation();
     });
 
     $('#clear_broadcast_sms_form').click(function() {
-        $('#broadcast_sms_form')[0].reset();
-        $('#messageCount').html($('#sms_content').val().length + "/160 ");
+        broadcast_sms.clearContent();
     });
 });
