2be896090903d31db1480543c6e60e618e7384a9
akshaysn <akshay.naval@gmail.com>
2011-11-29 11:14:11 +0530

    fixing the sms character count : Akshay/Kanchan

--- media/javascript/project/new_questionnaire_charcount.js
+++ media/javascript/project/new_questionnaire_charcount.js
@@ -1,17 +1,13 @@
 DW.charCount = function() {
     var questionnaire_code_len = $('#questionnaire-code').val().length;
-    var question_codes_len = 0;
-    var selected_question_code_difference = 0;
     var max_len = 160;
     var constraints_len = 0;
-    var space_len = 1;
     var delimiter_len = 1;
     var sms_number = 1;
     var sms_number_text = "";
     var i = 0;
     for (i; i < viewModel.questions().length; i=i+1) {
         var current_question = viewModel.questions()[i];
-        question_codes_len = question_codes_len + current_question.code().length + space_len + delimiter_len;
         var question_type = current_question.type();
         switch (question_type) {
             case 'integer':
@@ -40,7 +36,7 @@ DW.charCount = function() {
         }
         constraints_len = constraints_len + delimiter_len;
     }
-    var current_len = questionnaire_code_len + question_codes_len + constraints_len + selected_question_code_difference;
+    var current_len = questionnaire_code_len + constraints_len;
     if (current_len <= max_len) {
         $("#char-count").css("color", "#666666");
     }
