73a47491948fdf2adc97f256c3e0db79b5991759
akshaysn <akshay.naval@gmail.com>
2011-12-05 15:50:32 +0530

    changing the description of entity question with the change in subject type: Akshay

--- media/javascript/project/subject_warning_dialog_module.js
+++ media/javascript/project/subject_warning_dialog_module.js
@@ -1,8 +1,19 @@
+DW.change_description = function(){
+    $(viewModel.questions()).each(function (question) {
+        var question_title = this.title();
+        if (this.is_entity_question()) {
+            this.title(question_title.replace('subject', $('#id_entity_type').val()));
+        }
+      });
+    viewModel.questions.valueHasMutated();
+}
+
 //TODO For heaven's sake this needs to be named properly and we need to see if we can push logic from this to objects.
 DW.continue_flip = function(){
     DW.subject_warning_dialog_module.enable_or_disable_entity_type_control();
     if (DW.subject_warning_dialog_module.is_subject_selected()) {
         DW.init_view_model(subject_report_questions);
+        DW.change_description();
     }
     else {
         DW.init_view_model(activity_report_questions);
