6b5f3aea47482a9b6089a3cc9bbe0aae120f0eca
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-08 17:48:00 +0530

    Removed q1,q2.. from sms preview on questionnaire page: Neetu

--- media/javascript/project/sms_preview.js
+++ media/javascript/project/sms_preview.js
@@ -3,7 +3,6 @@ DW.smsPreview = function() {
     var i = 0;
     for (i; i < viewModel.questions().length; i=i+1) {
         var current_question = viewModel.questions()[i];
-        sms_preview += " q" + (i+1);
         var question_type = current_question.type();
         switch (question_type) {
             case 'integer':
