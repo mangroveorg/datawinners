298a7e797f97b9f1526700181ec4b3a3ae37c9ee
akshaysn <akshay.naval@gmail.com>
2011-12-08 14:17:27 +0530

    finishing the registration processor(integration is remaining) : Akshay

---
+++ accountmanagement/registration_processors.py
@@ -0,0 +1,60 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+from django.core.mail.message import EmailMessage
+from django.template.loader import render_to_string
+from registration.models import RegistrationProfile
+from datawinners import settings
+from datawinners.accountmanagement.models import PaymentDetails
+
+def get_registration_processor(organization):
+    return TrialAccountRegistrationProcessor(organization) if organization.in_trial_mode \
+                else PaidAccountRegistrationProcessor(organization)
+
+
+class TrialAccountRegistrationProcessor(object):
+
+    def __init__(self, organization):
+        self.organization = organization
+
+    def process(self, user, site, kwargs=None):
+        if not kwargs: kwargs = {}
+        ctx_dict = {'activation_key': RegistrationProfile.objects.get(user=user).activation_key,
+                    'expiration_days': settings.ACCOUNT_ACTIVATION_DAYS,
+                    'site': site,
+                    'name':user.first_name + ' ' + user.last_name}
+        subject = render_to_string('registration/activation_email_subject_for_trial_account.txt')
+        message = render_to_string('registration/activation_email_for_trial_account.html',
+                                   ctx_dict)
+        email = EmailMessage(subject, message, settings.EMAIL_HOST_USER, [user.email], [settings.HNI_SUPPORT_EMAIL_ID])
+        email.content_subtype = "html"
+        email.send()
+
+
+
+
+class PaidAccountRegistrationProcessor(object):
+
+    def __init__(self, organization):
+        self.organization = organization
+
+    def process(self, user, site, kwargs):
+        self._make_payment_details(kwargs)
+        self._send_activation_email(site, user)
+
+    def _make_payment_details(self, kwargs):
+        invoice_period = kwargs['invoice_period']
+        preferred_payment = kwargs['preferred_payment']
+        payment_details = PaymentDetails.objects.model(organization=self.organization, invoice_period=invoice_period,
+                                                       preferred_payment=preferred_payment)
+        payment_details.save()
+
+    def _send_activation_email(self, site, user):
+        ctx_dict = {'activation_key': RegistrationProfile.objects.get(user=user).activation_key,
+                    'expiration_days': settings.ACCOUNT_ACTIVATION_DAYS,
+                    'site': site}
+        subject = render_to_string('registration/activation_email_subject.txt',
+                                   ctx_dict)
+        subject = ''.join(subject.splitlines()) # Email subject *must not* contain newlines
+        message = render_to_string('registration/activation_email.txt',
+                                   ctx_dict)
+        user.email_user(subject, message, settings.EMAIL_HOST_USER)
+
---
+++ accountmanagement/test/test_registration_processor.py
@@ -0,0 +1,115 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+from django.contrib.auth.models import User
+from django.utils import unittest
+from registration.models import RegistrationProfile
+from datawinners.accountmanagement.models import Organization, NGOUserProfile, PaymentDetails
+from datawinners.accountmanagement.organization_id_creator import OrganizationIdCreator
+from datawinners.accountmanagement.registration_processors import get_registration_processor, PaidAccountRegistrationProcessor, TrialAccountRegistrationProcessor
+from django.contrib.sites.models import Site
+import dircache
+import os
+from django.conf import settings
+from mangrove.utils.types import is_not_empty
+
+
+class TestRegistrationProcessor(unittest.TestCase):
+    def prepare_organization(self):
+        self.paid_organization = Organization(name='test_org_for_paid_account',
+                                              sector='PublicHealth', address='add',
+                                              city='Pune', country='India',
+                                              zipcode='411006', in_trial_mode=False,
+                                              org_id=OrganizationIdCreator().generateId())
+        
+        self.trial_organization = Organization(name='test_org_for_trial_account',
+                                                            sector='PublicHealth', address='add',
+                                                            city='Pune', country='India',
+                                                            zipcode='411006', in_trial_mode=True,
+                                                            org_id=OrganizationIdCreator().generateId())
+        self.paid_organization.save()
+        self.trial_organization.save()
+
+    def setUp(self):
+        User.objects.filter(username = 'paid_account@mail.com').delete()
+        User.objects.filter(username = 'trial_account@mail.com').delete()
+        Organization.objects.filter(name='test_org_for_paid_account').delete()
+        Organization.objects.filter(name='test_org_for_trial_account').delete()
+
+        self.user1 = RegistrationProfile.objects.create_inactive_user(username='paid_account@mail.com', email='paid_account@mail.com', password='hi', site=Site(), send_email=False)
+        self.user2 = RegistrationProfile.objects.create_inactive_user(username='trial_account@mail.com', email='trial_account@mail.com', password='hi', site=Site(), send_email=False)
+
+        self.user1.first_name = 'first_name1'
+        self.user1.last_name = 'last_name1'
+
+        self.user2.first_name = 'first_name2'
+        self.user2.last_name = 'last_name2'
+
+        self.user1.save()
+        self.user2.save()
+
+        self.prepare_organization()
+
+        NGOUserProfile(user = self.user1,title = 'Mr.',org_id = self.paid_organization.org_id).save()
+        NGOUserProfile(user = self.user2,title = 'Ms.',org_id = self.trial_organization.org_id).save()
+
+    def tearDown(self):
+        self.user1.delete()
+        self.user2.delete()
+        self.paid_organization.delete()
+        self.trial_organization.delete()
+
+
+    def test_should_get_the_correct_registration_processor(self):
+        self.assertTrue(isinstance(get_registration_processor(self.trial_organization), TrialAccountRegistrationProcessor))
+
+        self.assertTrue(isinstance(get_registration_processor(self.paid_organization), PaidAccountRegistrationProcessor))
+
+
+    def test_should_process_registration_data_for_paid_acccount(self):
+        settings.EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+        settings.EMAIL_FILE_PATH = 'email'
+
+        processor = get_registration_processor(self.paid_organization)
+
+        site = Site(domain='test', name='test_site')
+        kwargs = dict(invoice_period='', preferred_payment='')
+
+
+        processor.process(self.user1, site, kwargs)
+
+        self.assertTrue(is_not_empty(PaymentDetails.objects.filter(organization=self.paid_organization)))
+
+        list = dircache.listdir('email')
+        emails = ''
+        for email in list:
+            emails += (open('email/'+email, 'r').read())
+            os.remove('email/'+email)
+
+        self.assertIn('From: ' + settings.EMAIL_HOST_USER, emails)
+        self.assertIn('To: paid_account@mail.com', emails)
+        self.assertIn('Subject: Account activation on test_site', emails)
+        activation_link = 'http://test/activate/'+ (RegistrationProfile.objects.get(user=self.user1)).activation_key + '/'
+        self.assertIn(activation_link, emails)
+
+    def test_should_process_registration_data_for_trial_acccount(self):
+        settings.EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+        settings.EMAIL_FILE_PATH = 'email'
+
+        processor = get_registration_processor(self.trial_organization)
+
+        site = Site(domain='test', name='test_site')
+        kwargs = dict(invoice_period='', preferred_payment='')
+
+        processor.process(self.user2, site, kwargs)
+
+        list = dircache.listdir('email')
+        emails = ''
+        for email in list:
+            emails += (open('email/'+email, 'r').read())
+            os.remove('email/'+email)
+
+        self.assertIn('From: ' + settings.EMAIL_HOST_USER, emails)
+        self.assertIn('To: trial_account@mail.com', emails)
+        self.assertIn('Subject: DataWinners Trial Account Activation', emails)
+        self.assertIn('Hello first_name2 last_name2,', emails)
+        activation_link = 'http://test/activate/'+ (RegistrationProfile.objects.get(user=self.user2)).activation_key + '/'
+        self.assertIn(activation_link, emails)
