fdad88f986509dcdff76bdecd06c3888f061ed18
Kanchan Motlag <kanchan.motlag09@gmail.com>
2011-12-08 17:17:48 +0530

    Refactored code for submissions view : Kanchan

--- submission/urls.py
+++ submission/urls.py
@@ -2,7 +2,9 @@
 from django.conf.urls.defaults import patterns
 
 from datawinners.submission.views import sms
+from datawinners.submission.views import web_sms
 
 urlpatterns = patterns('',
     (r'^submission$', sms),
+    (r'^web_submission$', web_sms),
 )
