33d02c7e5517f91d859637b70915152a818aed37
roov7 <rova;ratovo@gmail.com>
2011-12-12 16:54:55 +0530

    Fixed #957 - Fixed bug on import subject or datasenders dialog box

--- media/javascript/project/import_section.js
+++ media/javascript/project/import_section.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $(".import_subject_form").dialog({
         autoOpen: false,
-        width: 450,
+        width: 520,
         modal: true,
         title: gettext('Import a List'),
         close: function() {
