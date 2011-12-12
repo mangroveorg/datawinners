d1165d319918174737327ff13690f86b61d67e2d
herihaja Rabe <herihaja@hni.org>
2011-12-12 12:44:50 +0530

    Hiding video in get-started page

--- media/javascript/home/video.js
+++ media/javascript/home/video.js
@@ -7,48 +7,48 @@ var viewModel = {
 }
 
 
-//chargement de swfobject à partir du CDN de google
-    google.load("swfobject", "2.1");
+//loading swfobject from Google CDN
+google.load("swfobject", "2.1");
 
-    // Update a particular HTML element with a new value
-    function updateHTML(elmId, value) {
-      document.getElementById(elmId).innerHTML = value;
-    }
+// Update a particular HTML element with a new value
+function updateHTML(elmId, value) {
+  document.getElementById(elmId).innerHTML = value;
+}
 
-    // chargement de la vidéo concerné dans le player
-    function loadVideoById(idvid) {
-      var videoID = idvid;
-      if(ytplayer) {
-        ytplayer.loadVideoById(videoID);
-      }
-    }
+// loading video in the player
+function loadVideoById(idvid) {
+  var videoID = idvid;
+  if(ytplayer) {
+    ytplayer.loadVideoById(videoID);
+  }
+}
 
-    // fonction appellé en cas d'erreur
-    function onPlayerError(errorCode) {
-      alert("An error occured of type:" + errorCode);
-    }
+// function called when an error occured
+function onPlayerError(errorCode) {
+  alert("An error occured of type:" + errorCode);
+}
 
-    // fonction appellé au chargement du player
-    function onYouTubePlayerReady(playerId) {
-      ytplayer = document.getElementById("ytPlayer");
-      ytplayer.addEventListener("onError", "onPlayerError");
-    }
+// function called on loading player
+function onYouTubePlayerReady(playerId) {
+  ytplayer = document.getElementById("ytPlayer");
+  ytplayer.addEventListener("onError", "onPlayerError");
+}
 
 
-    function loadPlayer() {
-      var videoID = 'TYk6LenOnG4';
-      var params = { allowScriptAccess: "always" };
-      var atts = { id: "ytPlayer", wmode:"transparent" };
+function loadPlayer() {
+  var videoID = 'TYk6LenOnG4';
+  var params = { allowScriptAccess: "always" };
+  var atts = { id: "ytPlayer", wmode:"transparent" };
 
-      swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "&enablejsapi=1&playerapiid=player1",
-            "videoDiv",
-            440,
-            240,
-            "8", null, null, params, atts);
-    }
-    function _run() {
-      loadPlayer();
-    }
+  swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "&enablejsapi=1&playerapiid=player1",
+        "videoDiv",
+        440,
+        240,
+        "8", null, null, params, atts);
+}
+function _run() {
+  loadPlayer();
+}
 
 
 $(document).ready(function(){
