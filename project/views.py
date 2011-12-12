7330e85970fdda10eb927134b78c52fbd9379e4e
roov7 <rova;ratovo@gmail.com>
2011-12-12 17:20:46 +0530

    Fix excel file extension fo windows

--- project/views.py
+++ project/views.py
@@ -329,7 +329,7 @@ def export_data(request):
 
 
 def _create_excel_response(raw_data_list, file_name):
-    response = HttpResponse(mimetype="application/ms-excel")
+    response = HttpResponse(mimetype="application/vnd.ms-excel")
     response['Content-Disposition'] = 'attachment; filename="%s.xls"' % (file_name,)
     wb = utils.get_excel_sheet(raw_data_list, 'data_log')
     wb.save(response)
