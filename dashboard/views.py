15c1d93a19773ddc1d9841c34516063a73ac4d48
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-12-13 12:25:39 +0530

    Added some help text in the reminder UI

--- dashboard/views.py
+++ dashboard/views.py
@@ -50,7 +50,7 @@ def get_submission_breakup(request, project_id):
     response = json.dumps([submission_success, submission_errors])
     return HttpResponse(response)
 
-def get_submission_about_project(request, project_id):
+def get_submissions_about_project(request, project_id):
     dbm = get_database_manager(request.user)
     project = Project.load(dbm.database, project_id)
     form_model = FormModel.get(dbm, project.qid)
