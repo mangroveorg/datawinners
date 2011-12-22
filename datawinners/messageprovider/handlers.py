8b0412401302abf333770729c30c775b19688736
akshaysn <akshay.naval@gmail.com>
2011-12-22 10:59:55 +0530

    creating the exception handlers : Akshay/Kanchan

--- datawinners/messageprovider/handlers.py
+++ datawinners/messageprovider/handlers.py
@@ -1,25 +1,41 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from django.utils.translation import ugettext
+from django.utils import translation
+from form_model.form_model import get_form_model_by_code
 import mangrove.errors.MangroveException as ex
 from messageprovider.message_handler import get_exception_message_for
 from messageprovider.messages import SMS
 
 def default_exception_handler(exception, request):
+    return  get_exception_message_for(exception=exception, channel=SMS)
+
+def default_exception_handler_with_logger(exception, request):
     response = request['datawinner_log'].error = get_exception_message_for(exception=exception, channel=SMS)
     request['datawinner_log'].save()
     return response
+
+def data_object_not_found_handler(exception, request):
+    return get_exception_message_for(exception=exception, channel=SMS, formatter=data_object_not_found_formatter)
+
+def sms_parser_wrong_number_of_answers_handler(exception, request):
+    _activate_language(exception, request)
+    return default_exception_handler_with_logger(exception, request)
+
+exception_handlers = {
+
+    ex.DataObjectNotFound : data_object_not_found_handler,
+    ex.FormModelDoesNotExistsException : default_exception_handler_with_logger,
+    ex.NumberNotRegisteredException : default_exception_handler_with_logger,
+    ex.SubmissionParseException : default_exception_handler_with_logger,
+    ex.SMSParserInvalidFormatException : default_exception_handler_with_logger,
+    ex.MultipleSubmissionsForSameCodeException : default_exception_handler_with_logger,
+    ex.SMSParserWrongNumberOfAnswersException : sms_parser_wrong_number_of_answers_handler,
+}
 
 def data_object_not_found_formatter(data_object_not_found_exception, message):
     entity_type, param, value = data_object_not_found_exception.data
     return message % (entity_type,value,entity_type)
 
-
-def data_object_not_found_handler(exception, request):
-    return get_exception_message_for(exception=exception, channel=SMS, formatter=data_object_not_found_formatter)
-
-exception_handlers = {
-
-    ex.DataObjectNotFound : data_object_not_found_handler
-
-}
+def _activate_language(exception, request):
+    form_model = get_form_model_by_code(request['dbm'], exception.data[0])
+    translation.activate(form_model.activeLanguages[0])
 
--- datawinners/messageprovider/tests/test_exception_handler.py
+++ datawinners/messageprovider/tests/test_exception_handler.py
@@ -1,7 +1,10 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 import unittest
-from mangrove.errors.MangroveException import SubmissionParseException, NumberNotRegisteredException, DataObjectNotFound
-from mock import Mock
+from mangrove.datastore.database import DatabaseManager
+from django.utils.translation import get_language
+from mangrove.form_model.form_model import FormModel
+from mangrove.errors.MangroveException import NumberNotRegisteredException, DataObjectNotFound, SMSParserWrongNumberOfAnswersException
+from mock import Mock, patch
 from messageprovider.exception_handler import handle
 from messageprovider.message_handler import get_exception_message_for
 from messageprovider.messages import SMS, exception_messages
@@ -29,10 +32,21 @@ class TestExceptionHandler(unittest.TestCase):
         exception_message_dict = exception_messages[type(exception)]
         expected_message = exception_message_dict.get(SMS) % ('test_entity','123','test_entity')
         response = handle(exception, self.request)
-
         self.assertEqual(expected_message, response)
 
+    def test_should_handle_SMSParserWrongNumberOfAnswersException(self):
+        exception = SMSParserWrongNumberOfAnswersException('test_code')
 
+        form_model_mock = Mock(spec=FormModel)
+        patcher = patch('messageprovider.handlers.get_form_model_by_code')
+        get_form_model_mock = patcher.start()
+        get_form_model_mock.return_value = form_model_mock
+        form_model_mock.activeLanguages = ['en']
+        self.request['dbm'] = Mock(spec=DatabaseManager)
+        expected_message = get_exception_message_for(exception, channel=SMS)
 
+        response = handle(exception, self.request)
 
-
+        self.assertEqual(expected_message, response)
+        self.assertEqual('en', get_language())
+        patcher.stop()
