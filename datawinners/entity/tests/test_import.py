16e6add78774c0a6fccccb5dfff87ed1f4b0bfb3
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-27 23:40:47 +0530

    Added a test for testing file imports

--- datawinners/entity/tests/test_import.py
+++ datawinners/entity/tests/test_import.py
@@ -17,6 +17,13 @@ class TestImport(MangroveTestCase):
 "REG","reporter","Reporter2 Maintirano ","Maintirano ","-18.05  44.0333","This is a reporter in Maintirano ","r3",987654333
 "REG","reporter","Reporter3 Mananjary ","Mananjary ","-21.2  48.3667","This is a reporter in Mananjary ","r4",987654334
 """
+        self.csv_data_without_short_code = """
+"form_code","t","n","l","g","d","m"
+"REG","reporter","Aàman Farafangana ","Farafangana ","-21.8  46.8333","This is a Clinic in near to Farafangana ",987654328
+"REG","reporter","Reporter1 Fianarantsoa ","mahajanga ","-20.45  45.1","C'est une clinique à Fianarantsoa",987654329
+"REG","reporter","Reporter2 Maintirano ","Maintirano ","-18.05  44.0333","This is a reporter in Maintirano ",987654333
+"REG","reporter","Reporter3 Mananjary ","Mananjary ","-21.2  48.3667","This is a reporter in Mananjary ",987654334
+"""
         initializer.run(self.manager)
 
     def tearDown(self):
@@ -35,3 +42,17 @@ class TestImport(MangroveTestCase):
         self.assertEqual('reporter', imported_entities["r2"])
         self.assertEqual('reporter', imported_entities["r3"])
         self.assertEqual('reporter', imported_entities["r4"])
+
+    def test_should_generate_short_code_and_import_data_senders_if_short_code_is_not_given(self):
+        file_name = "reporters.csv"
+        request = Mock()
+        request.GET = {'qqfile': file_name}
+        request.raw_post_data = self.csv_data_without_short_code
+        error_message, failure_imports, success_message, imported_entities = import_data(request=request,
+                                                                                         manager=self.manager)
+        self.assertEqual(4, get_entity_count_for_type(self.manager, entity_type="reporter"))
+        self.assertEqual(4, len(imported_entities))
+        self.assertEqual('reporter', imported_entities["rep1"])
+        self.assertEqual('reporter', imported_entities["rep2"])
+        self.assertEqual('reporter', imported_entities["rep3"])
+        self.assertEqual('reporter', imported_entities["rep4"])
