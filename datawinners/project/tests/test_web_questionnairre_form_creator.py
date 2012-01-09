c285c1936f9de85e75207506474f0e6bb41273bc
Kanchan Motlag <kanchan.motlag09@gmail.com>
2012-01-09 18:30:11 +0530

    Initial checkin for Questionnaire Web Form Creation kanchan/Asif

---
+++ datawinners/project/tests/test_web_questionnairre_form_creator.py
@@ -0,0 +1,84 @@
+import unittest
+from django.forms.fields import CharField, MultipleChoiceField
+from django.forms.forms import Form
+from mangrove.datastore.datadict import DataDictType
+from mangrove.form_model.field import TextField, SelectField, _get_text_field
+from mangrove.datastore.database import DatabaseManager
+from mangrove.form_model.form_model import FormModel
+from mock import Mock
+from project.web_questionnaire_form_creator import WebQuestionnaireFormCreater
+
+class TestWebQuestionnaireFormCreator(unittest.TestCase):
+    def setUp(self):
+        self.dbm = Mock(spec=DatabaseManager)
+        self.form_code="form_code"
+        self.form_model = FormModel(dbm=self.dbm,form_code=self.form_code,name="abc",fields=[])
+        self.field_name = "test"
+        self.field_code = "code"
+        self.instruction = "some instruction"
+        self.text_field_code="text"
+        self.select_field_code="select"
+
+
+    def test_should_create_web_questionnaire_for_char_field(self):
+        is_required = True
+        self.form_model.add_field(self._get_text_field(is_required))
+
+        questionnaire_form_class = WebQuestionnaireFormCreater().create(self.form_model)
+
+        web_text_field = questionnaire_form_class().fields[self.text_field_code]
+        self.assertEqual(CharField,type(web_text_field))
+        self.assertEqual(self.field_name,web_text_field.label)
+        self.assertEqual(self.instruction,web_text_field.help_text)
+        self.assertEqual(is_required,web_text_field.required)
+
+
+    def test_should_create_web_questionnaire_for_select_field(self):
+        is_required = False
+
+        expected_choices, text_field = self._get_select_field(is_required)
+        self.form_model.add_field(text_field)
+
+        questionnaire_form_class = WebQuestionnaireFormCreater().create(self.form_model)
+
+        web_text_field = questionnaire_form_class().fields[self.select_field_code]
+        self.assertEqual(MultipleChoiceField,type(web_text_field))
+        self.assertEqual(self.field_name,web_text_field.label)
+        self.assertEqual(is_required,web_text_field.required)
+        self.assertEqual(expected_choices,web_text_field.choices)
+
+    def test_should_create_hidden_field_for_form_code(self):
+
+        questionnaire_form_class = WebQuestionnaireFormCreater().create(self.form_model)
+
+        web_text_field = questionnaire_form_class().fields['form_code']
+        self.assertEqual(CharField,type(web_text_field))
+        self.assertEqual(self.form_code,web_text_field.initial)
+
+    def test_should_create_web_questionnaire(self):
+
+        self.form_model.add_field(self._get_text_field(False))
+        self.form_model.add_field(self._get_select_field(False)[1])
+
+        questionnaire_form_class = WebQuestionnaireFormCreater().create(self.form_model)
+
+        web_form = questionnaire_form_class()
+        self.assertEqual(3,len(web_form.fields))
+        self.assertEqual(CharField,type(web_form.fields[self.form_code]))
+        self.assertEqual(CharField,type(web_form.fields[self.text_field_code]))
+        self.assertEqual(MultipleChoiceField,type(web_form.fields[self.select_field_code]))
+
+
+    def _get_select_field(self, is_required):
+        choices = [("Red", "a"), ("Green", "b"), ("Blue", "c")]
+        expected_choices = [("a", "Red"), ("b", "Green"), ("c", "Blue")]
+        text_field = SelectField(name=self.field_name, code=self.select_field_code, label=self.field_name,
+                                 ddtype=Mock(spec=DataDictType),
+                                 options=choices, single_select_flag=False, required=is_required)
+        return expected_choices, text_field
+
+    def _get_text_field(self, is_required):
+        text_field = TextField(name=self.field_name, code=self.text_field_code, label=self.field_name,
+                               ddtype=Mock(spec=DataDictType),
+                               instruction=self.instruction, required=is_required)
+        return text_field
---
+++ datawinners/project/web_questionnaire_form_creator.py
@@ -0,0 +1,70 @@
+from django import forms
+from django.forms.fields import CharField, ChoiceField, MultipleChoiceField
+from django.forms.forms import Form
+from django.forms.widgets import CheckboxSelectMultiple, HiddenInput
+from mangrove.form_model.field import SelectField
+
+class WebQuestionnaireFormCreater(object):
+    def create(self, form_model):
+        properties = {field.code: self._get_django_field(field) for field in form_model.fields}
+        properties.update({'form_code': forms.CharField(widget=HiddenInput, initial=form_model.form_code)})
+        
+        return type('QuestionnaireForm', (Form, ), properties)
+
+    def _get_django_field(self,field):
+
+        try:
+            field_creation_map={SelectField:self._create_select_field}
+            return field_creation_map[type(field)](field)
+        except KeyError:
+            return self._create_char_field(field)
+
+
+#        if isinstance(field, SelectField):
+#            return  create_select_field(field, create_choices(field))
+#        if field.is_entity_field:
+#            return create_entity_select_field_for_reporter(field,associated_entities)
+#        display_field = CharField(label=field.name, initial=field.value, required=field.is_required(), help_text=field.instruction)
+#        display_field.widget.attrs["watermark"] = field.get_constraint_text()
+#        display_field.widget.attrs['style'] = 'padding-top: 7px;'
+#        #    display_field.widget.attrs["watermark"] = "18 - 1"
+#        return display_field
+
+    def _create_char_field(self,field):
+        return forms.CharField(label=field.name, initial=field.value,   required=field.is_required(), help_text=field.instruction)
+
+    def _create_select_field(self,field):
+        return forms.MultipleChoiceField(label=field.name, widget=forms.CheckboxSelectMultiple, choices=self._create_choices(field),
+                                  initial=field.value, required=field.is_required(), help_text=field.instruction)
+    def _create_choices(self,field):
+        choice_list = [('', '--None--')] if field.single_select_flag else []
+        choice_list.extend([(option['val'], option['text']['en']) for option in field.options])
+        choices = tuple(choice_list)
+        return choices
+
+def create_select_field(field, choices):
+    if field.single_select_flag:
+        return ChoiceField(choices=choices, required=field.is_required(), label=field.name, initial=field.value, help_text=field.instruction)
+    return MultipleChoiceField(label=field.name, widget=CheckboxSelectMultiple, choices=choices,
+                                  initial=field.value, required=field.is_required(), help_text=field.instruction)
+
+
+def create_choices(field):
+    choice_list = [('', '--None--')] if field.single_select_flag else []
+    choice_list.extend([(option['val'], option['text']['en']) for option in field.options])
+    choices = tuple(choice_list)
+    return choices
+
+
+def create_choices_for_reporter(choices):
+    choice_list = [('', '--None--')]
+    if choices is not None:
+        choice_list.extend(choice for choice in choices)
+    return tuple(choice_list)
+
+
+def create_entity_select_field_for_reporter(field,associated_entities):
+    choices = create_choices_for_reporter(associated_entities)
+    return ChoiceField(choices = choices,required=field.is_required,label=field.name,initial=field.value,help_text=field.instruction)
+
+
