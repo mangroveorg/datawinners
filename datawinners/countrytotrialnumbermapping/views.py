b48fcc457d6aed3ff6a8f6f9d6d09bb9d817bca4
Asif Momin <asifmom@thoughtworks.com>
2012-01-13 15:45:38 +0530

    fixing the build

--- datawinners/countrytotrialnumbermapping/views.py
+++ datawinners/countrytotrialnumbermapping/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.views.decorators.csrf import csrf_view_exempt
 from countrytotrialnumbermapping.helper import get_countries_in_display_format
-from datawinners.countrytotrialnumbermapping.models import Country, Network, Mapping
+from datawinners.countrytotrialnumbermapping.models import Country
 import json
 
 @csrf_view_exempt
@@ -12,6 +12,7 @@ def trial_account_phone_numbers(request, language):
     template = 'countrytotrialaccountmapping/trial_account_phone_number_%s.html' % (language, )
     if request.method == 'GET':
         countries = Country.objects.all()
-        return render_to_response(template,{'formatted_countries':get_countries_in_display_format(countries)}, context_instance=RequestContext(request))
+        return render_to_response(template, {'formatted_countries': get_countries_in_display_format(countries)},
+            context_instance=RequestContext(request))
     if request.method == 'POST':
-        return HttpResponse(json.dumps({'foo':'bar'}))
+        return HttpResponse(json.dumps({'foo': 'bar'}))
