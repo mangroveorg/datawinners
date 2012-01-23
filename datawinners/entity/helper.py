f7d983b8fe2af17014527d5ef3d4e0b95441e4f4
roov7 <rova;ratovo@gmail.com>
2012-01-23 14:09:39 +0300

    Making all questions, except unique ID, mandatory in subject registration form

--- datawinners/entity/helper.py
+++ datawinners/entity/helper.py
@@ -72,8 +72,7 @@ def _create_registration_form(manager, entity_name=None, form_code=None, entity_
                               instruction=_("Enter a %(entity_type)s last name") % {'entity_type':entity_name})
     question3 = HierarchyField(name=LOCATION_TYPE_FIELD_NAME, code=LOCATION_TYPE_FIELD_CODE,
                                label=_("What is the %(entity_type)s's location?") % {'entity_type':entity_name},
-                               language="en", ddtype=location_type, instruction="Enter a region, district, or commune",
-                               required=False)
+                               language="en", ddtype=location_type, instruction="Enter a region, district, or commune")
     question4 = GeoCodeField(name=GEO_CODE_FIELD, code=GEO_CODE, label=_("What is the %(entity_type)s's GPS co-ordinates?") % {'entity_type':entity_name},
                              language="en", ddtype=geo_code_type,
                              instruction="Enter lat and long. Eg 20.6, 47.3")
@@ -81,7 +80,7 @@ def _create_registration_form(manager, entity_name=None, form_code=None, entity_
                                      label=_("What is the %(entity_type)s's mobile telephone number?") % {'entity_type':entity_name},
                                      defaultValue="some default value", language="en", ddtype=mobile_number_type,
                                      instruction=_("Enter the %(entity_type)s's number") % {'entity_type':entity_name}, constraints=(
-                                     _create_constraints_for_mobile_number()), required=False)
+                                     _create_constraints_for_mobile_number()))
     question6 = TextField(name=SHORT_CODE_FIELD, code=SHORT_CODE, label=_("What is the %(entity_type)s's Unique ID Number") % {'entity_type':entity_name},
                               defaultValue="some default value", language="en", ddtype=name_type,
                               instruction="Enter an id, or allow us to generate it",
