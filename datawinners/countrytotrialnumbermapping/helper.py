0b8dcfb1062d95248900a032359312c470d24965
akshaysn <akshay.naval@gmail.com>
2012-01-24 14:29:36 +0530

    Sorting the countries alphabetically : Akshay

--- datawinners/countrytotrialnumbermapping/helper.py
+++ datawinners/countrytotrialnumbermapping/helper.py
@@ -1,7 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+from django.utils.datastructures import SortedDict
 
 def get_countries_in_display_format(countries):
-    return {country.country_name:_get_formatted_country_name(country) for country in countries}
+    country_dict = SortedDict()
+    for country in countries:
+        country_dict[country.country_name] = _get_formatted_country_name(country)
+    return country_dict
 
 def _get_formatted_country_name(country):
     return country.country_name + ' (' + country.country_code + ')'
