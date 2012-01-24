73cd3068227040edb848b6543346daba8f702641
Asif Momin <asifmom@thoughtworks.com>
2012-01-24 15:12:43 +0530

    changing the location of the shape files to import. this is needed due to change in project directory structure

--- datawinners/main/management/commands/loadshapes.py
+++ datawinners/main/management/commands/loadshapes.py
@@ -5,5 +5,5 @@ from datawinners.location.utils import load_from_madagascar_ocha_wgs84_shp_file
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        shape_file_dir = os.path.join(settings.PROJECT_DIR, "../../../shape_files/madawgs84/")
+        shape_file_dir = os.path.join(settings.PROJECT_DIR, "../../shape_files/madawgs84/")
         load_from_madagascar_ocha_wgs84_shp_file(shape_file_dir, verbose=True)
