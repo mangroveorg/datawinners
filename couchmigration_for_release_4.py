dfe5d94c4bdd5e9961be5f8641015ac4f03446a6
akshaysn <akshay.naval@gmail.com>
2012-02-08 10:38:41 +0530

    making couch migrations more verbose : Akshay

--- couchmigration_for_release_4.py
+++ couchmigration_for_release_4.py
@@ -16,35 +16,46 @@ function(doc){
 """
 
 def migrate_01(managers, map_fun_raw_form_model_docs):
+    failed_managers = []
     for manager in managers:
-        initializer.run(manager)
+        try:
+            print manager.database
+            print manager
+            print 'running initializer'
+            initializer.run(manager)
+            print 'syncing form models'
+            form_models = manager.database.query(map_fun_raw_form_model_docs)
+            for form_model in form_models:
+                document = form_model.key
+                language = document['metadata']['activeLanguages'][0]
+                if document['form_code'] == 'reg':
+                    document['metadata']['activeLanguages'] = ["en"]
+                    document['is_registration_model'] = True
+                    document["validators"] = [
+                            {"cls": "mandatory"},
+                            {"cls": "mobile_number_mandatory_for_reporter"},
+                            {"cls": "at_least_one_location_field_must_be_answered_validator"}
+                    ]
+                else:
+                    document['is_registration_model'] = False
+                    for index, json_field in enumerate(document['json_fields']):
+                        json_field['label'][language] = json_field['name']
+                        document['json_fields'][index] = json_field
+                    document["validators"] = [{"cls": "mandatory"}]
+                manager.database.save(document)
+            print 'syncing entity reg forms'
+            entity_types = get_all_entity_types(manager)
+            # django.core.management.base forces the locale to en-us.
+            translation.activate('en')
+            for entity_type in entity_types:
+                if entity_type != ['reporter']:
+                    create_registration_form(manager, entity_type)
+        except Exception as e:
+            failed_managers.append((manager,e.message))
 
-        form_models = manager.database.query(map_fun_raw_form_model_docs)
-        for form_model in form_models:
-            document = form_model.key
-            language = document['metadata']['activeLanguages'][0]
-            if document['form_code'] == 'reg':
-                document['metadata']['activeLanguages'] = ["en"]
-                document['is_registration_model'] = True
-                document["validators"] = [
-                        {"cls": "mandatory"},
-                        {"cls": "mobile_number_mandatory_for_reporter"},
-                        {"cls": "at_least_one_location_field_must_be_answered_validator"}
-                ]
-            else:
-                document['is_registration_model'] = False
-                for index, json_field in enumerate(document['json_fields']):
-                    json_field['label'][language] = json_field['name']
-                    document['json_fields'][index] = json_field
-                document["validators"] = [{"cls": "mandatory"}]
-            manager.database.save(document)
-        
-        entity_types = get_all_entity_types(manager)
-        # django.core.management.base forces the locale to en-us.
-        translation.activate('en')
-        for entity_type in entity_types:
-            if entity_type != ['reporter']:
-                create_registration_form(manager, entity_type)
+    print 'failed managers if any'
+    for manager,exception_mesage in failed_managers:
+        print manager + 'failed. the reason --> ' + exception_mesage
 
 migrate_01(managers, map_fun_raw_form_model_docs)
 
