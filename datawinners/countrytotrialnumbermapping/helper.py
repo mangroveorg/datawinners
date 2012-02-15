157a74ce846f97c5e3484ef4c54c24a30a411937
roov7 <rova;ratovo@gmail.com>
2012-02-15 17:14:23 +0300

    Adding french names for country dropdown list

--- datawinners/countrytotrialnumbermapping/helper.py
+++ datawinners/countrytotrialnumbermapping/helper.py
@@ -1,8 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from django.utils.datastructures import SortedDict
+from django.utils.translation import ugettext as _
 
 def get_countries_in_display_format(countries):
     return [(country.country_name, _get_formatted_country_name(country)) for country in countries]
 
 def _get_formatted_country_name(country):
-    return country.country_name + ' (' + country.country_code + ')'
+    return _(country.country_name) + ' (' + country.country_code + ')'
+
+
