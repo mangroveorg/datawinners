9771b0b7a1aa431161dfc5a1fc663b7215278144
Geet Chandratre <geetchandratre@gmail.com>
2012-03-06 11:04:15 +0530

    Geet/Asif | Modifying handler for custom_report

--- datawinners/custom_report_router/report_router.py
+++ datawinners/custom_report_router/report_router.py
@@ -1,15 +1,14 @@
-from mangrove.utils.types import  is_not_empty
+from mangrove.utils.types import  is_not_empty, is_empty
+
+
+custom_report_routing_table={}
 
 class ReportRouter(object):
-    def __init__(self, routing_table):
-        self.routing_table = routing_table
-
 
     def route(self, organization_id, form_submission):
         if self.has_error_in_submission(form_submission): return
-        custom_report_handler = self.routing_table.get(organization_id, self.fake_custom_report_handler)
+        custom_report_handler = custom_report_routing_table.get(organization_id, self.fake_custom_report_handler)
         custom_report_handler(form_submission)
-
 
     def has_error_in_submission(self, form_submission):
         return is_not_empty(form_submission.errors)
--- datawinners/custom_report_router/tests/test_report_router.py
+++ datawinners/custom_report_router/tests/test_report_router.py
@@ -1,6 +1,6 @@
 import unittest
 from mock import Mock
-from datawinners.custom_report_router.report_router import ReportRouter
+from datawinners.custom_report_router.report_router import ReportRouter, custom_report_routing_table
 
 
 class TestReportRouter(unittest.TestCase):
@@ -8,21 +8,23 @@ class TestReportRouter(unittest.TestCase):
         self.form_submission=Mock()
         self.client_specific_custom_report_handler = Mock()
         self.org_id_for_custom_report = '123'
-        self.routing_table={'123':self.client_specific_custom_report_handler}
+        custom_report_routing_table.update({'123':self.client_specific_custom_report_handler})
         self.form_submission.errors = None
 
     def test_should_route_to_appropriate_client_handler(self):
-        ReportRouter(self.routing_table).route(self.org_id_for_custom_report, self.form_submission)
+        ReportRouter().route(self.org_id_for_custom_report, self.form_submission)
         self.client_specific_custom_report_handler.assert_called_once_with(self.form_submission)
 
     def test_should_not_route_if_organization_handler_is_not_present(self):
-        ReportRouter(self.routing_table).route('345',self.form_submission)
+        ReportRouter().route('345',self.form_submission)
         self.assertEqual(0,self.client_specific_custom_report_handler.call_count)
 
     def test_should_not_call_client_handler_if_errors_exist_in_form_submission(self):
         self.form_submission.errors = ["some error"]
-        ReportRouter(self.routing_table).route(self.org_id_for_custom_report,self.form_submission)
+        ReportRouter().route(self.org_id_for_custom_report,self.form_submission)
         self.assertEqual(0,self.client_specific_custom_report_handler.call_count)
+
+
 
 
 
