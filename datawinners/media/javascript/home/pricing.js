46bbb3c58ac84500dcece63687d81f39b5dee061
herihaja Rabe <herihaja@hni.org>
2012-03-21 17:35:14 +0300

    Updating pricing page <herihaja/Sandra>

--- datawinners/media/javascript/home/pricing.js
+++ datawinners/media/javascript/home/pricing.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
         e.preventDefault();
         $('html,body').animate({scrollTop: $('a[name="a3"]').offset().top},'slow');
     });
+    for(i in country){
+        $("#country_select").append('<option value="'+i+'">'+i+'</option>');
+    }
+
+
+    $("#country_select").change(function(){
+        var html = '';
+        for(i in country[$(this).val()]){
+            html += '<li>'+country[$(this).val()][i]+'</li>';
+        }
+        $("#networks_list").html(html);
+    });
 });
