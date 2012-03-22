b338ac60797adb2c47b4784afb8f87d373fbcb1b
Argha C <007.argha@gmail.com>
2012-03-22 17:00:20 +0530

    changing the network fixtures : Argha/Asif

--- datawinners/countrytotrialnumbermapping/helper.py
+++ datawinners/countrytotrialnumbermapping/helper.py
@@ -7,10 +7,12 @@ import re
 def get_countries_in_display_format(countries):
     return [(country.country_name, _get_formatted_country_name(country)) for country in countries]
 
+
 def _get_formatted_country_name(country):
     return _(country.country_name) + ' (' + country.country_code + ')'
 
 
 def get_trial_numbers():
-    networks = Network.objects.values_list('trial_sms_number',flat=True).distinct()
-    return [re.sub("(\-)|(\+)", "", network) for network in networks if network != 'None']
+    :networks = Network.objects.values_list('trial_sms_number', flat=True).distinct()
+    return [network for network in networks if network != 'None']
+
