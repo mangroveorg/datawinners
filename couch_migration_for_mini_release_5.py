4aee611dc3419dc3a1eb2e3b274e715e1126d501
akshaysn <akshay.naval@gmail.com>
2012-03-22 15:46:27 +0530

    making the migration a little bit more verbose : Akshay

--- couch_migration_for_mini_release_5.py
+++ couch_migration_for_mini_release_5.py
@@ -7,16 +7,28 @@ from mangrove.bootstrap.initializer import sync_views
 managers = load_all_managers()
 
 def migrate_01(managers):
+    failed_managers = []
     for manager in managers:
         try:
-            form_model = get_form_model_by_code(manager, ENTITY_DELETION_FORM_CODE)
-            form_model.delete()
-        except Exception:
-            pass
-        finally:
-            create_default_delete_form_model(manager)
+            print manager.database
+            print manager
+            print "Syncing views"
+            sync_views(manager)
+            print "making the delete form model"
+            try:
+                form_model = get_form_model_by_code(manager, ENTITY_DELETION_FORM_CODE)
+                form_model.delete()
+            except Exception:
+                pass
+            finally:
+                create_default_delete_form_model(manager)
+        except Exception as e:
+            failed_managers.append((manager,e.message))
 
-        sync_views(manager)
+    print 'failed managers if any'
+    for manager,exception_mesage in failed_managers:
+        print " %s failed. the reason :  %s" %(manager,exception_mesage)
+
 
 migrate_01(managers)
 
