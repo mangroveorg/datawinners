7717bc58d35a3813cf7e10f1ad4c0941ea33cfcf
Ashish Kumar <ashishk.kgp@gmail.com>
2012-05-11 16:25:08 +0530

    Ashish/Argha - #104 - adding translations.

--- datawinners/alldata/helper.py
+++ datawinners/alldata/helper.py
@@ -1,7 +1,8 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from datawinners.local_settings import CRS_ORG_ID
+from datawinners.settings import CRS_ORG_ID
 from datawinners.main.utils import get_database_manager
 from datawinners.project import models
+from django.utils.translation import ugettext_lazy as _
 
 def get_all_project_for_user(user):
     if user.get_profile().reporter:
@@ -27,13 +28,13 @@ def link(report_name):
 def get_reports_list(org_id):
     if org_id == CRS_ORG_ID:
         return [
-                {'link' : link('waybill_sent_and_received'),'name': 'Way Bill Sent vs Received', 'desc': 'Way bill sent vs received'},
-                {'link' : link('CSR'),'name': 'CSR', 'desc': 'CSR'},
-                {'link' : link('LSR'),'name': 'LSR', 'desc': 'LSR'},
-                {'link' : link('MSL'),'name': 'MSL', 'desc': 'MSL'},
-                {'link' : link('recipient_status_report'),'name': 'Recipient Status Report', 'desc': 'Recipient Status Report'},
-                {'link' : link('return_report'),'name': 'Return Report', 'desc': 'Return Report'},
-                {'link' : link('theoretical_vs_physical_inventory'),'name': 'Theoretical vs Physical Inventory Report', 'desc': 'Theoretical vs Physical Inventory Report'},
-                {'link' : link('billOfLadingVsWayBillPort'),'name': 'Bill Of Lading vs Way Bill Port', 'desc': 'Bill Of Lading vs Way Bill Port'},
+                {'link' : link('waybill_sent_and_received'),'name': _('Way Bill Sent vs Received Report'), 'desc': _('Way bill sent vs received description')},
+                {'link' : link('CSR'),'name': _('CSR'), 'desc': _('CSR description')},
+                {'link' : link('LSR'),'name': _('LSR'), 'desc': _('LSR description')},
+                {'link' : link('MSL'),'name': _('MSL'), 'desc': _('MSL description')},
+                {'link' : link('csr_warehouse'),'name': _('CSR Warehouse'), 'desc': _('CSR Warehouse description')},
+                {'link' : link('recipient_status_report'),'name': _('Recipient Status Report'), 'desc': _('Recipient Status Report description')},
+                {'link' : link('return_report'),'name': _('Return Report'), 'desc': _('Return Report description')},
+                {'link' : link('billOfLadingVsWayBillPort'),'name': _('Bill Of Lading vs Way Bill Port'), 'desc': _('Bill Of Lading vs Way Bill Port description')},
         ]
     return  []
