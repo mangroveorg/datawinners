0e0ec0b5788c1f793c3bb7e775e43146811ba21f
Geet Chandratre <geetchandratre@gmail.com>
2012-05-14 10:49:59 +0530

    Geet | Ashwini | Trying to fix functional tests might break for a while

--- func_tests/tests/registrationtests/registration_tests.py
+++ func_tests/tests/registrationtests/registration_tests.py
@@ -61,6 +61,8 @@ class TestRegistrationPage(unittest.TestCase):
         self.driver.go_to(DATA_WINNER_REGISTER_PAGE)
         registration_page = RegistrationPage(self.driver)
         registration_page.register_with(WITHOUT_ENTERING_REQUIRED_FIELDS)
+        print registration_page.get_error_message()
+        print WITHOUT_ENTERING_REQUIRED_FIELDS_ERROR_MESSAGE
         self.assertEquals(registration_page.get_error_message(),WITHOUT_ENTERING_REQUIRED_FIELDS_ERROR_MESSAGE)
 
     @attr('functional_test')
