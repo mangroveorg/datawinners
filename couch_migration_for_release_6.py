b0380bbce1dd96224612ff3e3c4c8916039aca1a
Fu Ying <yingfu@thoughtworks.com>
2012-05-15 14:49:27 +0800

    HaiYang/Fu Ying | #1215 modify the migration to save the update

--- couch_migration_for_release_6.py
+++ couch_migration_for_release_6.py
@@ -1,6 +1,4 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from django.conf import settings
-from mangrove.datastore.database import get_db_manager
 from datawinners.main.initial_couch_fixtures import load_all_managers
 
 managers = load_all_managers()
@@ -11,20 +9,21 @@ def migrate_01(managers):
     for manager in managers:
         print ("Database %s") % (manager.database_name,)
         try:
-            projects = manager.load_all_rows_in_view('all_projects')
-            for project in projects:
-                if 'web' in project['value']['devices'] and 'smartPhone' not in project['value']['devices']:
-                    project['value']['devices'] = ["sms", "web", "smartPhone"]
+            rows = manager.load_all_rows_in_view('all_projects')
+            for row in rows:
+                document = row['value']
+                if 'web' in document['devices'] and 'smartPhone' not in document['devices']:
+                    document['devices'] = ["sms", "web", "smartPhone"]
 
-                    print ("Updating project %s devices to have smartPhone option") % (project['value']['name'],)
-                    manager.database.save(project)
+                    print ("Updating project %s devices to have smartPhone option") % (document['name'],)
+                    manager.database.save(document)
 
         except Exception as e:
             failed_managers.append((manager, e.message))
 
     print 'failed managers if any'
     for manager,exception_message in failed_managers:
-        print " %s failed. the reason :  %s" %(manager,exception_message)
+        print " %s failed. the reason :  %s" %(manager, exception_message)
 
 
 migrate_01(managers)
