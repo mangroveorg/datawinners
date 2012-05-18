9a750f123e119b034e815129e0eae980b58098cb
Argha <argha@argha-VirtualBox.(none)>
2012-05-18 11:47:04 +0530

    Ashish/Argha:#1220 - fixing func tests

--- func_tests/tests/adddatasenderstests/add_data_senders_data.py
+++ func_tests/tests/adddatasenderstests/add_data_senders_data.py
@@ -24,7 +24,7 @@ VALID_DATA = {NAME: "Mickey Duck",
               SUCCESS_MSG: "Registration successful. ID is: rep"}
 
 VALID_DATA_WITH_EMAIL = {NAME: "Mickey Duck",
-              MOBILE_NUMBER: "9876-544-2102",
+              MOBILE_NUMBER: "9876-544-2103",
               COMMUNE: "MAHAVELO,AMBOTAKA,MANAKARA ATSIMO,VATOVAVY FITOVINANY",
               EMAIL_ADDRESS: "mickey1@duck12.com",
               GPS: "-21.7622088847 48.0690991394",
