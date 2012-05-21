b7cec44007b2d8d7af227b86adb9e1fa9e0d0332
Fu Ying <yingfu@thoughtworks.com>
2012-05-21 16:32:48 +0800

    HaiYang/Fu Ying | # 1222 modify unit test for test_create_single_web_user

--- datawinners/entity/tests/test_views_unit_test.py
+++ datawinners/entity/tests/test_views_unit_test.py
@@ -1,12 +1,13 @@
 from unittest.case import TestCase
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.sites.models import Site, get_current_site
+from django.contrib.sites.models import  get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import int_to_base36
+from mangrove.datastore.database import DatabaseManager
+from mangrove.datastore.entity import Entity
 from mock import  Mock, patch
 from accountmanagement.models import Organization, NGOUserProfile
-from accountmanagement.organization_id_creator import OrganizationIdCreator
 from entity.views import send_activation_email_for_data_sender, create_single_web_user
 from django.core import mail
 from tests.test_email_utils import set_email_settings
@@ -15,47 +16,46 @@ WEB_USER_TEST_EMAIL = "test_email_for_create_single_web_user@test.com"
 
 
 class TestView(TestCase):
-    def prepare_organization(self):
-        self.organization = Organization(name='test_org_for_paid_account',
-            sector = 'PublicHealth', address = 'add',
-            city = 'Pune', country = 'India',
-            zipcode = '411006', in_trial_mode = False,
-            org_id = OrganizationIdCreator().generateId())
-
-        self.organization.save()
 
     def setUp(self):
         set_email_settings()
-        self.prepare_organization()
-        User.objects.filter(email = WEB_USER_TEST_EMAIL).delete()
-
-    def tearDown(self):
-        users = User.objects.filter(email = WEB_USER_TEST_EMAIL)
-        NGOUserProfile.objects.filter(org_id = self.organization.org_id).delete()
-        users.delete()
-        Organization.objects.filter(name = 'test_org_for_paid_account').delete()
 
     def test_create_single_web_user(self):
+        org = Mock(spec = Organization)
+        org.org_id = "org_id"
+
+        mock_entity = Mock(spec = Entity)
+        mock_entity.value.return_value = 'test'
+
         with patch("django.contrib.auth.tokens.default_token_generator.make_token") as make_token:
             make_token.return_value = "token"
-            create_single_web_user(self.organization.org_id, WEB_USER_TEST_EMAIL, "test","en")
-            user = User.objects.filter(email = WEB_USER_TEST_EMAIL)[0]
-            emails = [mail.outbox.pop() for i in range(len(mail.outbox))]
+            with patch("entity.views.get_database_manager_for_org") as get_dbm:
+                get_dbm.return_value = Mock(spec = DatabaseManager)
+                with patch("datawinners.accountmanagement.models.Organization.objects.get") as get_organization_mock:
+                    get_organization_mock.return_value = org
+                    with patch("entity.views.get_by_short_code") as reporter_entity:
+                        reporter_entity.return_value = mock_entity
+                        create_single_web_user(org.org_id, WEB_USER_TEST_EMAIL, "test", "en")
+                        user = User.objects.filter(email = WEB_USER_TEST_EMAIL)[0]
+                        emails = [mail.outbox.pop() for i in range(len(mail.outbox))]
 
-            self.assertEqual(1, len(emails))
-            sent_email = emails[0]
+                        self.assertEqual(1, len(emails))
+                        sent_email = emails[0]
 
-            self.assertEqual(settings.EMAIL_HOST_USER, sent_email.from_email)
-            self.assertEqual([WEB_USER_TEST_EMAIL], sent_email.to)
-            ctx_dict = {
-                'domain': "localhost:8000",
-                'uid': int_to_base36(user.id),
-                'user': user,
-                'token': "token",
-                'protocol': 'http',
-                }
-            self.assertEqual(render_to_string('registration/password_reset_email_en.html', ctx_dict), sent_email.body)
+                        self.assertEqual(settings.EMAIL_HOST_USER, sent_email.from_email)
+                        self.assertEqual([WEB_USER_TEST_EMAIL], sent_email.to)
+                        ctx_dict = {
+                            'domain': "localhost:8000",
+                            'uid': int_to_base36(user.id),
+                            'user': user,
+                            'token': "token",
+                            'protocol': 'http',
+                            }
+                        self.assertEqual(render_to_string('registration/password_reset_email_en.html', ctx_dict), sent_email.body)
 
+        users = User.objects.filter(email = WEB_USER_TEST_EMAIL)
+        NGOUserProfile.objects.filter(org_id = org.org_id).delete()
+        users.delete()
 
     def test_should_send_correct_email_in_html_format_in_english(self):
         site = get_current_site(None)
