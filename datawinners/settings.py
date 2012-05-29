e6e01948655a15a3a1a5dc88e129e5938d147ece
ubuntu11 <ashish@ashish-dw-VirtualBox.(none)>
2012-05-29 16:17:59 +0530

    Ashish - introducing feature toggle for smartphone access.

--- datawinners/settings.py
+++ datawinners/settings.py
@@ -26,6 +26,7 @@ VUMI_USER = "vumi"
 VUMI_PASSWORD = "vumi"
 REFRESH_RATE = None
 USE_NEW_VUMI = False
+SMARTPHONE_ENABLED = False
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
