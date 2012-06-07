e31e56223daf3fae55b3fc587ef44ec2df45c01e
Haiyang He <hyhe@thoughtworks.com>
2012-06-07 11:57:09 +0800

    HaiYang | #1217 refactor the questionnaire_instruction_and_preview.js

--- datawinners/media/javascript/project/questionnaire_instruction_and_preview.js
+++ datawinners/media/javascript/project/questionnaire_instruction_and_preview.js
@@ -1,39 +1,41 @@
-DW.instruction_and_preview = {
-    init: function(){
-        DW.instruction_and_preview.bind_sms_preview();
+DW.instruction_and_preview = function (preview_url, preview_navigation_item) {
+    this.preview_url = preview_url;
+    this.preview_navigation_item = preview_navigation_item;
+}
+
+DW.instruction_and_preview.prototype = {
+    bind_preview_navigation_item:function () {
+        var that = this;
+        $(this.preview_navigation_item).live('click', function () {
+
+            return function(load_preview){
+                if (DW.questionnaire_form_validate()) {
+                    load_preview.apply(that);
+                }
+            }(that.load_preview_content);
+        });
     },
 
-    bind_sms_preview: function(){
-        $(".navigation-sms-preview").live('click', DW.instruction_and_preview.sms_preview);
-    },
 
-    sms_preview: function () {
-        if(DW.questionnaire_form_validate()){
-            var post_data = DW.instruction_and_preview.post_data();
-            DW.instruction_and_preview.load_preview_content(sms_preview_link, post_data);
-        }
-        return;
-    },
-
-    post_data: function() {
-        var questionnaire_data = JSON.stringify(ko.toJS(questionnaireViewModel.questions()), null, 2);
-        return {'questionnaire-code':$('#questionnaire-code').val(),
-            'question-set':questionnaire_data,
-            'profile_form':basic_project_info.values(),
-            'project_state':"Test"};
-    },
-
-    load_preview_content: function(preview_link, post_data) {
-        $.post(preview_link, post_data, function(response_data){
-            $("#questionnaire_content").html(response_data);
-            $("#questionnaire_preview_instruction").show();
-            $(".navigation-sms-preview").addClass("shadow-background");
-        }, 'html')
+    load_preview_content:function () {
+        var post_data = {'questionnaire-code':$('#questionnaire-code').val(),
+                         'question-set':JSON.stringify(ko.toJS(questionnaireViewModel.questions()), null, 2),
+                         'profile_form':basic_project_info.values(),
+                         'project_state':"Test"};
+        $.post(this.preview_url,
+               post_data,
+               function (response_data) {
+                    $("#questionnaire_content").html(response_data);
+                    $("#questionnaire_preview_instruction").show();
+                    $(this.preview_navigation_item).addClass("shadow-background");
+                },
+                'html');
     }
-
-};
-
+}
 
 $(function () {
-    DW.instruction_and_preview.init();
+    var sms_preview = new DW.instruction_and_preview(sms_preview_link, '.navigation-sms-preview');
+    sms_preview.bind_preview_navigation_item();
+    var web_preview = new DW.instruction_and_preview(web_preview_link, '.navigation-web-preview');
+    web_preview.bind_preview_navigation_item();
 });
