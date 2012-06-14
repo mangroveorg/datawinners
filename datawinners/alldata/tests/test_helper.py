e62e6e71c047254ae17a11176dc180f5436e2601
Ashish Kumar <akumar@thoughtworks.com>
2012-06-14 12:19:53 +0530

    Ashish - fixing test to use mock, instead of stub.

--- datawinners/alldata/tests/test_helper.py
+++ datawinners/alldata/tests/test_helper.py
@@ -13,70 +13,56 @@ class TestHelper(unittest.TestCase):
         self.all_projects = helper.models.get_all_projects
         helper.get_database_manager = stub_get_database_manager
 
+    def _get_normal_user(self):
+        user = Mock(User)
+        normal_profile = Mock(NGOUserProfile)
+        normal_profile.reporter = False
+        user.get_profile.return_value = normal_profile
+        return user
+
+    def _get_reporter_user(self):
+        user = Mock(User)
+        reporter_profile = Mock(NGOUserProfile)
+        reporter_profile.reporter = True
+        reporter_profile.reporter_id = 'something'
+        user.get_profile.return_value = reporter_profile
+        return user
+
     def test_should_return_all_projects(self):
-        user = StubUser(NormalGuy())
+        user = self._get_normal_user()
         helper.models.get_all_projects = stub_get_all_projects
         projects = get_all_project_for_user(user)
-        assert projects["project_name"] == "helloworld"
+        assert projects["project_name"] == "hello world"
 
     def test_should_return_all_projects_for_user(self):
-        user = StubUser(ReporterProfile())
+        user = self._get_reporter_user()
         helper.models.get_all_projects = stub_get_all_projects_for_reporter
         projects = get_all_project_for_user(user)
-        assert projects["project_name"] == "helloworld"
+        assert projects["project_name"] == "hello world"
 
     def test_should_return_disabled_and_display_none_for_reporter(self):
-        user = StubUser(ReporterProfile())
+        user = self._get_reporter_user()
         disabled, hide = helper.get_visibility_settings_for(user)
         assert disabled  == 'disable_link_for_reporter'
         assert hide == 'none'
 
     def test_should_return_enabled_and_display_for_other(self):
-        user = StubUser(NormalGuy())
+        user = self._get_normal_user()
         disabled, hide = helper.get_visibility_settings_for(user)
         assert hide  == ""
         assert disabled == ""
 
     def test_should_return_DataSubmission_for_reporter(self):
-        user = StubUser(ReporterProfile())
+        user = self._get_reporter_user()
         assert helper.get_page_heading(user) == 'Data Submission'
 
-    def test_should_return_AllData_for_reporter(self):
-        user = StubUser(NormalGuy())
+    def test_should_return_AllData_for_other(self):
+        user = self._get_normal_user()
         assert helper.get_page_heading(user) == 'All Data'
 
     def tearDown(self):
         helper.models.get_all_projects = self.all_projects
         helper.get_database_manager = self.database_manager
-
-
-
-class StubUser(User):
-
-    def __init__(self, profile):
-        self.profile= profile
-
-    def get_profile(self):
-        return self.profile
-
-
-class ReporterProfile(NGOUserProfile):
-
-    def __init__(self):
-        self.reporter_id = "007"
-
-    @property
-    def reporter(self):
-        return True
-
-
-class NormalGuy(NGOUserProfile):
-
-    @property
-    def reporter(self):
-        return False
-
-
 
 def stub_get_database_manager(*args):
     return Mock()
@@ -84,8 +70,8 @@ def stub_get_database_manager(*args):
 def stub_get_all_projects_for_reporter(*args):
     assert args[0] is not None
     assert args[1] is not None
-    return {"project_name": "helloworld"}
+    return {"project_name": "hello world"}
 
 def stub_get_all_projects(*args):
     assert args[0] is not None
-    return {"project_name": "helloworld"}
+    return {"project_name": "hello world"}
