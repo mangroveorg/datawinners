5a5aa807af3f2d183bece2216938ac6e37b4ce6f
Fu Ying <yingfu@thoughtworks.com>
2012-06-14 15:38:54 +0800

    Fu Ying/HaiYang | #1224 add tooltips for the devices on datasender page

---
+++ datawinners/media/javascript/datasenders/device_tool_tips.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".device-help").tooltip({
+        position: "top left",
+        relative: true,
+        opacity:0.8,
+        events: {
+            def:     "mouseover,mouseout",
+            input:   "focus,blur",
+            widget:  "focus mouseover,blur mouseout",
+            tooltip: "click,click"
+        }
+
+    }).dynamic({ bottom: { direction: 'down', bounce: true } });
+});
