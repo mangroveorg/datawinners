883f2c66c3cb1f6046da6e0c4044e755fb131a86
Ashish Kumar <akumar@thoughtworks.com>
2012-06-22 15:13:34 +0530

    Ashish - adding tests for custom home. behaviour when trying to go to http://www.datawinners.com/

--- datawinners/home/tests/test_home.py
+++ datawinners/home/tests/test_home.py
@@ -4,7 +4,7 @@ from django.utils import unittest
 from django.test import Client
 from django.conf import settings
 from mock import Mock
-from datawinners.home.views import switch_language
+from datawinners.home.views import switch_language, custom_home
 
 class TestHome(unittest.TestCase):
 
@@ -30,4 +30,28 @@ class TestHome(unittest.TestCase):
         self.assertIsInstance(response, HttpResponseRedirect)
         self.assertEqual(response._headers['location'][1], '/project/subjects/2e358c46bb6c11e187d60800276384f8/')
         self.assertEqual(request.session['django_language'], 'fr')
+
+    def test_should_redirect_to_home_page_if_unauthorized(self):
+        request = Mock()
+        request.session = {'django_language': 'en'}
+        user = Mock()
+        user.is_authenticated.return_value = False
+        request.user = user
+        response = custom_home(request)
+        self.assertIsNotNone(response)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertEqual(response._headers['location'][1], '/en/home/')
+
+
+    def test_should_redirect_to_login_redirect_url_if_unauthorized(self):
+        request = Mock()
+        request.session = {'django_language': 'en'}
+        user = Mock()
+        user.is_authenticated.return_value = True
+        request.user = user
+        settings.LOGIN_REDIRECT_URL = '/dashboard'
+        response = custom_home(request)
+        self.assertIsNotNone(response)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertEqual(response._headers['location'][1], '/dashboard')
 
