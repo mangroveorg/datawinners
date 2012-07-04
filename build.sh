989f0eca61942854518bfb865264fe6fc1a983e7
Fu Ying <yingfu@thoughtworks.com>
2012-07-04 18:00:00 +0800

    SL | N/A add init function to build script

--- build.sh
+++ build.sh
@@ -1,86 +1,24 @@
 #!/bin/bash
-#set -x
-function prepare_mangrove_env {
-	cd ../mangrove && \
-	pip install -r requirements.pip && \
-	python setup.py develop && \
-	cd ../datawinners
-}
-
-function prepare_datawinner_env {
-	pip install -r requirements.pip && \
-	cp datawinners/local_settings_example.py datawinners/local_settings.py
-}
-
-function migrate_db {
-	python datawinners/manage.py migrate
-}
-
-function pre_commit {
-    update_source && \
-	prepare_mangrove_env && \
-	prepare_datawinner_env && \
-	restore_database && \
-	cd .. & \
-	unit_test && \
-	function_test
-}
-
-function unit_test {
-	echo "running unit test" && \
-	cd datawinners && \
-	python manage.py recreatedb && \
-	python manage.py test &&\
-	cd ..
-}
-
-function function_test {
-	echo "running function test" && \
-	cp datawinners/local_settings_example.py func_tests/resources/local_settings.py && \
-	restore_database && \
-	python manage.py recreatedb && \
-	cd ../func_tests && \
-	nosetests && \
-	killall -9 chromedriver
-}
-
-function restore_database {
-	echo "recreating database" && \
-	dropdb geodjango && \
-	createdb -T template_postgis geodjango && \
-	cd datawinners && \
-	python manage.py syncdb --noinput && \
-	python manage.py migrate && \
-	python manage.py loadshapes
-}
-
-function show_help {
-	echo "Usage: build.sh [COMMAND]"
-	echo "COMMAND"
-	echo "pc: \tthis will run all the unit test and function test"
-	echo "ut: \tthis will run all the unit test"
-	echo "ft: \tthis will run all the function test"
-	echo "rd: \tdestory and recreate database"
-	echo "us: \tupdate source codes of mangrove and datawinners"
-}
+DWROOT_DIR=$(pwd)
+MGROOT_DIR=$DWROOT_DIR/../mangrove
+. go.sh
 
 function update_source {
     echo "Update source code ...."
-    cd ../mangrove && \
-    git pull --rebase && \
-    cd ../datawinners && \
     git pull --rebase
+    (cd $MGROOT_DIR && git pull --rebase)
 }
 
 function main {
 	case $1 in
+		init) init_env;;
 		pc) pre_commit;;
 		ut) unit_test;;
 		ft) function_test;;
 		rd) restore_database;;
 		us) update_source;;
 		*) show_help && exit 1;;
-	esac
+	esac	
 }
 
 main $@
---
+++ go.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function prepare_mangrove_env {
+	(cd $MGROOT_DIR && pip install -r requirements.pip && python setup.py develop)
+}
+
+function prepare_datawinner_env {
+        cp "$DWROOT_DIR/datawinners/local_settings_example.py" "$DWROOT_DIR/datawinners/local_settings.py"
+ 	pip install -r requirements.pip
+}
+
+function migrate_db {
+	echo "go migrate database"
+	python "$DWROOT_DIR/datawinners/manage.py" migrate
+}
+
+function pre_commit {
+	echo "go pre commit"
+        prepare_env && \
+	unit_test && function_test
+}
+
+function prepare_env {
+  	update_source && prepare_mangrove_env && prepare_datawinner_env && restore_postgresql_database
+}
+
+function unit_test {
+	echo "running unit test"
+	recreate_couch_db && \
+	(cd "$DWROOT_DIR/datawinners" && python manage.py test)
+}
+
+function recreate_couch_db {
+	(cd "$DWROOT_DIR/datawinners" && python manage.py recreatedb)
+}
+
+function function_test {
+	echo "running function test"
+	cp "$DWROOT_DIR/datawinners/local_settings_example.py" "$DWROOT_DIR/func_tests/resources/local_settings.py"
+	restore_postgresql_database && \
+	recreate_couch_db && \
+	(cd "$DWROOT_DIR/func_tests" && nosetests)
+}
+
+function restore_postgresql_database {
+	echo "recreating database"
+	dropdb geodjango && createdb -T template_postgis geodjango && \
+	(cd "$DWROOT_DIR/datawinners" && python manage.py syncdb --noinput && python manage.py migrate && python manage.py loadshapes)
+}
+
+function init_env {
+	bash -c dw
+	prepare_env && \
+	(cd "$DWROOT_DIR/datawinners" &&  python manage.py compilemessages) && \
+	unit_test && function_test
+}
+
+function show_help {
+	echo "Usage: build.sh [COMMAND]"
+	echo "COMMAND"
+	echo "pc: \tthis will run all the unit test and function test"
+	echo "ut: \tthis will run all the unit test"
+	echo "ft: \tthis will run all the function test"
+	echo "rd: \tdestory and recreate database"
+	echo "us: \tupdate source codes of mangrove and datawinners"
+	echo "init: \t initialize environment for the first time"
+}
