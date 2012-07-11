a36a5581593bd6e0ce41e6bebf38c01c062b7803
herihaja Rabe <herihaja@hni.org>
2012-07-11 10:08:03 +0300

    Fixing javascript issue on questionnaire

--- datawinners/media/javascript/entity/questionnaire_view_model.js
+++ datawinners/media/javascript/entity/questionnaire_view_model.js
@@ -28,7 +28,7 @@ var questionnaireViewModel =
         return questionnaireViewModel.questions().length > 2;
     },
     renumberQuestions: function(){
-        var questionPattern = /^Question \d+$/
+        var questionPattern = /^Question \d+$/;
         for (var i = 0; i < questionnaireViewModel.questions().length; i++){
             var question = questionnaireViewModel.questions()[i];
             if (questionPattern.test(question.title()) )
