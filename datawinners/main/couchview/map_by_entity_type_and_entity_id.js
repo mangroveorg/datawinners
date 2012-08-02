3f7703e640c2ee29f442f90f62d9223d727298aa
Haiyang He <hyhe@thoughtworks.com>
2012-08-02 16:19:31 +0800

    Yue Ding/Haiyang | #1418 add the javascript of by_entity_type_and_entity_id view

---
+++ datawinners/main/couchview/map_by_entity_type_and_entity_id.js
@@ -0,0 +1,17 @@
+function(doc) {
+    if (!doc.void && doc.document_type == "DataRecord")
+    {
+        var date = Date.parse(doc.event_time);
+        var entity = doc.entity;
+        var entity_type = entity.aggregation_paths['_type'];
+        var entity_id = entity.short_code;
+        var form_code = doc.submission.form_code;
+        var data = {};
+        for (k in doc.data) {
+            value = doc.data[k].value;
+            data[k] = value;
+        }
+        key = [entity_type,entity_id,date];
+        emit(key, data);
+    }
+}
