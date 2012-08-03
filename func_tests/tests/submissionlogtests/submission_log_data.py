f20b2dbc5d83c5dda1e01d6b8c99509f8c22b31d
herihaja Rabe <herihaja@hni.org>
2012-08-03 12:01:12 +0300

    Fixing some failing functional tests

--- func_tests/tests/submissionlogtests/submission_log_data.py
+++ func_tests/tests/submissionlogtests/submission_log_data.py
@@ -7,17 +7,17 @@ FAILURE_MSG = "failure_msg"
 
 PROJECT_NAME = "clinic2 test project"
 
-SMS_DATA_LOG = {SMS_SUBMISSION: "Success No cid005 Mr. Tessy 58 17.05.2011 b ade 27.178057 -78.007789",
+SMS_DATA_LOG = {SMS_SUBMISSION: "Success No cid005 Mr. Tessy 58 17.05.2011 O- Rapid weight loss, Memory loss, Neurological disorders 27.178057 -78.007789",
                 UNIQUE_VALUE: "Mr. Tessy"}
 
-EXCEED_WORD_LIMIT_LOG = {SMS_SUBMISSION: "Error Yes CID005 Mr. O'brain 58 17.05.2011 b ade 27.178057 -78.007789",
+EXCEED_WORD_LIMIT_LOG = {SMS_SUBMISSION: "Error Yes CID005 Mr. O'brain 58 17.05.2011 O- Rapid weight loss, Memory loss, Neurological disorders 27.178057 -78.007789",
                          UNIQUE_VALUE: "Mr. O'brain",
                          FAILURE_MSG: "Answer Mr. O'brain for question NA is longer than allowed."}
 
-EXTRA_PLUS_IN_BTW_LOG = {SMS_SUBMISSION: "Success No cid002 Mr. Dessy 58 17.05.2011 b ade 27.178057 -78.007789",
+EXTRA_PLUS_IN_BTW_LOG = {SMS_SUBMISSION: "Success No cid002 Mr. Dessy 58 17.05.2011 O- Rapid weight loss, Memory loss, Neurological disorders 27.178057 -78.007789",
                          UNIQUE_VALUE: "Mr. Dessy"}
 
 WITH_INVALID_GEO_CODE_FORMAT_LOG = {
-    SMS_SUBMISSION: "Error Yes cid002 Mr. De`Melo 58 17.05.2011 ab ade 127.178057 -78.007789 a",
+    SMS_SUBMISSION: "Error Yes cid002 Mr. De`Melo 58 17.05.2011 O+, O- Rapid weight loss, Memory loss, Neurological disorders 127.178057 -78.007789 Hivid",
     UNIQUE_VALUE: "Mr. De`Melo",
     FAILURE_MSG: "Answer Mr. De`Melo for question NA is longer than allowed.,Answer ab for question BG contains more than one value.,The answer 127.178057 must be between -90 and 90"}
