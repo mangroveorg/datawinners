c423074697918b6ea27978806c28a381a424005e
Haiyang He <hyhe@thoughtworks.com>
2012-08-07 11:06:32 +0800

    Erdong/Haiyang | #1418 refact the data extraction api function tests

--- func_tests/tests/dataextractionapitests/data_extraction_api_tests.py
+++ func_tests/tests/dataextractionapitests/data_extraction_api_tests.py
@@ -1,6 +1,7 @@
 import json
 from urllib2 import urlopen
-from framework.base_test import BaseTest
+from nose.plugins.attrib import attr
+from framework.base_test import BaseTest, setup_driver, teardown_driver
 from framework.utils.common_utils import generateId
 from framework.utils.data_fetcher import fetch_, from_
 from pages.addsubjectpage.add_subject_page import AddSubjectPage
@@ -14,103 +15,142 @@ from tests.dataextractionapitests.data_extraction_api_data import *
 
 
 class DataExtractionAPITestCase(BaseTest):
-    def login(self):
-        self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
-        login_page = LoginPage(self.driver)
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = setup_driver()
+        cls.prepare_submission_data()
+
+    @classmethod
+    def tearDownClass(cls):
+        teardown_driver(cls.driver)
+
+    @classmethod
+    def login(cls):
+        cls.driver.go_to(DATA_WINNER_LOGIN_PAGE)
+        login_page = LoginPage(cls.driver)
         login_page.do_successful_login_with(VALID_CREDENTIALS)
 
-    def prepare_subject_type(self):
-        self.driver.go_to(DATA_WINNER_ALL_SUBJECT)
-        add_subject_type_page = AddSubjectTypePage(self.driver)
+    @classmethod
+    def prepare_subject_type(cls):
+        cls.driver.go_to(DATA_WINNER_ALL_SUBJECT)
+        add_subject_type_page = AddSubjectTypePage(cls.driver)
         add_subject_type_page.click_on_accordian_link()
-        self.subject_type = SUBJECT_TYPE + generateId()
-        self.subject_type = self.subject_type.strip()
-        add_subject_type_page.add_entity_type_with(self.subject_type)
+        cls.subject_type = SUBJECT_TYPE + generateId()
+        cls.subject_type = cls.subject_type.strip()
+        add_subject_type_page.add_entity_type_with(cls.subject_type)
 
-    def prepare_subject(self):
-        self.driver.go_to(DATA_WINNER_ADD_SUBJECT + self.subject_type)
-        add_subject_page = AddSubjectPage(self.driver)
+    @classmethod
+    def prepare_subject(cls):
+        cls.driver.go_to(DATA_WINNER_ADD_SUBJECT + cls.subject_type)
+        add_subject_page = AddSubjectPage(cls.driver)
         add_subject_page.add_subject_with(VALID_DATA)
         add_subject_page.submit_subject()
         flash_message = add_subject_page.get_flash_message()
-        self.subject_id = flash_message[flash_message.find(":") + 1:].strip()
+        cls.subject_id = flash_message[flash_message.find(":") + 1:].strip()
 
-    def create_project(self):
-        self.driver.go_to(DATA_WINNER_DASHBOARD_PAGE)
-        dashboard_page = DashboardPage(self.driver)
+    @classmethod
+    def create_project(cls):
+        cls.driver.go_to(DATA_WINNER_DASHBOARD_PAGE)
+        dashboard_page = DashboardPage(cls.driver)
 
         create_project_page = dashboard_page.navigate_to_create_project_page()
-        VALID_PROJECT_DATA[SUBJECT] = self.subject_type
+        VALID_PROJECT_DATA[SUBJECT] = cls.subject_type
         create_project_page.create_project_with(VALID_PROJECT_DATA)
         create_project_page.continue_create_project()
-        create_questionnaire_page = CreateQuestionnairePage(self.driver)
+        create_questionnaire_page = CreateQuestionnairePage(cls.driver)
         create_questionnaire_page.add_question(QUESTION)
         create_questionnaire_page.save_and_create_project_successfully()
-        self.driver.wait_for_page_with_title(15, fetch_(PAGE_TITLE, from_(VALID_PROJECT_DATA)))
-        self.assertEqual(self.driver.get_title(),
-            fetch_(PAGE_TITLE, from_(VALID_PROJECT_DATA)))
+        cls.driver.wait_for_page_with_title(15, fetch_(PAGE_TITLE, from_(VALID_PROJECT_DATA)))
 
-    def activate_project(self):
-        overview_page = ProjectOverviewPage(self.driver)
+    @classmethod
+    def activate_project(cls):
+        overview_page = ProjectOverviewPage(cls.driver)
         activate_project_light_box = overview_page.open_activate_project_light_box()
         activate_project_light_box.activate_project()
 
-    def submit_data(self):
-        overview_page = ProjectOverviewPage(self.driver)
+    @classmethod
+    def submit_data(cls):
+        overview_page = ProjectOverviewPage(cls.driver)
         data_page = overview_page.navigate_to_data_page()
         web_submission_tab = data_page.navigate_to_web_submission_tab()
         [web_submission_tab.fill_and_submit_answer(answer) for answer in VALID_ANSWERS]
 
-    def prepare_submission_data(self):
-        self.login()
-        self.prepare_subject_type()
-        self.prepare_subject()
-        self.create_project()
-        self.activate_project()
-        self.submit_data()
+    @classmethod
+    def prepare_submission_data(cls):
+        cls.login()
+        cls.prepare_subject_type()
+        cls.prepare_subject()
+        cls.create_project()
+        cls.activate_project()
+        cls.submit_data()
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
 
     def get_data_by_uri(self, uri):
         http_response = urlopen(url(uri))
 
         return json.loads(http_response.read())
 
-    def test_get_data_for_subject(self):
-        self.prepare_submission_data()
-        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/" % (self.subject_type, self.subject_id))
+    @attr('functional_test')
+    def test_get_data_for_subject_with_subject_type_and_subject_id(self):
+        result = self.get_data_by_uri(
+            "/api/get_for_subject/%s/%s/" % (self.__class__.subject_type, self.__class__.subject_id))
         value = result['value']
         self.assertTrue(result['success'])
         self.assertIsInstance(result, dict)
         self.assertTrue(len(value), 5)
         self.assertEqual(value[0][QUESTION[QUESTION_NAME]], VALID_ANSWERS[0][1][ANSWER])
 
-        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/%s/%s" % (self.subject_type, self.subject_id, "03-08-2012", "03-08-2012"))
+    @attr('functional_test')
+    def test_get_data_for_subject_with_subject_type_and_subject_id_and_same_date(self):
+        result = self.get_data_by_uri(
+            "/api/get_for_subject/%s/%s/%s/%s" % (
+                self.__class__.subject_type, self.__class__.subject_id, "03-08-2012", "03-08-2012"))
         value = result['value']
         self.assertTrue(result['success'])
         self.assertIsInstance(result, dict)
         self.assertTrue(len(value), 1)
         self.assertEqual(value[0][QUESTION[QUESTION_NAME]], VALID_ANSWERS[0][1][ANSWER])
 
-        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/%s/%s" % (self.subject_type, self.subject_id, "03-08-2012", "06-08-2012"))
+    @attr('functional_test')
+    def test_get_data_for_subject_with_subject_type_and_subject_id_and_different_date(self):
+        result = self.get_data_by_uri(
+            "/api/get_for_subject/%s/%s/%s/%s" % (
+                self.__class__.subject_type, self.__class__.subject_id, "03-08-2012", "06-08-2012"))
         value = result['value']
         self.assertTrue(result['success'])
         self.assertIsInstance(result, dict)
         self.assertTrue(len(value), 4)
         self.assertEqual(value[0][QUESTION[QUESTION_NAME]], VALID_ANSWERS[0][1][ANSWER])
 
+    @attr('functional_test')
+    def test_get_data_for_subject_with_not_exist_subject_type(self):
         result = self.get_data_by_uri("/api/get_for_subject/%s/%s/" % ("not_exist", "001"))
         self.assertFalse(result['success'])
         self.assertEqual(result['message'], "Entity type [not_exist] is not defined.")
 
-        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/" % (self.subject_type, "not_exist"))
+    @attr('functional_test')
+    def test_get_data_for_subject_with_subject_type_and_not_exist_subject_id(self):
+        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/" % (self.__class__.subject_type, "not_exist"))
         self.assertFalse(result['success'])
         self.assertEqual(result['message'], "Entity [not_exist] is not registered.")
 
-        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/%s/%s" % (self.subject_type, self.subject_id, "03082012", "06082012"))
+    @attr('functional_test')
+    def test_get_data_for_subject_with_subject_type_and_subject_id_and_wrong_date_format(self):
+        result = self.get_data_by_uri(
+            "/api/get_for_subject/%s/%s/%s/%s" % (
+                self.__class__.subject_type, self.__class__.subject_id, "03082012", "06082012"))
         self.assertFalse(result['success'])
         self.assertEqual(result['message'], "The format of start and end date should be DD-MM-YYYY.")
 
-        result = self.get_data_by_uri("/api/get_for_subject/%s/%s/%s/%s" % (self.subject_type, self.subject_id, "06-08-2012", "03-08-2012"))
+    @attr('functional_test')
+    def test_get_data_for_subject_with_subject_type_and_subject_id_and_wrong_date(self):
+        result = self.get_data_by_uri(
+            "/api/get_for_subject/%s/%s/%s/%s" % (
+                self.__class__.subject_type, self.__class__.subject_id, "06-08-2012", "03-08-2012"))
         self.assertFalse(result['success'])
         self.assertEqual(result['message'], "Start date must before end date.")
-
-
