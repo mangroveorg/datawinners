c4f8b7d5583bb5e6bc425b24d96bd4d570dc59d8
erdong feng <edfeng@thoughtworks.com>
2012-09-10 10:17:55 +0800

    edfeng&jfwu | #1514 using digest authentication in data extraction

--- datawinners/dataextraction/views.py
+++ datawinners/dataextraction/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django_digest.decorators import httpdigest
 from dataextraction.helper import  encapsulate_data_for_subject, encapsulate_data_for_form, convert_to_json_file_download_response, generate_filename
 from main.utils import get_database_manager
 
+@httpdigest
 def get_for_subject(request, subject_type, subject_short_code, start_date=None, end_date=None):
     if request.method == 'GET':
         user = request.user
@@ -12,6 +13,7 @@ def get_for_subject(request, subject_type, subject_short_code, start_date=None, 
         return convert_to_json_file_download_response(data_for_subject, generate_filename('%s_%s' %(subject_type,subject_short_code), start_date, end_date))
     return HttpResponse("Error. Only support GET method.")
 
+@httpdigest
 def get_for_form(request, form_code, start_date=None, end_date=None):
     if request.method == 'GET':
         user = request.user
