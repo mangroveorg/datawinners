6573b727a6dddd9282e04da0f9cd779786669855
Haiyang He <hyhe@thoughtworks.com>
2012-10-12 10:47:46 +0800

    jfwu/qszhuan: Add COMPRESS_ROOT for compressor update

--- datawinners/settings.py
+++ datawinners/settings.py
@@ -59,6 +59,8 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+COMPRESS_ROOT = os.path.join(PROJECT_DIR, 'media')
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
