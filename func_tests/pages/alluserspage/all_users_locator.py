0d03d620d2c31ab5faaaf25e5e00172311a8372e
killera <zhuanqingshan@163.com>
2012-11-01 19:12:05 +0800

    qingshan & Beany | fixed build.

--- func_tests/pages/alluserspage/all_users_locator.py
+++ func_tests/pages/alluserspage/all_users_locator.py
@@ -5,6 +5,6 @@ ALL_USERS_ACTION_SELECT = by_css("select#action")
 ERROR_CONTAINER = by_css("div#error")
 CONFIRM_DELETE_BUTTON = by_css("#delete_user_warning_dialog a.yes_button")
 CANCEL_DELETE_BUTTON = by_css("#delete_user_warning_dialog a.no_button")
-MESSAGES_CONTAINER = by_css("ul.messages li")
+MESSAGES_CONTAINER = by_css("span.message-span")
 CHECK_NTH_USER_LOCATOR = "#users_list table tbody tr:nth-child(%s) td:nth-child(1) input"
 ADD_USER_LINK = by_css("a[href='/account/user/new/']")
