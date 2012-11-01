febb1924f5ee00e05628ba11e3be1b2867736024
killera <zhuanqingshan@163.com>
2012-11-01 16:23:09 +0800

    Qingshan & Beany: #1667 Fix trivial typo.

--- func_tests/tests/alluserstests/all_users_data.py
+++ func_tests/tests/alluserstests/all_users_data.py
@@ -5,7 +5,7 @@ def url(path):
     return full_path
 ALL_USERS_URL = url("/account/users/")
 DELETE = "delete"
-SELECT_ATLEAST_1_USER_MSG = u'Please select atleast 1 user'
+SELECT_ATLEAST_1_USER_MSG = u'Please select at least 1 user'
 ADMIN_CANT_BE_DELETED = u"Your organization's account Administrator Tester Pune cannot be deleted"
 SMS_SUBMISSION_SUCCESS_MSG = u'Thank you Mino Rakoto. We received'
 SUCCESSFULLY_DELETED_USER_MSG = u"User(s) successfully deleted."
