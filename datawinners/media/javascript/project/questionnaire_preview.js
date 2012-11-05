1c3f731d8e241c4c0667da39fd853305c7908735
twer <twer@datawinner-mac-mini-one.local>
2012-11-05 16:46:24 +0800

    beany/qszhuan | fix js compress error

--- datawinners/media/javascript/project/questionnaire_preview.js
+++ datawinners/media/javascript/project/questionnaire_preview.js
@@ -1,50 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    DW.questionnaire_preview = {
+        init:function () {
+            DW.questionnaire_preview.init_sms_questionnaire_preview();
+            DW.questionnaire_preview.init_questionnaire_preview();
+        },
+        init_sms_questionnaire_preview:function () {
+            DW.questionnaire_preview.init_dialog(".sms-questionnaire-preview", sms_questionnaire_preview_link);
+            DW.questionnaire_preview.bind(".sms-questionnaire-preview", ".sms-preview");
+        },
+        init_questionnaire_preview:function () {
+            DW.questionnaire_preview.init_dialog(".questionnaire_preview", questionnaire_preview_link);
+            DW.questionnaire_preview.bind(".questionnaire_preview", ".preview");
+        },
+        init_dialog:function (dialog_element, questionnaire_preview_link) {
+            var questionnaire_preview = $(dialog_element);
+            questionnaire_preview.dialog({
+                    title:questionnaire_preview.attr("src_data") || gettext("Questionnaire Preview"),
+                    modal:true,
+                    autoOpen:false,
+                    height:700,
+                    width:800,
+                    closeText:'hide',
+                    zIndex:1300,
+                    open:function () {
+                        // Here I load the content. This is the content of your link.
+                        $("body > div").addClass("none_for_print");
+                        questionnaire_preview.load(questionnaire_preview_link, function () {
+                        });
+                    },
+                    close:function () {
+                        $("body > div").removeClass("none_for_print");
+                    }
+                }
+            );
+        },
+        bind:function (dialog_element, preview_link) {
+            $(preview_link).bind("click", function () {
+                $(dialog_element).dialog("open");
+                return false;
+            });
+        }
+    }
     DW.questionnaire_preview.init();
 });
-
-DW.questionnaire_preview = {
-    init:function(){
-        DW.questionnaire_preview.init_sms_questionnaire_preview();
-        DW.questionnaire_preview.init_questionnaire_preview();
-    },
-
-    init_sms_questionnaire_preview:function(){
-        DW.questionnaire_preview.init_dialog(".sms-questionnaire-preview", sms_questionnaire_preview_link);
-        DW.questionnaire_preview.bind(".sms-questionnaire-preview", ".sms-preview");
-    },
-
-    init_questionnaire_preview:function(){
-        DW.questionnaire_preview.init_dialog(".questionnaire_preview", questionnaire_preview_link);
-        DW.questionnaire_preview.bind(".questionnaire_preview", ".preview");
-    },
-
-    init_dialog:function(dialog_element, questionnaire_preview_link){
-        var questionnaire_preview = $(dialog_element);
-        questionnaire_preview.dialog({
-                title: questionnaire_preview.attr("src_data") || gettext("Questionnaire Preview"),
-                modal: true,
-                autoOpen: false,
-                height: 700,
-                width: 800,
-                closeText: 'hide',
-                zIndex: 1300,
-                open: function() {
-                    // Here I load the content. This is the content of your link.
-                    $("body > div").addClass("none_for_print");
-                    questionnaire_preview.load(questionnaire_preview_link, function() {});
-                },
-                close: function(){
-                    $("body > div").removeClass("none_for_print");
-                }
-            }
-        );
-    },
-
-    bind: function(dialog_element, preview_link){
-        $(preview_link).bind("click", function() {
-            $(dialog_element).dialog("open");
-            return false;
-        });
-    }
-
-}
