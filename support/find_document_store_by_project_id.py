3e72694214fe232ce4f6ade8ae4032fc562e3f75
fedcuit <fedcuit@gmail.com>
2012-12-25 13:15:02 +0800

    erdong | N/A Enable find organization name when query by project id.

--- support/find_document_store_by_project_id.py
+++ support/find_document_store_by_project_id.py
@@ -1,33 +1,39 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-import logging
+import os
+from accountmanagement.models import OrganizationSetting
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "datawinners.settings"
+
 from find_all_db_managers import all_db_names
 from mangrove.datastore.database import get_db_manager, remove_db_manager
 
-db_server = "http://10.18.2.237:5984"
+db_server = "http://localhost:5984"
 
 map_project_id_to_organization = """
 function(doc) {
     if (doc.document_type == 'Project') {
-        if(!doc.void && doc._id =='25f80836ece711e18d91fefdb24fb922' ){
+        if(!doc.void && doc._id =='49bd74ec28e011e297bcfefdb24fb922' ){
             emit([doc.created,doc.name], doc);
         }
     }
 }
 """
 
-logger = logging.getLogger("django")
 
 def find_organization():
 
     dbs = all_db_names(db_server)
     for db in dbs:
-        logger.info(db)
         print db
         manager = get_db_manager(server=db_server, database=db)
         database_query = manager.database.query(map_project_id_to_organization)
         if database_query:
-            logger.info("********************** document store found **********************\n" + db)
-            print ("********************** document store found **********************\n" + db)
+            organization_setting = OrganizationSetting.objects.filter(document_store=db)[0]
+            organization_name = organization_setting.organization.name
+            print ("********************************************")
+            print "Document store: %s" % db
+            print "Organization name: %s" % organization_name
+
 
             break
         remove_db_manager(manager)
