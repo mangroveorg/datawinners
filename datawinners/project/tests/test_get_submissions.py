894da1638ebb659821fcc79fe10fb47e771c5ed7
fedcuit <fedcuit@gmail.com>
2012-12-31 13:44:20 +0800

    Erdong/Beany | Add SubmissionBuilder.

---
+++ datawinners/project/tests/test_get_submissions.py
@@ -0,0 +1,40 @@
+from mangrove.form_model.form_model import FORM_CODE
+from mangrove.utils.test_utils.mangrove_test_case import MangroveTestCase
+from project.submission_router import successful_submissions, undeleted_submissions
+
+from mangrove.utils.test_utils.submission_builder import SubmissionBuilder
+
+class TestGetSubmissions(MangroveTestCase):
+    def setUp(self):
+        super(TestGetSubmissions, self).setUp()
+        self.submission_builder = SubmissionBuilder(self.manager)
+
+    def test_should_get_successful_submissions(self):
+        submissions = self.submission_builder.build_two_successful_submissions()
+        self.submission_builder.build_two_error_submission()
+
+        success_submissions = successful_submissions(self.manager, FORM_CODE)
+
+        self.assertEqual(2, len(success_submissions))
+
+        self._assertSubmissionsEqual(submissions, success_submissions)
+
+    def test_should_get_undeleted_submissions(self):
+        submissions = self.submission_builder.build_four_submissions()
+        submissions[-1].void()
+        submissions.remove(submissions[-1])
+
+        expected_undeleted_submission = undeleted_submissions(self.manager, FORM_CODE)
+
+        self.assertEqual(3, len(expected_undeleted_submission))
+
+        self._assertSubmissionsEqual(expected_undeleted_submission, submissions)
+
+    def _assertSubmissionsEqual(self, submission_list1, submission_list2):
+        submission_list1_ids = map(lambda x: x.id, submission_list1)
+        submission_list2_ids = map(lambda x: x.id, submission_list2)
+
+        self.assertItemsEqual(submission_list1_ids, submission_list2_ids)
+        
+
+
