023ea17bab00037a12a435007e4355d0041e7fb1
lakshmirp <rp.lakshmi.45@gmail.com>
2013-02-13 11:41:59 +0530

    ashwin /Shruthi - css changes for all_subject page and fixed error msg bugs.

--- datawinners/media/javascript/entity/all_subjects.js
+++ datawinners/media/javascript/entity/all_subjects.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#all_subjects .subject-container').hide();
 
-    $('#all_subjects .list_header').click(function(){
-        $(this).addClass('highlight').siblings('.list_header').removeClass('highlight')
+    $('#all_subjects .list_header').click(function () {
+        $(this).toggleClass('highlight').siblings('.list_header').removeClass('highlight');
         $('#all_subjects .subject-container:visible').not($(this).next()).hide();
         $(this).next().slideToggle('fast').scrollTop(0);
-    })
-    var entity_type = "waterpoint";
+    });
 });
