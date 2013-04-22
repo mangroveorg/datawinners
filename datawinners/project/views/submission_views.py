eab871480083c867ef59a9179be4f8495cba78ea
lakshmirp <rp.lakshmi.45@gmail.com>
2013-04-22 12:56:22 +0530

    Anishek /Ashwin | #1815 | Reverting to Heri's changes

--- datawinners/project/views/submission_views.py
+++ datawinners/project/views/submission_views.py
@@ -188,18 +188,20 @@ def edit(request, project_id, survey_response_id, tab=0):
             return render_to_response("project/web_questionnaire.html", form_ui_model,
                 context_instance=RequestContext(request))
 
-        created_request = helper.create_request(survey_response_form, request.user.username)
-        response = WebPlayerV2(manager).edit_survey_response(created_request, survey_response, websubmission_logger)
-        if response.success:
-            ReportRouter().route(get_organization(request).org_id, response)
-            success_message = _("Your changes have been saved.")
-            form_ui_model.update({'success_message': success_message})
-            _update_static_info_block_status(form_ui_model, is_errored_before_edit)
-            log_edit_action(original_survey_response, survey_response, request, project.name, questionnaire_form_model)
-            if request.POST.get("redirect_url"):
-                return HttpResponseRedirect(request.POST.get("redirect_url"))
-        else:
-            survey_response_form._errors = helper.errors_to_list(response.errors, questionnaire_form_model.fields)
+        success_message = _("Your changes have been saved.")
+        form_ui_model.update({'success_message': success_message})
+        if len(survey_response_form.changed_data) or is_errored_before_edit:
+            created_request = helper.create_request(survey_response_form, request.user.username)
+            response = WebPlayerV2(manager).edit_survey_response(created_request, survey_response, websubmission_logger)
+            if response.success:
+                ReportRouter().route(get_organization(request).org_id, response)
+                _update_static_info_block_status(form_ui_model, is_errored_before_edit)
+                log_edit_action(original_survey_response, survey_response, request, project.name, questionnaire_form_model)
+                if request.POST.get("redirect_url"):
+                    return HttpResponseRedirect(request.POST.get("redirect_url"))
+            else:
+                del form_ui_model["success_message"]
+                survey_response_form._errors = helper.errors_to_list(response.errors, questionnaire_form_model.fields)
         return render_to_response("project/web_questionnaire.html", form_ui_model,
             context_instance=RequestContext(request))
 
