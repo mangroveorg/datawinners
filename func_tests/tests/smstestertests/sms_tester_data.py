a744ccd3eb46b250c232cd580445248b14fd7977
herihaja <herihaja@herihaja-samsung.(none)>
2013-04-25 07:49:05 +0300

    Heri | #2097: Fixing ft

--- func_tests/tests/smstestertests/sms_tester_data.py
+++ func_tests/tests/smstestertests/sms_tester_data.py
@@ -76,7 +76,7 @@ REGISTER_EXISTING_SUBJECT_SHORT_CODE = {SENDER: "1234567890",
 REGISTER_INVALID_GEO_CODE = {SENDER: "1234567890",
                              RECEIVER: "919880734937",
                              SMS: "REG .T Clinic .m   123456 .l Agra .G 127.178057 -78.007789 .n Clinic Agra .S CLIAGRA",
-                             ERROR_MSG: "Error. Incorrect answer for q4. Please resend entire message."}
+                             ERROR_MSG: "Error. Incorrect answer for q5. Please resend entire message."}
 
 WITH_INVALID_GEO_CODE_FORMAT = {SENDER: "1234567890",
                                 RECEIVER: "919880734937",
