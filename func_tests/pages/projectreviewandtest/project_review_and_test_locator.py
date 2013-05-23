bae31fb921c888e48db734e73a1d8826d7041eaa
anishek.agarwal <aagarwal@thoughtworks.com>
2013-05-23 13:51:55 +0530

    Anishek | Fixed the click selector for the reminders tab as it was not opening the accordian

--- func_tests/pages/projectreviewandtest/project_review_and_test_locator.py
+++ func_tests/pages/projectreviewandtest/project_review_and_test_locator.py
@@ -1,4 +1,4 @@
 from framework.utils.common_utils import *
 
-REMINDER_SECTION = by_id("reminders")
+REMINDER_SECTION = by_css("#reminders h5")
 REMINDER_STATUS = by_css("#reminders div ul li strong")
