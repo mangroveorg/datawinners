8c365c2113ad2f69aea5b576220eedcbf00b577b
dileepbapat <dileepbapat@gmail.com>
2013-06-13 00:40:46 +0530

    Pooja,Dileep| 0000 | Fixing snapshot job on prod.

--- deployment/ec2/create_snapshot.py
+++ deployment/ec2/create_snapshot.py
@@ -3,24 +3,22 @@ from boto.ec2.connection import EC2Connection
 from datetime import datetime
 import sys
 
-# Substitute your access key and secret key here
-aws_access_key = 'AKIAI763UW5YW37B4UIQ'
-aws_secret_key = 'E1KijH+j2zczrFeoXVB+EI1m/GB60EB0S9ufM8pi'
-
-if len(sys.argv) < 3:
-    print "Usage: python create_snapshots.py volume_id number_of_snapshots_to_keep description"
+if len(sys.argv) < 5:
+    print "Usage: python create_snapshots.py volume_id number_of_snapshots_to_keep aws_key aws_secret description"
     print "volume id and number of snapshots to keep are required. description is optional"
     sys.exit(1)
 
 vol_id = sys.argv[1]
 keep = int(sys.argv[2])
+aws_access_key = sys.argv[3]
+aws_secret_key = sys.argv[4]
 conn = EC2Connection(aws_access_key, aws_secret_key)
 volumes = conn.get_all_volumes([vol_id])
 print "%s" % repr(volumes)
 volume = volumes[0]
 description = 'Created automatically at ' + datetime.today().isoformat(' ')
-if len(sys.argv) > 3:
-    description = sys.argv[3]
+if len(sys.argv) > 5:
+    description = sys.argv[5]
 
 if volume.create_snapshot(description):
     print 'Snapshot created with description: ' + description
@@ -36,6 +34,7 @@ def date_compare(snap1, snap2):
     return 1
 
 snapshots.sort(date_compare)
+snapshots = [s for s in snapshots if s.description.index("Created automatically at ") == 0]
 delta = len(snapshots) - keep
 for i in range(delta):
     print 'Deleting snapshot ' + snapshots[i].description
