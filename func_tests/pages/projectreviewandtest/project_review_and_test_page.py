4e1a006feb1e7f022f00f1b31b5d533971c27382
dileepbapat <dileepbapat@gmail.com>
2013-06-12 21:28:57 +0530

    Pooja,Dileep| 2160 | Functional test only failing in CI, trying to click on accordion header. try 2

--- func_tests/pages/projectreviewandtest/project_review_and_test_page.py
+++ func_tests/pages/projectreviewandtest/project_review_and_test_page.py
@@ -9,6 +9,7 @@ class ProjectReviewTestPage(Page):
     def get_reminder_status(self):
         wait = ui.WebDriverWait(self.driver,15)
         wait.until(lambda driver: driver.find(REMINDER_SECTION))
+        wait.until(lambda driver: driver.find(REMINDER_STATUS).text == "")
         self.driver.find(REMINDER_SECTION).click()
         wait.until(lambda driver: driver.find(REMINDER_STATUS).text)
         return self.driver.find(REMINDER_STATUS).text
