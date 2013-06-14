31373880d8033c95c6c84d073f392c49631db47e
ashwink <ashwin.konale@gmail.com>
2013-06-14 13:39:53 +0530

    Anishek / Ashwin | #2193 | Log the exception when syncview fails.

--- datawinners/main/management/commands/syncviews.py
+++ datawinners/main/management/commands/syncviews.py
@@ -8,12 +8,12 @@ import traceback
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for database_name in document_stores_to_process(args):
-            print ("Database %s") % (database_name)
+            print "Database %s" % database_name
             manager = get_db_manager(database_name)
             try:
                 SyncOnlyChangedViews().sync_view(manager)
             except Exception as e:
-                print ("ERROR %s" % e.message)
+                print "ERROR " + e.message
                 traceback.print_exc(file=sys.stdout)
             print "Done."
 
