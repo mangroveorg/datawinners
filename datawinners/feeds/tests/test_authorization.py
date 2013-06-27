e58bc3dc93addcaa4496a1d01950aade91f897a7
ashwink <ashwin.konale@gmail.com>
2013-06-27 11:55:35 +0530

    Ashwin /Anishek | # 2095 | Correcting tests.

--- datawinners/feeds/tests/test_authorization.py
+++ datawinners/feeds/tests/test_authorization.py
@@ -17,7 +17,8 @@ class TestAuthorization(TestCase):
         mock_request.user.get_profile.return_value = mock_user
         response = foo(mock_request)
         self.assertEquals(400, response.status_code)
-        self.assertEquals("You are not authorized to view this content", response.content)
+        self.assertEquals("You do not have the required permissions to access "
+                          "this information. Please contact your system administrator", response.content)
 
     def test_should_allow_others(self):
         def foo(request):
