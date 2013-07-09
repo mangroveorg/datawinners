8405badd3d961bec52182d30409b52c97046e313
anishek.agarwal <anishek@gmail.com>
2013-07-09 15:41:18 +0530

    anishek | #2277 | expanding the django admin functions for the user. Added the sms api user group

--- datawinners/accountmanagement/admin.py
+++ datawinners/accountmanagement/admin.py
@@ -1,12 +1,20 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from forms import forms
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from django.core.exceptions import ValidationError
+from django.forms import ChoiceField, CharField
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+
+from forms import forms
 from datawinners.accountmanagement.models import OrganizationSetting, SMSC, PaymentDetails, MessageTracker, Organization, NGOUserProfile, OutgoingNumberSetting
 from mangrove.utils.types import is_empty, is_not_empty
 from datawinners.countrytotrialnumbermapping.models import Country, Network
 
+
 admin.site.disable_action('delete_selected')
+
 
 class DatawinnerAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
@@ -14,6 +22,7 @@ class DatawinnerAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
+
 
 class OrganizationSettingAdmin(DatawinnerAdmin):
     list_display = ('organization_name', 'organization_id', 'type', 'payment_details', 'activation_date')
@@ -27,7 +36,7 @@ class OrganizationSettingAdmin(DatawinnerAdmin):
 
     def payment_details(self, obj):
         organization = obj.organization
-        payment_details = PaymentDetails.objects.filter(organization = organization)
+        payment_details = PaymentDetails.objects.filter(organization=organization)
         if not is_empty(payment_details):
             return payment_details[0].preferred_payment
 
@@ -60,7 +69,9 @@ class MessageTrackerAdmin(DatawinnerAdmin):
 
 
 class OrganizationAdmin(DatawinnerAdmin):
-    list_display = ('organization_name', 'complete_address', 'office_phone', 'website', 'paid', 'active_date','admin_name','admin_email','admin_mobile_number','admin_office_phone')
+    list_display = (
+        'organization_name', 'complete_address', 'office_phone', 'website', 'paid', 'active_date', 'admin_name',
+        'admin_email', 'admin_mobile_number', 'admin_office_phone')
 
     def organization_name(self, obj):
         return obj.name
@@ -93,32 +104,35 @@ class OrganizationAdmin(DatawinnerAdmin):
         complete_address = [obj.address, obj.addressline2, obj.city, obj.zipcode, obj.state, obj.country_name()]
         return ", ".join([element for element in complete_address if is_not_empty(element)])
 
-    def _get_full_name(self,user):
+    def _get_full_name(self, user):
         return user.first_name + ' ' + user.last_name
 
 
 class NullAdmin:
     def __init__(self):
-        self.email=''
-        self.mobile_phone=''
-        self.office_phone=''
-        self.first_name=''
-        self.last_name=''
+        self.email = ''
+        self.mobile_phone = ''
+        self.office_phone = ''
+        self.first_name = ''
+        self.last_name = ''
 
     def get_profile(self):
         return self
 
+
 class CountryAdmin(admin.ModelAdmin):
     ordering = ('country_name_en',)
-    list_display = ('country_name_en','country_code')
+    list_display = ('country_name_en', 'country_code')
+
 
 class NetworkAdmin(admin.ModelAdmin):
     ordering = ('network_name',)
-    list_display = ('network_name','trial_sms_number', 'country_name')
+    list_display = ('network_name', 'trial_sms_number', 'country_name')
     filter_horizontal = ['country']
 
-    def country_name(self,obj):
+    def country_name(self, obj):
         return ' ,'.join([country.country_name for country in obj.country.all()])
+
 
 class UserAdminForm(forms.ModelForm):
     class Meta:
@@ -132,12 +146,14 @@ class UserAdminForm(forms.ModelForm):
                 raise forms.ValidationError("This email address is required")
             existing_users_with_username = User.objects.filter(username=username)
             if existing_users_with_username.count() > 0 and existing_users_with_username[0] != self.instance:
-                raise forms.ValidationError("This email address is already in use. Please supply a different email address")
+                raise forms.ValidationError(
+                    "This email address is already in use. Please supply a different email address")
             cleaned_data['email'] = username
         return cleaned_data
 
+
 class NgoUserAdmin(DatawinnerAdmin):
-    list_display = ('organization_name', 'country', 'organization_id','admin_name','admin_email')
+    list_display = ('organization_name', 'country', 'organization_id', 'admin_name', 'admin_email')
     fields = ('email', )
     form = UserAdminForm
 
@@ -159,7 +175,7 @@ class NgoUserAdmin(DatawinnerAdmin):
 
     def queryset(self, request):
         qs = super(NgoUserAdmin, self).queryset(request)
-        return qs.filter(groups = Group.objects.filter(name="NGO Admins"))
+        return qs.filter(groups=Group.objects.filter(name="NGO Admins"))
 
     def save_model(self, request, obj, form, change):
         username = form.cleaned_data['email']
@@ -167,14 +183,78 @@ class NgoUserAdmin(DatawinnerAdmin):
         obj.email = username
         obj.save()
 
+
+class DWUserChangeForm(UserChangeForm):
+    organization_id = CharField(label="Organization ID")
+
+    class Meta:
+        model = User
+
+    def __init__(self, *args, **kwargs):
+        super(DWUserChangeForm, self).__init__(*args, **kwargs)
+        self.fields['organization_id'] = CharField(label="Organization ID")
+        if self.instance:
+            org_id = ''
+            try:
+                user_profile = NGOUserProfile.objects.get(user=self.instance)
+                org_id = user_profile.org_id
+            except:
+                pass
+            self.fields['organization_id'] = CharField(label="Organization ID", initial=org_id)
+
+    def clean_organization_id(self):
+        org_id = self.cleaned_data.get('organization_id', '')
+        try:
+            org = Organization.objects.get(org_id__iexact=org_id)
+            return org.org_id
+        except Organization.DoesNotExist:
+            raise ValidationError('Organization with id : %s does not exist.Please enter a valid id' % org_id)
+
+
+class DWUserAdmin(UserAdmin):
+    list_filter = ('groups__name',)
+    UserAdmin.fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Membership'), {'fields': ('groups', 'organization_id')}),
+    )
+    readonly_fields = ('last_login', 'date_joined')
+    list_display = UserAdmin.list_display + ('organization_name', 'organization_id')
+    form = DWUserChangeForm
+
+    def organization_name(self, obj):
+        org_id = NGOUserProfile.objects.get(user=obj).org_id
+        return Organization.objects.get(org_id=org_id).name
+
+
+    def organization_id(self, obj):
+        return NGOUserProfile.objects.get(user=obj).org_id
+
+    def save_model(self, request, obj, form, change):
+        super(DWUserAdmin, self).save_model(request, obj, form, change)
+        if form.cleaned_data.get('organization_id') is not None:
+            try:
+                user_profile = NGOUserProfile.objects.get(user=obj)
+                user_profile.org_id = form.cleaned_data['organization_id']
+                user_profile.save()
+            except NGOUserProfile.DoesNotExist:
+                user_profile = NGOUserProfile()
+                user_profile.org_id = form.cleaned_data['organization_id']
+                user_profile.title = 'Title'
+                user_profile.user = obj
+                user_profile.save()
+
+
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
 admin.site.register(OrganizationSetting, OrganizationSettingAdmin)
-admin.site.register(OutgoingNumberSetting,admin.ModelAdmin)
-admin.site.register(SMSC,admin.ModelAdmin)
+admin.site.register(OutgoingNumberSetting, admin.ModelAdmin)
+admin.site.register(SMSC, admin.ModelAdmin)
 admin.site.register(MessageTracker, MessageTrackerAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Country, CountryAdmin)
 admin.site.register(Network, NetworkAdmin)
-admin.site.register(User, NgoUserAdmin)
+admin.site.register(User, DWUserAdmin)
