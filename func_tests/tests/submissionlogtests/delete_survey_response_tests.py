d0a84e1ca29be937bcb587ed3f4cb4b9ce271734
ashwink <ashwin.konale@gmail.com>
2013-07-12 11:22:26 +0530

    Ashwin/ Pooja | #0000 | fixing failing test

--- func_tests/tests/submissionlogtests/delete_survey_response_tests.py
+++ func_tests/tests/submissionlogtests/delete_survey_response_tests.py
@@ -1,7 +1,10 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 import unittest
+import time
+
 from nose.plugins.attrib import attr
-from framework.base_test import  setup_driver, teardown_driver
+from framework.base_test import setup_driver, teardown_driver
+from framework.utils.common_utils import by_id
 from framework.utils.data_fetcher import fetch_, from_
 from pages.createquestionnairepage.create_questionnaire_page import CreateQuestionnairePage
 from pages.dashboardpage.dashboard_page import DashboardPage
@@ -9,7 +12,6 @@ from pages.loginpage.login_page import LoginPage
 from pages.projectoverviewpage.project_overview_page import ProjectOverviewPage
 from pages.smstesterpage.sms_tester_page import SMSTesterPage
 from pages.submissionlogpage.submission_log_locator import DELETE_BUTTON
-from pages.submissionlogpage.submission_log_page import SubmissionLogPage
 from pages.warningdialog.warning_dialog_page import WarningDialog
 from testdata.test_data import DATA_WINNER_SMS_TESTER_PAGE, DATA_WINNER_LOGIN_PAGE, DATA_WINNER_DASHBOARD_PAGE
 from tests.createquestionnairetests.create_questionnaire_data import QUESTIONNAIRE_DATA
@@ -48,18 +50,18 @@ class TestDeleteSurveyResponse(unittest.TestCase):
     @attr('functional_test')
     def test_should_delete_a_submission(self):
         project_name, questionnaire_code = self._get_project_details()
+
         self._submit_sms_data(get_sms_data_with_questionnaire_code(questionnaire_code))
 
         submission_log_page = self._navigate_to_submission_log_page_from_project_dashboard(project_name=project_name)
-
         total_records = submission_log_page.get_total_count_of_records()
-
         submission_log_page.check_submission_by_row_number(1)
         submission_log_page.choose_on_dropdown_action(DELETE_BUTTON)
         warning_dialog = WarningDialog(self.driver)
         warning_dialog.confirm()
-
-        submission_log_page = self._navigate_to_submission_log_page_from_project_dashboard()
+        time.sleep(2)
+        self.assertEquals(self.driver.find_visible_element(by_id('message_text')).text,
+                          'The selected records have been deleted')
         self.assertEquals(int(total_records) - 1, int(submission_log_page.get_shown_records_count()))
 
     def _navigate_to_submission_log_page_from_project_dashboard(self, project_name=PROJECT_NAME):
@@ -79,5 +81,4 @@ class TestDeleteSurveyResponse(unittest.TestCase):
         page = SMSTesterPage(self.driver)
         page.send_sms_with(sms_data)
         self.assertEqual(page.get_response_message(), fetch_(MESSAGE, from_(sms_data)))
-        return self._navigate_to_submission_log_page_from_project_dashboard()
 
