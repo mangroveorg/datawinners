350dd7de24624b7ba5bbbfdf5cabaa5046ed80fa
dileepbapat <dileepbapat@gmail.com>
2013-07-18 00:10:04 +0530

    Dileep | disabling restart of couchdb in between migrations

--- fabfile.py
+++ fabfile.py
@@ -332,7 +332,7 @@ def migrate_couchdb(context):
             with cd('%s/datawinners' % context.code_dir):
                 for migration in migration_files:
                     if not (migration.__contains__('.log') or migration.__eq__('__init__.py')):
-                        restart_couchdb()
+                        #restart_couchdb()
                         print 'Running migration: %s' % migration
                         activate_and_run(context.virtual_env,
                             "python %s/%s" % (context.couch_migrations_folder, migration))
