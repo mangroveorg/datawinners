f1f18cc4bc26ee8ccc5b6b0c9007ba133db6e5b4
PoojaS <poojas2406@gmail.com>
2013-07-18 11:03:36 +0530

    Ashwin/Pooja | #2298 | Cleaning up sms_tester_light_box_tests to remove tests of invalid scenarios

--- func_tests/tests/smstesterlightboxtests/sms_tester_light_box_data.py
+++ func_tests/tests/smstesterlightboxtests/sms_tester_light_box_data.py
@@ -7,10 +7,7 @@ from datawinners.messageprovider.tests.test_message_handler import THANKS
 PROJECT_NAME = "project_name"
 SMS = "sms"
 RESPONSE_MESSAGE = "message"
-MESSAGE = "message"
 
-PROJECT_DATA = {PROJECT_NAME: "clinic5 test project"}
-PROJECT_DATA_WITH_ACTIVITY_REPORT = {PROJECT_NAME: "Clinic13 Test Project"}
 
 VALID_DATA = {SMS: "cli005 .EID cid003 .NA Mr. Tessy .FA 58 .RD 17.05.2011 .BG b .SY ade .GPS 27.178057  -78.007789",
               RESPONSE_MESSAGE: THANKS }#+ " SY: Rapid weight loss,Memory loss,Neurological disorders  BG: O- NA: Mr. Tessy RD: 17.05.2011 FA: 58 EID: cid003 GPS: 27.178057,-78.007789"}
@@ -24,11 +21,3 @@ VALID_DATA2 = {SMS: "cli005 .EID cid004 .NA Mr. O'man .FA 58 .RD 17.05.2011 .BG 
 
 VALID_ORDERED_SMS_DATA = {SMS: "cli005 cid004 O'man 58 17.05.2011 b ade 27.178057,-78.007789",
                RESPONSE_MESSAGE: (THANKS % "Test") + " for Clinic Khandwa Clinic (cid004)." }#+ " EID: cid004 NA: O'man FA: 58 RD: 17.05.2011 BG: O- SY: Rapid weight loss,Memory loss,Neurological disorders  GPS: 27.178057, -78.007789"}
-
-VALID_ORDERED_SMS_DATA_WITH_ACTIVITY_REPORT = {SMS: "cli017 O'man 58 17.05.2011 b ade",
-               RESPONSE_MESSAGE: THANKS + " NA: O'man FA: 58 RD: 17.05.2011 BG: O- SY: Rapid weight loss,Memory loss,Neurological disorders"}
-
-
-SMS_WITH_UNICODE = {
-    SMS: u"cli005 .EID CID003 .NA Mr. O'brain .FA 58 .RD 17.05.2º11 .BG å .SY åde .GPS 27.178057º  -78.007789",
-    RESPONSE_MESSAGE: "Error. Incorrect answer for na, rd, bg, sy, gps. Please resend entire message."}
--- func_tests/tests/smstesterlightboxtests/sms_tester_light_box_tests.py
+++ func_tests/tests/smstesterlightboxtests/sms_tester_light_box_tests.py
@@ -1,75 +1,58 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from unittest.case import skipUnless, skipIf, skip
+import unittest
 
 from nose.plugins.attrib import attr
-from nose.tools import nottest
-from framework.base_test import BaseTest
+from framework.base_test import  setup_driver, teardown_driver
 from framework.utils.data_fetcher import fetch_, from_
 from pages.loginpage.login_page import LoginPage
 from testdata.test_data import DATA_WINNER_LOGIN_PAGE
 from tests.logintests.login_data import VALID_CREDENTIALS
-from nose.plugins.skip import SkipTest
-from tests.smstesterlightboxtests.sms_tester_light_box_data import PROJECT_DATA, VALID_DATA2, EXCEED_NAME_LENGTH, RESPONSE_MESSAGE, SMS_WITH_UNICODE, PROJECT_DATA_WITH_ACTIVITY_REPORT, VALID_ORDERED_SMS_DATA, VALID_ORDERED_SMS_DATA_WITH_ACTIVITY_REPORT, VALID_DATA, PROJECT_NAME
+from tests.smstesterlightboxtests.sms_tester_light_box_data import  VALID_DATA2, EXCEED_NAME_LENGTH, RESPONSE_MESSAGE, VALID_DATA, VALID_ORDERED_SMS_DATA
 
+@attr('suit_3')
+class TestSMSTesterLightBox(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = setup_driver()
 
-class BasePrepare(BaseTest):
-    @nottest
-    def prerequisites_of_sms_tester_light_box(self, project_to_open):
-        # doing successful login with valid credentials
-        self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
-        login_page = LoginPage(self.driver)
+        cls.driver.go_to(DATA_WINNER_LOGIN_PAGE)
+        login_page = LoginPage(cls.driver)
         global_navigation = login_page.do_successful_login_with(VALID_CREDENTIALS)
 
         # going on all project page
         all_project_page = global_navigation.navigate_to_view_all_project_page()
-        project_overview_page = all_project_page.navigate_to_project_overview_page(
-            fetch_(PROJECT_NAME, from_(project_to_open)))
-        return project_overview_page.open_sms_tester_light_box()
+        project_overview_page = all_project_page.navigate_to_project_overview_page("clinic5 test project")
+        cls.sms_tester_page = project_overview_page.open_sms_tester_light_box()
 
-@attr('suit_3')
-class TestSMSTesterLightBox(BasePrepare):
 
     @attr('functional_test', 'smoke')
     def test_successful_sms_submission(self):
         """
         Function to test the successful SMS submission
         """
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
+        project_overview_page = self.all_project_page.navigate_to_project_overview_page("clinic5 test project")
+        sms_tester_page = project_overview_page.open_sms_tester_light_bofirex()
         sms_tester_page.send_sms_with(VALID_DATA)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_DATA)))
 
     @attr('functional_test')
     def test_sms_player_for_exceeding_word_length(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
-        sms_tester_page.send_sms_with(EXCEED_NAME_LENGTH)
-        self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(EXCEED_NAME_LENGTH)))
+        self.sms_tester_page.send_sms_with(EXCEED_NAME_LENGTH)
+        self.assertEqual(self.sms_tester_page.get_response_message(),
+            fetch_(RESPONSE_MESSAGE, from_(EXCEED_NAME_LENGTH)))
 
     @attr('functional_test', 'smoke')
     def test_successful_sms_submission(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
-        sms_tester_page.send_sms_with(VALID_DATA2)
-        self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_DATA2)))
+        self.sms_tester_page.send_sms_with(VALID_DATA2)
+        self.assertEqual(self.sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_DATA2)))
 
-    @SkipTest
     @attr('functional_test', 'smoke')
-    def test_sms_submission_for_unicode(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
-        sms_tester_page.send_sms_with(SMS_WITH_UNICODE)
-        self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(SMS_WITH_UNICODE)))
-
-@attr('suit_3')
-class TestOrderedSMSTesterLightBox(BasePrepare):
-
-    @attr('functional_test')
     def test_successful_ordered_sms_submission(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
-        sms_tester_page.send_sms_with(VALID_ORDERED_SMS_DATA)
-        self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_ORDERED_SMS_DATA)))
+        self.sms_tester_page.send_sms_with(VALID_ORDERED_SMS_DATA)
+        self.assertEqual(self.sms_tester_page.get_response_message(),
+            fetch_(RESPONSE_MESSAGE, from_(VALID_ORDERED_SMS_DATA)))
 
-    @attr('functional_test')
-    @skip
-    def test_sms_submission_for_project_with_activity_report(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA_WITH_ACTIVITY_REPORT)
-        sms_tester_page.send_sms_with(VALID_ORDERED_SMS_DATA_WITH_ACTIVITY_REPORT)
-        self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_ORDERED_SMS_DATA)))
+    @classmethod
+    def tearDownClass(cls):
+        teardown_driver(cls.driver)
 
