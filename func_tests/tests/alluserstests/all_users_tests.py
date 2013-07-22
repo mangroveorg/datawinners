0dcb631a6eb5c146972df21d1bfc1c53d4d1a8e4
dileepbapat <dileepbapat@gmail.com>
2013-07-22 17:17:05 +0530

    Dileep,Pooja | 2318 | Functional test fix for deleted user in activity log

--- func_tests/tests/alluserstests/all_users_tests.py
+++ func_tests/tests/alluserstests/all_users_tests.py
@@ -115,7 +115,4 @@ class TestAllUsers(unittest.TestCase):
         username = activity_log_page.get_data_on_cell(2, 1)
         project_name_on_log_page = activity_log_page.get_data_on_cell(2, 4)
         self.assertEqual(project_name_on_log_page, project_name)
-        self.assertTrue("Mamy" in username)
-        
-
-  
+        self.assertTrue("Deleted User" in username, username)
