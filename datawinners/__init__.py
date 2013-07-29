cb6d7ec6140dfe42df1222b2b3d862a470f9d211
anishek.agarwal <anishek@gmail.com>
2013-07-29 14:30:09 +0530

    Anishek,Dileep | #2312 |  Adding elastic search mapping for date fields in subject

--- datawinners/__init__.py
+++ datawinners/__init__.py
@@ -4,9 +4,10 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'datawinners.settings'
 
 
-from mangrove.datastore.documents import EntityDocument
-from datawinners.search.subject_search import entity_search_update
+from mangrove.datastore.documents import EntityDocument, FormModelDocument
+from datawinners.search.subject_search import entity_search_update, update_subject_index
 
 
 #Registering search update methods
 EntityDocument.register_post_update(entity_search_update)
+FormModelDocument.register_post_update(update_subject_index)
--- datawinners/search/subject_search.py
+++ datawinners/search/subject_search.py
@@ -1,8 +1,33 @@
 import elasticutils
 from mangrove.datastore.entity import Entity
 from datawinners.entity.import_data import get_entity_type_fields, _tabulate_data
-from mangrove.form_model.form_model import get_form_model_by_entity_type
+from mangrove.form_model.field import DateField
+from mangrove.form_model.form_model import get_form_model_by_entity_type, REGISTRATION_FORM_CODE, FormModel
 from datawinners.settings import ELASTIC_SEARCH_URL
+
+
+def add_date_field_mapping(mapping_fields, field):
+    mapping_fields.update(
+        {field.name: {"type": "multi_field", "fields": {
+            field.name: {"type": "string"},
+            field.name + "_value": {"type": "date", "format": DateField.FORMAT_DATE_DICTIONARY.get(field.date_format)}
+        }}})
+
+
+def get_subject_mapping(form_model):
+    mapping_fields = {}
+    mapping = {"properties": mapping_fields}
+    for field in form_model.fields:
+        if isinstance(field, DateField):
+            add_date_field_mapping(mapping_fields, field)
+    return {form_model.form_code: mapping}
+
+
+def update_subject_index(form_model_doc, dbm):
+    form_model = FormModel.new_from_doc(dbm, form_model_doc)
+    if form_model.is_entity_registration_form() and form_model.form_code != REGISTRATION_FORM_CODE:
+        es = elasticutils.get_es(urls=ELASTIC_SEARCH_URL)
+        es.put_mapping(dbm.database_name, form_model.form_code, get_subject_mapping(form_model))
 
 
 def entity_search_update(entity_doc, dbm):
