033db7ba64d89a8a78540fdbf6610ad5b8b430c2
anishek.agarwal <anishek@gmail.com>
2013-08-02 17:05:09 +0530

    anishek/ashwin |  #2304 | moving the registartion to the datbasemanager package of DW as we were having some circular dependency issues with application level registration

--- datawinners/__init__.py
+++ datawinners/__init__.py
@@ -2,11 +2,3 @@
 import os
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'datawinners.settings'
-
-from datawinners.search.entity_search import entity_search_update, update_index
-from mangrove.datastore.documents import EntityDocument, FormModelDocument
-
-
-#Registering search update methods
-EntityDocument.register_post_update(entity_search_update)
-FormModelDocument.register_post_update(update_index)
