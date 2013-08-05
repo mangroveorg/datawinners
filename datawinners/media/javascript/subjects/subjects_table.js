249d6f43a378c873dc20a8db0c6ac497c8c95b6e
dileepbapat <dileepbapat@gmail.com>
2013-08-05 23:21:52 +0530

    Dileep/Pooja | 2254 | Changing the view to display subject type.

--- datawinners/media/javascript/subjects/subjects_table.js
+++ datawinners/media/javascript/subjects/subjects_table.js
@@ -7,9 +7,11 @@ $(document).ready(function () {
         "aLengthMenu": [10, 25, 50, 100],
         "iDisplayLength":25,
         "sDom":"ipfrtipl",
+        "sInput":"",
         "oLanguage":{"sInfoFiltered":"",
-                     "sProcessing":"<img class=\"search-loader\"src=\"/media/images/ajax-loader.gif\"></img>","sInfo": "<b>_START_ to _END_</b> of _TOTAL_ Subject",
-                     "sInfoEmpty": "Showing 0 to 0 of 0 Subjects",
+                     "sLengthMenu":"Show _MENU_ "+subject_type,
+                     "sProcessing":"<img class=\"search-loader\"src=\"/media/images/ajax-loader.gif\"></img>","sInfo": "<b>_START_ to _END_</b> of _TOTAL_ " + subject_type,
+                     "sInfoEmpty": "Showing 0 to 0 of 0 " + subject_type,
                      "oPaginate": {"sFirst": "", "sPrevious": "◀", "sNext": "▶", "sLast": ""}},
         "sPaginationType": "dw_pagination",
         "sAjaxSource": window.location.pathname + 'ajax/',
@@ -33,5 +35,7 @@ $(document).ready(function () {
     $("select[name='subjects_table_length']").change(function(){
         $('#subjects_table').dataTable({"bRetrieve":true}).fnPageChange("first")
     });
+
+    $("#subjects_table_filter").find("input").attr('placeholder','Enter any information you want to find')
 });
 
