860c5aa472ab0a4aca4762eb76d04b5297ca214b
Herihaja <hery.imiary@gmail.com>
2013-08-05 13:58:54 +0300

    Heri | #2307: Adding script to update Harivola Fokontany

---
+++ migration/couch/release_7/6.migration_add_geometry_data_for_harivola_fokontany.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+if __name__ == "__main__" and __package__ is None:
+    sys.path.insert(0, ".")
+
+from datawinners.main.database import get_db_manager
+from migration.couch.utils import mark_start_of_migration, migrate
+from mangrove.datastore.entity import get_by_short_code
+
+def add_geometry_data(db_name):
+    logger = logging.getLogger(db_name)
+
+    logger.info('Start migration on database')
+    try:
+        manager = get_db_manager(db_name)
+        mark_start_of_migration(db_name)
+
+        entity = get_by_short_code(manager, "110701001", ["fokontany"])
+
+        if not "coordinates" in entity.geometry:
+            geometry = {'type':'Point', 'coordinates':[0,0]}
+            entity.set_location_and_geo_code(entity.location_path, geometry)
+            entity.save()
+            
+        logger.info('End migration on database')
+    except Exception as e:
+        logger.exception(e.message)
+
+
+migrate(['hni_psi-madagascar_qmx864597'], add_geometry_data, version=(7, 0, 6))
