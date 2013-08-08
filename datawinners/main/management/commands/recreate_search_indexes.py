95cc5bf23b956e0bbb69c93f62548eb68a297f8b
dileepbapat <dileepbapat@gmail.com>
2013-08-08 11:44:18 +0530

    Dileep/Pooja | #2253 | creating migration script for creating indexes for existing subjects

--- datawinners/main/management/commands/recreate_search_indexes.py
+++ datawinners/main/management/commands/recreate_search_indexes.py
@@ -1,7 +1,8 @@
+import logging
 from django.core.management.base import BaseCommand
 import elasticutils
+from datawinners.main.couchdb.utils import all_db_names
 from datawinners.main.database import get_db_manager
-from datawinners.main.management.commands.utils import document_stores_to_process
 from datawinners.search import subject_search_update, subject_model_change_handler
 from datawinners.settings import ELASTIC_SEARCH_URL
 from mangrove.datastore.documents import FormModelDocument
@@ -12,17 +13,24 @@ def update_subject_index(dbname):
     dbm = get_db_manager(dbname)
     for row in dbm.load_all_rows_in_view('questionnaire'):
         form_model_doc = FormModelDocument.wrap(row["value"])
-        subject_model_change_handler(form_model_doc,dbm)
+        subject_model_change_handler(form_model_doc, dbm)
     for entity in get_all_entities(dbm):
         subject_search_update(entity, dbm)
+
+
+def recreate_index_for_db(database_name, es):
+    try:
+        es.delete_index(database_name)
+    except Exception as e:
+        logging.info("Could not delete index " + e.message)
+    response = es.create_index(database_name, settings={"number_of_shards": 1, "number_of_replicas": 0})
+    logging.info('%s search index created : %s' % (database_name, response.get('ok')))
+    update_subject_index(database_name)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         es = elasticutils.get_es(urls=ELASTIC_SEARCH_URL)
-        es.delete_all_indexes()
-        for database_name in document_stores_to_process(args):
-            response = es.create_index(database_name, settings={"number_of_shards": 1, "number_of_replicas": 0})
-            print '%s search index created : %s' % (database_name, response.get('ok'))
-            update_subject_index(database_name)
+        for database_name in all_db_names():
+            recreate_index_for_db(database_name, es)
         print 'Done'
---
+++ migration/couch/release_8/1.migration_to_create_search_indexes_for_subjects.py
@@ -0,0 +1,25 @@
+import sys
+import elasticutils
+from datawinners.main.couchdb.utils import all_db_names
+from datawinners.main.management.commands.recreate_search_indexes import recreate_index_for_db
+from datawinners.settings import ELASTIC_SEARCH_URL
+
+if __name__ == "__main__" and __package__ is None:
+    sys.path.insert(0, ".")
+
+import logging
+from migration.couch.utils import migrate, mark_start_of_migration
+
+
+def create_search_indices_for_subjects(db_name):
+    logger = logging.getLogger(db_name)
+    try:
+        mark_start_of_migration(db_name)
+        logger.info('Starting indexing')
+        recreate_index_for_db(db_name, es)
+    except Exception as e:
+        logger.exception("Failed DB: %s with message %s" % (db_name, e.message))
+    logger.info('Completed Indexing')
+
+es = elasticutils.get_es(urls=ELASTIC_SEARCH_URL)
+migrate(all_db_names(), create_search_indices_for_subjects, version=(8, 0, 1))
