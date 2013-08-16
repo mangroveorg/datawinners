28965fe40b5419e2cbbfb2f6993f76af232994bc
anishek.agarwal <anishek@gmail.com>
2013-08-16 15:33:30 +0530

    Revert "Anishek | email is mamytest not mamy"
    This reverts commit 12a3408d94e64e1bded998b15dddd9a2a748928f.

--- datawinners/main/database.py
+++ datawinners/main/database.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import User
 from datawinners.accountmanagement.models import OrganizationSetting
 from datawinners.main.utils import get_database_name
-from mangrove.datastore.database import get_db_manager as mangrove_db_manager
+from mangrove.datastore.database import  get_db_manager as mangrove_db_manager
 from datawinners import settings
 
 TEST_EMAILS = ['tester150411@gmail.com', 'chinatwu2@gmail.com', 'chinatwu3@gmail.com', 'gerard@mailinator.com',
-               'samuel@mailinator.com', 'mamytest@mailinator.com']
-
+               'samuel@mailinator.com', 'mamy@mailinator.com']
 
 def test_document_stores():
     document_stores = []
@@ -21,7 +20,7 @@ def document_stores():
 
 def get_db_manager(database_name):
     return mangrove_db_manager(server=settings.COUCH_DB_SERVER, database=database_name,
-                               credentials=settings.COUCHDBMAIN_CREDENTIALS)
+        credentials=settings.COUCHDBMAIN_CREDENTIALS)
 
 
 def get_database_manager(user):
