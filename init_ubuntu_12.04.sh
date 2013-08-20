0d4f57205cb029f0af9aa2f1332e7564ad54b55a
dileep <dileepbapat@gmail.com>
2013-08-19 23:06:19 -0700

    Anishek,Dileep| 2268 | adding init script for ubuntu 12.04

---
+++ init_ubuntu_12.04.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+
+function log {
+  echo "***** $1"
+  echo ""
+}
+
+function create_geodjango_db {
+ log "create geodjango db"
+ createdb -E UTF8 geodjango -T template0
+ createlang plpgsql geodjango
+ psql -d geodjango -f '/usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql'
+ psql -d geodjango -f '/usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql'
+}
+
+function create_postgis_template_db {
+ log "create postgis template db..."
+ createdb -E UTF8 template_postgis -T template0
+ createlang -d template_postgis plpgsql
+ psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
+ psql -d template_postgis -f '/usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql' # Loading the PostGIS SQL routines
+ psql -d template_postgis -f '/usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql'
+ psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" # Enabling users to alter spatial tables.
+ psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
+ psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+}
+
+function create_db {
+ log "create db..."
+ create_geodjango_db && \
+ create_postgis_template_db
+}
+
+function main {
+  create_db && \
+  log 'environment is ok!'
+}
+
+main
