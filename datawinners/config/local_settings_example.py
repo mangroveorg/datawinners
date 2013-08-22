fa09ca05e9487c2953524de20097b73ee0c5b44b
Dileep Bapat <dileepbapat@gmail.com>
2013-08-22 11:05:11 +0530

    Revert "OS env('USER') will be available only when user logged in"
    This reverts commit 21409833cb3975a1bbac6df0dcf5f4824b75fc92.

--- datawinners/config/local_settings_example.py
+++ datawinners/config/local_settings_example.py
@@ -1,13 +1,12 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 import os
-import getpass
 
 SITE_ID = 1
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'geodjango',                      # Or path to database file if using sqlite3.
-        'USER': getpass.getuser(),                      # Not used with sqlite3. 
+        'USER': os.getenv("USER"),                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
