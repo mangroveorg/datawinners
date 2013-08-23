474763de4c86bb85e5b1e0ec5c95c465ed94e170
dileepbapat <dileepbapat@gmail.com>
2013-08-23 17:48:01 +0530

    using jenkins db user on ci

--- datawinners/config/local_settings_test.py
+++ datawinners/config/local_settings_test.py
@@ -3,7 +3,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'testingdb',                      # Or path to database file if using sqlite3.
-        'USER': 'mangrover',                      # Not used with sqlite3.
+        'USER': 'jenkins',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
