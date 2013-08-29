ef5e95b6a3d442e58af4279afb7d0303e0bca899
ajay <ajay_886@hotmail.com>
2013-08-29 17:38:10 +0530

    Ajay/Ashwin | #2121 | Migration to add eid fields for survey responses wherever absent.
    eid field is populated from survey response's owner id.

---
+++ migration/couch/release_8/3.migration_to_add_eid_to_survey_responses_not_having_eid.py
@@ -0,0 +1,42 @@
+import logging
+from datawinners.main.couchdb.utils import all_db_names
+from datawinners.main.database import get_db_manager
+from mangrove.datastore.documents import SurveyResponseDocument, DocumentBase, EntityDocument
+from mangrove.datastore.entity import Entity
+from mangrove.form_model.form_model import get_form_model_by_code
+from mangrove.transport.contract.survey_response import SurveyResponse
+from migration.couch.utils import migrate
+
+
+def _get_survey_responses_with_no_eid(dbm, logger):
+    rows = dbm.database.view("surveyresponse/surveyresponse", reduce=False, include_docs=True)
+    inconsistent_survey_response_list = []
+    for row in rows:
+        try:
+            doc = SurveyResponseDocument.wrap(row['value'])
+            survey_response = SurveyResponse.new_from_doc(dbm, doc)
+            form_model = get_form_model_by_code(dbm, survey_response.form_code)
+            if form_model.entity_defaults_to_reporter() and "eid" not in survey_response.values.keys():
+                inconsistent_survey_response_list.append(survey_response)
+        except Exception as e:
+            logger.exception(e)
+    return inconsistent_survey_response_list
+
+
+def add_eid_field_for_survey_response_with_missing_eid_field(db_name):
+    logger = logging.getLogger(db_name)
+    logger.info('Starting Migration')
+    dbm = get_db_manager(db_name)
+
+    inconsistent_survey_response_list = _get_survey_responses_with_no_eid(dbm, logger)
+    for survey_response in inconsistent_survey_response_list:
+        if survey_response.owner_uid:
+            data_sender = Entity.get(dbm, survey_response.owner_uid)
+            survey_response.values['eid'] = data_sender.short_code
+            logger.info("Migrated survey response: %s" % survey_response.uuid)
+        else:
+            logger.warning("Missing owner id for survey_response: %s, form_code: %s" % (
+                survey_response.uuid, survey_response.form_code))
+        logger.info("Number of survey responses migrated: %s" % len(inconsistent_survey_response_list))
+
+migrate(all_db_names(), add_eid_field_for_survey_response_with_missing_eid_field, version=(8, 0, 3))
