f13fbfede54bc00f9eecb5c824caa1c5cad02986
Herihaja <hery.imiary@gmail.com>
2013-09-03 10:03:46 +0300

    Heri|#2197: Making logo clickable and link to homepage

--- datawinners/dashboard/urls.py
+++ datawinners/dashboard/urls.py
@@ -1,11 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 
 from datawinners.dashboard.views import dashboard, get_submission_breakup, get_submissions_about_project, map_entities, render_map
 from datawinners.dashboard.views import start
 
 urlpatterns = patterns('',
-    (r'^dashboard/$', dashboard),
+    url(r'^dashboard/$', dashboard, name='dashboard'),
     (r'^start/$', start),
     (r'^submission/breakup/(?P<project_id>.+?)/$', get_submission_breakup),
     (r'^submission/details/(?P<project_id>.+?)/$', get_submissions_about_project),
