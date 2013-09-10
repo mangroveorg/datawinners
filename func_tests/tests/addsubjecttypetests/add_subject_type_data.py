5af75a42f5bfc2d0d84532993081a811d9aabb32
Dileep Bapat <dileepbapat@gmail.com>
2013-09-10 22:34:31 +0530

    Dileep | Fixing funcitonal test related to error message change

--- func_tests/tests/addsubjecttypetests/add_subject_type_data.py
+++ func_tests/tests/addsubjecttypetests/add_subject_type_data.py
@@ -10,5 +10,5 @@ VALID_ENTITY = {ENTITY_TYPE: "Hospital", SUCCESS_MESSAGE: "hospital"}
 # already exist entity
 ALREADY_EXIST_ENTITY = {ENTITY_TYPE: "Clinic", ERROR_MESSAGE: "clinic already registered as a subject type."}
 # Blank entity
-BLANK = {ENTITY_TYPE: "", ERROR_MESSAGE: "Only letters and numbers are valid and you must provide more than just whitespaces."}
-INVALID_ENTITY = {ENTITY_TYPE: "W@terpo!nt", ERROR_MESSAGE: "Only letters and numbers are valid and you must provide more than just whitespaces."}
+BLANK = {ENTITY_TYPE: "", ERROR_MESSAGE: u"You can't leave this empty."}
+INVALID_ENTITY = {ENTITY_TYPE: "W@terpo!nt", ERROR_MESSAGE: u"Please use only letters (a-z), numbers, and spaces."}
