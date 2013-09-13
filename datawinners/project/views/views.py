f2a37da4c23426be2624c5f5a1252efa15de400e
Dileep Bapat <dileepbapat@gmail.com>
2013-09-13 03:57:29 -0700

    Yogesh,Dileep | 2478 | Retain the subject values in fields after editing a subject

--- datawinners/project/views/views.py
+++ datawinners/project/views/views.py
@@ -684,7 +684,6 @@ class SubjectWebQuestionnaireRequest():
             if response.success:
                 ReportRouter().route(organization.org_id, response)
                 success_message = self.success_message(response.short_code)
-                questionnaire_form = self.form(country=organization.country_name())
             else:
                 questionnaire_form._errors = helper.errors_to_list(response.errors, self.form_model.fields)
         except DataObjectNotFound as exception:
