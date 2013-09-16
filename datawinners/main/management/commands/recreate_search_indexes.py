05caa99e6be4c0366ce71231c8905d6975839edb
anishek@gmail.com <anishek@gmail.com>
2013-09-16 18:17:39 +0530

    anishek/dc | using an iterview as for larger databases with lot of subjects loading all will increase the heap space

--- datawinners/main/management/commands/recreate_search_indexes.py
+++ datawinners/main/management/commands/recreate_search_indexes.py
@@ -6,7 +6,7 @@ from datawinners.main.database import get_db_manager
 from datawinners.search import subject_search_update, subject_model_change_handler
 from datawinners.settings import ELASTIC_SEARCH_URL, ELASTIC_SEARCH_TIMEOUT
 from mangrove.datastore.documents import FormModelDocument
-from mangrove.datastore.entity import get_all_entities
+from mangrove.datastore.entity import Entity
 
 
 def update_subject_index(dbname):
@@ -14,8 +14,10 @@ def update_subject_index(dbname):
     for row in dbm.load_all_rows_in_view('questionnaire'):
         form_model_doc = FormModelDocument.wrap(row["value"])
         subject_model_change_handler(form_model_doc, dbm)
-    for entity in get_all_entities(dbm):
-        subject_search_update(entity, dbm)
+
+    rows = dbm.database.iterview('by_short_codes/by_short_codes', 100, reduce=False, include_docs=True)
+    for row in rows:
+        subject_search_update(Entity.__document_class__.wrap(row.get('doc')), dbm)
 
 
 def recreate_index_for_db(database_name, es):
@@ -34,7 +36,7 @@ class Command(BaseCommand):
         if len(args) > 0:
             databases_to_index = args[0:]
         else:
-            databases_to_index  = all_db_names()
+            databases_to_index = all_db_names()
         for database_name in databases_to_index:
             recreate_index_for_db(database_name, es)
             print 'Done' + database_name
