f4f19f8f2a3035b325f2ba7ac23179e839f59af5
Dileep Bapat <dileepbapat@gmail.com>
2013-09-16 02:27:13 -0700

    Dileep | Increasing Elastic search timeout for manage.py command

--- datawinners/main/management/commands/recreate_search_indexes.py
+++ datawinners/main/management/commands/recreate_search_indexes.py
@@ -4,7 +4,7 @@ import elasticutils
 from datawinners.main.couchdb.utils import all_db_names
 from datawinners.main.database import get_db_manager
 from datawinners.search import subject_search_update, subject_model_change_handler
-from datawinners.settings import ELASTIC_SEARCH_URL
+from datawinners.settings import ELASTIC_SEARCH_URL, ELASTIC_SEARCH_TIMEOUT
 from mangrove.datastore.documents import FormModelDocument
 from mangrove.datastore.entity import get_all_entities
 
@@ -30,7 +30,7 @@ def recreate_index_for_db(database_name, es):
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        es = elasticutils.get_es(urls=ELASTIC_SEARCH_URL)
+        es = elasticutils.get_es(urls=ELASTIC_SEARCH_URL, timeout=ELASTIC_SEARCH_TIMEOUT)
         if len(args) > 0:
             databases_to_index = args[0:]
         else:
