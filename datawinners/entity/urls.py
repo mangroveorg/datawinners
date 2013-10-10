92de796916747864569e5b864d03dffb6065ca9a
ajay <ajay_886@hotmail.com>
2013-10-10 18:19:49 +0530

    Revert "Ajay | #2164 | Refactoring - separated out get and post views for all ds"
    This reverts commit 954cb93c6c1a03b629da9063e311f95287245dd7.

--- datawinners/entity/urls.py
+++ datawinners/entity/urls.py
@@ -1,12 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 from django.conf.urls.defaults import patterns, url
-from datawinners.common.urlextension import method_splitter
-from datawinners.entity.view.datasenders import all_datasenders_get, all_datasenders_post, disassociate_datasenders, all_datasenders_ajax, associate_datasenders
-from datawinners.entity.views import create_data_sender, create_multiple_web_users, edit_subject_questionnaire, save_questionnaire, edit_data_sender, edit_subject
+from datawinners.entity.views import create_data_sender, disassociate_datasenders, associate_datasenders, create_multiple_web_users, edit_subject_questionnaire, save_questionnaire, edit_data_sender, edit_subject, all_datasenders_ajax
 from datawinners.entity.views import create_subject
 from datawinners.entity.views import create_type
 from datawinners.entity.views import all_subject_types, delete_data_senders, delete_subjects, all_subjects, all_subjects_ajax
+from datawinners.entity.views import all_datasenders
 from datawinners.entity.views import import_subjects_from_project_wizard
 from datawinners.entity.views import export_subject
 from datawinners.entity.views import export_template
@@ -16,18 +15,13 @@ urlpatterns = patterns('',
                        (r'datasender/edit/(?P<reporter_id>.+?)/$', edit_data_sender),
                        (r'webuser/create', create_multiple_web_users),
                        url(r'subject/create/(?P<entity_type>.+?)/$', create_subject, name='create_subject'),
-                       url(r'subject/edit/(?P<entity_type>.+?)/(?P<entity_id>.+?)/$', edit_subject,
-                           name="edit_subject"),
+                       url(r'subject/edit/(?P<entity_type>.+?)/(?P<entity_id>.+?)/$', edit_subject, name="edit_subject"),
                        (r'type/create', create_type),
                        (r'subjects/delete/$', delete_subjects),
                        (r'subjects/(?P<subject_type>.+?)/ajax/$', all_subjects_ajax),
                        url(r'subjects/(?P<subject_type>.+?)/$', all_subjects, name="all_subjects"),
                        (r'delete/$', delete_data_senders),
-                       url(r'datasenders/$', method_splitter,
-                                        {'GET': all_datasenders_get,
-                                         'POST': all_datasenders_post
-                                        },
-                                        name='all_datasenders'),
+                       url(r'datasenders/$', all_datasenders, name='all_datasenders'),
                        (r'datasenders/ajax/$', all_datasenders_ajax),
                        (r'disassociate/$', disassociate_datasenders),
                        (r'associate/$', associate_datasenders),
