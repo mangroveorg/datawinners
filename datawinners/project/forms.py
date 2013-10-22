70e6333dd3640de671b7e103c39d51040f9b486a
Alia Sophie <azalia@MacBook-Pro-de-Alia.local>
2013-10-22 10:36:11 +0300

    Heri & Sophie | #2397 | Updating french translations on Reminder page

--- datawinners/project/forms.py
+++ datawinners/project/forms.py
@@ -139,14 +139,17 @@ class ReminderForm(Form):
 
     should_send_reminders_before_deadline = BooleanField(required=False, initial=False)
     number_of_days_before_deadline = DecimalField(label=ugettext_lazy("days before deadline"), required=False)
-    reminder_text_before_deadline = CharField(widget=forms.Textarea, required=False)
+    reminder_text_before_deadline = CharField(label=ugettext_lazy("Reminder text before deadline"),
+                                              widget=forms.Textarea, required=False)
 
     should_send_reminders_on_deadline = BooleanField(label=ugettext_lazy("The day of the deadline"), required=False, initial=False)
-    reminder_text_on_deadline = CharField(widget=forms.Textarea, required=False)
+    reminder_text_on_deadline = CharField(label=ugettext_lazy("Reminder text on deadline"),widget=forms.Textarea,
+                                          required=False)
 
     should_send_reminders_after_deadline = BooleanField(label=ugettext_lazy("days after the deadline"), required=False, initial=False)
     number_of_days_after_deadline = DecimalField(required=False, label=ugettext_lazy("number of days after deadline"))
-    reminder_text_after_deadline = CharField(widget=forms.Textarea, required=False)
+    reminder_text_after_deadline = CharField(label=ugettext_lazy("Reminder text after deadline"), widget=forms.Textarea,
+                                             required=False)
 
     whom_to_send_message = BooleanField(label=ugettext_lazy("Only send reminders to senders who have not already submitted data for the current deadline."),
                                        required=False, initial=True)
