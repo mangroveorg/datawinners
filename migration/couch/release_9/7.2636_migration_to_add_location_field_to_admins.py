5fa19b333250e3202046d980d4adf8803952bc13
ajay <ajay_886@hotmail.com>
2013-11-08 16:01:36 +0530

    Ajay | #2636 | Migration - to add location field for admins

---
+++ migration/couch/release_9/7.2636_migration_to_add_location_field_to_admins.py
@@ -0,0 +1,58 @@
+import sys
+#noinspection PyUnresolvedReferences
+import datawinners.search
+from django.core.exceptions import ObjectDoesNotExist
+from datawinners.accountmanagement.models import NGOUserProfile, OrganizationSetting
+from mangrove.datastore.datadict import get_datadict_type_by_slug
+from mangrove.datastore.entity import get_by_short_code_include_voided
+from mangrove.transport.repository.reporters import REPORTER_ENTITY_TYPE
+
+if __name__ == "__main__" and __package__ is None:
+    sys.path.insert(0, ".")
+
+from datawinners.main.database import get_db_manager
+from datawinners.main.couchdb.utils import all_db_names
+
+import logging
+from migration.couch.utils import migrate
+
+
+def _add_location_field_if_absent(admin, dbm, logger):
+    if "location" not in admin.data:
+        logger.info('Adding location field to admin %s' % admin.short_code)
+        data = ("location", admin.aggregation_paths["_geo"], get_datadict_type_by_slug(dbm, slug='location'))
+        admin.update_latest_data([data])
+    else:
+        logger.info('Location field already present for admin %s' % admin.short_code)
+
+
+def _get_admin_document(admin_profiles, dbm):
+    admin_profile = admin_profiles[0]
+    admin = get_by_short_code_include_voided(dbm, admin_profile.reporter_id, REPORTER_ENTITY_TYPE)
+    return admin
+
+
+def _get_admin_profiles(dbm):
+    org_id = OrganizationSetting.objects.get(document_store=dbm.database_name).organization_id
+    admin_profiles = NGOUserProfile.objects.filter(org_id=org_id, user__is_superuser=True)
+    return admin_profiles
+
+
+def migration_to_add_location_field_to_admins(db_name):
+    logger = logging.getLogger(db_name)
+    dbm = get_db_manager(db_name)
+    try:
+        logger.info('Migration started for database %s' % db_name)
+        admin_profiles = _get_admin_profiles(dbm)
+        if not admin_profiles or len(admin_profiles) == 0:
+            logger.error('No admin for database %s' % db_name)
+            return
+        admin = _get_admin_document(admin_profiles, dbm)
+        _add_location_field_if_absent(admin, dbm, logger)
+    except ObjectDoesNotExist:
+        logger.error("Entry not present for database: %s" % db_name)
+    except Exception as e:
+        logger.exception("Update of location failed for database: %s with exception: %s" % (db_name, e.message))
+
+
+migrate(all_db_names(), migration_to_add_location_field_to_admins, version=(9, 0, 7))
