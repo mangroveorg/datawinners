185c1f19858217f02ad767e1a9ac9d2b106b1952
ashwinkonale <ashwin.konale@gmail.com>
2013-11-11 10:26:02 +0530

    Ashwin| #0000 | Correcting tests.

--- datawinners/search/tests/test_submission_index.py
+++ datawinners/search/tests/test_submission_index.py
@@ -4,21 +4,21 @@ from mangrove.datastore.documents import EnrichedSurveyResponseDocument
 
 
 class TestSubmissionIndex(unittest.TestCase):
-
     def test_should_update_search_dict_with_form_field_questions_for_success_submissions(self):
         search_dict = {}
-        values = {'eid':{'answer': {'deleted': False, 'id':'cid005', 'name': 'Test'}, 'is_entity_question': 'true'},
+        values = {'eid': {'answer': {'deleted': False, 'id': 'cid005', 'name': 'Test'}, 'is_entity_question': 'true'},
                   'q2': {'answer': 'name', 'type': 'text', 'label': 'First Name'},
                   'q3': {'answer': {u'b': 'two', u'c': 'three'}, 'type': 'select', 'label': 'Question 2'},
                   'q4': {'answer': '3,3', 'type': 'geocode', 'label': 'gps'},
                   'q5': {'answer': '11.12.2012', 'format': 'mm.dd.yyyy', 'type': 'date', 'label': 'date'}}
         submission_doc = EnrichedSurveyResponseDocument(values=values, status="success")
         _update_with_form_model_fields(submission_doc, search_dict)
-        self.assertEquals({'eid':'Test',"entity_short_code":"cid005",'q2': 'name', 'q3': 'three,two', 'q4': '3,3', 'q5': '11.12.2012'}, search_dict)
+        self.assertEquals({'eid': 'Test', "entity_short_code": "cid005", 'q2': 'name', 'q3': 'three,two', 'q4': '3,3',
+                           'q5': '11.12.2012', 'void': False}, search_dict)
 
     def test_should_update_search_dict_with_form_field_questions_for_error_submissions(self):
         search_dict = {}
         values = {'q2': 'wrong number', 'q3': 'wrong text'}
         submission_doc = EnrichedSurveyResponseDocument(values=values, status="error")
         _update_with_form_model_fields(submission_doc, search_dict)
-        self.assertEquals({'q2': 'wrong number', 'q3': 'wrong text'}, search_dict)
+        self.assertEquals({'q2': 'wrong number', 'q3': 'wrong text', 'void': False}, search_dict)
