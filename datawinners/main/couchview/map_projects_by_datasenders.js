27afc76e7b3d035c3385634452f61698f50c7797
Dileep Bapat <dileepbapat@gmail.com>
2013-11-11 16:56:14 +0530

    Ashwin,Dileep,DC | Fixing project by ds view issue, view was failing to create view index

--- datawinners/main/couchview/map_projects_by_datasenders.js
+++ datawinners/main/couchview/map_projects_by_datasenders.js
@@ -2,7 +2,7 @@ function(doc) {
     if (doc.document_type == 'Project') {
         if(!doc.void){
             for(i = 0; i< doc.data_senders.length; i++){
-                emit(doc.data_senders[i], doc);
+                emit(doc.data_senders[i], null);
 	    }
         }
     }
