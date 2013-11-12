0442f9a324d6bb7f90ff48decdf4eb90d4fba011
Dileep Bapat <dileepbapat@gmail.com>
2013-11-12 12:00:33 +0530

    DC,Dileep | Fixing error get project by datasender

--- datawinners/project/models.py
+++ datawinners/project/models.py
@@ -326,7 +326,10 @@ class Project(DocumentBase):
 
 def get_all_projects(dbm, data_sender_id=None):
     if data_sender_id:
-        return dbm.load_all_rows_in_view('projects_by_datasenders', startkey=data_sender_id, endkey=data_sender_id, include_docs=True)
+        rows = dbm.load_all_rows_in_view('projects_by_datasenders', startkey=data_sender_id, endkey=data_sender_id, include_docs=True)
+        for row in rows:
+            row.update({'value':row["doc"]})
+        return rows
     return dbm.load_all_rows_in_view('all_projects')
 
 
