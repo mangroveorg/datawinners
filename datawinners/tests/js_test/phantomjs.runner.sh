648d370a6198ab42f0d34607f251c4a218daabda
ajay <ajay_886@hotmail.com>
2013-11-18 12:31:24 +0530

    Ajay,Yogesh | Installing phantom only in usr/bin

--- datawinners/tests/js_test/phantomjs.runner.sh
+++ datawinners/tests/js_test/phantomjs.runner.sh
@@ -4,10 +4,8 @@ function installPhantomjs(){
     echo "installing phantom"
 
     cd /usr/local/share
-    wget http://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
-    tar xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
-    sudo ln -s /usr/local/share/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-    sudo ln -s /usr/local/share/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+    sudo wget http://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
+    sudo tar xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
     sudo ln -s /usr/local/share/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
 
     echo "Done installing phantom"
