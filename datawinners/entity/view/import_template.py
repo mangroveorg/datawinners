b59696235858049bfd499ff7a0b045d740ded931
ajay <ajay_886@hotmail.com>
2013-11-27 17:23:16 +0530

    Pooja/Ajay | #2683 | Made all columns in import template as text

--- datawinners/entity/view/import_template.py
+++ datawinners/entity/view/import_template.py
@@ -11,41 +11,36 @@ from datawinners.utils import get_excel_sheet
 from mangrove.form_model.form_model import get_form_model_by_code, GEO_CODE_FIELD_NAME
 
 
+def _get_submission_form_fields_for_user(form_model, request):
+    form_fields = form_model.form_fields
+    if form_model.entity_defaults_to_reporter():
+        if not is_org_user(request.user):
+            form_fields.remove(form_model.entity_question)
+    return form_fields
+
+
 @valid_web_user
 def import_template(request, form_code):
     manager = get_database_manager(request.user)
-    form_model=get_form_model_by_code(manager,form_code)
+    form_model = get_form_model_by_code(manager, form_code)
     if form_model.is_entity_registration_form():
         form_fields = form_model.form_fields
         headers = get_subject_headers(form_fields)
-        fields = field_names(form_fields)
         field_codes = field_code(form_fields)
         sheet_name = request.GET["filename"]
     else:
-        form_fields = form_model.form_fields
-        if form_model.entity_defaults_to_reporter():
-            if not is_org_user(request.user):
-                form_fields.remove(form_model.entity_question)
-        fields = field_names(form_fields)
+        form_fields = _get_submission_form_fields_for_user(form_model, request)
         field_codes = field_code(form_fields)
         headers = get_submission_headers(form_fields)
-
         sheet_name = "Import_Submissions"
 
-    index_geocode = _get_geo_code_index(fields)
     filename = unquote(request.GET["filename"])
-    uid_index = len(fields) - 1
-    data = [headers]
     workbook_response_factory = WorkBookResponseFactory(form_code, filename, sheet_name)
-    return workbook_response_factory.create_workbook_response(index_geocode, uid_index, data, field_codes)
-
-
-def field_names(fields):
-    return [field['name'] for field in fields]
+    return workbook_response_factory.create_workbook_response([headers], field_codes)
 
 
 def field_code(fields):
-     return [field['code'] for field in fields]
+    return [field['code'] for field in fields]
 
 
 def _get_geo_code_index(fields):
@@ -62,20 +57,18 @@ class WorkBookResponseFactory:
         self.file_name = file_name
         self.sheet_name = sheet_name
 
-    def _add_styles(self, index_geocode, uid_index, wb):
+    def _add_styles(self, wb):
         ws = wb.get_sheet(0)
         style = xlwt.XFStyle()
         style.num_format_str = '@'
-        gps_col = ws.col(index_geocode)
-        gps_col.set_style(style)
-        uid_col = ws.col(uid_index)
-        uid_col.set_style(style)
+        for column in ws.get_cols().values():
+            column.set_style(style)
 
-    def create_workbook_response(self, geo_code_index, uid_index, data, field_codes):
+    def create_workbook_response(self, data, field_codes):
         response = HttpResponse(mimetype='application/vnd.ms-excel')
         response['Content-Disposition'] = 'attachment; filename="%s.xls"' % slugify(self.file_name)
         wb = get_excel_sheet(data, self.sheet_name)
         add_codes_sheet(wb, self.form_code, field_codes)
-        self._add_styles(geo_code_index, uid_index, wb)
+        self._add_styles(wb)
         wb.save(response)
         return response
