13b7030c378516fa240b2c29dcb9fea0de8387ea
ajay <ajay_886@hotmail.com>
2013-11-29 01:00:24 +0530

    Ajay | #0000 | Created script for deleting users given email addresses

---
+++ support/delete_users_for_given_email_addresses.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from datawinners.accountmanagement.models import NGOUserProfile, Organization
+from datawinners.entity.helper import delete_datasender_for_trial_mode
+from datawinners.utils import get_database_manager_for_org
+from mangrove.utils.types import is_not_empty
+
+org_id = 'SLX364903'
+email_addresses = ['ajay@test.com']
+
+organization = Organization.objects.get(org_id=org_id)
+
+print "Organization in trail mode:%s" % organization.in_trial_mode
+dbm = get_database_manager_for_org(organization)
+
+
+def _delete_user_entry(profiles):
+    user_profile = profiles[0]
+    profile_reporter_id = user_profile.reporter_id
+    if profile_reporter_id.lower() == profile_reporter_id:
+        print "Not deleting user since the reporter id is already lowercase."
+    else:
+        print "Deleting user."
+        user_profile.user.delete()
+        if organization.in_trial_mode:
+            delete_datasender_for_trial_mode(dbm, [profile_reporter_id.lower()], 'reporter')
+
+def _is_only_datasender(user):
+    return user.groups.filter(name__in=["NGO Admins", "Project Managers"]).count() <= 0
+
+for email_id in email_addresses:
+    try:
+        print "Processing User with email_id: '%s'" % user.email
+        user = User.objects.get(email=email_id)
+        if _is_only_datasender(user):
+            profiles = NGOUserProfile.objects.filter(user=user)
+            if is_not_empty(profiles):
+                _delete_user_entry(profiles)
+            else:
+                print "User with email_id: '%s' has no profile" % user.email
+        else:
+            print "User with email_id: '%s' is not a simple DS and will not be deleted." % user.email
+    except ObjectDoesNotExist:
+         print "User with email_id: '%s' does not exist" % user.email
