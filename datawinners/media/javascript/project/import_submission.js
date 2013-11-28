bc91eac6181b340087c3f550a923fd49be7047bf
ajay <ajay_886@hotmail.com>
2013-11-28 23:51:33 +0530

    Ajay | #2683 | Added js tests for import submission popup

--- datawinners/media/javascript/project/import_submission.js
+++ datawinners/media/javascript/project/import_submission.js
@@ -1,18 +1,39 @@
-$(document).ready(function(){
-    var import_dialog = $("#submission_import").dialog({
-            title: gettext("Import Submission(s)"),
-            modal: true,
-            autoOpen: false,
-            width: 500,
-            closeText: 'hide'
-        }
-    );
+DW.SubmissionImportPopup = function(){
+    var self = this;
 
-    $('.import_link').click(function(){
-        var project_name = $(this).attr("data-projectname");
-        var form_code = $(this).attr("data-formcode");
-        var template_download_url = import_template_url.replace("<project_name>", project_name).replace("form_code", form_code);
-        $("#template-download-link").attr("href", template_download_url);
-        import_dialog.dialog("open");
+    self.init = function(download_url_template){
+        self.import_dialog = $("#submission_import").dialog({
+                title: gettext("Import Submission(s)"),
+                modal: true,
+                autoOpen: false,
+                width: 500,
+                closeText: 'hide'
+            }
+        );
+        self.download_link = $("#template-download-link");
+        self.download_url_template = download_url_template;
+        return self;
+    };
+
+    self.updateTemplateDownloadLink = function(target){
+        var selected_link_element = $(target);
+        var project_name = selected_link_element.attr("data-projectname");
+        var form_code = selected_link_element.attr("data-formcode");
+        var template_download_url = self.download_url_template.replace("<project_name>", project_name).replace("form_code", form_code);
+        self.download_link.attr("href", template_download_url);
+        return self;
+    };
+
+    self.open = function(){
+        self.import_dialog.dialog("open");
+        return self;
+    };
+};
+
+$(function(){
+    var submissionPopup = new DW.SubmissionImportPopup().init(import_template_url);
+    $(document).on("click", '.import_link', function(event){
+        submissionPopup.updateTemplateDownloadLink(event.currentTarget);
+        submissionPopup.open();
     });
 });
---
+++ datawinners/tests/js_test/specs/import_submission_spec.js
@@ -0,0 +1,38 @@
+describe("Import Submission Popup", function(){
+
+    beforeEach(function(){
+        setFixtures("<a class='import_link' data-projectname='projectname123' \
+            data-formcode='formcode123'></a><a id='template-download-link'></a>\
+            <div id='submission_import'></div>")
+    });
+
+    it("should create template download link url from data attributes of selected project questionnare import link", function(){
+        var popup = new DW.SubmissionImportPopup().init("/domain/project/subject/form_code?filename=<project_name>");
+
+        popup.updateTemplateDownloadLink($(".import_link"));
+
+        expect($("#template-download-link")).toHaveAttr("href", "/domain/project/subject/formcode123?filename=projectname123")
+    });
+
+    it("should show dialog when popup is opened", function(){
+        spyOn(window, "gettext").andReturn("someText");
+        var chainedDialogSpy = jasmine.createSpy("chainedDialog");
+        var mockDialog = spyOn($.fn, "dialog").andReturn({
+            "dialog": chainedDialogSpy
+        });
+        var popup = new DW.SubmissionImportPopup().init("/domain/project/subject/form_code?filename=<project_name>");
+
+        popup.open();
+
+        expect(mockDialog).toHaveBeenCalledWith({
+                                                  title: "someText",
+                                                  modal: true,
+                                                  autoOpen: false,
+                                                  width: 500,
+                                                  closeText: 'hide'
+                                                });
+        expect(chainedDialogSpy).toHaveBeenCalledWith("open")
+    });
+
+
+});
