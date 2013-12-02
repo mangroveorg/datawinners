d82e31b73cc613600ebd211a5c95f571e5fe6529
Deepthi-Chand <deepthia@thoughtworks.com>
2013-12-02 18:13:08 +0530

    Dc/Ashwin/Melanie | #2693 | Updating submission log upon edit of subject

--- datawinners/search/submission_index.py
+++ datawinners/search/submission_index.py
@@ -1,6 +1,8 @@
 from string import lower
 from babel.dates import format_datetime
+import elasticutils
 from datawinners.feeds.database import get_feed_db_from_main_db_name
+from datawinners.settings import ELASTIC_SEARCH_URL
 from mangrove.errors.MangroveException import DataObjectNotFound
 from mangrove.datastore.documents import SurveyResponseDocument, EnrichedSurveyResponseDocument
 from datawinners.main.database import get_db_manager
@@ -18,15 +20,40 @@ def create_submission_mapping(dbm, form_model):
     mapping = get_fields_mapping(form_model.id, fields, 'code')
     es.put_mapping(dbm.database_name, form_model.id, mapping)
 
-def submission_update_on_edit_DS(entity_doc, dbm):
-    entity_uid = entity_doc.id
-    feeds_dbm = get_feed_db_from_main_db_name(dbm.database_name)
-    survey_responses = dbm.load_all_rows_in_view('survey_response_by_datasender', key=entity_uid, include_docs=False)
-    for survey_response in survey_responses:
-        enriched_survey_response = feeds_dbm._load_document(survey_response.id, EnrichedSurveyResponseDocument)
-        if enriched_survey_response is not None:
-            update_submission_search_index(enriched_survey_response, feeds_dbm, refresh_index=False)
 
+def submission_update_on_edit_DS(entity_doc, dbm):
+    if entity_doc.entity_type != ['reporter']:
+        update_submission_search_for_subjects(entity_doc, dbm)
+    else:
+        entity_uid = entity_doc.id
+        feeds_dbm = get_feed_db_from_main_db_name(dbm.database_name)
+        survey_responses = dbm.load_all_rows_in_view('survey_response_by_datasender', key=entity_uid,
+                                                     include_docs=False)
+        for survey_response in survey_responses:
+            enriched_survey_response = feeds_dbm._load_document(survey_response.id, EnrichedSurveyResponseDocument)
+            if enriched_survey_response is not None:
+                update_submission_search_index(enriched_survey_response, feeds_dbm, refresh_index=False)
+
+
+def update_submission_search_for_subjects(entity_doc, dbm):
+    entity_short_code = entity_doc.short_code
+    entity_type = entity_doc.entity_type
+    projects = dbm.load_all_rows_in_view('projects_by_subject_type', key=entity_type[0], include_docs=True)
+    form_model_ids = []
+    for project in projects:
+        form_model_ids.append(project.doc['qid'])
+
+    es = elasticutils.S().es(urls=ELASTIC_SEARCH_URL).indexes(dbm.database_name)
+    feeds_dbm = get_feed_db_from_main_db_name(dbm.database_name)
+
+    for id in form_model_ids:
+        query = es.doctypes(id)
+        query_all = query[:query.count()]
+        filtered_query = query_all.filter(entity_short_code=entity_short_code)
+        for survey_response in filtered_query.all():
+            enriched_survey_response = feeds_dbm._load_document(survey_response._id, EnrichedSurveyResponseDocument)
+            if enriched_survey_response is not None:
+                update_submission_search_index(enriched_survey_response, feeds_dbm, refresh_index=False)
 
 
 def update_submission_search_index(feed_submission_doc, feed_dbm, refresh_index=True):
@@ -37,7 +64,6 @@ def update_submission_search_index(feed_submission_doc, feed_dbm, refresh_index=
     search_dict = _meta_fields(feed_submission_doc, submission_doc, dbm)
     _update_with_form_model_fields(dbm, feed_submission_doc, search_dict, form_model)
     es.index(dbm.database_name, form_model.id, search_dict, id=feed_submission_doc.id, refresh=refresh_index)
-
 
 
 def _metadata_mapping(dbm):
@@ -61,12 +87,13 @@ def _meta_fields(feed_submission_doc, submission_doc, dbm):
 
 
 def lookup_entity_name(dbm, id, entity_type):
-    try :
+    try:
         if id:
             return get_by_short_code_include_voided(dbm, id, entity_type).value("name")
     except DataObjectNotFound:
         pass
     return id or "NA"
+
 
 def _update_with_form_model_fields(dbm, submission_doc, search_dict, form_model):
     for key in submission_doc.values:
@@ -75,7 +102,7 @@ def _update_with_form_model_fields(dbm, submission_doc, search_dict, form_model)
 
         if entity_fields:
             id = field if submission_doc.status == 'error' else field.get("answer").get("id")
-            value = field.get("answer").get("name")if isinstance(field, dict) else lookup_entity_name(dbm, id, form_model.entity_type)
+            value = lookup_entity_name(dbm, id, form_model.entity_type)
             search_dict.update({"entity_short_code": id})
         else:
             value = field.get("answer") if isinstance(field, dict) else field
