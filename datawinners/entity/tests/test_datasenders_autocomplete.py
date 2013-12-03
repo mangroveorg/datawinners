b9cab29bb656778b9fe7072e1870e21da1f43492
Dileep Bapat <dileepbapat@gmail.com>
2013-12-03 16:55:53 +0530

    Pooja,Dileep | #2705 | adding login in test

--- datawinners/entity/tests/test_datasenders_autocomplete.py
+++ datawinners/entity/tests/test_datasenders_autocomplete.py
@@ -5,9 +5,11 @@ from django.test import TestCase, Client
 class TestAllDataSenderAutoCompleteView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.client.login(username='tester150411@gmail.com', password='tester150411')
+
 
     def test_query_data_sender(self):
-        response = self.client.get('/entity/datasenders/autocomplete/')
+        response = self.client.get('/entity/datasenders/autocomplete/?term=a')
         response = json.loads(response.content)
         self.assertGreaterEqual(len(response), 0)
         print response
