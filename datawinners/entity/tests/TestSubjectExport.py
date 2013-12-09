2ab4c209a9b32de9fc2473bb17b92d5e7a3cc897
ashwinkonale <ashwin.konale@gmail.com>
2013-12-09 15:13:06 +0530

    Ashwin/Ajay | #2744 | fixing export of subject

--- datawinners/entity/tests/TestSubjectExport.py
+++ datawinners/entity/tests/TestSubjectExport.py
@@ -17,13 +17,20 @@ class TestSubjectExport(TestCase):
 
     def test_export(self):
         resp = self.client.post('/entity/subject/export/',
-                                {'subject_type': 'clinic','query_text':self.mobile_number})
+                                {'subject_type': 'clinic', 'query_text': self.mobile_number})
         xlfile_fd, xlfile_name = tempfile.mkstemp(".xls")
         os.write(xlfile_fd, resp.content)
         os.close(xlfile_fd)
         workbook = xlrd.open_workbook(xlfile_name)
         print "file is %s" % xlfile_name
         sheet = workbook.sheet_by_index(0)
+        self.assertEqual(
+            [u"What is the clinic's first name?\nAnswer must be a word\n\n",
+             u"What is the clinic's last name?\nAnswer must be a word\n\n",
+             u"What is the clinic's location?\nEnter name of the location.\n\nExample: Nairobi",
+             u"What is the clinic's GPS co-ordinates?\nAnswer must be GPS co-ordinates in the following format: xx.xxxx,yy.yyyy.\n\nExample: -18.1324,27.6547",
+             u"What is the clinic's mobile telephone number?\nAnswer must be country code plus telephone number\n\nExample: 261333745269"],
+            sheet.row_values(0, 0, 5))
         self.assertEqual([u'firstname', u'lastname', u'location', u'3.0, 3.0', self.mobile_number],
                          sheet.row_values(1, 0, 5))
 
