e043a2a2ad1a1426c329761ad06d03cfedb43634
PoojaS <poojas2406@gmail.com>
2013-12-10 17:24:41 +0530

    Pooja | #2705 | Adding view tests for datawinners autocomplete

--- datawinners/entity/tests/test_datasenders_autocomplete.py
+++ datawinners/entity/tests/test_datasenders_autocomplete.py
@@ -8,8 +8,14 @@ class TestAllDataSenderAutoCompleteView(TestCase):
         self.client.login(username='tester150411@gmail.com', password='tester150411')
 
 
-    def test_query_data_sender(self):
-        response = self.client.get('/entity/datasenders/autocomplete/?term=a')
+    def test_query_data_sender_by_datasender_name(self):
+        datasender_name = "Tester Pune"
+        response = self.client.get('/entity/datasenders/autocomplete/?term=' + datasender_name)
         response = json.loads(response.content)
-        self.assertGreaterEqual(len(response), 0)
-        print response
+        self.assertEquals(response[0].get('label'), datasender_name)
+
+    def test_query_data_sender_by_datasender_id(self):
+        repid = "rep276"
+        response = self.client.get('/entity/datasenders/autocomplete/?term=' + repid)
+        response = json.loads(response.content)
+        self.assertEquals(response[0].get('id'), repid)
