5ee0702cf3b302db4958620f590ed64326d0a206
Deepthi-Chand <deepthia@thoughtworks.com>
2013-12-11 11:39:09 +0530

    DC/Ashwin | Adding test for subject autocomplete

--- datawinners/entity/tests/test_autocomplete.py
+++ datawinners/entity/tests/test_autocomplete.py
@@ -2,11 +2,10 @@ import json
 from django.test import TestCase, Client
 
 
-class TestAllDataSenderAutoCompleteView(TestCase):
+class TestAutoCompleteView(TestCase):
     def setUp(self):
         self.client = Client()
         self.client.login(username='tester150411@gmail.com', password='tester150411')
-
 
     def test_query_data_sender_by_datasender_name(self):
         datasender_name = "Tester Pune"
@@ -19,3 +18,17 @@ class TestAllDataSenderAutoCompleteView(TestCase):
         response = self.client.get('/entity/datasenders/autocomplete/?term=' + repid)
         response = json.loads(response.content)
         self.assertEquals(response[0].get('id'), repid)
+
+    def test_query_subject_by_name(self):
+        subject_name = "test"
+        response = self.client.get('/entity/Clinic/autocomplete/?term=' + subject_name)
+        response = json.loads(response.content)
+        self.assertGreaterEqual(len(response), 0)
+        print response
+
+    def test_query_subject_by_id(self):
+        subject_id = "cid001"
+        response = self.client.get('/entity/Clinic/autocomplete/?term=' + subject_id)
+        response = json.loads(response.content)
+        self.assertGreaterEqual(len(response), 0)
+        print response
