125a67bef12acb2638b3ce1c3e7aab97d05957d2
ajay <ajay_886@hotmail.com>
2013-12-16 18:02:39 +0530

    Ajay | #2688 | Refactoring - Removed unused code in analysis js file

--- datawinners/media/javascript/project/submission_analysis_index.js
+++ datawinners/media/javascript/project/submission_analysis_index.js
@@ -14,15 +14,6 @@ $(document).ready(function () {
         new DW.SubmissionLogTable(submission_table_options);
     };
 
-    function _activate_tab(submissionTabs) {
-
-        $('#search_box .dataTables_filter').remove();
-        $('.submission_table').dataTable().fnDestroy();
-        $('.submission_table').empty();
-        DW.loading();
-        _initTable(submissionTabs);
-    };
-
     var _initialize_filters = function(){
         new DW.FilterSubmissionTableByDate().init();
         new DW.FilterSubmissionTableByDataSender().init();
@@ -33,27 +24,6 @@ $(document).ready(function () {
     _initTable(submissionTabs);
     _initialize_filters();
     $('#page_hint_section').text($('#page_hint').find('>div:first').text())
-
-//    $("#tabs").tabs().find('>ul>li>a[href$=tab_template]').click(function () {
-//        var tab_index = $(this).parent().index();
-//
-//        if (submissionTabs.getActiveTabIndex() === tab_index) {
-//            return;
-//        }
-//        submissionTabs.setActiveTabIndex(tab_index);
-//        _activate_tab(submissionTabs);
-//        return true;
-//    });
-
-//    var all_tabs = $("#tabs").tabs().find('>ul>li>a[href$=tab_template]');
-//    for (var i = 0; i < all_tabs.length; i++) {
-//        if (i == submissionTabs.getActiveTabIndex()) {
-//            $($(all_tabs[i]).parent()).addClass('ui-tabs-selected ui-state-active')
-//        } else {
-//            $($(all_tabs[i]).parent()).removeClass('ui-tabs-selected ui-state-active')
-//        }
-//
-//    };
 
     $(".ui-corner-all").removeClass("ui-corner-all");
     $(".ui-corner-top").removeClass("ui-corner-top");
