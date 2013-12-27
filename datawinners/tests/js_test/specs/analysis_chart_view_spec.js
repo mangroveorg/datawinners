158c96e2bfdbf7a4da729fbd686535d64b678c30
Dileep Bapat <dileepbapat@gmail.com>
2013-12-27 13:41:23 +0530

    Melanie,Dileep | 2689 | Adding chart view js tests

---
+++ datawinners/tests/js_test/specs/analysis_chart_view_spec.js
@@ -0,0 +1,27 @@
+describe("dwtable test", function () {
+    beforeEach(function () {
+        $("#data_sender_filter").autocomplete();
+        spyOn(jQuery, "ajax").andCallFake(function (params) {
+            return params.success(
+                {"total": 1, "result": {"mcq1": {"field_type": "select1", "data": [
+                    {"count": 1, "term": "q"},
+                    {"count": 0, "term": "a"}
+                ]}, "Question 3": {"field_type": "select", "data": [
+                    {"count": 1, "term": "a"},
+                    {"count": 0, "term": "b"}
+                ]}}}
+            );
+        });
+        var generator = new DW.SubmissionAnalysisChartGenerator();
+        generator.generateCharts();
+    });
+    it('should show one bar chart and one pie chart', function () {
+        expect($(".pieDiv").filter(":visible").length).toEqual(1);
+        expect($(".barDiv").filter(":visible").length).toEqual(1);
+    });
+    it('test selecting bar chart option', function () {
+        $("#bar-li-0 a").trigger("click");
+        expect($(".pieDiv").filter(":visible").length).toEqual(0);
+        expect($(".barDiv").filter(":visible").length).toEqual(2);
+    });
+});
