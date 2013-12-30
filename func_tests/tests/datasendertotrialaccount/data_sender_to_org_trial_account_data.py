dfc95c8ebbd63f3a14bca074ae0e7e0840ab2037
PoojaS <poojas2406@gmail.com>
2013-12-30 13:59:48 +0530

    Melanie/Pooja | #2629 | Fixing functional tests

--- func_tests/tests/datasendertotrialaccount/data_sender_to_org_trial_account_data.py
+++ func_tests/tests/datasendertotrialaccount/data_sender_to_org_trial_account_data.py
@@ -12,7 +12,7 @@ SMS = "sms"
 SUCCESS_MESSAGE = "message"
 
 VALID_DATA = {SENDER: "1234567890",
-              RECEIVER: '17752374679',
+              RECEIVER: '447860034166',
               SMS: "cli051 .EID cid003 .NA Mr. Tessy .FA 38 .RD 17.01.2012 .BG b .SY ade .GPS 27.178057 -78.007789 .RM a",
               SUCCESS_MESSAGE: "Thank you Shweta. We received : EID: cid003 NA: Mr. Tessy FA: 58.0 RD: 17.05.2011 BG: O- SY: Rapid weight loss,Memory loss,Neurological disorders GPS: 27.178057,-78.007789"}
 
