f89281f3664f8640c818d4f3ab0842cf4790054b
Deepthi-Chand <deepthia@thoughtworks.com>
2014-01-02 16:20:18 +0530

    Ashwin/DC| #2784 | Handling the exception

--- datawinners/tasks.py
+++ datawinners/tasks.py
@@ -1,5 +1,7 @@
+import logging
 from celery import Celery
 from celery.task import current
+from datawinners.feeds.migrate import ProjectNotFoundException
 from datawinners.main.database import get_db_manager
 
 from mangrove.transport.repository.survey_responses import survey_responses_by_form_code
@@ -7,16 +9,21 @@ from mangrove.transport.repository.survey_responses import survey_responses_by_f
 app = Celery('tasks', backend='amqp', broker='amqp://guest@127.0.0.1:5672//')
 
 
-@app.task(max_retries=5)
+@app.task(max_retries=3, throw=False)
 def update_associated_submissions(update_dict):
     try:
-        manager = get_db_manager(update_dict['database_name'])
-        survey_responses = survey_responses_by_form_code(manager, update_dict['old_form_code'])
-        documents = []
-        for survey_response in survey_responses:
-            survey_response.form_code = update_dict['new_form_code']
-            survey_response.form_model_revision = update_dict["new_revision"]
-            documents.append(survey_response._doc)
-        manager._save_documents(documents)
+        try:
+            manager = get_db_manager(update_dict['database_name'])
+            survey_responses = survey_responses_by_form_code(manager, update_dict['old_form_code'])
+            documents = []
+            for survey_response in survey_responses:
+                survey_response.form_code = update_dict['new_form_code']
+                survey_response.form_model_revision = update_dict["new_revision"]
+                documents.append(survey_response._doc)
+            manager._save_documents(documents)
+        except Exception as e:
+            current.retry(exc=e)
     except Exception as e:
-        current.retry(exc = e)
+        logger = logging.getLogger('datawinners')
+        logger.exception('Failed for arguments: '+str(update_dict))
+        logger.exception(e)
