61ce16426e52e389556933c6a3acfd1e2c423b97
ashwinkonale <ashwin.konale@gmail.com>
2014-01-02 10:50:07 +0530

    Ashwin/DC| #2784 | Adding retry to asyntask

--- datawinners/tasks.py
+++ datawinners/tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.task import current
 from datawinners.main.database import get_db_manager
 
 from mangrove.transport.repository.survey_responses import survey_responses_by_form_code
@@ -6,13 +7,16 @@ from mangrove.transport.repository.survey_responses import survey_responses_by_f
 app = Celery('tasks', backend='amqp', broker='amqp://guest@127.0.0.1:5672//')
 
 
-@app.task
+@app.task(max_retries=5)
 def update_associated_submissions(update_dict):
-    manager = get_db_manager(update_dict['database_name'])
-    survey_responses = survey_responses_by_form_code(manager, update_dict['old_form_code'])
-    documents = []
-    for survey_response in survey_responses:
-        survey_response.form_code = update_dict['new_form_code']
-        survey_response.form_model_revision = update_dict["new_revision"]
-        documents.append(survey_response._doc)
-    manager._save_documents(documents)
+    try:
+        manager = get_db_manager(update_dict['database_name'])
+        survey_responses = survey_responses_by_form_code(manager, update_dict['old_form_code'])
+        documents = []
+        for survey_response in survey_responses:
+            survey_response.form_code = update_dict['new_form_code']
+            survey_response.form_model_revision = update_dict["new_revision"]
+            documents.append(survey_response._doc)
+        manager._save_documents(documents)
+    except Exception as e:
+        current.retry(exc = e)
