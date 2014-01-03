c7202abaa7baf4d6d2f28a85b48eae268f6c44b4
Deepthi-Chand <deepthia@thoughtworks.com>
2014-01-03 10:59:52 +0530

    DC/Ashwin | #2784 | Enabling Worker restart from flower

--- datawinners/tasks.py
+++ datawinners/tasks.py
@@ -1,3 +1,8 @@
 from celery import Celery
-
+from celery.app.control import Inspect
 app = Celery('tasks', backend='amqp', broker='amqp://guest@127.0.0.1:5672//', include=['datawinners.project.wizard_view'])
+
+app.conf.update(
+    CELERYD_POOL_RESTARTS=True,
+    CELERYD_LOG_COLOR=True,
+)
