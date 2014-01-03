a8f832b5893c1c6cc6f7577df860594b1fb26b1d
ajay <ajay_886@hotmail.com>
2014-01-03 16:12:14 +0530

    Ajay | #0000 | Flushing MemCache before recreating_db

--- datawinners/main/management/commands/recreatedb.py
+++ datawinners/main/management/commands/recreatedb.py
@@ -5,11 +5,13 @@ from datawinners.main.initial_couch_fixtures import load_data
 from datawinners.main.database import get_db_manager
 from datawinners.main.management.sync_changed_views import SyncOnlyChangedViews
 from datawinners.main.management.commands.utils import document_stores_to_process
+from mangrove.datastore.cache_manager import get_cache_manager
 from mangrove.datastore.database import _delete_db_and_remove_db_manager
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        get_cache_manager().flush_all()
         for database_name in all_db_names():
             print ("Database %s") % (database_name,)
             print 'Deleting...'
