b8047c6e850737ea2a891759593084f942a1a96c
ashwinkonale <ashwin.konale@gmail.com>
2014-01-10 11:34:37 +0530

    Ashwin | #2868 | Changes to migration script

--- migration/couch/release_10/4.migration_to_change_subject_mapping.py
+++ migration/couch/release_10/4.migration_to_change_subject_mapping.py
@@ -28,22 +28,22 @@ def recreate_subject_index(db_name):
         logger.info('Starting indexing')
         dbm = get_db_manager(db_name)
         form_models = dbm.database.query(map_form_model_for_subjects)
+        es = get_elasticsearch_handle()
         for row in form_models:
             form_model = FormModel.get(dbm, row.id)
+            entity_type = form_model.entity_type[0]
+            es.delete_all(db_name, entity_type)
             create_subject_mapping(dbm, form_model)
             entity_docs = []
-            for entity_doc in get_all_entities(dbm, form_model.entity_type):
+            for entity_doc in get_all_entities(dbm, [entity_type]):
                 if entity_doc.data:
-                    subject = subject_dict(form_model.entity_type, entity_doc, dbm, form_model)
+                    subject = subject_dict(entity_type, entity_doc, dbm, form_model)
                     subject.update({'id': entity_doc.id})
                     entity_docs.append(subject)
 
-            es = get_elasticsearch_handle()
-            es.bulk_index(dbm.database_name, form_model.entity_type, entity_docs)
+            es.bulk_index(dbm.database_name, entity_type, entity_docs)
             es.refresh(dbm.database_name)
-                #entity_search_update(subject, db_name)
-
-            logger.info('Changed index for subject with codes '+str([a.get('id') for a in entity_docs]))
+            logger.info('Changed index for subject with codes ' + str([a.get('id') for a in entity_docs]))
         logger.info('Completed Indexing')
     except Exception as e:
         logger.exception(e.message)
