5189e2b4b8bc5d4f207372bf49b83b8c580baef4
ajay <ajay_886@hotmail.com>
2014-01-17 18:14:02 +0530

    Ajay/Melanie | #0000 | Fixing functional test

--- func_tests/tests/smstesterlightboxtests/sms_tester_light_box_tests.py
+++ func_tests/tests/smstesterlightboxtests/sms_tester_light_box_tests.py
@@ -1,7 +1,9 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 import unittest
+from datetime import datetime
 
 from nose.plugins.attrib import attr
+
 from framework.base_test import setup_driver, teardown_driver
 from framework.utils.common_utils import by_css
 from framework.utils.data_fetcher import fetch_, from_
@@ -10,14 +12,12 @@ from pages.projectspage.projects_page import ProjectsPage
 from testdata.test_data import DATA_WINNER_LOGIN_PAGE, url
 from tests.logintests.login_data import VALID_CREDENTIALS
 from tests.smstesterlightboxtests.sms_tester_light_box_data import EXCEED_NAME_LENGTH, RESPONSE_MESSAGE, \
-    VALID_ORDERED_SMS_DATA, SUBJECT_REGISTRATION_VIA_SMS, SMS
+    VALID_ORDERED_SMS_DATA, SUBJECT_REGISTRATION_VIA_SMS
 from tests.testsettings import UI_TEST_TIMEOUT
 from datawinners.accountmanagement.models import Organization
 from datawinners.tests.data import DEFAULT_TEST_ORG_ID
-from datetime import datetime
 
 
-@attr('suit_3')
 class TestSMSTesterLightBox(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -31,6 +31,7 @@ class TestSMSTesterLightBox(unittest.TestCase):
     @attr('functional_test')
     def test_sms_player_for_exceeding_word_length(self):
         # going on all project page
+        TestSMSTesterLightBox.driver.go_to(url("/project/"))
         all_project_page = self.global_navigation.navigate_to_view_all_project_page()
         TestSMSTesterLightBox.driver.create_screenshot("exceeding_word_length.png")
         project_overview_page = all_project_page.navigate_to_project_overview_page("clinic5 test project")
@@ -86,6 +87,8 @@ class TestSMSTesterLightBox(unittest.TestCase):
         sms_tester_page.send_sms_with(message)
         self.wait_while_loading()
         return sms_tester_page
+
+
 
     @classmethod
     def tearDownClass(cls):
