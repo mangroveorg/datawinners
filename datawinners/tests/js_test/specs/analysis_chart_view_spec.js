0959b01c8240dc7ffbfbebef1cd7092a173823bb
ajay <ajay_886@hotmail.com>
2014-01-22 11:49:02 +0530

    Ajay/Pooja | #2809 | Fixing broken JS tests

--- datawinners/tests/js_test/specs/analysis_chart_view_spec.js
+++ datawinners/tests/js_test/specs/analysis_chart_view_spec.js
@@ -1,4 +1,4 @@
-describe("dwtable test", function () {
+describe("analysis chart test", function () {
     beforeEach(function () {
         $("#data_sender_filter").autocomplete();
         spyOn(jQuery, "ajax").andCallFake(function (params) {
@@ -6,10 +6,10 @@ describe("dwtable test", function () {
                 {"total": 1, "result": {"mcq1": {"field_type": "select1", "data": [
                     {"count": 1, "term": "q"},
                     {"count": 0, "term": "a"}
-                ]}, "Question 3": {"field_type": "select", "data": [
+                ], 'count': 1}, "Question 3": {"field_type": "select", "data": [
                     {"count": 1, "term": "a"},
                     {"count": 0, "term": "b"}
-                ]}}}
+                ], 'count': 1}}}
             );
         });
         var generator = new DW.SubmissionAnalysisChartGenerator();
