f1ec18e990ec4693f5e1e87e28438c80f29ba407
melanie <melaniem@thoughtworks.com>
2014-01-24 11:16:00 +0530

    Melanie/DC | #0000 | Added missing translations

--- datawinners/project/submission/submission_import.py
+++ datawinners/project/submission/submission_import.py
@@ -52,9 +52,7 @@ class SubmissionImporter():
                                                                                         user_profile, valid_rows)
             if ignored_entries:
                 ignored_row_start = total_submissions - len(ignored_entries) + 1
-                message = gettext(
-                    "You have crossed the 1000 submissions limit for a trial account. Submissions from row %s have been ignored and were not imported." % str(
-                        ignored_row_start))
+                message = gettext("You have crossed the 1000 Submissions limit for your Basic account. Submissions from row %s have been ignored and were not imported.") % str(ignored_row_start)
             else:
                 message = gettext('%s of %s Submissions imported. Please check below for details.') % (len(saved_entries), len(q_answer_dicts))
         except InvalidFileFormatException as e:
