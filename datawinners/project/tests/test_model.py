061bd2a09b99ee9a855a566b8b645c4a82ca7831
Dileep Bapat <dileepbapat@gmail.com>
2014-01-24 12:16:26 +0530

    Ashwin,Dileep | optimizing unit test

--- datawinners/project/tests/test_model.py
+++ datawinners/project/tests/test_model.py
@@ -1,11 +1,15 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from datetime import date
+import unittest
 
-from mangrove.utils.test_utils.mangrove_test_case import MangroveTestCase
+from mangrove.datastore.cache_manager import get_cache_manager
+from mangrove.utils.test_utils.database_utils import uniq
 from mock import Mock, patch
+
+from mangrove.bootstrap import initializer
 from datawinners.main.utils import create_views
 from datawinners.project.models import Project, get_all_projects, ProjectState, get_all_project_names
-from mangrove.datastore.database import DatabaseManager
+from mangrove.datastore.database import DatabaseManager, get_db_manager, _delete_db_and_remove_db_manager
 from mangrove.datastore.datadict import DataDictType
 from mangrove.datastore.documents import attributes
 from mangrove.datastore.entity import Entity
@@ -14,47 +18,46 @@ from mangrove.form_model.form_model import FormModel, REPORTER
 from mangrove.form_model.validation import TextLengthConstraint
 
 
-class TestProjectModel(MangroveTestCase):
-    def setUp(self):
-        MangroveTestCase.setUp(self)
-        create_views(self.manager)
-        self.project1 = Project(name="Test1", goals="Testing", project_type="Survey", entity_type="Clinic",
+project1_name = uniq('Test1')
+project2_name = uniq('Test2')
+
+class TestProjectModel(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.db_name = 'mangrove-test'
+        cls.manager = get_db_manager('http://localhost:5984/', cls.db_name)
+        initializer._create_views(cls.manager)
+        create_views(cls.manager)
+        cls.project1 = Project(name=project1_name, goals="Testing", project_type="Survey", entity_type="Clinic",
             devices=['web'])
-        self.project1_id = self.project1.save(self.manager)
-        self.project2 = Project(name="Test2", goals="Testing", project_type="Survey", entity_type="Clinic",
+        cls.project1_id = cls.project1.save(cls.manager)
+        cls.project2 = Project(name=project2_name, goals="Testing", project_type="Survey", entity_type="Clinic",
             devices=['web'])
-        self.project2_id = self.project2.save(self.manager)
+        cls.project2_id = cls.project2.save(cls.manager)
 
-        self._create_form_model_for_project(self.project1)
+        cls._create_form_model_for_project(cls.project1)
 
-    def tearDown(self):
-        MangroveTestCase.tearDown(self)
+    @classmethod
+    def _create_form_model_for_project(cls, project):
+        ddtype = DataDictType(cls.manager, name='Default String Datadict Type', slug='string_default',
+            primitive_type='string')
+        question1 = TextField(name="entity_question", code="ID", label="What is associated entity", entity_question_flag=True, ddtype=ddtype)
+        question2 = TextField(name="question1_Name", code="Q1", label="What is your name",
+            defaultValue="some default value",
+            constraints=[TextLengthConstraint(5, 10)],
+            ddtype=ddtype)
+        cls.form_model = FormModel(cls.manager, name=cls.project1.name, form_code="abc",
+            fields=[question1, question2],
+            entity_type=["Clinic"], state=attributes.INACTIVE_STATE)
+        qid = cls.form_model.save()
+        project.qid = qid
+        project.save(cls.manager)
 
-    def test_get_all_projects(self):
-        projects = get_all_projects(self.manager)
-        self.assertEquals(len(projects), 2)
-
-    def test_get_all_projects_names(self):
-        projects_names = get_all_project_names(self.manager)
-        self.assertTrue("test1" in projects_names)
-        self.assertTrue("test2" in projects_names)
-
-    def test_get_one_project(self):
-        self.assertEquals(Project.load(self.manager.database, self.project1_id)['_id'], self.project1_id)
-
-    def test_should_update_project(self):
-        self.project1.update(dict(name='Test1', devices=['web', 'sms'], goals="New goals"))
-        self.project1.save(self.manager)
-        self.assertEquals(self.project1.name, 'test1')
-        self.assertEquals(self.project1.goals, 'New goals')
-        self.assertEquals(self.project1.devices, ['web', 'sms'])
-
-    def test_project_name_should_be_unique(self):
-        project = Project(name="Test2", goals="Testing", project_type="Survey", entity_type="Clinic", devices=['web'])
-        with self.assertRaises(Exception) as cm:
-            project.save(self.manager)
-        the_exception = cm.exception
-        self.assertEqual(the_exception.message, "Project with Name = 'test2' already exists.")
+    @classmethod
+    def tearDownClass(cls):
+        cls.manager = get_db_manager('http://localhost:5984/', cls.db_name)
+        _delete_db_and_remove_db_manager(cls.manager)
+        get_cache_manager().flush_all()
 
     def test_get_associated_data_senders(self):
         entity = Entity(self.manager,entity_type=["reporter"], short_code="rep1")
@@ -68,34 +71,46 @@ class TestProjectModel(MangroveTestCase):
         self.assertEquals(result[0].short_code, entity.short_code)
         self.assertEquals(result[0].id, entity.id)
 
-    def test_project_name_should_be_case_insensitively_unique(self):
-        project = Project(name="tEsT2", goals="Testing", project_type="Survey", entity_type="Clinic", devices=['web'])
+    def test_get_all_projects(self):
+        projects = get_all_projects(self.manager)
+        self.assertEquals(len(projects), 2)
+
+    def test_get_all_projects_names(self):
+        projects_names = get_all_project_names(self.manager)
+        self.assertTrue(project1_name.lower() in projects_names)
+        self.assertTrue(project2_name.lower() in projects_names)
+
+    def test_get_one_project(self):
+        self.assertEquals(Project.load(self.manager.database, self.project1_id)['_id'], self.project1_id)
+
+    def test_should_update_project(self):
+        self.project1 = Project.load(self.manager.database, self.project1_id)
+        self.project1.update(dict(name=project1_name, devices=['web', 'sms'], goals="New goals"))
+        self.project1.save(self.manager)
+        self.assertEquals(self.project1.name, project1_name.lower())
+        self.assertEquals(self.project1.goals, 'New goals')
+        self.assertEquals(self.project1.devices, ['web', 'sms'])
+
+    def test_project_name_should_be_unique(self):
+        project = Project(name=project2_name, goals="Testing", project_type="Survey", entity_type="Clinic", devices=['web'])
         with self.assertRaises(Exception) as cm:
             project.save(self.manager)
         the_exception = cm.exception
-        self.assertEqual(the_exception.message, "Project with Name = 'test2' already exists.")
+        self.assertEqual(the_exception.message, "Project with Name = '%s' already exists."%project2_name.lower())
+
+    def test_project_name_should_be_case_insensitively_unique(self):
+        project = Project(name=project2_name.upper(), goals="Testing", project_type="Survey", entity_type="Clinic", devices=['web'])
+        with self.assertRaises(Exception) as cm:
+            project.save(self.manager)
+        the_exception = cm.exception
+        self.assertEqual(the_exception.message, "Project with Name = '%s' already exists."%project2_name.lower())
 
     def test_should_check_for_unique_name_while_update_project(self):
-        self.project1.update(dict(name='Test2', devices=['web', 'sms'], goals="New goals"))
+        self.project1.update(dict(name=project2_name, devices=['web', 'sms'], goals="New goals"))
         with self.assertRaises(Exception) as cm:
             self.project1.save(self.manager)
         the_exception = cm.exception
-        self.assertEqual(the_exception.message, "Project with Name = 'test2' already exists.")
-
-    def _create_form_model_for_project(self, project):
-        ddtype = DataDictType(self.manager, name='Default String Datadict Type', slug='string_default',
-            primitive_type='string')
-        question1 = TextField(name="entity_question", code="ID", label="What is associated entity", entity_question_flag=True, ddtype=ddtype)
-        question2 = TextField(name="question1_Name", code="Q1", label="What is your name",
-            defaultValue="some default value",
-            constraints=[TextLengthConstraint(5, 10)],
-            ddtype=ddtype)
-        self.form_model = FormModel(self.manager, name=self.project1.name, form_code="abc",
-            fields=[question1, question2],
-            entity_type=["Clinic"], state=attributes.INACTIVE_STATE)
-        qid = self.form_model.save()
-        project.qid = qid
-        project.save(self.manager)
+        self.assertEqual(the_exception.message, "Project with Name = '%s' already exists."%project2_name.lower())
 
     def test_should_update_questionnaire(self):
         self.project1.name = "New Name"
@@ -189,6 +204,7 @@ class TestProjectModel(MangroveTestCase):
         self.assertIn("rep5", [ds["short_code"] for ds in data_senders])
 
     def test_should_delete_datasender_from_project(self):
+        self.project1 = Project.load(self.manager.database, self.project1_id)
         self.project1.data_senders = ['rep1', 'rep2']
         datasender_to_be_deleted = 'rep1'
         with patch("datawinners.search.datasender_index.update_datasender_index_by_id") as update_datasender_index_by_id:
@@ -197,12 +213,4 @@ class TestProjectModel(MangroveTestCase):
             self.project1 = Project.load(self.manager.database, self.project1_id)
             expected_data_senders = ['rep2']
             self.assertEqual(self.project1.data_senders, expected_data_senders)
-
-
-
-
-
-
-
-
 
