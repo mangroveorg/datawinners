45acc374644145e7625b1999e751428dc572dc26
Deepthi-Chand <deepthia@thoughtworks.com>
2014-01-27 11:09:46 +0530

    Dc/Melanie | #2915 | Removing unused views

--- datawinners/main/couchview/map_reporters_by_number_and_name.js
+++
@@ -1,7 +0,0 @@
-//function(doc) {
-//    if (doc.document_type == 'Entity') {
-//        if (doc.aggregation_paths['_type'][0] == "reporter") {
-//            emit(doc.data['mobile_number']['value'], doc.data['name']['value']);
-//        }
-//    }
-//}
