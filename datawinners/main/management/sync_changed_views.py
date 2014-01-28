b91817a4a7a15391bc09a6a7de917ebbcb315a7e
ajay <ajay_886@hotmail.com>
2014-01-28 22:26:43 +0530

    Ajay | #2915 | Sync views deletes old views

--- datawinners/main/management/sync_changed_views.py
+++ datawinners/main/management/sync_changed_views.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +24,7 @@ class SyncOnlyChangedViews():
         mangrover_views = _find_views()
         new_views = dict(datawinners_views.items() + mangrover_views.items())
         self.update_views(current_views, new_views, manager)
+        self.delete_old_views(current_views, new_views, manager)
 
     def sync_feed_views(self, manager):
         from datawinners.main.utils import find_views
@@ -35,13 +37,21 @@ class SyncOnlyChangedViews():
         view_dict = {}
         for design_doc in design_docs.rows:
             view_dict.update(design_doc.doc['views'])
+            key_group = re.search('_design/(.+)', design_doc['id'])
+            key = key_group.group(1)
+            view_dict[key]['_id'] = design_doc['doc']['_id']
+            view_dict[key]['_rev'] = design_doc['doc']['_rev']
         return view_dict
 
-        # def current_view_dictionary(self, manager):
-        #     design_docs = manager._get_design_docs()
-        #     view_dict = {}
-        #     for design_doc in design_docs:
-        #         for view in design_doc['views']:
-        #             view_dict.update({view: design_doc['views'][view]})
-        #     return view_dict
-        #
+    def delete_old_views(self, current_views, new_views, manager):
+        for current_view_name in current_views.keys():
+            if current_view_name not in new_views:
+                print 'Deleting view: %s from db: %s' % (current_view_name, manager.database.name)
+                logging.info('Deleting view: %s from db: %s', current_view_name, manager.database.name)
+                try:
+                    manager.database.delete(current_views[current_view_name])
+                except Exception as e:
+                    print 'Failed to delete view: %s from db: %s' % (current_view_name, manager.database.name)
+                    logging.error('Failed to delete view: %s from db: %s', current_view_name, manager.database.name)
+                    logging.error('Exception: %s', e)
+
