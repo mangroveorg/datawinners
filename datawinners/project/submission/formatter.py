6ea79eaca8aa70da8c5f1d59512e19037fd7d4e2
melanie <melaniem@thoughtworks.com>
2014-02-07 17:15:52 +0530

    Pooja/Melanie | #2973 | Formatting submissions correctly in case of data type changes

--- datawinners/project/submission/formatter.py
+++ datawinners/project/submission/formatter.py
@@ -29,28 +29,34 @@ class SubmissionFormatter(object):
     def _format_row(self, row):
         result = []
         for field_code in self.columns.keys():
-            if self.columns[field_code].get("type") == "date" or field_code == "date":
-                date_format = self.columns[field_code].get("format")
-                py_date_format = DateField.DATE_DICTIONARY.get(date_format) or SUBMISSION_DATE_FORMAT_FOR_SUBMISSION
-                try:
-                    col_val = ExcelDate(datetime.strptime(row[field_code], py_date_format),
-                                        date_format or "submission_date")
-                except Exception:
-                    col_val = row.get(field_code) or ""
-                result.append(col_val)
-            elif self.columns[field_code].get("type") == GEODCODE_FIELD_CODE:
-                col_val = self._split_gps(row.get(field_code))
+            try:
+                parsed_value= ""
+                if row.get(field_code):
+                    parsed_value = ', '.join(row.get(field_code)) if isinstance(row.get(field_code),list) else row.get(field_code)
+
+                if self.columns[field_code].get("type") == "date" or field_code == "date":
+                    date_format = self.columns[field_code].get("format")
+                    py_date_format = DateField.DATE_DICTIONARY.get(date_format) or SUBMISSION_DATE_FORMAT_FOR_SUBMISSION
+                    try:
+                        col_val = ExcelDate(datetime.strptime(row[field_code], py_date_format),
+                                            date_format or "submission_date")
+                    except Exception:
+                        col_val = row.get(field_code) or ""
+                    result.append(col_val)
+                elif self.columns[field_code].get("type") == GEODCODE_FIELD_CODE:
+                        col_val = self._split_gps(parsed_value)
+                        result.extend(col_val)
+                elif self.columns[field_code].get("type") == 'select':
+                    result.append(parsed_value)
+                elif self.columns[field_code].get("type") == 'integer':
+                    col_val_parsed = try_parse(float, parsed_value)
+                    result.append(col_val_parsed)
+                else:
+                    result.append(parsed_value)
+            except Exception:
+                col_val = row.get(field_code) or ""
                 result.extend(col_val)
-            elif self.columns[field_code].get("type") == 'select':
-                value = row.get(field_code)
-                col_val = ", ".join(value) if value else ""
-                result.append(col_val)
-            elif self.columns[field_code].get("type") == 'integer':
-                col_val_parsed = try_parse(float, row.get(field_code))
-                result.append(col_val_parsed)
-            else:
-                col_val = row.get(field_code)
-                result.append(col_val)
+
         return result
 
     def _split_gps(self, value):
@@ -63,7 +69,7 @@ class SubmissionFormatter(object):
         else:
             coordinates_split = value.split()
             if len(coordinates_split) == 1:
-                return [try_parse(float, value)]
+                return [try_parse(float, value),""]
             return [try_parse(float, coordinates_split[0]), try_parse(float, coordinates_split[1])]
 
 
