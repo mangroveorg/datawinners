25a91935b7861c90ce9570dda7e11d84ea5cb1be
ajay <ajay_886@hotmail.com>
2014-02-10 15:40:53 +0530

    Ajay/DC | #2713 | renamed file name

--- deployment/utility/check_nginx_recently_used.py
+++ deployment/utility/check_nginx_recently_used.py
@@ -10,4 +10,4 @@ time_stamp = match.group(1)
 time = time_stamp.split(' ')[0]
 logged_date = datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
 current_time = datetime.now()- timedelta(minutes=10)
-sys.exit(101) if logged_date < current_time else sys.exit(1)
+sys.exit(1) if logged_date < current_time else sys.exit(0)
