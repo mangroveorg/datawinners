7be3d4b5949daee43bfd3453a8c062e44400fb5c
ajay <ajay_886@hotmail.com>
2014-02-10 15:28:49 +0530

    Ajay/DC | #2713 | utility to check nginx usage

---
+++ deployment/utility/check_nginx_usage.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+import re
+from subprocess import check_output
+import sys
+
+output = check_output(["tail", "-1", "/var/log/nginx/datawinners.access.log"])
+pattern = re.compile('.*\[(.*)\].*')
+match = pattern.match(output)
+time_stamp = match.group(1)
+time = time_stamp.split(' ')[0]
+logged_date = datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
+current_time = datetime.now()- timedelta(minutes=10)
+sys.exit(101) if logged_date < current_time else sys.exit(1)
