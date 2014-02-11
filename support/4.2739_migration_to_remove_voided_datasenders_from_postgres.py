1185531a3504f9ec35ff9f0ca5ee12c3472f98ee
ajay <ajay_886@hotmail.com>
2014-02-11 16:17:28 +0530

    Ajay/DC | #2739 | Moving migration script to support folder since no occurrence in prod dump

--- support/4.2739_migration_to_remove_voided_datasenders_from_postgres.py
+++ support/4.2739_migration_to_remove_voided_datasenders_from_postgres.py
@@ -24,25 +24,28 @@ def _delete_user_entry(dbm, user_profile, logger):
     if profile_reporter_id.lower() == profile_reporter_id:
         logger.info("Not deleting user since the reporter id is already lowercase.")
     else:
-        user_profile.user.delete()
+        # user_profile.user.delete()
+        logger.info("Deleting user with id: %s", profile_reporter_id)
         organization = Organization.objects.get(org_id=user_profile.org_id)
         if organization.in_trial_mode:
-            delete_datasender_for_trial_mode(dbm, [profile_reporter_id.lower()], 'reporter')
+            logger.info("Deleting trail user with id: %s", profile_reporter_id)
+            # delete_datasender_for_trial_mode(dbm, [profile_reporter_id.lower()], 'reporter')
 
 def delete_voided_datasenders(db_name):
     logger = logging.getLogger(db_name)
     try:
+        logger.info('Starting migration')
         dbm = get_db_manager(db_name)
-        for datasender in dbm.database.query(map_voided_datasenders):
+        for row in dbm.database.query(map_voided_datasenders):
             try:
-                user_profiles = NGOUserProfile.objects.filter(reporter_id=datasender)
+                user_profiles = NGOUserProfile.objects.filter(reporter_id=row['key'])
                 user = user_profiles[0].user
                 if _is_only_datasender(user):
                     _delete_user_entry(dbm, user_profiles[0],logger)
-            except ObjectDoesNotExist:
-                logger.info("User with reporter-id: '%s' does not exist" % datasender)
+            except IndexError:
+                logger.info("User with reporter-id: '%s' does not exist" % row['key'])
     except Exception as e:
         logger.exception(e.message)
 
 
-migrate(all_db_names(), delete_voided_datasenders, version=(10, 1, 4), threads=2)
+migrate(all_db_names(), delete_voided_datasenders, version=(10, 1, 4), threads=1)
