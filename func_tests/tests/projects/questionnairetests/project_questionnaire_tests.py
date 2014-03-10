de7da416a0371931776fa4452cd67ad74cc30346
ajay <ajay_886@hotmail.com>
2014-03-10 10:03:37 +0530

    Ajay | #2978 | Added FT validation scenarios on Questionnaire Tab page

--- func_tests/tests/projects/questionnairetests/project_questionnaire_tests.py
+++ func_tests/tests/projects/questionnairetests/project_questionnaire_tests.py
@@ -7,6 +7,7 @@ import time
 
 from framework.base_test import setup_driver, teardown_driver
 from framework.utils.data_fetcher import fetch_, from_
+from pages.createquestionnairepage.create_questionnaire_page import MANDATORY_FIELD_ERROR_MESSAGE
 from pages.dashboardpage.dashboard_page import DashboardPage
 from pages.globalnavigationpage.global_navigation_page import GlobalNavigationPage
 from pages.loginpage.login_page import LoginPage
@@ -70,8 +71,96 @@ class TestProjectQuestionnaire(unittest.TestCase):
         questionnaire_tab_page = self.questionnaire_tab_page
         questionnaire_tab_page.refresh()
         self._validate_word_answer_type()
-        #self._validate_number_answer_type()
-        #self._validate_multiple_choice_type()
+        self._validate_number_answer_type()
+        self._validate_multiple_choice_type()
+
+    def _validate_multiple_choice_type(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        questionnaire_tab_page.click_add_question_link()
+        questionnaire_tab_page.set_question_title("Choice question")
+        questionnaire_tab_page.change_question_type(QUESTIONS_WITH_INVALID_ANSWER_DETAILS[3])
+        questionnaire_tab_page.click_add_question_link()
+        self._validate_errored_choice_inputs()
+        self._validate_valid_choice_inputs()
+        #cleaning up state
+        questionnaire_tab_page.delete_question(10)
+        questionnaire_tab_page.delete_question(9)
+
+    def _validate_valid_choice_inputs(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        questionnaire_tab_page.change_nth_option_of_choice(1, "choice1")
+        questionnaire_tab_page.change_nth_option_of_choice(2, "choice2")
+        questionnaire_tab_page.change_nth_option_of_choice(3, "choice3")
+        questionnaire_tab_page.click_add_question_link()
+        self.assertEqual(questionnaire_tab_page.get_existing_questions_count(), 10,
+                         "Choice question should not have validation errors")
+
+    def _validate_errored_choice_inputs(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        is_visible, message = questionnaire_tab_page.get_choice_error_message(index=1)
+        self.assertTrue(is_visible, "Choice1 error message message not displayed")
+        self.assertEqual(message, MANDATORY_FIELD_ERROR_MESSAGE, "Error message is incorrect for choice1")
+        is_visible, message = questionnaire_tab_page.get_choice_error_message(index=2)
+        self.assertTrue(is_visible, "Choice2 error message message not displayed")
+        self.assertEqual(message, MANDATORY_FIELD_ERROR_MESSAGE, "Error message is incorrect for choice2")
+        is_visible, message = questionnaire_tab_page.get_choice_error_message(index=3)
+        self.assertTrue(is_visible, "Choice3 error message message not displayed")
+        self.assertEqual(message, MANDATORY_FIELD_ERROR_MESSAGE, "Error message is incorrect for choice3")
+        questionnaire_tab_page.delete_option_for_multiple_choice_question(3)
+        questionnaire_tab_page.delete_option_for_multiple_choice_question(2)
+        questionnaire_tab_page.add_choice_option_to_selected_question()
+        is_visible, message = questionnaire_tab_page.get_choice_error_message(index=2)
+        self.assertFalse(is_visible, "Newly added question should not have error message")
+        questionnaire_tab_page.add_choice_option_to_selected_question()
+        is_visible, message = questionnaire_tab_page.get_choice_error_message(index=3)
+        self.assertFalse(is_visible, "Newly added question should not have error message")
+
+
+
+    def _validate_number_answer_type(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        questionnaire_tab_page.click_add_question_link()
+        questionnaire_tab_page.set_question_title("Some title")
+        self._validate_range_with_non_number_input()
+        self._validate_range_for_max_less_than_min_input()
+        self._validate_range_for_valid_input()
+        #cleaning up state
+        questionnaire_tab_page.delete_question(10)
+        questionnaire_tab_page.delete_question(9)
+
+    def _validate_range_for_valid_input(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        questionnaire_tab_page.set_min_range_limit(5)
+        questionnaire_tab_page.click_add_question_link()
+        is_visible, message = questionnaire_tab_page.get_min_range_error_message()
+        self.assertFalse(is_visible, "Min range error message should not displayed for valid input")
+        is_visible, message = questionnaire_tab_page.get_max_range_error_message()
+        self.assertFalse(is_visible, "Max range error message should not displayed for valid input")
+
+
+    def _validate_range_for_max_less_than_min_input(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        questionnaire_tab_page.change_number_question_limit(max_value=10, min_value=100)
+        questionnaire_tab_page.click_add_question_link()
+        is_visible, message = questionnaire_tab_page.get_max_range_error_message()
+        self.assertTrue(is_visible, "Max range error message not displayed")
+        self.assertEqual(message, "Max should be greater than min.", "Error message is incorrect")
+        is_visible, message = questionnaire_tab_page.get_min_range_error_message()
+        self.assertFalse(is_visible, "Min range error message should not displayed for valid input")
+
+
+    def _validate_range_with_non_number_input(self):
+        questionnaire_tab_page = self.questionnaire_tab_page
+        questionnaire_tab_page.change_question_type(QUESTIONS_WITH_INVALID_ANSWER_DETAILS[1])
+        questionnaire_tab_page.click_add_question_link()
+        is_visible, message = questionnaire_tab_page.get_min_range_error_message()
+        self.assertTrue(is_visible, "Min range error message not displayed")
+        self.assertEqual(message, "Please insert a valid number.", "Error message is incorrect")
+        is_visible, message = questionnaire_tab_page.get_max_range_error_message()
+        self.assertTrue(is_visible, "Max range error message not displayed")
+        self.assertEqual(message, "Please insert a valid number.", "Error message is incorrect")
+
+
 
     def _validate_word_answer_type(self):
         questionnaire_tab_page = self.questionnaire_tab_page
