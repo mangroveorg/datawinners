610be43d7441523a36da83c390accb7d109a5d2b
Dileep Bapat <dileepbapat@gmail.com>
2014-03-18 14:07:42 +0530

    DC,Dileep | 3058 | Using headless runner for  Submit sms for trial account

--- func_tests/tests/datasendertotrialaccount/data_sender_to_org_trial_account_tests.py
+++ func_tests/tests/datasendertotrialaccount/data_sender_to_org_trial_account_tests.py
@@ -3,22 +3,27 @@ from nose.plugins.attrib import attr
 
 from testdata.test_data import DATA_WINNER_SMS_TESTER_PAGE, DATA_WINNER_LOGIN_PAGE, LOGOUT
 
-from framework.base_test import BaseTest
+from framework.base_test import BaseTest, HeadlessRunnerTest
 from data_sender_to_org_trial_account_data import VALID_DATA, PROJECT_NAME, VALID_PAID_DATA, \
     RE_TRIAL_SMS_DATA, RE_PAID_SMS_DATA
-from pages.loginpage.login_page import LoginPage
+from pages.loginpage.login_page import LoginPage, login
 from pages.smstesterpage.sms_tester_page import SMSTesterPage
 from tests.logintests.login_data import TRIAL_CREDENTIALS_VALIDATES, VALID_CREDENTIALS
+from tests.submissionlogtests.submission_log_tests import send_valid_sms_with
 
 
 ''' usecase : Two organisations have datasender with same mobile number.
  This test tests that submissions made through sms reach the accounts based on the receiver end points'''
 
 
-class TestDataSenderAssociationWithTrialAccount(BaseTest):
+class TestDataSenderAssociationWithTrialAccount(HeadlessRunnerTest):
+
+    def tearDown(self):
+        self.driver.go_to(LOGOUT)
+
     @attr('functional_test')
     def test_SMS_sent_by_data_sender_registered_for_trial_and_paid_orgs_to_trial_org_is_saved_in_right_trial_org(self):
-        self.send_sms(VALID_DATA)
+        send_valid_sms_with(VALID_DATA)
         analysis_page = self.go_to_analysis_page(TRIAL_CREDENTIALS_VALIDATES)
         first_submission = analysis_page.get_all_data_on_nth_row(1)
         self.assertRegexpMatches(' '.join(first_submission), RE_TRIAL_SMS_DATA)
@@ -31,7 +36,7 @@ class TestDataSenderAssociationWithTrialAccount(BaseTest):
 
     @attr('functional_test')
     def test_SMS_sent_by_data_sender_registered_for_trial_and_paid_orgs_to_paid_org_is_saved_in_paid_org(self):
-        self.send_sms(VALID_PAID_DATA)
+        send_valid_sms_with(VALID_PAID_DATA)
         analysis_page = self.go_to_analysis_page(VALID_CREDENTIALS)
         first_submission = analysis_page.get_all_data_on_nth_row(1)
         self.assertRegexpMatches(' '.join(first_submission), RE_PAID_SMS_DATA)
@@ -41,15 +46,8 @@ class TestDataSenderAssociationWithTrialAccount(BaseTest):
         first_submission = analysis_page.get_all_data_on_nth_row(1)
         self.assertNotRegexpMatches(' '.join(first_submission), RE_PAID_SMS_DATA)
 
-    def send_sms(self, sms_content):
-        self.driver.go_to(DATA_WINNER_SMS_TESTER_PAGE)
-        sms_tester_page = SMSTesterPage(self.driver)
-        sms_tester_page.send_valid_sms_with(sms_content)
-
     def go_to_analysis_page(self, credentials):
-        self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
-        login_page = LoginPage(self.driver)
-        global_navigation = login_page.do_successful_login_with(credentials)
+        global_navigation = login(self.driver, credentials)
         all_data_page = global_navigation.navigate_to_all_data_page()
         analysis_page = all_data_page.navigate_to_data_analysis_page(PROJECT_NAME)
         analysis_page.wait_for_table_data_to_load()
