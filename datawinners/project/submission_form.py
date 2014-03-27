4028dea759536922334fc303c75deba0bbf7d10a
Dileep Bapat <dileepbapat@gmail.com>
2014-03-27 11:05:09 +0530

    fixing merge issues

--- datawinners/project/submission_form.py
+++ datawinners/project/submission_form.py
@@ -7,16 +7,16 @@ from datawinners.project.subject_question_creator import SubjectQuestionFieldCre
 
 
 class EditSubmissionForm(Form):
-    def __init__(self, manager, questionnaire_form_model, data):
+    def __init__(self, manager, project, data):
         super(EditSubmissionForm, self).__init__(data=data)
-        self.form_model = questionnaire_form_model
-        self.fields['form_code'] = CharField(widget=HiddenInput, initial=questionnaire_form_model.form_code)
+        self.form_model = project
+        self.fields['form_code'] = CharField(widget=HiddenInput, initial=project.form_code)
         choices = as_choices(project.get_data_senders(manager))
         self.fields['dsid'] = ChoiceField(label=_('I am submitting this data on behalf of'),
                                           choices=choices,
                                           help_text=_('Choose Data Sender from this list.'))
 
-        for field in questionnaire_form_model.fields:
+        for field in project.fields:
             if field.is_entity_field:
                 self.fields[field.code] = SubjectQuestionFieldCreator(manager, self.form_model).create(field)
             else:
