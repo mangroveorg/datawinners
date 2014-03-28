40b53afdcb4da07a5fa9a40ecd8b7ccde14af65f
Deepthi-Chand <deepthia@thoughtworks.com>
2014-03-28 11:42:45 +0530

    DC | #3066 | changing project_name view to exclude delete form model

--- datawinners/main/couchview/map_project_names.js
+++ datawinners/main/couchview/map_project_names.js
@@ -1,9 +1,8 @@
 function(doc) {
-    if (doc.document_type == 'FormModel' && !doc.is_registration_model) {
+    if (doc.document_type == 'FormModel' && !doc.is_registration_model && doc.form_code != 'delete') {
         if(!doc.void){
             emit(doc.name, doc._id);
         }
     }
 }
-
 
