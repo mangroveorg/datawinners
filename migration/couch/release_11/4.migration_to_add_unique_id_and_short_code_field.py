66c67f8c41e2bd5a267dbe59eebfdce9285a4265
Deepthi-Chand <deepthia@thoughtworks.com>
2014-03-28 17:31:25 +0530

    DC/Pooja | #3070 |added migration to merge project and formmodel documents

--- migration/couch/release_11/4.migration_to_add_unique_id_and_short_code_field.py
+++ migration/couch/release_11/4.migration_to_add_unique_id_and_short_code_field.py
@@ -2,10 +2,11 @@ import logging
 from datawinners.main.couchdb.utils import all_db_names
 from datawinners.main.database import get_db_manager
 from mangrove.errors.MangroveException import DataObjectAlreadyExists
+from mangrove.form_model.field import ShortCodeField
 from migration.couch.utils import migrate, mark_as_completed
-from mangrove.datastore.documents import FormModelDocument, SurveyResponseDocument
-from mangrove.transport.contract.survey_response import SurveyResponse
+from mangrove.datastore.documents import FormModelDocument
 from mangrove.form_model.form_model import FormModel
+from mangrove.form_model.form_model import SHORT_CODE_FIELD
 
 
 list_all_form_models = """
@@ -16,12 +17,24 @@ function(doc) {
 }
 """
 
-def remove_state_from_form_model(dbm):
+def add_unique_id_and_short_code_field(dbm):
     for row in dbm.database.query(list_all_form_models, include_docs=True):
         try:
             document_data = row.doc
-            document_data.pop('state')
+            if document_data.get('is_registration_model'):
+                for f in document_data.get('json_fields'):
+                    if f.get('name')=='short_code':
+                        short_code_field = ShortCodeField(f.name,f.code,f.label,f.constraints,f.defaultValue,f.instruction,f.required)
+                        break
+
             form_model = FormModel.new_from_doc(dbm, (FormModelDocument.wrap(document_data)))
+            if form_model.is_entity_registration_form():
+                for f in form_model.fields:
+                    if f.name == SHORT_CODE_FIELD:
+                        short_code_field = ShortCodeField(f.name,f.code,f.label,f.constraints,f.defaultValue,f.instruction,f.required)
+                        form_model.fields.append(short_code_field)
+                        form_model.fields.remove(f)
+                        break
             form_model.save()
         except DataObjectAlreadyExists as d:
             form_model.create_snapshot()
@@ -35,29 +48,15 @@ def remove_state_from_form_model(dbm):
             logging.error(e)
 
 
-def remove_state_from_survey_response(dbm):
-    rows = dbm.database.iterview("surveyresponse/surveyresponse", 1000, reduce=False, include_docs=True)
-    for row in rows:
-        try:
-            document_data = row.doc
-            document_data.pop('test')
-            SurveyResponse.new_from_doc(dbm, SurveyResponseDocument.wrap(document_data)).save()
-        except Exception as e:
-            logging.error('Removing state in survey response failed for database : %s, doc with id: %s', dbm.database_name,
-                          row.id)
-            logging.error(e)
-
-
-def migrate_project_to_remove_state(db_name):
+def migrate_form_model_to_add_eid_fields(db_name):
     logger = logging.getLogger(db_name)
     try:
         logger.info('Starting migration')
         dbm = get_db_manager(db_name)
-        remove_state_from_form_model(dbm)
-        remove_state_from_survey_response(dbm)
+        add_unique_id_and_short_code_field(dbm)
         mark_as_completed(db_name)
     except Exception as e:
         logger.exception(e.message)
 
 
-migrate(all_db_names(), migrate_project_to_remove_state, version=(11, 0, 2))
+migrate(all_db_names(), migrate_form_model_to_add_eid_fields, version=(11, 0, 4))
