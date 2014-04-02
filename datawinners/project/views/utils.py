3cd3c0ed95b08b817fac294532d0d8a34beeb148
Dileep Bapat <dileepbapat@gmail.com>
2014-04-02 16:46:23 +0530

    Fixing Edit submission

--- datawinners/project/views/utils.py
+++ datawinners/project/views/utils.py
@@ -49,7 +49,7 @@ def _get_subject_info(manager, project):
 
 def get_project_details_dict_for_feed(project):
     additional_feed_dictionary = {}
-    project_details = {'id': project.id, 'name': project.name, 'type': project.entity_type[0]}
+    project_details = {'id': project.id, 'name': project.name, 'type': project.entity_type[0] if len(project.entity_type) else ''}
     additional_feed_dictionary.update({'project': project_details})
     return additional_feed_dictionary
 
