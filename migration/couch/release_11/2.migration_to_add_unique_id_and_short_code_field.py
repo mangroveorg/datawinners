73228291a0d72275276b2cf25c0498ab598aa0a9
PoojaS <poojas2406@gmail.com>
2014-04-03 11:07:04 +0530

    Pooja | #3070 | Added migration to add uniqueid and short code field ,remove on behalf of question and merge project and form model

---
+++ migration/couch/release_11/2.migration_to_add_unique_id_and_short_code_field.py
@@ -0,0 +1,96 @@
+import logging
+from mangrove.form_model.validators import UniqueIdExistsValidator
+from datawinners.main.couchdb.utils import all_db_names
+from datawinners.main.database import get_db_manager
+from mangrove.form_model.field import ShortCodeField, UniqueIdField
+from migration.couch.utils import migrate, mark_as_completed
+from mangrove.datastore.documents import FormModelDocument, SurveyResponseDocument
+from mangrove.form_model.form_model import FormModel
+from mangrove.form_model.form_model import SHORT_CODE_FIELD
+
+#This migration creates a ShortCodeField for entity registrations and UniqueIdField for individual projects.It removes the on behalf of
+# question from the summary form models and the corresponding answer from its survey responses. This migration also removes 'test' field
+# from survey response which was to indicate if the project was active or in test mode
+
+list_all_form_models = """
+function(doc) {
+    if (doc.document_type == 'FormModel') {
+            emit(doc._id, null);
+    }
+}
+"""
+
+
+def add_unique_id_and_short_code_field(dbm):
+    for row in dbm.database.query(list_all_form_models, include_docs=True):
+        try:
+            document_data = row.doc
+            json_data = document_data.get('json_fields')
+            validator = None
+            short_code_field = None
+            short_code_dict = None
+            index = 0
+            if document_data.get('is_registration_model') or document_data.get("form_code") == "delete":
+                for index, f in enumerate(json_data):
+                    if f.get('name') == SHORT_CODE_FIELD:
+                        short_code_field = ShortCodeField(f.get('name'), f.get('code'), f.get('label'),
+                                                          defaultValue=f.get('defaultValue'),
+                                                          instruction=f.get('instruction'), required=f.get('required'))
+                        short_code_dict = f
+                        break
+            else:
+                for index, f in enumerate(json_data):
+                    if f.get('entity_question_flag'):
+                        start_key = [document_data.get('form_code')]
+                        end_key = [document_data.get('form_code'), {}]
+                        survey_response_rows = dbm.database.iterview("surveyresponse/surveyresponse", 1000, reduce=False, include_docs=False, startkey=start_key, endkey=end_key)
+
+                        if document_data.get('entity_type') != ['reporter']:
+                            short_code_field = UniqueIdField(document_data.get('entity_type')[0], f.get('name'),
+                                                             f.get('code'),
+                                                             f.get('label'), defaultValue=f.get('defaultValue'),
+                                                             instruction=f.get('instruction'),
+                                                             required=f.get('required'))
+                            validator = UniqueIdExistsValidator
+                            #Remove test field from survey responses
+                            for row in survey_response_rows:
+                                if row.get('value').get('test'):
+                                    row.get('value').pop('test')
+                        else:
+                            for row in survey_response_rows:
+                                row.get('value').get('values').pop(f.get('code'))
+                                if row.get('value').get('test'):
+                                    row.get('value').pop('test')
+                                survey_response = SurveyResponseDocument._wrap_row(row)
+                                dbm._save_document(survey_response)
+                        short_code_dict = f
+                        break
+                #Remove entity type from questionnaire form models.
+                if document_data.get('entity_type'):
+                    document_data.pop('entity_type')
+            if short_code_dict:
+                json_data.remove(short_code_dict)
+                form_model = FormModel.new_from_doc(dbm, (FormModelDocument.wrap(document_data)))
+                if short_code_field:
+                    form_model._form_fields.insert(index,short_code_field)
+                if validator:
+                    form_model.add_validator(validator)
+                form_model.save()
+        except Exception as e:
+            logging.error('Failed form model for database : %s, doc with id: %s', dbm.database_name,
+                          row.id)
+            logging.error(e)
+
+
+def migrate_form_model_to_add_eid_fields(db_name):
+    logger = logging.getLogger(db_name)
+    try:
+        logger.info('Starting migration')
+        dbm = get_db_manager(db_name)
+        add_unique_id_and_short_code_field(dbm)
+        mark_as_completed(db_name)
+    except Exception as e:
+        logger.exception(e.message)
+
+
+migrate(all_db_names(), migrate_form_model_to_add_eid_fields, version=(11, 0, 2))
---
+++ migration/couch/release_11/3.migration_to_merge_project_and_formmodel.py
@@ -0,0 +1,77 @@
+import logging
+from datawinners.main.couchdb.utils import all_db_names
+from datawinners.main.database import get_db_manager
+from datawinners.project.models import Project, Reminder, ReminderLog
+from migration.couch.utils import migrate, mark_as_completed
+from mangrove.datastore.documents import ProjectDocument
+from mangrove.form_model.form_model import FormModel
+
+#This migration removes state in form model, copies over project details into form model and updates
+# reminders to have form model ID rather than project id
+
+list_all_projects = """
+function(doc) {
+    if (doc.document_type == 'Project') {
+            emit(doc._id, null);
+    }
+}
+"""
+
+
+def update_reminders(dbm, project_data):
+    project_id = project_data.get('id')
+    for reminder in (Reminder.objects.filter(project_id=project_id)):
+        try:
+            form_model_id = project_data.get("qid")
+            reminder.project_id = form_model_id
+            #Bulk update?
+            reminder.save()
+            rows = dbm.view.reminder_log(startkey=project_id, endkey=project_id, include_docs=True)
+            for row in rows:
+                doc = ReminderLog.__document_class__.wrap(row['doc'])
+                doc.project_id = form_model_id
+                dbm._save_document(doc)
+        except Exception as e:
+            logging.error("Reminder save failed for database %s for project id %s" % (dbm.database_name, project_id))
+            logging.error(e)
+
+
+def merge_project_and_form_model_for(dbm):
+    for row in dbm.database.query(list_all_projects, include_docs=True):
+        try:
+            project_data = row.doc
+            form_model = FormModel.get(dbm, project_data.get("qid"))
+            form_model_doc = form_model._doc
+
+            form_model_doc['goals'] = project_data['goals']
+            form_model_doc['name'] = project_data['name']
+            form_model_doc['devices'] = project_data['devices']
+            form_model_doc['data_senders'] = project_data['data_senders']
+            form_model_doc['reminder_and_deadline'] = project_data['reminder_and_deadline']
+            form_model_doc['sender_group'] = project_data['sender_group']
+            try:
+                del form_model_doc['state']
+            except KeyError as e:
+                logging.warn(e)
+            questionnaire = Project.new_from_doc(dbm, (ProjectDocument.wrap(form_model_doc)))
+            super(Project, questionnaire).save()
+            update_reminders(dbm, project_data)
+            dbm.database.delete(row.doc)
+        except Exception as e:
+            logging.error('Merging project and form_model failed for database : %s, project_doc with id: %s',
+                          dbm.database_name, row.id)
+            logging.error(e)
+
+
+def migrate_to_merge_form_model_and_project(db_name):
+    logger = logging.getLogger(db_name)
+    try:
+        logger.info('Starting migration')
+        dbm = get_db_manager(db_name)
+        merge_project_and_form_model_for(dbm)
+        mark_as_completed(db_name)
+    except Exception as e:
+        logger.exception(e.message)
+
+
+migrate(all_db_names(), migrate_to_merge_form_model_and_project, version=(11, 0, 3))
