65218ec6589a4b20ee4eaad295dde6b08dfc24c8
PoojaS <poojas2406@gmail.com>
2014-04-04 16:58:58 +0530

    DC/Pooja | #3070 | Added exception handling for recreating submission indexes

--- support/recreate_submission_index.py
+++ support/recreate_submission_index.py
@@ -1,3 +1,4 @@
+import logging
 from django.core.management import call_command
 from datawinners.main.couchdb.utils import all_db_names
 from datawinners.search.manage_index import populate_submission_index
@@ -6,13 +7,26 @@ from datawinners.search import form_model_change_handler
 from datawinners.main.database import get_db_manager
 
 
+logging.basicConfig(filename='/var/log/datawinners/migration_release_recreate_index.log', level=logging.DEBUG,
+                        format="%(asctime)s | %(thread)d | %(levelname)s | %(name)s | %(message)s")
+
 def create_index():
     databases_to_index = all_db_names()
     for database_name in databases_to_index:
-        dbm = get_db_manager(database_name)
-        for row in dbm.load_all_rows_in_view('questionnaire'):
-            form_model_change_handler(FormModelDocument.wrap(row["value"]), dbm)
-        populate_submission_index(dbm)
+        try:
+            dbm = get_db_manager(database_name)
+            for row in dbm.load_all_rows_in_view('questionnaire'):
+                form_model_doc = FormModelDocument.wrap(row["value"])
+                form_model_change_handler(form_model_doc, dbm)
+            try:
+                populate_submission_index(dbm)
+            except Exception as e:
+                logging.error("Index update failed for database %s" % database_name)
+                logging.error(e)
+        except Exception as e:
+            logging.error(
+                "Mapping update failed for database %s for form model %s " % (database_name, form_model_doc.form_code))
+            logging.error(e)
 
 
 call_command("syncviews")
