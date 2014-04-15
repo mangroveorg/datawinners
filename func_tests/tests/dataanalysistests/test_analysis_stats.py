9f7ab5e049b51f2da75c91c225d1748f093d7400
ajay <ajay_886@hotmail.com>
2014-04-15 16:15:15 +0530

    Ajay/Pooja | #3143 | Added rest test for multiple date filters

--- func_tests/tests/dataanalysistests/test_analysis_stats.py
+++ func_tests/tests/dataanalysistests/test_analysis_stats.py
@@ -14,21 +14,37 @@ VALID_SMS_BLOOD_A = {SENDER: '1234123413', RECEIVER: '919880734937', SMS: "%s a"
 
 @attr('functional_test')
 class TestAnalysisStats(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = Client()
+        cls.client.login(username='tester150411@gmail.com', password='tester150411')
+        create_multi_choice_project(cls.client, form_code)
+        for answer in ["a 12.03.2014 01.2015", "b 10.05.2014 01.2015", "d 10.05.2015 03.2015", "a 10.03.2014 04.2015"]:
+            send_valid_sms_with({SENDER: '1234123413', RECEIVER: '919880734937', SMS: "%s %s" % (form_code, answer)})
+
     def test_get_stats(self):
-        self.client = Client()
-        self.client.login(username='tester150411@gmail.com', password='tester150411')
-
-        create_multi_choice_project(self.client, form_code)
-        for option in ["a", "b", "d", "a"]:
-            send_valid_sms_with({SENDER: '1234123413', RECEIVER: '919880734937', SMS: "%s %s" % (form_code, option)})
-
         data = {"search_filters": "{\"search_text\":\"\",\"dateQuestionFilters\":{}}"}
+
         res = self.client.post("/project/submissions/%s/analysis" % form_code, data)
+
         expected = set([(2, "A"), (1, "B"), (0, "AB"), (1, "O")])
         response = json.loads(res.content)
         actual_stat = set([(i["count"], i["term"]) for i in response['result'].get('Blood Group').get('data')])
         self.assertEquals(actual_stat, expected)
         self.assertEquals(response['result'].get('Blood Group').get('field_type'), 'select1')
+
+    def test_get_stats_with_date_filters(self):
+        data = {"search_filters": "{\"search_text\":\"\",\"dateQuestionFilters\":{\"q3\":\"10.03.2014 - 10.05.2014\", \"q4\":\"01.2015 - 02.2015\" }}"}
+
+        res = self.client.post("/project/submissions/%s/analysis" % form_code, data)
+
+        expected = set([(1, "A"), (1, "B"), (0, "AB"), (0, "O")])
+        response = json.loads(res.content)
+        actual_stat = set([(i["count"], i["term"]) for i in response['result'].get('Blood Group').get('data')])
+        self.assertEquals(actual_stat, expected)
+        self.assertEquals(response['result'].get('Blood Group').get('field_type'), 'select1')
+
 
 
 
--- func_tests/tests/testdatasetup/project.py
+++ func_tests/tests/testdatasetup/project.py
@@ -5,12 +5,14 @@ from framework.utils.common_utils import random_string
 def blood_test_project_name(code = random_string()):
     return "Blood Group Survey %s"%code
 
-blood_group_question_json = {"title":"Blood Group","code":"code","type":"select1","choices":[{"value":{"text":"A","val":"a"}},{"value":{"text":"B","val":"b"}},{"value":{"text":"AB","val":"c"}},{"value":{"text":"O","val":"d"}}], "instruction":"Choose 1 answer from the list. Example: a"}
+blood_group_question_json = {"title":"Blood Group","code":"q2","type":"select1","choices":[{"value":{"text":"A","val":"a"}},{"value":{"text":"B","val":"b"}},{"value":{"text":"AB","val":"c"}},{"value":{"text":"O","val":"d"}}], "instruction":"Choose 1 answer from the list. Example: a"}
+date_question1_json = {"title":"When did you donate","code":"q3","type":"date",'date_format': "dd.mm.yyyy", "instruction":"Enter a date"}
+date_question2_json = {"title":"When did you donate next","code":"q4","type":"date",'date_format': "mm.yyyy", "instruction":"Enter a date"}
 clinic_unique_id_question = {"title":"Clinic","code":"q2","type":"unique_id","uniqueIdType":"clinic","required":True,"display":"test","instruction":"Answer must be 20 characters maximum"}
 people_unique_id_question = {"uniqueIdType":"people", "title": "patient", "code": "code", "type": "unique_id", "required": True, "display": "patient", "instruction": "Answer must be 20 characters maximum"}
 
 def create_multi_choice_project(client, questionnaire_code=random_string()):
-    return create_project(client, questionnaire_code, [blood_group_question_json])
+    return create_project(client, questionnaire_code, [blood_group_question_json, date_question1_json, date_question2_json])
 
 def create_unique_id_project(client, questionnaire_code=random_string()):
     return create_project(client, questionnaire_code, [clinic_unique_id_question])
