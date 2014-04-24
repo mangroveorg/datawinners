4118a9e9982deb876cb76d0ec902508ae7932722
ajay <ajay_886@hotmail.com>
2014-04-24 17:21:49 +0530

    Ajay | #0000 | Fixing functional test

--- func_tests/tests/submissionlogtests/test_submission_import.py
+++ func_tests/tests/submissionlogtests/test_submission_import.py
@@ -25,7 +25,7 @@ class TestSubmissionImport(unittest.TestCase):
         self.assertEqual(
             [
                 u'I am submitting this data on behalf of\n\nIf you are sending data on behalf of someone, you can enter their Data Sender ID. Otherwise you can leave it blank.\n\nExample: rep42',
-                u'What is associat\xe9d entity?\n\nEnter the unique ID for each clinic.\nYou can find the clinic List on the My Subjects page.\n\nExample: cli01',
+                u'What is associat\xe9d entity?\n\nEnter the unique ID for each clinic.\nYou can find the clinic List on the My clinic page.\n\nExample: cli01',
                 u'Name\n\nAnswer must be a word 10 characters maximum\n\n',
                 u'Father age\n\nEnter a number between 18-100.\n\n',
                 u'Report date\n\nAnswer must be a date in the following format: day.month.year\n\nExample: 25.12.2011',
