6506457c92ae12ecbdaef08b17d2b3c7129c1a55
ajay <ajay_886@hotmail.com>
2014-04-29 14:25:10 +0530

    Ajay | #000 | Added space to datasender legend appearing on map

--- datawinners/media/javascript/mapping.js
+++ datawinners/media/javascript/mapping.js
@@ -49,7 +49,7 @@ function init_map() {
         add_layer(entity_type, image_url, geo_url + "/" + entity_type)
     }
 
-    add_layer(gettext('Datasenders'), '/media/images/pin_datasender.png', geo_url);
+    add_layer(gettext('Data Senders'), '/media/images/pin_datasender.png', geo_url);
 
     var legends = new OpenLayers.Control.LayerSwitcher({ascending: false});
     map.addControl(legends);
