455418bd0b3c0aef6a8355b274c7ef0d9b4174cf
ashwinkonale <ashwin.konale@gmail.com>
2014-04-30 16:14:05 +0530

    Ashwin/Dileep | #3301 | Adding indexOf method to Array prototype in ie.

--- datawinners/media/javascript/global.js
+++ datawinners/media/javascript/global.js
@@ -52,3 +52,17 @@ if(typeof String.prototype.trim !== 'function') {
     return this.replace(/^\s+|\s+$/g, '');
   }
 }
+
+// Add ECMA262-5 Array methods if not supported natively
+//
+if (!('indexOf' in Array.prototype)) {
+    Array.prototype.indexOf= function(find, i /*opt*/) {
+        if (i===undefined) i= 0;
+        if (i<0) i+= this.length;
+        if (i<0) i= 0;
+        for (var n= this.length; i<n; i++)
+            if (i in this && this[i]===find)
+                return i;
+        return -1;
+    };
+}
