59d4dc5cbc593f5f518124b10e8ebb2c632362a5
PoojaS <poojas2406@gmail.com>
2014-05-06 14:51:56 +0530

    DC/Pooja | #3318 | Displaying unique project names on all subjects page

--- datawinners/main/couchview/map_projects_by_subject_type.js
+++ datawinners/main/couchview/map_projects_by_subject_type.js
@@ -1,10 +1,12 @@
 function (doc) {
     if (doc.document_type == 'FormModel') {
         if (!doc.void) {
+            var unique_id_types = [];
             for (i in doc.json_fields) {
                 var field = doc.json_fields[i];
-                if (field.type == "unique_id") {
-                    emit(field.unique_id_type, doc.name);
+                if (field.type == "unique_id" && unique_id_types.indexOf(field.unique_id_type) == -1) {
+                        unique_id_types.push(field.unique_id_type);
+                        emit(field.unique_id_type, doc.name);
                 }
             }
 
