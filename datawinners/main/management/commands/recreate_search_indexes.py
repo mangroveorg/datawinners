a51a54921435c6d3156e48d695287a4408e41db0
ajay <ajay_886@hotmail.com>
2014-05-13 15:09:04 +0530

    Ajay | #000 | Removed migration.utils usage from recreate_search_index script

--- datawinners/main/management/commands/recreate_search_indexes.py
+++ datawinners/main/management/commands/recreate_search_indexes.py
@@ -4,9 +4,7 @@ from datawinners.main.database import get_db_manager
 from datawinners.search.index_utils import get_elasticsearch_handle
 from datawinners.search.manage_index import create_all_mappings, create_all_indices
 from datawinners.main.couchdb.utils import all_db_names
-from migration.couch.utils import configure_csv, mark_as_completed, should_not_skip
 
-configure_csv("/var/log/datawinners/recreate_search_index.csv")
 logging.basicConfig(filename='/var/log/datawinners/recreate_search_index.log', level=logging.DEBUG,
                         format="%(asctime)s | %(thread)d | %(levelname)s | %(name)s | %(message)s")
 
@@ -14,7 +12,7 @@ def recreate_index_for_db(database_name, es, logger):
     try:
         es.delete_index(database_name)
     except Exception as e:
-        logging.info("Could not delete index " + str(e.message))
+        logger.info("Could not delete index " + str(e.message))
     response = es.create_index(database_name, settings={"number_of_shards": 1, "number_of_replicas": 0})
     logger.info('%s search index created : %s' % (database_name, response.get('ok')))
     dbm = get_db_manager(database_name)
@@ -32,10 +30,8 @@ class Command(BaseCommand):
         else:
             databases_to_index = all_db_names()
         for database_name in databases_to_index:
-            if should_not_skip(database_name):
-                logger = logging.getLogger(database_name)
-                recreate_index_for_db(database_name, es, logger)
-                mark_as_completed(database_name)
-                logger.info('Done')
+            logger = logging.getLogger(database_name)
+            recreate_index_for_db(database_name, es, logger)
+            logger.info('Done')
 
         print 'Completed!'
