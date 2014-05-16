35596d39bd767f3405b4b3f0a34dea5acdcd20a3
PoojaS <poojas2406@gmail.com>
2014-05-16 18:01:41 +0530

    Pooja/Dileep | #3357 | Refactoring to use knockout for save button

--- datawinners/media/javascript/common/lang/language_view_model.js
+++ datawinners/media/javascript/common/lang/language_view_model.js
@@ -4,16 +4,19 @@ $(document).ready(function () {
         self.availableLanguages = languages;
         self.language = ko.observable();
         self.customizedMessages = ko.observableArray();
+        self.saveButtonText = ko.observable(gettext("Save"));
         self.language.subscribe(function () {
             $.getJSON("/languages/custom_messages", {'language': languageViewModel.language()}).success(function (data) {
+                var customized_messages = [];
                 for (var i in data) {
-                    var messageItem = DW.ko.createValidatableObservable({value:data[i].message});
+                    var messageItem = DW.ko.createValidatableObservable({value: data[i].message});
                     var customized_message_item = { "code": data[i].code, "title": data[i].title, "message": messageItem };
                     messageItem.subscribe(function () {
-                        DW.ko.mandatoryValidator(this.message,gettext("Enter reply SMS text."));
+                        DW.ko.mandatoryValidator(this.message, gettext("Enter reply SMS text."));
                     }, customized_message_item);
-                    languageViewModel.customizedMessages.push(customized_message_item);
+                    customized_messages.push(customized_message_item);
                 }
+                languageViewModel.customizedMessages(customized_messages);
             });
         }, self, 'change');
     }
@@ -21,5 +24,20 @@ $(document).ready(function () {
     window.languageViewModel = new LanguageViewModel();
     ko.applyBindings(languageViewModel);
     languageViewModel.language(current_language);
+
+    $(".save").click(function () {
+        DW.loading();
+        languageViewModel.saveButtonText(gettext("Saving..."));
+        $.post('/languages/custom_messages', {
+                'data': JSON.stringify(ko.toJS(languageViewModel))},
+            function (data) {
+                data = JSON.parse(data);
+                $(".save").text(gettext('Save'));
+                $('.success-message-box').text(data["message"]);
+                $('.success-message-box').show();
+            }
+        );
+
+    });
 });
 
