7ee3b8221c4f5d82ef714b02fc826b5c991554ae
Dileep Bapat <dileepbapat@gmail.com>
2014-06-05 18:19:51 +0530

    Ashwin,Dileep | 3481 | Fixing widget for ie

--- datawinners/media/javascript/jquery-textntags/texttag.js
+++ datawinners/media/javascript/jquery-textntags/texttag.js
@@ -37,21 +37,28 @@ $.widget("dw.TextNTags", {
         var self = this;
         var el = self.element;
         var before;
+        el.on('keydown', function(e){
+            if(e.keyCode==13) {e.preventDefault(); return false;}
+        });
+        el.on('dragstart', function(){return false;});
         el.on('focus',function () {
             before = el.html();
-        }).on('blur keyup keydown paste', function (e) {
-                if(e.keyCode==13) {e.preventDefault(); return false;}
+        }).on('blur keyup paste', function (e) {
+                if (e.keyCode == 0) return;
                 if ($(el).find('span.tags').text() != self.tags.join("").replace(/{/g, '').replace(/}/g, '')) {
                     
                     var after = $(el).html().replace("<br>","");
                     for(start=0; start<before.length && start< after.length; start++) if(before[start]!=after[start]) break;
+                    if (start > 0 && before[start-1] == '<') start--;
                     for(end=0; end<before.length && end< after.length; end++) if(before[before.length - 1 - end]!=after[after.length - 1 - end]) break;
+                    if (end<(before.length-1) && before[before.length-end] == '>') end--; // tag close element is same for span and div
                     var diff = before.substring(start, before.length-end);
                     console.log( "Diff is "+diff);
-                    diff = diff.replace(/<div[^>]*[>][^<]*[<][\/]div[>]/g, "")
-                        .replace(/[^<]*[<][\/]div[>]/, "</div>")
-                        .replace(/[<]div[^>]*[>].*/, "<div class=\"nonTags\">");
+                    diff = diff.replace(/<div[^>]*[>][^<]*[<][\/]div[>]/gi, "")
+                        .replace(/[^<]*[<][\/]div[>]/i, "</div>")
+                        .replace(/[<]div[^>]*[>].*/i, "<div class=\"nonTags\">");
                     var modified_html = before.substring(0, start) + diff + before.substring(before.length-end);
+                    console.log(modified_html);
                     el.html(modified_html);
                 }
                 before = el.html();
@@ -72,18 +79,20 @@ $.widget("dw.TextNTags", {
             $('<div class="nonTags">&nbsp;</div>').insertAfter(lastSpan)
         }
 
-        //Insert Empty span in between tags.
-        $(this.element).find('div.nonTags').each(function (i, e) {
-            if ($(e).text() == '') {
-                $(e).html('&nbsp;');
-            }
-        });
         $(this.element).find('span.tags').each(function (i, e) {
             if ($(e).next().hasClass('tags')) {
                 $('<div class="nonTags">&nbsp;</div>').insertAfter(e)
             }
         })
-        $(this.element).remove('br');
+        $(this.element).find('br').remove();
+        if ($.browser.mozilla) {
+            var text_in_div = /([<][\/][^>]*[>])([^<]+)([<][^>]*[>])?/;
+            if(text_in_div.test($("#myTextTags").html()))
+               $("#myTextTags").html($("#myTextTags").html().replace(text_in_div, "$1<div class=\"nonTags\">$2</div>$3"));
+            var text_at_start = /^(\S+)([<][^>]*[>])/;
+            if(text_at_start.test($("#myTextTags").html()))
+               $("#myTextTags").html($("#myTextTags").html().replace(text_at_start, "<div class=\"nonTags\">$1</div>$2"));
+        }
     },
 
     reset: function () {
