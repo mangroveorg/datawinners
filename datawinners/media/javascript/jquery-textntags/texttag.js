ef772b6b82afccfc9821ef95f772e19e2aafd625
ajay <ajay_886@hotmail.com>
2014-06-10 17:26:08 +0530

    Ajay | #0000 | Cleanup - Removing commented out code

--- datawinners/media/javascript/jquery-textntags/texttag.js
+++ datawinners/media/javascript/jquery-textntags/texttag.js
@@ -23,7 +23,6 @@ $.widget("dw.TextNTags", {
         var self = this;
         self.tags = plainText.match(/\{(.*?)\}/gi);
         var el = self.element;
-//        var styledText = '<div class="nonTags">' + plainText.replace(/{/g, '</div><span class="tags">').replace(/}/g, '</span><div class="nonTags">');
         var styledText = '' + plainText.replace(/{/g, '<span class="tags">').replace(/}/g, '</span>');
         el.html(styledText);
         self.handleAdjacentTags();
@@ -41,13 +40,6 @@ $.widget("dw.TextNTags", {
         var before;
         el.on('keydown', function(e){
             if(e.keyCode==13) {e.preventDefault(); return false;}
-//            if($.browser.msie && (e.keyCode==46 || e.keyCode == 8)){
-//                var range = document.selection.createRange();
-//                if (range.htmlText && range.htmlText.indexOf("SPAN")!= -1) {
-//                    e.preventDefault();
-//                    return false;
-//                }
-//            }
         });
         el.on('dragstart', function(){return false;});
         el.on('focus',function () {
@@ -60,21 +52,12 @@ $.widget("dw.TextNTags", {
                     for(start=0; start<before.length && start< after.length; start++) if(before[start]!=after[start]) break;
                     if (start > 0 && before[start-1] == '<') start--;
                     for(end=0; end<before.length && end< after.length; end++) if(before[before.length - 1 - end]!=after[after.length - 1 - end]) break;
-                    //if (end<(before.length-1) && before[before.length-end] == '>') end--; // tag close element is same for span and div
                     var diff = before.substring(start, before.length-end);
                     if (/([^<]*<span[^>]*>[^<]*<\/span>[^<]*)*/gi.exec(diff)[0] != diff) {
                         el.html(before);
                     }else {
-
-                        // <span sdfsdf> abc</span><span sdfsdf> abd</span>
-                        // <span sdfsdf> abd</span>
-                        console.log( "Diff is "+diff);
                         diff = diff.replace(/[^<]*(<span[^>]*>[^<]*<\/span>)?[^<]*/gi, "$1");
-    //                        replace(/<div[^>]*[>][^<]*[<][\/]div[>]/gi, "")
-    //                        .replace(/[^<]*[<][\/]div[>]/i, "</div>")
-    //                        .replace(/[<]div[^>]*[>].*/i, "<div class=\"nonTags\">");
                         var modified_html = before.substring(0, start) + diff + before.substring(before.length-end);
-                        console.log(modified_html);
                         el.html(modified_html);
                     }
                 }
@@ -85,36 +68,10 @@ $.widget("dw.TextNTags", {
     },
 
     handleAdjacentTags: function () {
-//        var firstSpan = $(this.element).find('span,div')[0];
-//        var lastSpan = $(this.element).find('span,div')[$(this.element).find('span,div').length - 1];
-//
-        //Insert empty span at the beginning
-//        if (!$(firstSpan).hasClass('nonTags')) {
-//            $('<div class="nonTags">&nbsp;</div>').insertBefore(firstSpan)
-//        }
-//        //Insert empty Span at the end
-//        if (!$(lastSpan).hasClass('nonTags')) {
-//            $('<div class="nonTags">&nbsp;</div>').insertAfter(lastSpan)
-//        }
-//
-//        $(this.element).find('span.tags').each(function (i, e) {
-//            if ($(e).next().hasClass('tags')) {
-//                $('<div class="nonTags">&nbsp;</div>').insertAfter(e)
-//            }
-//        })
         if(/></.test($(this.element).html())) {
             $(this.element).html($(this.element).html().replace(/></ , "> <"));
         }
         $(this.element).find('br').remove();
-//        if ($.browser.mozilla) {
-//            var text_in_div = /([<][\/][^>]*[>])([^<]+)([<][^>]*[>])?/;
-//            if(text_in_div.test($("#myTextTags").html()))
-//               $("#myTextTags").html($("#myTextTags").html().replace(text_in_div, "$1<div class=\"nonTags\">$2</div>$3"));
-//            var text_at_start = /^(\S+)([<][^>]*[>])/;
-//            if(text_at_start.test($("#myTextTags").html()))
-//               $("#myTextTags").html($("#myTextTags").html().replace(text_at_start, "<div class=\"nonTags\">$1</div>$2"));
-//        }
-//        $(this.element).find(".nonTags").attr('contenteditable', 'true');
     },
 
     reset: function () {
