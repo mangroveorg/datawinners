c9c4e69f8ada26fefa90b2f700a711d78d992d20
Dileep Bapat <dileepbapat@gmail.com>
2014-06-10 17:22:28 +0530

    Dileep | Installing phantom driver in user directory

--- datawinners/tests/js_test/phantomjs.runner.sh
+++ datawinners/tests/js_test/phantomjs.runner.sh
@@ -2,11 +2,12 @@
 
 function installPhantomjs(){
     echo "installing phantom"
-
-    cd /usr/local/share
-    sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-    sudo tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
-    sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+    mkdir -p ~/phantom
+    mkdir -p ~/bin
+    cd ~/phantom
+    wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+    tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+    ln -s ~/phantom/bin/phantomjs ~/bin/phantomjs
 
     echo "Done installing phantom"
 }
