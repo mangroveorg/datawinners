c3a75bc7e045e4ecd16126e01681ba9c6aba7246
ajay <ajay_886@hotmail.com>
2014-07-02 17:58:39 +0530

    Ajay | #000 | Fixing ut - updated url

--- datawinners/alldata/tests/test_failed_submissions.py
+++ datawinners/alldata/tests/test_failed_submissions.py
@@ -9,10 +9,10 @@ class TestFailedSubmissions(TestCase):
         self.client = Client()
 
     def  test_should_redirect_failed_submissions_view_if_not_logged_in(self):
-        response = self.client.get('/allfailedsubmissions')
+        response = self.client.get('/allfailedsubmissions/')
         self.assertEquals(response.status_code, 302)
 
     def test_should_render_failed_submissions_view_if_logged_in(self):
         self.client.login(username= 'tester150411@gmail.com', password= 'tester150411')
-        response = self.client.get('/allfailedsubmissions')
+        response = self.client.get('/allfailedsubmissions/')
         self.assertEquals(response.status_code,200)
