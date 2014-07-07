3d92804d663de3e9d99531efd02280a1e01c400b
Deepthi-Chand <deepthia@thoughtworks.com>
2014-07-07 13:43:23 +0530

    DC | #3632 | Fixing the cancel lnk redirection

--- datawinners/project/wizard_view.py
+++ datawinners/project/wizard_view.py
@@ -110,7 +110,7 @@ def create_project(request):
     ngo_admin = NGOUserProfile.objects.get(user=request.user)
     active_language = request.LANGUAGE_CODE
     if request.method == 'GET':
-        cancel_link = reverse('dashboard') if request.GET.get('prev', None) == 'dash' else reverse('index')
+        cancel_link = reverse('dashboard') if request.GET.get('prev', None) == 'dash' else reverse('alldata_index')
         return render_to_response('project/create_project.html',
                                   {'preview_links': get_preview_and_instruction_links(),
                                    'questionnaire_code': helper.generate_questionnaire_code(manager),
