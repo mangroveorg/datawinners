5a04bb63f133335f0d77b1a8e27c8178479f5cdb
ajay <ajay_886@hotmail.com>
2014-07-09 13:49:18 +0530

    Ajay/DC | #3629 | Bugfix - Tab not highlighted

--- datawinners/dashboard/views.py
+++ datawinners/dashboard/views.py
@@ -103,13 +103,13 @@ def dashboard(request):
 @valid_web_user
 def start(request):
     text_dict = {'project': _('Questionnaires'), 'datasenders': _('Data Senders'),
-                 'subjects': _('Identification Numbers'), 'alldata': _('Submissions')}
+                 'subjects': _('Identification Numbers'), 'alldata': _('Questionnaires')}
 
     title_dict = {'project': _('Questionnaires'), 'datasenders': _('Data Senders'),
-                 'subjects': _('Identification Numbers'), 'alldata': _('All Data')}
+                 'subjects': _('Identification Numbers'), 'alldata': _('Questionnaires')}
 
     tabs_dict = {'project': 'questionnaires', 'datasenders': 'data_senders',
-                 'subjects': 'subjects', 'alldata': 'all_data'}
+                 'subjects': 'subjects', 'alldata': 'questionnaires'}
     page = request.GET['page']
     page = page.split('/')
     url_tokens = [each for each in page if each != '']
