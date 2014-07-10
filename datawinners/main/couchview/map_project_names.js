e58ac9d486a9c76644b4d666ba41e0fd33f41fd0
ajay <ajay_886@hotmail.com>
2014-07-10 11:18:04 +0530

    DC/Ajay | #3459 | Story - Copy from project should not show excel questionnaires

--- datawinners/main/couchview/map_project_names.js
+++ datawinners/main/couchview/map_project_names.js
@@ -1,6 +1,6 @@
 function(doc) {
     if (doc.document_type == 'FormModel' && !doc.is_registration_model && doc.form_code != 'delete') {
-        if(!doc.void){
+        if(!doc.void && !doc.xform){
             emit(doc.name.toLowerCase(), {"id":doc._id, "name":doc.name});
         }
     }
