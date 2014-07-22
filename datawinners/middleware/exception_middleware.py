58ebd2cd7786900eb307b4b499e79c69450f7eab
ajay <ajay_886@hotmail.com>
2014-07-22 16:44:01 +0530

    Ajay | #0000 | Reordering info in exception middleware to handle when db manager is stale

--- datawinners/middleware/exception_middleware.py
+++ datawinners/middleware/exception_middleware.py
@@ -8,10 +8,10 @@ class ExceptionMiddleware(object):
 
     def process_exception(self, request, exception):
         try:
+            request.META['user_email_id'] = request.user.username
             profile = request.user.get_profile()
             organization = Organization.objects.get(org_id=profile.org_id)
             request.META['organization_details'] = ('%s (%s)' % (organization.name, profile.org_id))
-            request.META['user_email_id'] = request.user.username
         except Exception as ex:
             self.logger.error("error while getting meta info to be provided via email", ex)
         return None
