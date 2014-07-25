1e68e84c7c2b0aa8fa51ee329ea703a7d351f6d7
PoojaS <poojas2406@gmail.com>
2014-07-25 11:22:39 +0530

    Pooja | #3650 | Fixing test

--- datawinners/messageprovider/tests/test_error_translation_process.py
+++ datawinners/messageprovider/tests/test_error_translation_process.py
@@ -16,9 +16,9 @@ class TestErrorTranslationProcess(unittest.TestCase):
         expected = OrderedDict([('en1', u'Answer number for question q1 is of the wrong type.'),
                                 ('en2', u'Answer longword for question q2 is longer than allowed.'),
                                 ('en3', u'Answer 300 for question q3 is greater than allowed.| |'),
-                                ('fr1', u'Le type de la r\xe9ponse number pour la question "q1" est erron\xe9.'),
+                                ('fr1', u'Le type de r\xe9ponse number pour la question q1 est erron\xe9.'),
                                 ('fr2', u'La r\xe9ponse longword \xe0 la question q2 est plus longue que la limite autoris\xe9e.'),
-                                ('fr3', u'La r\xe9ponse 300 pour la question "q3" est sup\xe9rieure \xe0 ce qui est permise.')])
+                                ('fr3', u'La r\xe9ponse 300 pour la question q3 est sup\xe9rieure \xe0 ce qui est permise.')])
         translated = TranslationProcessor(self.form_model, None).process()
         self.assertEqual(expected, translated)
   
