2976ee75e9f046585fd28ccce23d8d52af669351
ajay <ajay_886@hotmail.com>
2014-08-04 11:36:44 +0530

    Ajay | #0000 | Changed logger for exception middleware

--- datawinners/middleware/exception_middleware.py
+++ datawinners/middleware/exception_middleware.py
@@ -4,7 +4,7 @@ from datawinners.accountmanagement.models import Organization
 
 class ExceptionMiddleware(object):
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger('datawinners')
 
     def process_exception(self, request, exception):
         try:
@@ -13,6 +13,7 @@ class ExceptionMiddleware(object):
             organization = Organization.objects.get(org_id=profile.org_id)
             request.META['organization_details'] = ('%s (%s)' % (organization.name, profile.org_id))
         except Exception as ex:
+            self.logger.error("Failed to get customer data for: %s", request.user.username)
             self.logger.error("error while getting meta info to be provided via email", ex)
         return None
 
