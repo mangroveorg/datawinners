c83f35da2937a7ea36e83492da267e899d6bec23
Deepthi-Chand <deepthia@thoughtworks.com>
2014-08-08 14:03:21 +0530

    Dc/Melanie | #3746 | Adding bootstrap drop down class to support apperance minimal

--- datawinners/media/javascript/common/form_edited_warning_dialog.js
+++ datawinners/media/javascript/common/form_edited_warning_dialog.js
@@ -66,7 +66,7 @@ DW.CancelWarningDialog = function (options) {
     };
 
     this.initializeLinkBindings = function () {
-        var default_ignore_links = ".add_link, .preview-navigation a, .sms_tester, .delete_project, #dw_help_link";
+        var default_ignore_links = ".add_link, .preview-navigation a, .sms_tester, .delete_project, #dw_help_link, .option-wrapper";
         var ignore_links = options.ignore_links ? default_ignore_links + "," + options.ignore_links : default_ignore_links;
 
         $("a[href]:visible, a#back_to_create_options, a#cancel_questionnaire").not(ignore_links).bind('click', {self: this}, function (event) {
