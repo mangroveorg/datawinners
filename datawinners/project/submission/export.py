f28b02a15a47105095dce124e77a7cbedd7f1162
ajay <ajay_886@hotmail.com>
2014-08-15 19:55:04 +0530

    Ajay | #3606 | Export of more than 256 questions to multiple sheets

--- datawinners/project/submission/export.py
+++ datawinners/project/submission/export.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+import math
+import xlwt
 from datawinners import utils
 from django.template.defaultfilters import slugify
 
@@ -7,9 +9,20 @@ def create_excel_response(headers, raw_data_list, file_name):
     response = HttpResponse(mimetype="application/vnd.ms-excel")
     response['Content-Disposition'] = 'attachment; filename="%s.xls"' % (slugify(file_name),)
 
-    wb = utils.get_excel_sheet([headers] + raw_data_list, 'data_log')
-    wb.save(response)
+    wb = xlwt.Workbook()
+    data_list = [headers] + raw_data_list
+    total_column_count = len(headers)
+    number_of_sheets = math.ceil(total_column_count / 256.0)
+    sheet_number = 1
+    column_number = 1
 
+    while sheet_number <= number_of_sheets:
+        data_list_with_max_allowed_columns = [l[column_number-1: column_number+255] for l in data_list]
+        utils.workbook_add_sheet(wb, data_list_with_max_allowed_columns, 'data_log_%d' % sheet_number)
+        column_number += 256
+        sheet_number += 1
+
+    wb.save(response)
     return response
 
 def export_filename(submission_type, project_name):
