5c1e3c4bf167e6d02caa15a9e549f6b82e8c2358
PoojaS <poojas2406@gmail.com>
2014-09-09 11:39:49 +0530

    Pooja | #3700 | Adding error message translations for some more scenarios

--- datawinners/blue/utils.py
+++ datawinners/blue/utils.py
@@ -4,10 +4,15 @@ import re
 
 def transform_error_message(message):
     if "Duplicate column header" in message:
-        return get_duplicate_column_error(message)
-    if "two survey elements named" in message:
-        return get_duplicate_element_names_error_in_survey_sheet(message)
-    return parse(message) + _(" Update your XLSForm and upload again.")
+        message = get_duplicate_column_error(message)
+    elif "two survey elements named" in message:
+        message = get_duplicate_element_names_error_in_survey_sheet(message)
+    elif "List name not in choices" in message or "Unmatched begin statement" in message:
+        pattern = "\[\D*\d*\]|:\s\d*\D*"
+        message = parse(message,pattern)
+    else:
+        message = parse(message)
+    return message + _(" Update your XLSForm and upload again.")
 
 
 def get_duplicate_column_error(message):
@@ -18,12 +23,11 @@ def get_duplicate_column_error(message):
 def escape_special_characters(pattern):
     special_chars = ['[', ']', '$', '^', '&', '*', '.', '\\', '?', '(', ')']
     original_pattern = pattern
-    for char in original_pattern:
-        if char in special_chars:
+    for char in special_chars:
+        if char in original_pattern:
             char_pattern = "\\" + char
             pattern = re.sub(char_pattern, '\\' + char, pattern)
     return pattern
-
 
 def replace_param_for_string_substitution(arguments, message):
     for pattern in arguments:
@@ -32,16 +36,18 @@ def replace_param_for_string_substitution(arguments, message):
     return message
 
 
-def parse(message):
-    arguments = re.findall("\[\D*\d*\]", message)
+def parse(message, pattern=None):
+    matching_pattern = pattern or "\[([^\]\[]+)\]" #matches anything between []
+    arguments = re.findall(matching_pattern, message)
     if arguments:
         message = replace_param_for_string_substitution(arguments, message)
         return _(message) % tuple([pattern for pattern in arguments])
     return _(message)
 
+
 def get_duplicate_element_names_error_in_survey_sheet(message):
-    element_names = re.findall("\'(\w*)\'",message)
+    element_names = re.findall("\'(\w*)\'", message)
     if element_names:
-        escaped_element_names = ['\''+name+'\'' for name in element_names]
-        message = replace_param_for_string_substitution(escaped_element_names,message)
+        escaped_element_names = ['\'' + name + '\'' for name in element_names]
+        message = replace_param_for_string_substitution(escaped_element_names, message)
         return _(message) % element_names[0]
