c7a795db518dd5539913d48711ebb04d7cf8b8d6
melaniem <dwdev@dwdev.dwdev>
2014-09-18 16:57:27 +0530

    Ajay/Melanie |#3871| Exposing a new API for subjects with deleted state

--- datawinners/dataextraction/urls.py
+++ datawinners/dataextraction/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns
-from datawinners.dataextraction.views import get_for_form
+from datawinners.dataextraction.views import get_for_form, get_unique_id_for_form_code
 
 urlpatterns = patterns('',
     (r'^api/get_for_form/(?P<form_code>\w+?)/$', get_for_form),
+    (r'^api/unique-id/(?P<form_code>\w+?)/$', get_unique_id_for_form_code),
     (r'^api/get_for_form/(?P<form_code>\w+?)/(?P<start_date>[-0-9]+?)/$', get_for_form),
     (r'^api/get_for_form/(?P<form_code>\w+?)/(?P<start_date>[-0-9]+?)/(?P<end_date>[-0-9]+?)/$', get_for_form)
 
