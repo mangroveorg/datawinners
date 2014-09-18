f489857ef13b942074cfae4acb73fa2511c3fbba
Deepthi-Chand <deepthia@thoughtworks.com>
2014-09-18 12:37:49 +0530

    DC/Vishnu | #3870 | Updating migration to use existing view

--- migration/couch/release_13_1/1.migration_to_make_submissions_point_to_document-id_instead_of_form_code.py
+++ migration/couch/release_13_1/1.migration_to_make_submissions_point_to_document-id_instead_of_form_code.py
@@ -7,27 +7,21 @@ from datawinners.main.couchdb.utils import all_db_names
 from datawinners.main.database import get_db_manager
 from migration.couch.utils import migrate, mark_as_completed
 
-list_all_survey_responses = """
-function(doc) {
- if (doc.document_type == 'SurveyResponse') {
-        emit(doc.form_code, doc);
-    }
-}
-"""
-
 
 def make_survey_response_link_to_document_id(db_name):
     dbm =get_db_manager(db_name)
     logger = logging.getLogger(db_name)
-    survey_responses = dbm.database.query(list_all_survey_responses, include_docs=True)
+    survey_responses = dbm.load_all_rows_in_view("survey_response_by_survey_response_id", include_docs=True)
     survey_responses = [SurveyResponse.new_from_doc(dbm=dbm, doc=SurveyResponse.__document_class__.wrap(survey_response['value'])) for survey_response in survey_responses]
     try:
         for survey_response in survey_responses:
-            form_model = get_form_model_by_code(dbm, survey_response.form_code)
-            del survey_response._doc['form_code']
-            survey_response._doc['form_model_id'] = form_model.id
-            survey_response = SurveyResponse.new_from_doc(dbm, survey_response._doc)
-            survey_response.save()
+            if survey_response.form_code:
+                form_model = get_form_model_by_code(dbm, survey_response.form_code)
+                del survey_response._doc['form_code']
+                survey_response._doc['form_model_id'] = form_model.id
+                survey_response.save()
+            else:
+                logger.info("form_code not present in survey response:"+survey_response.id)
     except Exception as e:
         logger.error(e.message + db_name)
     mark_as_completed(db_name)
