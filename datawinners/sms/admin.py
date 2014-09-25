6e4bdf5a348573191c946c75959607c7d27b769c
PoojaS <poojas2406@gmail.com>
2014-09-25 11:15:07 +0530

    Pooja | #3582 | Minor Refactoring

--- datawinners/sms/admin.py
+++ datawinners/sms/admin.py
@@ -7,30 +7,27 @@ from datawinners.project.submission.export import create_excel_response
 from datawinners.sms.models import SMS
 
 
-def export_sms_details_to_excel(modeladmin,request,query_set):
+def export_sms_details_to_excel(modeladmin, request, query_set):
     list = []
     for sms in query_set:
-        details = []
-        details.append(sms.organization_id)
-        details.append(sms.status)
-        delivered_date_time = ExcelDate(datetime.combine(sms.delivered_at, datetime.min.time()),'dd.mm.yyyy') if sms.delivered_at else None
-        details.append(delivered_date_time)
-        details.append(sms.msg_from)
-        details.append(sms.msg_to)
-        details.append(sms.message)
-        list.append(details)
+        delivered_date_time = ExcelDate(datetime.combine(sms.delivered_at, datetime.min.time()),
+                                        'dd.mm.yyyy') if sms.delivered_at else None
+        list.append([sms.organization_id, sms.status, delivered_date_time, sms.msg_from, sms.msg_to, sms.message])
 
     headers = ['Organisation Id', 'Status', 'Delivery Date', 'Message from Number', 'Message to Number', 'Content']
     response = create_excel_response(headers, list, 'sms_list')
     return response
 
+
 class SMSAdmin(admin.ModelAdmin):
-    list_display = ('message_id', 'organization', 'status','delivered_at', 'message', 'msg_from', 'msg_to', 'msg_type', 'smsc')
-    list_filter = ['delivered_at', "status","smsc", "msg_type"]
-    search_fields = ['msg_to','organization__org_id']
+    list_display = (
+    'message_id', 'organization', 'status', 'delivered_at', 'message', 'msg_from', 'msg_to', 'msg_type', 'smsc')
+    list_filter = ['delivered_at', "status", "smsc", "msg_type"]
+    search_fields = ['msg_to', 'organization__org_id']
     actions = [export_sms_details_to_excel]
+
     class Media:
-        js=("/admin/jsi18n",)
+        js = ("/admin/jsi18n",)
 
 
-admin.site.register(SMS,SMSAdmin)
+admin.site.register(SMS, SMSAdmin)
