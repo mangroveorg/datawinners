8f704172771e054160d1f30d540ef7bf3a089bdd
Ajay Divakaran <ajay@dwdev.ajay>
2014-10-01 16:42:13 +0530

    Ajay | Added further parallelism

--- migration/couch/release_13_1/1.migration_to_make_submissions_point_to_document-id_instead_of_form_code.py
+++ migration/couch/release_13_1/1.migration_to_make_submissions_point_to_document-id_instead_of_form_code.py
@@ -1,4 +1,5 @@
 import logging
+from multiprocessing.pool import Pool
 
 from mangrove.datastore.documents import FormModelDocument
 from mangrove.form_model.form_model import FormModel
@@ -35,44 +36,58 @@ def _get_survey_responses(dbm):
     return dbm.database.iterview("survey_response_by_survey_response_id/survey_response_by_survey_response_id", 20000, include_docs=True)
 
 
+def _process_survey_response_wrapper(params):
+    _process_survey_response(*params)
+
+
+def _process_survey_response(survey_response_doc, db_name):
+    dbm = get_db_manager(db_name)
+    logger = logging.getLogger(db_name)
+
+    try:
+        survey_response = SurveyResponse.new_from_doc(dbm=dbm, doc=SurveyResponse.__document_class__.wrap(survey_response_doc['value']))
+        if 'form_code' not in survey_response._doc:
+            logger.info("form_code not present in survey response:%s" % survey_response.uuid)
+            return
+        form_models = _get_form_models(dbm, survey_response)
+        if not form_models:
+            logger.info("No Questionnaire found for survey response:%s with form_code: %s" %
+                        survey_response.uuid, survey_response._doc['form_code'])
+        elif len(form_models) > 1:
+            form_models.sort(key=lambda form_model: form_model._doc.created,reverse= True)
+            matching_form_model = _get_matching_form_model(survey_response.created, form_models)
+            if matching_form_model:
+                del survey_response._doc['form_code']
+                survey_response.form_model_id = matching_form_model.id
+                survey_response.save(process_post_update=False)
+                logger.info('.')
+            else:
+                logger.info(
+                    "No Questionnaire found with matching date for survey response: %s and form_code:%s" % survey_response.uuid,
+                    survey_response._doc['form_code'])
+        elif len(form_models) == 1:
+            del survey_response._doc['form_code']
+            survey_response.form_model_id = form_models[0].id
+            survey_response.save(process_post_update=False)
+            logger.info('.')
+
+    except Exception as e:
+            logger.exception("Exception for survey response:%s" % survey_response.uuid, e)
+
+
 def make_survey_response_link_to_form_model_document_id(db_name):
     dbm = get_db_manager(db_name)
     logger = logging.getLogger(db_name)
     survey_response_docs = _get_survey_responses(dbm)
+    p = Pool(processes=4)
     try:
         for survey_response_doc in survey_response_docs:
-            try:
-                survey_response = SurveyResponse.new_from_doc(dbm=dbm, doc=SurveyResponse.__document_class__.wrap(survey_response_doc['value']))
-                if 'form_code' not in survey_response._doc:
-                    logger.info("form_code not present in survey response:%s" % survey_response.uuid)
-                    continue
-                form_models = _get_form_models(dbm, survey_response)
-                if not form_models:
-                    logger.info("No Questionnaire found for survey response:%s with form_code: %s" %
-                                survey_response.uuid, survey_response._doc['form_code'])
-                elif len(form_models) > 1:
-                    form_models.sort(key=lambda form_model: form_model._doc.created,reverse= True)
-                    matching_form_model = _get_matching_form_model(survey_response.created, form_models)
-                    if matching_form_model:
-                        del survey_response._doc['form_code']
-                        survey_response.form_model_id = matching_form_model.id
-                        survey_response.save(process_post_update=False)
-                        logger.info('.')
-                    else:
-                        logger.info(
-                            "No Questionnaire found with matching date for survey response: %s and form_code:%s" % survey_response.uuid,
-                            survey_response._doc['form_code'])
-                elif len(form_models) == 1:
-                    del survey_response._doc['form_code']
-                    survey_response.form_model_id = form_models[0].id
-                    survey_response.save(process_post_update=False)
-                    logger.info('.')
-
-            except Exception as e:
-                logger.exception("Exception for survey response:%s" % survey_response.uuid, e)
+            p.apply(_process_survey_response, (survey_response_doc, db_name))
     except Exception as e:
         logger.error(e.message + db_name)
+    p.close()
+    p.join()
     mark_as_completed(db_name)
 
 
-migrate(all_db_names(), make_survey_response_link_to_form_model_document_id, version=(13, 1, 1), threads=3)
+migrate(all_db_names(), make_survey_response_link_to_form_model_document_id, version=(13, 1, 1), threads=2)
