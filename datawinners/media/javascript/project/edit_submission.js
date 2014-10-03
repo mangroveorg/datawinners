c7f8af8d59d4f83b4449bf3117e1e49de6fe1f28
Alia Sophie <azalia@MacBook-Pro-de-Alia.local>
2014-10-03 11:39:25 +0300

    Sophie | #3730 | UI changes and translations

--- datawinners/media/javascript/project/edit_submission.js
+++ datawinners/media/javascript/project/edit_submission.js
@@ -46,4 +46,33 @@ $(document).ready(function(){
         var element = $("." + $("#click_after_reload").val());
         element.trigger("click");
     }
+
+    var popup = $("#edit_ds_popup").dialog({
+        title: gettext("Change Data Sender"),
+        modal: true,
+        autoOpen: false,
+        height: 180,
+        width: 550
+    });
+    popup.parent().appendTo($("#edit_submission_form"));
+
+    $("#change_ds_link").unbind().bind("click", function () {
+        $("#edit_ds_popup").dialog("open");
+    });
+
+    $("#edit_ds_popup .cancel_link").bind("click", function(){
+        $("#edit_ds_popup").dialog("close");
+    });
+    $('#choice_field span select#id_dsid option:selected').addClass("margin_top_20")
+    $("#save_ds").bind("click", function(){
+        selected_ds = $('#edit_ds_popup span select#id_dsid option:selected').text();
+        selected_ds_split = selected_ds.split("  ");
+        datasender_name = selected_ds_split[0].replace(/^\w/, function($0) { return $0.toUpperCase(); });
+        datasender_id = selected_ds_split[1].replace(')','').replace('(','')
+
+        $(".datasender").text(datasender_id);
+        $(".ds_id").text(datasender_name);
+        $(".datasender_error_message").addClass("none");
+        $("#edit_ds_popup").dialog("close");
+    });
 });
