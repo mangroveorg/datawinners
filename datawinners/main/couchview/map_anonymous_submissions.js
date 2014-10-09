5156b70d23b528bb7fde803a9f26b47d602ec15d
Alia Sophie <azalia@MacBook-Pro-de-Alia.local>
2014-10-09 17:24:59 +0300

    Sophie | Updating anonymous submission view

--- datawinners/main/couchview/map_anonymous_submissions.js
+++ datawinners/main/couchview/map_anonymous_submissions.js
@@ -2,7 +2,7 @@ function(doc) {
     var isNotNull = function(o) {
         return !((o === undefined) || (o == null));
     };
-    if (doc.document_type == 'SurveyResponse' && isNotNull(doc.anonymous_submission) && isNotNull(doc.form_model_id)) {
+    if (doc.document_type == 'SurveyResponse' && isNotNull(doc.is_anonymous_submission) && isNotNull(doc.form_model_id)) {
         emit(doc.created_by, doc);
     }
 }
