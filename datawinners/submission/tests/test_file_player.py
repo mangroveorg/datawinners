98cfe7ac02a477ad243c3a0b579f86b79c8285e2
melaniem <dwdev@dwdev.dwdev>
2014-10-28 14:20:07 +0530

    Melanie |#3996| Removing commented tests

--- datawinners/submission/tests/test_file_player.py
+++ datawinners/submission/tests/test_file_player.py
@@ -27,7 +27,6 @@ UPLOAD_DATA = """
 
                                 clf12,14,205,Dr. E,CLP005
 """
-@SkipTest
 class FilePlayerTest(unittest.TestCase):
     def setUp(self):
         organization = Organization.objects.get(pk=DEFAULT_TEST_ORG_ID)
