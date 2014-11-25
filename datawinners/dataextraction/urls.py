a7284696dce206ddc1f7c93090ce5bbced3b0e7e
Alia Sophie <azalia@MacBook-Pro-de-Alia-2.local>
2014-11-25 18:39:27 +0300

    Sophie | #4072 | Failed Submissions API

--- datawinners/dataextraction/urls.py
+++ datawinners/dataextraction/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns
 from datawinners.dataextraction.unique_ids import get_unique_ids_for_form_code
 from datawinners.dataextraction.view.entity import get_entity_actions
-from datawinners.dataextraction.views import get_for_form
+from datawinners.dataextraction.views import get_for_form, get_failed_submissions
 
 urlpatterns = patterns('',
     (r'^api/get_for_form/(?P<form_code>\w+?)/$', get_for_form),
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     (r'^api/entity/actions/(?P<start_date>[-0-9]+?)/$', get_entity_actions),
     (r'^api/entity/actions/(?P<start_date>[-0-9]+?)/(?P<end_date>[-0-9]+?)/$', get_entity_actions),
     (r'^api/get_for_form/(?P<form_code>\w+?)/(?P<start_date>[-0-9]+?)/$', get_for_form),
-    (r'^api/get_for_form/(?P<form_code>\w+?)/(?P<start_date>[-0-9]+?)/(?P<end_date>[-0-9]+?)/$', get_for_form)
-
+    (r'^api/get_for_form/(?P<form_code>\w+?)/(?P<start_date>[-0-9]+?)/(?P<end_date>[-0-9]+?)/$', get_for_form),
+    (r'^api/get_failed_submissions/', get_failed_submissions)
 )
--- datawinners/dataextraction/views.py
+++ datawinners/dataextraction/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
-
 from django_digest.decorators import httpdigest
-
-from datawinners.dataextraction.helper import  encapsulate_data_for_form, convert_to_json_file_download_response, generate_filename
-
+from datawinners.dataextraction.helper import  encapsulate_data_for_form, convert_to_json_file_download_response
+from datawinners.dataextraction.helper import generate_filename, check_date_format
 from datawinners.main.database import get_database_manager
+from datawinners.submission.models import DatawinnerLog
+from datawinners.utils import get_organization
+from django.db.models import Q
+import operator
+from datetime import datetime
 
 
 @httpdigest
@@ -15,6 +18,36 @@ def get_for_form(request, form_code, start_date=None, end_date=None):
         data_for_form = encapsulate_data_for_form(dbm, form_code, start_date, end_date)
         return convert_to_json_file_download_response(data_for_form, generate_filename(form_code, start_date, end_date))
     return HttpResponse("Error. Only support GET method.")
+
+@httpdigest
+def get_failed_submissions(request):
+    organization = get_organization(request)
+    start_date = request.GET.get('start_date') if check_date_format(request.GET.get('start_date')) else None
+    end_date = request.GET.get('end_date') if check_date_format(request.GET.get('end_date')) else None
+    to_number = request.GET.get('to_number')
+    
+    criteria = [Q(organization=organization)]
+
+    if start_date is not None:
+        criteria.append(Q(created_at__gte=datetime.strptime(start_date, '%d-%m-%Y')))
+
+    if end_date is not None:
+        criteria.append(Q(created_at__lte=datetime.strptime(end_date + " 23:59:59", '%d-%m-%Y %H:%M:%S')))
+
+    if to_number is not None:
+        criteria.append(Q(to_number=to_number))
+
+    failed_submissions = list(DatawinnerLog.objects.filter(reduce(operator.and_, criteria))
+                              .extra(select={'submission_date': 'created_at'})
+                              .values('from_number', 'to_number', 'message', 'error', 'submission_date')
+                              .order_by('created_at')
+                              )
+    if len(failed_submissions):
+        failed_submissions_dict = {"message": "You can access the data in submissions field.", "submissions": failed_submissions, "success": True}
+    else:
+        failed_submissions_dict = {"message": "No failed submissions available", "submissions": [], "success": False}
+
+    return convert_to_json_file_download_response(failed_submissions_dict, "failed_submissions")
 
 
 
