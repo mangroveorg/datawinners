69f7fdce2bc8c0d5ed8e63e17b5d973b549361f5
melaniem <dwdev@dwdev.dwdev>
2014-12-02 14:38:16 +0530

    Dc/Melanie |#4119 #4116 #4120| Ui changes

--- datawinners/project/wizard_view.py
+++ datawinners/project/wizard_view.py
@@ -252,7 +252,7 @@ def _reminder_info_about_project(project):
         else:
             data['should_send_reminders_before_deadline'] = False
             data['number_of_days_before_deadline'] = 2
-            data['reminder_text_before_deadline'] = ugettext("Hello. We have not received your data yet for this month. Please send it to us within the next 2 days. Thank you.")
+            data['reminder_text_before_deadline'] = ugettext("Hello. We have not received your data yet for this period. Please send it to us within the next 2 days. Thank you.")
 
         reminder_on_deadline = Reminder.objects.filter(reminder_mode=ReminderMode.ON_DEADLINE, project_id=project.id)
         if reminder_on_deadline.count() > 0:
@@ -271,7 +271,7 @@ def _reminder_info_about_project(project):
         else:
             data['should_send_reminders_after_deadline'] = False
             data['number_of_days_after_deadline'] = 2
-            data['reminder_text_after_deadline'] = ugettext("Hello. We have not received your data yet for this month. Please send it to us by the end of today. Thank you.")
+            data['reminder_text_after_deadline'] = ugettext("Hello. We have not received your data yet for this period. Please send it to us by the end of today. Thank you.")
 
     data['whom_to_send_message'] = not deadline_information['should_send_reminder_to_all_ds']
 
