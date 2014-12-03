b7e122a8674b6ad3041cc6d168a47ed1a35585b0
Ajay Divakaran <ajay@dwdev.ajay>
2014-12-03 16:19:11 +0530

    Revert "Ajay | #0000 | Added logging to verify memory usage for export"
    This reverts commit 158a40f81c7dc4578e6aee86d058d13f644e65b7.

--- datawinners/project/submission/exporter.py
+++ datawinners/project/submission/exporter.py
@@ -1,12 +1,8 @@
-import logging
-import resource
 from datawinners.main.utils import get_database_name
 from datawinners.project.Header import SubmissionExcelHeader
 from datawinners.project.submission.export import create_excel_response, export_filename
 from datawinners.project.submission.formatter import SubmissionFormatter
 from datawinners.search.submission_query import SubmissionQuery
-
-logger = logging.getLogger("datawinners")
 
 
 class SubmissionExporter:
@@ -19,27 +15,16 @@ class SubmissionExporter:
 
     def _create_response(self, columns, submission_list, submission_type):
         header_list, formatted_values = SubmissionFormatter(columns).format_tabular_data(submission_list)
-
-        logger.error('Memory after tabulation: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-
-
         return create_excel_response(header_list, formatted_values, export_filename(submission_type, self.project_name))
 
     def create_excel_response(self, submission_type, query_params):
         columns = SubmissionExcelHeader(self.form_model, submission_type, self.language).get_columns()
 
-        logger.error('Memory before query: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
         entity_headers, paginated_query, query_with_criteria = SubmissionQuery(self.form_model,
                                                                                query_params).query_to_be_paginated(
                                                                                 self.form_model.id,
                                                                                 self.user)
-
-        logger.error('Memory after query: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-
         submission_list = query_with_criteria.values_dict()
-
-        logger.error('Memory after query dict: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-
 
         return self._create_response(columns, submission_list, submission_type)
