5a17b8934f66bc110be3c39a1b02600a53b202b4
Ajay Divakaran <ajay@dwdev.ajay>
2014-12-03 15:49:19 +0530

    Ajay | #4102 | Added logging to verify memory usage

--- datawinners/project/submission/exporter.py
+++ datawinners/project/submission/exporter.py
@@ -1,7 +1,11 @@
+import logging
+import resource
 from datawinners.project.Header import SubmissionExcelHeader
 from datawinners.project.submission.export import export_filename, create_zipped_excel_response
 from datawinners.project.submission.formatter import SubmissionFormatter
 from datawinners.project.submission.submission_search import get_scrolling_submissions_query
+
+logger = logging.getLogger("datawinners")
 
 
 class SubmissionExporter:
@@ -15,13 +19,22 @@ class SubmissionExporter:
     def _create_response(self, columns, submission_list, submission_type):
         header_list, formatted_values = SubmissionFormatter(columns, self.local_time_delta).format_tabular_data(
             submission_list)
+
+        logger.error('Memory usage after tabulation: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+
         return create_zipped_excel_response(header_list, formatted_values,
                                             export_filename(submission_type, self.project_name))
 
     def create_excel_response(self, submission_type, query_params):
         columns = SubmissionExcelHeader(self.form_model, submission_type, self.language).get_columns()
 
+        logger.error("Before es query")
+        logger.error('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+        memory_before = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         search_results, query_fields = get_scrolling_submissions_query(self.dbm, self.form_model, query_params,
                                                                        self.local_time_delta)
+        memory_after = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        logger.error("Change in memeory: %s (kb)" % (memory_after-memory_before))
+        logger.error('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
         return self._create_response(columns, search_results, submission_type)
