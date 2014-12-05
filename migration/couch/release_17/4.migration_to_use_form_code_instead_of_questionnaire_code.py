8b7c96a3a8d60b0016fe8eb0e956665a75acc2b0
melaniem <dwdev@dwdev.dwdev>
2014-12-05 11:54:30 +0530

    Melanie |#3963|Adding migrations for feed document

--- migration/couch/release_17/4.migration_to_use_form_code_instead_of_questionnaire_code.py
+++ migration/couch/release_17/4.migration_to_use_form_code_instead_of_questionnaire_code.py
@@ -1,20 +1,10 @@
 import logging
 
 from datawinners.feeds.database import get_feed_db_from_main_db_name
+from datawinners.main.couchdb.utils import all_db_names
 from datawinners.main.database import get_db_manager
 from mangrove.datastore.documents import EnrichedSurveyResponseDocument
 from migration.couch.utils import migrate, mark_as_completed
-
-db_names = [
-    "hni_psi-madagascar_qmx864597",
-    "hni_usaid-mikolo_lei526034",
-    "hni_pnncseecaline_bpn692977",
-    "hni_unite-de-lutte-contre-le-sida_nps932546",
-    "hni_msanp_ubl254585",
-    "hni_palme_flm546389",
-    "hni_ingc-zambezia_gkg847094",
-    "hni_ingc-pemba_nlt434261",
-    "hni_chemonics-mozambique_ehh942480"]
 
 def make_feed_document_use_form_code_instead_of_form_model_id(db_name):
     dbm = get_db_manager(db_name)
@@ -35,4 +25,4 @@ def make_feed_document_use_form_code_instead_of_form_model_id(db_name):
             logger.exception("failed for questionnaire:"+questionnaire.id)
     mark_as_completed(db_name)
 
-migrate(db_names, make_feed_document_use_form_code_instead_of_form_model_id, version=(13, 1, 3), threads=3)
+migrate(all_db_names(), make_feed_document_use_form_code_instead_of_form_model_id, version=(13, 1, 3), threads=3)
