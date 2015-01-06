5eeb74ba2761b920cdd442dabe55690b7081feb6
Ajay Divakaran <ajay@dwdev.ajay>
2015-01-06 12:10:59 +0530

    Ajay | #4180 | Capturing media usage per questionnaire

--- datawinners/project/media_usage/calculate_media_usage.py
+++ datawinners/project/media_usage/calculate_media_usage.py
@@ -1,13 +1,18 @@
 from datawinners.accountmanagement.models import OrganizationSetting
 from datawinners.main.database import get_db_manager
+from mangrove.form_model.project import Project
 
 
 def calculate_usage(db_name, log_file_path):
     dbm = get_db_manager(db_name)
-    rows = dbm.view.media_attachment(reduce=True)
+    rows = dbm.view.all_media_details(reduce=True)
     total_size_in_mb = rows[0][u"value"] if rows else 0
     if total_size_in_mb > 0:
         organization = OrganizationSetting.objects.get(document_store=db_name).organization
+        text = "%s \nTotal used: %.2f mb\n" % (organization.org_id, total_size_in_mb)
+        usage_per_questionnaire = dbm.view.all_media_details(reduce=True, group=True)
+        for row in usage_per_questionnaire:
+            project = Project.get(dbm, row['key'])
+            text += "%s (%s): Used: %.2f mb\n\n" % (project.name, project.id, row['value'])
         with open(log_file_path, "a") as log_file:
-            log_file.write("%s : %.2f mb" % (organization.org_id, total_size_in_mb))
-
+            log_file.write(text)
