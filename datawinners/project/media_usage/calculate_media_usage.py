25f4b87a9366109e13ac8b5b5c21087f403aa21a
Ajay Divakaran <ajay@dwdev.ajay>
2015-01-08 12:09:01 +0530

    Ajay | #4180 | Corrected typo in command name and added org name to report

--- datawinners/project/media_usage/calculate_media_usage.py
+++ datawinners/project/media_usage/calculate_media_usage.py
@@ -9,7 +9,7 @@ def calculate_usage(db_name, log_file_path):
     total_size_in_mb = rows[0][u"value"] if rows else 0
     if total_size_in_mb > 0:
         organization = OrganizationSetting.objects.get(document_store=db_name).organization
-        text = "%s \nTotal used: %.2f mb\n" % (organization.org_id, total_size_in_mb)
+        text = "%s(%s) \nTotal used: %.2f mb\n" % (organization.name, organization.org_id, total_size_in_mb)
         usage_per_questionnaire = dbm.view.all_media_details(reduce=True, group=True)
         for row in usage_per_questionnaire:
             project = Project.get(dbm, row['key'])
