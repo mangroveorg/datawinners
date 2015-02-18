aed71a7677b23ce2cea5e53f287c4bad1c51e10d
Ajay Divakaran <ajay@dwdev.ajay>
2015-02-18 17:10:34 +0530

    Melanie/Ajay | #4282 | Added migration to separate Contact and UniqueId document

---
+++ migration/couch/release_21/1.migration_to_seperate_datasenders_and_contacts.py
@@ -0,0 +1,37 @@
+import sys
+
+from datawinners.main.couchdb.utils import all_db_names
+
+
+if __name__ == "__main__" and __package__ is None:
+    sys.path.insert(0, ".")
+
+import logging
+from datawinners.main.database import get_db_manager
+from migration.couch.utils import migrate, mark_as_completed
+
+
+datasender_document = """
+function(doc) {
+    if (doc.document_type == "Entity" && doc.aggregation_paths['_type'] == "reporter") {
+        emit(doc.short_code, doc);
+    }
+}"""
+
+
+def seperate_datasender_and_contact_document(db_name):
+    logger = logging.getLogger(db_name)
+    logger.info('Starting Migration')
+
+    manager = get_db_manager(db_name)
+    for row in manager.database.query(datasender_document):
+        try:
+            row['value']['document_type'] = 'Contact'
+            manager.database.save(row['value'], process_post_update=False)
+        except Exception as e:
+            logger.error("Failed to update document with id:%s" % row['value']['_id'])
+    logger.info('Completed Migration')
+    mark_as_completed(db_name)
+
+
+migrate(all_db_names(), seperate_datasender_and_contact_document, version=(21, 0, 1))
