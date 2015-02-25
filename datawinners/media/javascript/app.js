c2f42314bf2432eebb20f113ef72529c22cff32b
Deepthi-Chand <deepthia@thoughtworks.com>
2015-02-25 15:43:02 +0530

    Dc/Melanie |#4312| Removing file preview and filename when a large file is uploaded

--- datawinners/media/javascript/app.js
+++ datawinners/media/javascript/app.js
@@ -34757,14 +34757,19 @@ define( 'enketo-widget/file/filepicker',[ 'jquery', 'enketo-js/Widget', 'file-ma
                 .catch( function( error ) {
                     $input.val( '' );
                     that._showPreview( null );
+                    that._showFileName( null );
                     that._showFeedback( error.message, 'error' );
+                    $input.trigger( 'change.passthrough' );
                 } );
         } );
     };
 
     Filepicker.prototype._showFileName = function( file ) {
-        var fileName = ( typeof file === 'object' && file.name ) ? file.name : file;
-        this.$fakeInput.text( fileName );
+        if (file){
+            var fileName = ( typeof file === 'object' && file.name ) ? file.name : file;
+            this.$fakeInput.text( fileName );
+        }
+        else {this.$fakeInput.text( '' ); }
     };
 
     Filepicker.prototype._showFeedback = function( message, status ) {
@@ -34793,10 +34798,8 @@ define( 'enketo-widget/file/filepicker',[ 'jquery', 'enketo-js/Widget', 'file-ma
                 $el = $( '<span>No preview for this mediatype</span>' );
                 break;
         }
-
+        this.$preview.empty();
         if ( url ) {
-//            Clearing preview before updating
-            this.$preview.empty();
             this.$downloadLink.empty();
             this.$preview.append( $el.attr( 'src', url ) );
         }
