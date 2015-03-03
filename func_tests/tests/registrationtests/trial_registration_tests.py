8ba6bface4dfc19da991b886c1548026ca9656e9
melaniem <dwdev@dwdev.dwdev>
2015-03-03 11:32:05 +0530

    Melanie | #0000 | Fixing Ft

--- func_tests/tests/registrationtests/trial_registration_tests.py
+++ func_tests/tests/registrationtests/trial_registration_tests.py
@@ -1,11 +1,12 @@
 from nose.plugins.attrib import attr
 from framework.base_test import BaseTest, HeadlessRunnerTest
-from framework.utils.common_utils import by_css
+from framework.utils.common_utils import by_css, by_xpath
 from framework.utils.database_manager_postgres import DatabaseManager
 from pages.registrationpage.registration_page import RegistrationPage
 from testdata.test_data import DATA_WINNER_REGISTER_TRIAL_PAGE, DATA_WINNER_HOMEPAGE, DATA_WINNER_EN_PRICING_PAGE, DATA_WINNER_TRIAL_ACCOUNT_EXPIRED_PAGE
 from tests.registrationtests.registration_data import REGISTRATION_DATA_FOR_SUCCESSFUL_TRIAL_REGISTRATION, REGISTRATION_SUCCESS_MESSAGE, ORGANIZATION_SECTOR_DROP_DOWN_LIST, INVALID_EMAIL_ERROR_MESSAGE, \
     REGISTRATION_DATA_FOR_UNSUCCESSFUL_TRIAL_REGISTRATION
+from tests.testsettings import UI_TEST_TIMEOUT
 
 
 def register_and_get_email_for_trial(driver):
@@ -34,9 +35,9 @@ class TestTrialRegistrationPage(HeadlessRunnerTest):
     @attr('functional_test')
     def test_trial_link_from_pricing_page(self):
         self.driver.go_to(DATA_WINNER_EN_PRICING_PAGE)
-        self.driver.find(by_css("ul.obox-grid li:first-child.obox-column a.try_button")).click()
+        self.driver.find(by_xpath("//a[contains(@href,'/register/trial')]")).click()
         self.driver.wait_for_page_with_title(10, "Register")
-        self.assertEqual(self.driver.current_url, "https://app.datawinners.com/register/trial/")
+        self.assertEqual(self.driver.current_url, "http://localhost:8000/register/trial/")
 
     @attr('functional_test')
     def test_register_organization_sector_have_the_right_select_options(self):
