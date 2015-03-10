ac93bc0db8a24c361f21eb1ab064106701a7827d
Ajay Divakaran <ajay@dwdev.ajay>
2015-03-10 14:27:48 +0530

    Ajay/DC | #4372 | Removed uts that verify flat pages

--- datawinners/home/tests/test_home.py
+++ datawinners/home/tests/test_home.py
@@ -11,17 +11,6 @@ class TestHome(unittest.TestCase):
     def setUp(self):
         self.client_stub = Client()
 
-    def test_should_render_home_view(self):
-        for url in [ '/en/home/', '/fr/home/'] :
-            response = self.client_stub.get(url)
-            self.assertEquals(response.status_code,200)
-
-    def test_should_render_link_to_trial_registration(self):
-        settings.TRIAL_REGISTRATION_ENABLED = True
-        for url in [ '/en/home/', '/fr/home/'] :
-            response = self.client_stub.get(url, follow=True)
-            self.assertRegexpMatches(response.content, r'href="/register/trial"')
-
     def test_should_switch_language_and_reload_page(self):
         request = Mock()
         request.META = {'HTTP_REFERER': 'http://www.datawinners.com/project/subjects/2e358c46bb6c11e187d60800276384f8/'}
