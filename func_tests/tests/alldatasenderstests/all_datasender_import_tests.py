29469f8c84d82256124ddad12bec68d91ea369cc
melaniem <dwdev@dwdev.dwdev>
2015-03-16 12:00:45 +0530

    DC/Melanie | #0000 | Adding Ft for subject and datasenders import

--- func_tests/tests/alldatasenderstests/all_datasender_import_tests.py
+++ func_tests/tests/alldatasenderstests/all_datasender_import_tests.py
@@ -1,18 +1,32 @@
 import os
-from random import random
 
 from django.test import Client
 from nose.plugins.attrib import attr
+from openpyxl import load_workbook
 
 from framework.base_test import HeadlessRunnerTest, teardown_driver
 from pages.globalnavigationpage.global_navigation_page import GlobalNavigationPage
 from pages.loginpage.login_page import login
-from xlutils.copy import copy
-from xlrd import open_workbook
 from framework.utils.common_utils import random_number
 
-class TestAllDataSendersImport(HeadlessRunnerTest):
+workbook_row1 = {
+    'ds_name': "FName" + random_number(2),
+    'ds_number': random_number(6),
+    'ds_location': "Loc" + random_number(2),
+    'gps_coordinates': '%.1f, ' % float(random_number(1)) + '%.1f' % float(random_number(1)),
+    'ds_email': 'DsEmail' + random_number(3) + "@dw.com"
+}
 
+workbook_row2 = {
+    'ds_name': "FName" + random_number(2),
+    'ds_number': random_number(6),
+    'ds_location': "Loc" + random_number(2),
+    'gps_coordinates': '%.1f, ' % float(random_number(1)) + '%.1f' % float(random_number(1)),
+    'ds_email': 'DsEmail' + random_number(3) + "@dw.com"
+}
+
+
+class TestAllDataSendersImport(HeadlessRunnerTest):
     @classmethod
     def setUpClass(cls):
         HeadlessRunnerTest.setUpClass()
@@ -21,24 +35,6 @@ class TestAllDataSendersImport(HeadlessRunnerTest):
     @classmethod
     def tearDownClass(cls):
         teardown_driver(cls.driver)
-
-    def _edit_datasender_import_excel(self, file_path, row):
-        workbook = copy(open_workbook(file_path))
-        worksheet = workbook.get_sheet(0)
-
-        ds_name = "DsName" + random_number(2)
-        ds_number = random_number(6)
-        ds_location = 'loc' + random_number(2)
-        ds_email = 'DsEmail' + random_number(3) + "@dw.com"
-        gps_coordinates = '%.1f, ' % float(random_number(1)) + '%.1f' % float(random_number(1))
-
-        worksheet.write(row, 0, ds_name)
-        worksheet.write(row, 1, ds_number)
-        worksheet.write(row, 2, ds_location)
-        worksheet.write(row, 3, gps_coordinates)
-        worksheet.write(row, 4, ds_email)
-        workbook.save(file_path)
-        return ds_email, ds_location, ds_name, ds_number, gps_coordinates
 
     def _upload_imported_datasender(self, file_name, file_path):
         client = Client()
@@ -49,33 +45,44 @@ class TestAllDataSendersImport(HeadlessRunnerTest):
             content_type='application/octet-stream')
         self.assertEquals(response.status_code, 200)
 
+    def _assert_datasender_uploaded(self, all_datasender_page, workbook):
+        all_datasender_page.search_with(workbook['ds_name'])
+        self.assertEqual(workbook['ds_name'], all_datasender_page.get_cell_value(1, 2))
+        self.assertIn(workbook['ds_location'], all_datasender_page.get_cell_value(1, 4))
+        self.assertEqual(workbook['gps_coordinates'], all_datasender_page.get_cell_value(1, 5))
+        self.assertEqual(workbook['ds_number'], all_datasender_page.get_cell_value(1, 6))
+        self.assertEqual(workbook['ds_email'], all_datasender_page.get_cell_value(1, 7))
+
+    def _prepare_workbook(self):
+        file_name = 'DataWinners_ImportDataSenders.xlsx'
+        DIR = os.path.dirname(__file__) + '/'
+        file_path = os.path.join(DIR, file_name)
+        workbook = load_workbook(file_path)
+        worksheet = workbook.get_sheet_by_name('Import Data Senders')
+        worksheet['A2'] = workbook_row1['ds_name']
+        worksheet['B2'] = workbook_row1['ds_number']
+        worksheet['C2'] = workbook_row1['ds_location']
+        worksheet['D2'] = workbook_row1['gps_coordinates']
+        worksheet['E2'] = workbook_row1['ds_email']
+
+        worksheet['A3'] = workbook_row2['ds_name']
+        worksheet['B3'] = workbook_row2['ds_number']
+        worksheet['C3'] = workbook_row2['ds_location']
+        worksheet['D3'] = workbook_row2['gps_coordinates']
+        worksheet['E3'] = workbook_row2['ds_email']
+        return file_name, file_path, workbook
+
     @attr('functional_test')
-    def test_should_import_more_than_one_datasender(self):
+    def test_should_import_more_than_one_data_sender(self):
+        file_name, file_path, workbook = self._prepare_workbook()
+
+        workbook.save(file_path)
+
         global_navigation = GlobalNavigationPage(self.driver)
         all_datasender_page = global_navigation.navigate_to_all_data_sender_page()
 
-        file_name = 'DataWinners_ImportDataSenders.xls'
-        DIR = os.path.dirname(__file__) + '/'
-        file_path = os.path.join(DIR, file_name)
-
-        ds_email1, ds_location1, ds_name1, ds_number1, gps_coordinates1 = self._edit_datasender_import_excel(file_path, 1)
-
         self._upload_imported_datasender(file_name, file_path)
 
-        all_datasender_page.search_with(ds_name1)
-        self.assertEqual(ds_name1, all_datasender_page.get_cell_value(1, 2))
-        self.assertIn(ds_location1, all_datasender_page.get_cell_value(1, 4))
-        self.assertEqual(gps_coordinates1, all_datasender_page.get_cell_value(1, 5))
-        self.assertEqual(ds_number1, all_datasender_page.get_cell_value(1, 6))
-        self.assertEqual(ds_email1, all_datasender_page.get_cell_value(1, 7))
+        self._assert_datasender_uploaded(all_datasender_page, workbook_row1)
 
-        ds_email2, ds_location2, ds_name2, ds_number2, gps_coordinates2 = self._edit_datasender_import_excel(file_path, 2)
-        self._upload_imported_datasender(file_name, file_path)
-
-        all_datasender_page.search_with(ds_name2)
-
-        self.assertEqual(ds_name2, all_datasender_page.get_cell_value(1, 2))
-        self.assertIn(ds_location2, all_datasender_page.get_cell_value(1, 4))
-        self.assertEqual(gps_coordinates2, all_datasender_page.get_cell_value(1, 5))
-        self.assertEqual(ds_number2, all_datasender_page.get_cell_value(1, 6))
-        self.assertEqual(ds_email2, all_datasender_page.get_cell_value(1, 7))
+        self._assert_datasender_uploaded(all_datasender_page, workbook_row2)
