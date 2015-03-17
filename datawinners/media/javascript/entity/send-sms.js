bf097040fc6b7bc8b78219b17865776e9bab7093
Ajay Divakaran <ajay@dwdev.ajay>
2015-03-17 14:42:08 +0530

    Musa/Ajay | #4291 | Handle 160 character limit for sms text

--- datawinners/media/javascript/entity/send-sms.js
+++ datawinners/media/javascript/entity/send-sms.js
@@ -1,6 +1,17 @@
+$.valHooks.textarea = {
+  get: function(elem){
+      return elem.value.replace(/\r?\n/g, "\r\n");
+  }
+};
+
+
 $(function(){
 
-    $("#send-sms-section").dialog({
+    var maxAllowedSMSCharacters = 160;
+
+    var smsTextDialog = $("#send-sms-section");
+
+    smsTextDialog.dialog({
         autoOpen: false,
         modal: true,
         title: gettext("Send an SMS"),
@@ -16,10 +27,35 @@ $(function(){
         $("#send-sms-section").dialog('open');
     });
 
-    var smsViewModel = new SmsViewModel();
-    window.model = smsViewModel;
+    var smsTextLengthCheck = function (e) {
+        if (e.target.value.length > maxAllowedSMSCharacters) {
+            $(this).val(e.target.value.substring(0, maxAllowedSMSCharacters));
+        }
 
-    ko.applyBindings(smsViewModel, $("#send-sms-section")[0]);
+        smsViewModel.smsCharacterCount($(this).val().length + " of " + maxAllowedSMSCharacters + " characters used");
+    };
+
+    var smsTextElement = $("#sms-text");
+
+    smsTextElement.keyup(smsTextLengthCheck);
+
+    smsTextElement.keypress(function (e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        var enterKeyCode = 13;
+
+        if(code == enterKeyCode && e.target.value.length + 2 > maxAllowedSMSCharacters){
+            e.preventDefault();
+            return false;
+        }
+        return true;
+    });
+
+    smsTextElement.keydown(smsTextLengthCheck);
+
+    var smsViewModel = new SmsViewModel();
+    window.smsViewModel = smsViewModel;
+
+    ko.applyBindings(smsViewModel, smsTextDialog[0]);
 
 });
 
@@ -38,10 +74,7 @@ function SmsViewModel(){
 
   self.smsText = DW.ko.createValidatableObservable({value: ""});
 
-  self.smsCharacterCount = ko.computed(function(){
-      var length = this.smsText().length;
-      return length + " of 160 characters used";
-  }, self);
+  self.smsCharacterCount = ko.observable("0 of 160 characters used");
 
   self.smsOptionList = ko.observableArray(['Other People']);
 
@@ -65,7 +98,7 @@ function SmsViewModel(){
 
   self.validateSmsText = function(){
 
-    if(self.smsText() == ""){
+    if($("#sms-text").val() == ""){
         self.smsText.setError(gettext("This field is required."));
         return false;
     }
@@ -102,37 +135,44 @@ function SmsViewModel(){
     return self.validateSmsText() && self.validateOthersList();
   };
 
+  function _showFailedNumbersError(response) {
+       if (response.failednumbers) {
+            $("#failed-numbers").text(interpolate(gettext("failed numbers message: %(failed_numbers)s."), {"failed_numbers": response.failednumbers.join(", ")}, true));
+            $("#failed-numbers").removeClass("none");
+       }
+  }
+
+  function _showNoSMSCError(response) {
+        if (response.nosmsc) {
+              $("#no-smsc").removeClass("none");
+        }
+  }
+
+
   self.sendSms = function(){
 
       if(!self.validate()){
           return;
       }
 
-      //remove inline style which gets added as part of flash-message
       self._resetSuccessMessage();
       self._resetErrorMessages();
 
       $.post(send_sms_url, {
-          'sms-text': self.smsText(),
+          'sms-text': $("#sms-text").val(),
           'others': self.othersList(),
           'recipient': self.selectedSmsOption(),
           'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val()
       }).done(function(response){
 
           var response = $.parseJSON(response);
+
           if(response.successful){
               $("#sms-success").removeClass("none");
           }
           else {
-              if (response.nosmsc) {
-                  $("#no-smsc").removeClass("none");
-              }
-              else {
-                  if (response.failednumbers) {
-                      $("#failed-numbers").text(interpolate(gettext("failed numbers message: %(failed_numbers)s."), {"failed_numbers": response.failednumbers.join(", ")}, true));
-                      $("#failed-numbers").removeClass("none");
-                  }
-              }
+              _showNoSMSCError(response);
+              _showFailedNumbersError(response);
           }
       });
   };
