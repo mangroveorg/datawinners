ae6941847045ffc98788f1f740ac656622bb9214
melaniem <dwdev@dwdev.dwdev>
2015-03-17 17:54:00 +0530

    DC/Melanie |#4397 #4396 | Handling information boxes and fading away

--- datawinners/media/javascript/project/upload_questionnaire.js
+++ datawinners/media/javascript/project/upload_questionnaire.js
@@ -27,15 +27,16 @@ DW.showError = function(errors,message_prefix,message_suffix){
         html("<span>" + error_message_prefix + "</span>"+ error_messages +"<span>" + error_message_suffix + "</span>").show();
 };
 DW.showInfo = function(infos){
-    var info_messages = "<ul>";
+    var info_messages = '';
 
     $.each(infos, function(index, info){
-       info_messages += "<li>" + info + "</li>";
+       info_messages += "<div class='information_box'>" + info + "</div>";
     });
-    info_messages += "</ul>";
+//    info_messages += "</ul>";
     var flash_message = $("#xlx-info");
-    flash_message.removeClass("none").removeClass("success-message-box").addClass("information_box").
+    flash_message.removeClass("none").removeClass("success-message-box").
         html(info_messages).show();
+    flash_message.delay(5000).fadeOut();
 };
 DW.updateFilename = function(file_name){
     $("div.download_xls span.heading_block a").text(file_name);
