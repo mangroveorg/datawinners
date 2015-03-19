05f014958fe2257a3eed6ab6b2ebe445afc2c8f4
Ajay Divakaran <ajay@dwdev.ajay>
2015-03-19 12:30:21 +0530

    Ajay | #4362 | Fixing ut

--- datawinners/entity/tests/test_sms.py
+++ datawinners/entity/tests/test_sms.py
@@ -7,7 +7,7 @@ class TestSMS(TestCase):
 
     def test_should_return_unique_mobile_numbers(self):
         mock_request = MagicMock()
-        mock_request.POST = {"others": "87687687,9879879, 9879879"}
+        mock_request.POST = {"others": "87687687,9879879, 9879879", "recipient": "others"}
 
         actual_numbers = SendSMS()._other_numbers(mock_request)
         expected_numbers = list(set(["87687687", "9879879"]))
