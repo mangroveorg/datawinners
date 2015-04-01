a58fde079d94451c40acda15a5b586472984c52c
melaniem <dwdev@dwdev.dwdev>
2015-04-01 17:23:21 +0530

    DC/Melanie |#0000| Fixing FT

--- func_tests/pages/projectdatasenderspage/project_data_senders_locator.py
+++ func_tests/pages/projectdatasenderspage/project_data_senders_locator.py
@@ -19,18 +19,16 @@ BY = "by"
 
 ADD_A_DATA_SENDER_LINK = by_css("a.register_data_sender")
 ACCOUNT_LINK = by_css("a.account")
-DATA_SENDER_CHECK_BOX_BY_MOBILE_XPATH = "//tr/td[6][text()='%s']/../td[1]/input"
+DATA_SENDER_CHECK_BOX_BY_MOBILE_XPATH = "//tr/td[3][text()='%s']/../td[1]/input"
 DATA_SENDER_CHECK_BOX_BY_UID_XPATH = "//input[@value='%s']"
 DATA_SENDER_ROW_BY_UID_XPATH = "//input[@id='%s']/../.."
-DATA_SENDER_EMAIL_BY_UID_XPATH = "//input[@id='%s']/../../td[8]"
-DATA_SENDER_EMAIL_BY_MOBILE_NUMBER_XPATH = "//tr/td[6][text()='%s']/../td[8]"
+DATA_SENDER_EMAIL_BY_UID_XPATH = "//input[@id='%s']/../../td[4]"
+DATA_SENDER_EMAIL_BY_MOBILE_NUMBER_XPATH = "//tr/td[3][text()='%s']/../td[4]"
 PROJECT_CB_XPATH = "//div[contains(@class,'ui-dialog') and contains(@style, 'block')]/div/ul[@id='all_projects']/li[text()='%s']/input"
 ACTION_DROP_DOWN = by_css(".action")
 WEB_USER_BLOCK = by_css("div#web_user_block")
 WEB_USER_BLOCK_EMAIL = by_css("div#web_user_block input.ds-email")
 GIVE_ACCESS_LINK = by_css('a#web_user_button.button')
-PROJECT_NAME_LABEL_XPATH = "//tr/td/input[@id='%s']/../../td[7]"
-UID_LABEL_BY_MOBILE_XPATH = "//tr/td[7][text()='%s']/../td[2]"
 
 CANCEL_LINK = by_xpath("//div[contains(@class,'ui-dialog') and contains(@style, 'block')]/div/a[@id='cancel_link']")
 CONFIRM_BUTTON = by_xpath("//div[contains(@class,'ui-dialog') and contains(@style, 'block')]/div/a[text()='Confirm']")
