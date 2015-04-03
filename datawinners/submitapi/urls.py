04542f179848f0db25c680519d33270a417b8276
Musa Baloyi <musabaloyi@aims.ac.za>
2015-04-03 12:17:13 +0530

    Revert "[#4437] [Musa/DC] Add Submit API for Questionnaires"
    This reverts commit bc6239881a252b4d3eeb96706d0aa93ac5099958.

--- datawinners/submitapi/urls.py
+++
@@ -1,4 +0,0 @@
-from django.conf.urls.defaults import patterns
-from datawinners.submitapi.make_submission import  post_submission
-
-urlpatterns = patterns('', (r'^api/post_submission/$', post_submission))
