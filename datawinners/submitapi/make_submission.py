cc2f23ee0221393fb228934cafa6924563c910e4
Musa Baloyi <musabaloyi@aims.ac.za>
2015-04-03 16:25:00 +0530

    [#4437] [Musa/DC] Add Submit API for Questionnaires for Subjects

--- datawinners/submitapi/make_submission.py
+++ datawinners/submitapi/make_submission.py
@@ -4,8 +4,10 @@ from django_digest.decorators import httpdigest
 import json
 import jsonpickle
 from datawinners.dataextraction.helper import convert_to_json_file_download_response
+from datawinners.location.LocationTree import get_location_tree, get_location_hierarchy
 
 from datawinners.main.database import get_database_manager
+from datawinners.submission.location import LocationBridge
 from datawinners.utils import get_organization
 from mangrove.transport.player.new_players import SubmitApiPlayer
 
@@ -13,20 +15,16 @@ from mangrove.transport.player.new_players import SubmitApiPlayer
 @httpdigest
 def post_submission(request):
     input_request = jsonpickle.decode(request.raw_post_data)
-    organization = get_organization(request)
     manager = get_database_manager(request.user)
-    # client = SubmitClient()
     player = SubmitApiPlayer(manager)
     result = {}
     submission_reply = []
     submissions = input_request.get('message',[])
     reporter_id = input_request.get('reporter_id')
+    location_tree = LocationBridge(get_location_tree(), get_location_hierarchy)
     for submission in submissions:
-        # success, message = client.submit_questionnaire(manager, submission, reporter_id)
-        #client.submit_questionnaire(manager, submission, reporter_id)
-        success, message = player.submit(submission, reporter_id)
+        success, message = player.submit(submission, reporter_id, location_tree)
         submission_reply.append({ "success": success, 'message':message })
-        # organization.increment_incoming_submission_count()
     result['submission_reply'] = submission_reply
 
     return HttpResponse(content_type='application/json', content=json.dumps(result))
