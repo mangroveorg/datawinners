763f7e1e8b7cee9a229ab1834e68c8f399a76dc1
Deepthi-Chand <deepthia@thoughtworks.com>
2015-04-06 15:53:45 +0530

    DC | #4457 | ui changes as per feedback

--- datawinners/media/javascript/entity/datasender_actions.js
+++ datawinners/media/javascript/entity/datasender_actions.js
@@ -439,7 +439,7 @@ function init_dialog_box_for_datasender() {
         autoOpen: false,
         modal: true,
         zIndex: 1100,
-        width: 900,
+        width: 700,
         beforeClose: function () {
             $('#action').removeAttr("data-selected-action");
             $('#web_user_error').hide();
