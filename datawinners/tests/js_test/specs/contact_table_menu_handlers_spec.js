d85f1013c623ef3d8752f8c656aff678d307518e
melaniem <dwdev@dwdev.dwdev>
2015-04-09 15:44:52 +0530

    Venu/Melanie |#4412 | Adding JT

--- datawinners/tests/js_test/specs/contact_table_menu_handlers_spec.js
+++ datawinners/tests/js_test/specs/contact_table_menu_handlers_spec.js
@@ -2,13 +2,16 @@ describe('contact table menu items', function () {
 
     var $removeFromGroupParent;
     var $addToGroupParent;
+    var $removeFromQuestionnaireParent;
 
     beforeEach(function(){
         $removeFromGroupParent = $('#remove-from-group-parent');
-        $addToGroupParent = $('#add-to-group');
+        $addToGroupParent = $('#add-to-group-parent');
+        $removeFromQuestionnaireParent = $('#remove-from-questionnaire-parent');
     });
 
     describe("remove from group", function() {
+
         it("should disable when no groups present", function () {
             spyOn(groupViewModel, "isCustomGroupsPresent").andReturn(false);
             $removeFromGroupParent.removeClass('disabled');
@@ -43,6 +46,59 @@ describe('contact table menu items', function () {
             DW.allContactTableMenu.disableMenuItemWhenSelectedContactsHaveNoGroup();
 
             expect($removeFromGroupParent).not.toHaveClass('disabled');
+
+        });
+
+    });
+
+    describe("add to group", function() {
+
+        it('should disable when no group present', function(){
+            spyOn(groupViewModel, "isCustomGroupsPresent").andReturn(false);
+            $addToGroupParent.addClass('disabled');
+
+            DW.allContactTableMenu.disableGroupMenuItemsWhenNoGroupsPresent();
+
+            expect($addToGroupParent).toHaveClass('disabled');
+
+
+        });
+
+        it('should not disable when group present', function(){
+            spyOn(groupViewModel, "isCustomGroupsPresent").andReturn(true);
+            $addToGroupParent.addClass('disabled');
+
+            DW.allContactTableMenu.disableGroupMenuItemsWhenNoGroupsPresent();
+
+            expect($addToGroupParent).not.toHaveClass('disabled');
+
+        });
+
+    });
+
+    describe("remove from questionnaire", function() {
+
+        beforeEach(function() {
+            $('input:checkbox').removeAttr('checked');
+        });
+
+        it('should be disabled when no contacts are not linked to questionnaires', function(){
+            $removeFromQuestionnaireParent.removeClass('disabled');
+            $("#id5").click();
+
+            DW.allContactTableMenu.disableMenuItemWhenSelectedContactHaveNoQuestionnaire();
+
+            expect($removeFromQuestionnaireParent).toHaveClass('disabled');
+
+        });
+
+        it('should not be disabled when contacts are linked to questionnaires', function(){
+            $removeFromQuestionnaireParent.removeClass('disabled');
+            $("#id6").click();
+
+            DW.allContactTableMenu.disableMenuItemWhenSelectedContactHaveNoQuestionnaire();
+
+            expect($removeFromQuestionnaireParent).not.toHaveClass('disabled');
 
         });
 
