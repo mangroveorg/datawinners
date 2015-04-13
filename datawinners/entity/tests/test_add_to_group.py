86c2621773801d9abc286e1773a24b3a9ded7634
Ajay Divakaran <ajay@dwdev.ajay>
2015-04-13 11:42:28 +0530

    Ajay | #4454 | Bugfix | Add contact to multiple groups not working

--- datawinners/entity/tests/test_add_to_group.py
+++ datawinners/entity/tests/test_add_to_group.py
@@ -2,13 +2,13 @@ import unittest
 from mangrove.datastore.documents import ContactDocument
 
 
-class TestAddToGroup(unittest.TestCase):
+class ContactDocumentTest(unittest.TestCase):
 
     def test_to_add_existing_contacts_to_groups(self):
         group_name = 'group_name1'
         contact_doc = ContactDocument()
 
-        del(contact_doc._data['custom_groups'])
+        contact_doc._data.pop('custom_groups')
 
         contact_doc.add_custom_group(group_name)
 
@@ -21,3 +21,23 @@ class TestAddToGroup(unittest.TestCase):
         contact_doc.add_custom_group(group_name)
 
         self.assertEquals(contact_doc.custom_groups, [group_name])
+
+    def test_should_not_add_duplicate_groups(self):
+        contact_doc = ContactDocument()
+
+        contact_doc._data['custom_groups'].append('group2')
+
+        contact_doc.add_custom_group('group2')
+
+        self.assertEquals(contact_doc.custom_groups, ['group2'])
+
+
+    def test_should_not_remove_existing_groups(self):
+        contact_doc = ContactDocument()
+
+        contact_doc._data['custom_groups'].append('group1')
+
+        contact_doc.add_custom_group('group3')
+
+        self.assertEquals(contact_doc.custom_groups, ['group1', 'group3'])
+
