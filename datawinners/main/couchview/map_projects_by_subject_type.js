fbeef7c41cb77d9c7e23c884ef01d12e326e086a
melaniem <dwdev@dwdev.dwdev>
2015-04-17 17:45:50 +0530

    DC/Melanie |#4502| Fetching unique-id types from field set

--- datawinners/main/couchview/map_projects_by_subject_type.js
+++ datawinners/main/couchview/map_projects_by_subject_type.js
@@ -2,14 +2,20 @@ function (doc) {
     if (doc.document_type == 'FormModel') {
         if (!doc.void) {
             var unique_id_types = [];
-            for (i in doc.json_fields) {
-                var field = doc.json_fields[i];
-                if (field.type == "unique_id" && unique_id_types.indexOf(field.unique_id_type) == -1) {
-                        unique_id_types.push(field.unique_id_type);
-                        emit(field.unique_id_type, doc.name);
-                }
-            }
-
+            emit_unique_id_fields_in(doc.json_fields);
         }
     }
+    function emit_unique_id_fields_in(fields){
+        for (i in fields) {
+                    var field = doc.json_fields[i];
+                    if (field.type == "unique_id" && unique_id_types.indexOf(field.unique_id_type) == -1) {
+                            unique_id_types.push(field.unique_id_type);
+                            emit(field.unique_id_type, doc.name);
+                    }
+            if (field.type == "field_set") {
+                            emit_unique_id_fields_in(field.fields)
+                    }
+
+                }
+        }
 }
