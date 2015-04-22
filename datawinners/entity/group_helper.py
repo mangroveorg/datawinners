59258870a5bd6ebc91893e591ea19fbaedf362c4
Ajay Divakaran <ajay@dwdev.ajay>
2015-04-22 17:17:29 +0530

    Venu/Ajay | #4532 | Escaping/Unescaping where needed

--- datawinners/entity/group_helper.py
+++ datawinners/entity/group_helper.py
@@ -1,7 +1,8 @@
-import cgi
 from string import lower
-from datawinners.utils import is_empty_string
+from xml.sax.saxutils import escape
 from django.utils.translation import ugettext as _
+
+from datawinners.utils import is_empty_string
 from mangrove.datastore.documents import GroupDocument
 from mangrove.datastore.group import Group
 from mangrove.errors.MangroveException import DataObjectNotFound
@@ -12,7 +13,7 @@ def get_group_details(dbm):
     group_details = []
     rows = dbm.load_all_rows_in_view('group_by_name')
     for row in rows:
-        group_details.append({'name': cgi.escape(row['value']['name'])})
+        group_details.append({'name': escape(row['value']['name'])})
     return group_details
 
 
