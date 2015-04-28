b300d7593756ddf2385170d02ff22fb10d75c1a5
Ajay Divakaran <ajay@dwdev.ajay>
2015-04-28 10:52:02 +0530

    Venu/Ajay | Bugfix - Ignore soft deleted datasenders

--- datawinners/entity/datasender_search.py
+++ datawinners/entity/datasender_search.py
@@ -5,7 +5,9 @@ from elasticsearch_dsl import Search
 def datasender_count_with(email):
     es = Elasticsearch()
 
-    search = Search(using=es, doc_type='reporter').query("term", email_value=email.lower())
+    search = Search(using=es, doc_type='reporter').filter("term", email_value=email.lower(), cache=False)
+    search = search.filter("term", void=False)
+    search = search.extra(from_=0, size=1)
     body = search.to_dict()
 
     return es.search(doc_type='reporter', body=body, search_type='count')['hits']['total']
