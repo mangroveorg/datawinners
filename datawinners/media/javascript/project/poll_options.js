b584de352e3c0fe26d6e71a7bea8a2e54f03eb7c
DeepthiChand <deepthia@thoughtworks.com>
2015-06-29 11:38:39 +0530

    DC | #4745 | removing double is

--- datawinners/media/javascript/project/poll_options.js
+++ datawinners/media/javascript/project/poll_options.js
@@ -70,7 +70,7 @@ var PollOptionsViewModel = function() {
         self.status(gettext('Active'));
         self.activation('');
         self.deactivation(gettext('Deactivate'));
-        self.duration(gettext('is active From : ') + from_date + gettext('To : ') + to_date);
+        self.duration(gettext(' active From : ') + from_date + gettext('To : ') + to_date);
         self.change_days(gettext('Change'));
         self.number_of_days(get_current_number_of_days());
         self.active_dates_poll('<i class="italic_grey"><b> '+gettext('From : ')+'</b> '+ from_date + ' <b>&nbsp'+gettext(' To : ')+'</b>' + to_date +'</i>');
@@ -151,7 +151,7 @@ var PollOptionsViewModel = function() {
                 self.status(gettext('Active'));
                 self.deactivation(gettext('Deactivate'));
                 self.activation('');
-                self.duration(gettext('is active From : ') + self.from_date_poll() + gettext('To : ') + self.to_date_poll());
+                self.duration(gettext(' active From : ') + self.from_date_poll() + gettext('To : ') + self.to_date_poll());
                 self.active_dates_poll('<i class="italic_grey"><b>From:</b> '  + self.from_date_poll() + ' <b>&nbsp To: </b> ' + self.to_date_poll()+'</i>');
                 self.change_days(gettext('Change'));
                 DW.trackEvent('poll-deactivation-method', 'poll-deactivate-success');
