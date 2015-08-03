c6e9625012682c4a89cf7bdd0450ad0981ea2a36
Sairam Krish <haisairam@gmail.com>
2015-08-03 16:44:41 +0530

    #4753 | Sairam/Sudharshan | Added migration scripts for User Permissions

--- migration/couch/release_26/1.migration_to_add_user_permission_data.py
+++ migration/couch/release_26/1.migration_to_add_user_permission_data.py
@@ -5,25 +5,27 @@ from mangrove.form_model.project import Project
 from mangrove.datastore.documents import ProjectDocument
 from datawinners.main.couchdb.utils import all_db_names
 from migration.couch.utils import migrate, mark_as_completed
+from datawinners.project.views.views import questionnaire
+from mangrove.datastore.user_permission import UserPermission
 
+map_all_questionnaire_id = """
+function(doc) {
+    if (doc.document_type == 'FormModel' && !doc.void) {
+        emit(doc._id, null);
+    }
+}"""
 
-def add_and_populate_permission_fields(database_name):
+def populate_user_permission(database_name):
     organization_setting = OrganizationSetting.objects.get(document_store=database_name)
     org_id = organization_setting.organization_id
     ngo_admin = User.objects.filter(ngouserprofile__org_id=org_id, groups__name__in=["NGO Admins"])[0]
     project_managers = User.objects.filter(ngouserprofile__org_id=org_id, groups__name__in=["Project Managers"])\
         .values_list('id', flat=True)
-
     dbm = get_db_manager(database_name)
-
-    questionnaires = dbm.load_all_rows_in_view('all_projects')
-    for row in questionnaires:
-        doc = ProjectDocument.wrap(row['value'])
-        questionnaire = Project.new_from_doc(dbm, doc)
-        questionnaire._doc.creator = ngo_admin.id
-        questionnaire.users_as_datasender = []
-        questionnaire.users = project_managers
-        questionnaire.save()
-    mark_as_completed(database_name)
-
-migrate(all_db_names(), add_and_populate_permission_fields, version=(26, 0, 1), threads=1)
+    project_ids = [row.key for row in dbm.database.query(map_all_questionnaire_id)]
+    for user_id in project_managers:
+        user_permission = UserPermission(dbm, user_id, project_ids)
+        user_permission.save()
+    mark_as_completed(database_name)	
+   	
+migrate(all_db_names(), populate_user_permission, version=(26, 0, 1), threads=1)
