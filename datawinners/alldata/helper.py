c71321507d8e67d6b57bbca20da674354d2a3c8f
Sairam Krish <haisairam@gmail.com>
2015-08-05 15:36:58 +0530

    #4755 | Sairam/Sudharshan | Questionnaire listing page updated to show based on user permission

--- datawinners/alldata/helper.py
+++ datawinners/alldata/helper.py
@@ -1,17 +1,19 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-from datawinners.project.couch_view_helper import get_all_projects, remove_poll_questionnaires
+from datawinners.project.couch_view_helper import get_all_projects, remove_poll_questionnaires,\
+    get_all_projects_for_users
 from datawinners.settings import CRS_ORG_ID
 from datawinners.project import models
 from django.utils.translation import ugettext_lazy as _
 from datawinners.main.database import get_database_manager
 from datawinners.utils import get_organization
+from mangrove.datastore.user_permission import get_questionnaires_for_user
 
 
 def get_all_project_for_user(user):
     if user.get_profile().reporter:
         questionnaires = get_all_projects(get_database_manager(user), user.get_profile().reporter_id)
         return remove_poll_questionnaires(questionnaires)
-    return get_all_projects(get_database_manager(user))
+    return get_questionnaires_for_user(user.id, get_database_manager(user))
 
 
 def get_visibility_settings_for(user):
