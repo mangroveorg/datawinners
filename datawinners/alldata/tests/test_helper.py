204d8b586f6892d4ebdd265e585120c5a7b77475
Sudharshan R <sdn2525@gmail.com>
2015-08-10 11:17:57 +0530

    #4755 | Sairam/Sudharshan | Added Unit Test to getall questionnaires for user

--- datawinners/alldata/tests/test_helper.py
+++ datawinners/alldata/tests/test_helper.py
@@ -5,8 +5,6 @@ from django.contrib.auth.models import User
 from datawinners.accountmanagement.models import NGOUserProfile, Organization
 from datawinners.alldata import helper
 from datawinners.project.couch_view_helper import get_all_projects
-from datawinners.alldata.helper import get_all_project_for_user
-
 
 class TestHelper(unittest.TestCase):
 
@@ -17,8 +15,10 @@ class TestHelper(unittest.TestCase):
 
     def _get_normal_user(self):
         user = Mock(User)
+        user.id = 1
         normal_profile = Mock(NGOUserProfile)
         normal_profile.reporter = False
+        normal_profile.isNGOAdmin = False
         user.get_profile.return_value = normal_profile
         return user
 
@@ -26,7 +26,7 @@ class TestHelper(unittest.TestCase):
         user = Mock(User)
         reporter_profile = Mock(NGOUserProfile)
         reporter_profile.reporter = True
-        reporter_profile.reporter_id = 'something'
+        reporter_profile.reporter_id = 2
         user.get_profile.return_value = reporter_profile
         return user
 
@@ -34,34 +34,32 @@ class TestHelper(unittest.TestCase):
         user = Mock(User)
         ngo_admin_profile = Mock(NGOUserProfile)
         ngo_admin_profile.reporter = False
-        ngo_admin_profile.isNGOAdmin.return_value = True
+        ngo_admin_profile.isNGOAdmin = True
         user.get_profile.return_value = ngo_admin_profile
         return user
 
     def test_should_return_all_projects_for_user_as_reporter(self):
         user = self._get_reporter_user()
-        request = MagicMock()
         all_projects = {}
         all_projects_for_user = [{'_id':'d3456cc','name':'test questionnaire'}]
         with patch('datawinners.alldata.helper.get_all_projects') as mock_get_all_projects, patch('datawinners.alldata.helper.remove_poll_questionnaires') as mock_remove_poll_questionnaires:
             mock_get_all_projects.return_value = all_projects
             mock_remove_poll_questionnaires.return_value = all_projects_for_user
-            request.user = user
             assert helper.get_all_project_for_user(user) == all_projects_for_user
             
-#     def test_should_return_all_projects_for_user_as_project_manager(self):
-#         user = self._get_normal_user()
-#         request = MagicMock()
-#         all_project_for_user = [{''}]
+    def test_should_return_all_projects_for_user_as_project_manager(self):
+        user = self._get_normal_user()
+        all_projects_for_user = [{'value':{'_id':'d3456cc','name':'test questionnaire'}},{'value':{'_id':'256cc','name':'2nd questionnaire'}}]
+        with patch('datawinners.alldata.helper.get_questionnaires_for_user') as mock_get_questionnaires_for_user:
+            mock_get_questionnaires_for_user.return_value = all_projects_for_user
+            assert helper.get_all_project_for_user(user) == all_projects_for_user
 
     def test_should_return_all_projects_for_user_as_ngo_admin(self):
         user = self._get_ngo_admin()
-        request = MagicMock()
         all_projects = [{'value':{'_id':'d3456cc','name':'test questionnaire'}},{'value':{'_id':'256cc','name':'2nd questionnaire'}}]
         all_projects_expected = [{'_id':'d3456cc','name':'test questionnaire'},{'_id':'256cc','name':'2nd questionnaire'}]
         with patch('datawinners.alldata.helper.get_all_projects') as mock_get_all_projects:
             mock_get_all_projects.return_value = all_projects
-            request.user = user
             assert helper.get_all_project_for_user(user) == all_projects_expected
 
     def test_should_return_disabled_and_display_none_for_reporter(self):
