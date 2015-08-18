5acb1268efd5cd76638d8588b18708d159a09beb
Sudharshan R <sdn2525@gmail.com>
2015-08-18 21:22:58 +0530

    Kumaran/Sudharshan | #4749 | Functional tests for adding project manager as extended user

--- func_tests/tests/addusertests/add_user_data.py
+++ func_tests/tests/addusertests/add_user_data.py
@@ -1,15 +1,17 @@
 from framework.utils.common_utils import random_number, random_string
 
+def generate_user():
+    return {
+        TITLE: "Developer",
+        NAME: "Mino Rakoto",
+        USERNAME: random_string(7)+"@mailinator.com",
+        MOBILE_PHONE: random_number(9)
+    }
+
 TITLE = "title"
 NAME = "full_name"
 USERNAME = "username"
 MOBILE_PHONE = "mobile_phone"
-ADD_USER_DATA = {
-    TITLE: "Developer",
-    NAME: "Mino Rakoto",
-    USERNAME: random_string(7)+"@mailinator.com",
-    MOBILE_PHONE: random_number(9)
-}
 ADDED_USER_SUCCESS_MSG = u'User has been added successfully'
 DASHBOARD_PAGE_TITLE = u'Dashboard'
 DEFAULT_PASSWORD = "test123"
--- func_tests/tests/addusertests/add_user_tests.py
+++ func_tests/tests/addusertests/add_user_tests.py
@@ -2,53 +2,52 @@ from nose.plugins.attrib import attr
 
 from framework.utils.data_fetcher import fetch_
 from framework.base_test import HeadlessRunnerTest
+from framework.utils.common_utils import by_css
 from pages.loginpage.login_page import login
 from tests.logintests.login_data import VALID_CREDENTIALS, PASSWORD
 from pages.alluserspage.all_users_page import AllUsersPage
 from tests.addusertests.add_user_data import *
 from tests.alluserstests.all_users_data import ALL_USERS_URL
 
-
 class TestAddUser(HeadlessRunnerTest):
-    @attr('functional_test')
-    def test_should_add_a_new_extended_user_as_ngo_admin(self):
+
+    def setUp(self):
         self.global_navigation = login(self.driver, VALID_CREDENTIALS)
         self.driver.go_to(ALL_USERS_URL)
-        all_users_page = AllUsersPage(self.driver)
-        add_user_page = all_users_page.navigate_to_add_user()
-        add_user_page.select_role_as_administrator()
-        add_user_page.add_user_with(ADD_USER_DATA)
-        message = add_user_page.get_success_message()
+        self.all_users_page = AllUsersPage(self.driver)
+        self.add_user_page = self.all_users_page.navigate_to_add_user()
+
+    def _create_extended_user(self):
+        self.add_user_page.select_role_as_administrator()
+        user = generate_user()
+        self.add_user_page.add_user_with(user)
+        message = self.add_user_page.get_success_message()
         self.assertEqual(message, ADDED_USER_SUCCESS_MSG)
-        username = fetch_(USERNAME, ADD_USER_DATA)
+        self.username = fetch_(USERNAME, user)
         password = DEFAULT_PASSWORD
-        new_user_credential = {USERNAME: username, PASSWORD: password}
+        self.new_user_credential = {USERNAME: self.username, PASSWORD: password}
         self.driver.go_to(ALL_USERS_URL)
-        role_for_user = all_users_page.get_role_for(username)
+        role_for_user = self.all_users_page.get_role_for(self.username)
         self.assertEqual('Administrator', role_for_user, 'Expected role to be Administrator but was %s' % role_for_user)
         self.global_navigation.sign_out()
-        login(self.driver, new_user_credential)
+        login(self.driver, self.new_user_credential)
         title = self.driver.get_title()
         self.assertEqual(title, DASHBOARD_PAGE_TITLE)
-        self.global_navigation.sign_out()
 
-    @attr('functional_test')
-    def test_should_add_a_new_project_manager_as_ngo_admin(self):
-        self.global_navigation = login(self.driver, VALID_CREDENTIALS)
-        self.driver.go_to(ALL_USERS_URL)
-        all_users_page = AllUsersPage(self.driver)
-        add_user_page = all_users_page.navigate_to_add_user()
-        add_user_page.select_role_as_project_manager()
-        questionnaires = add_user_page.select_questionnaires(2)
-        add_user_page.add_user_with(ADD_USER_DATA)
-        message = add_user_page.get_success_message()
+    def _create_project_manager(self):
+        self.add_user_page.select_role_as_project_manager()
+        questionnaires = self.add_user_page.select_questionnaires(2)
+        user = generate_user()
+        self.add_user_page.add_user_with(user)
+        self.driver.wait_for_element(5, by_css('.success-message-box'), want_visible=True)
+        message = self.add_user_page.get_success_message()
         self.assertEqual(message, ADDED_USER_SUCCESS_MSG)
-        username = fetch_(USERNAME, ADD_USER_DATA)
+        self.username = fetch_(USERNAME, user)
         password = DEFAULT_PASSWORD
-        new_user_credential = {USERNAME: username, PASSWORD: password}
+        self.new_user_credential = {USERNAME: self.username, PASSWORD: password}
         self.driver.go_to(ALL_USERS_URL)
-        questionnaire_list_for_user = all_users_page.get_questionnaire_list_for(username)
-        role_for_user = all_users_page.get_role_for(username)
+        questionnaire_list_for_user = self.all_users_page.get_questionnaire_list_for(self.username)
+        role_for_user = self.all_users_page.get_role_for(self.username)
         self.assertEqual('Project Manager', role_for_user,
                          'Expected role to be Project Manager but was %s' % role_for_user)
         self.assertTrue(questionnaires[0] in questionnaire_list_for_user)
@@ -57,7 +56,26 @@ class TestAddUser(HeadlessRunnerTest):
                          'Expected the questionnaires length to be 2 but was %s' %
                          len(questionnaire_list_for_user))
         self.global_navigation.sign_out()
-        login(self.driver, new_user_credential)
+        login(self.driver, self.new_user_credential)
         title = self.driver.get_title()
         self.assertEqual(title, DASHBOARD_PAGE_TITLE)
+
+    @attr('functional_test')
+    def test_should_add_a_new_extended_user_as_ngo_admin(self):
+        self._create_extended_user()
+
+    @attr('functional_test')
+    def test_should_add_a_new_project_manager_as_ngo_admin(self):
+        self._create_project_manager()
+
+    @attr('functional_test')
+    def test_should_add_a_new_project_manager_as_extended_user(self):
+        self._create_extended_user()
+        self.driver.go_to(ALL_USERS_URL)
+        self.all_users_page = AllUsersPage(self.driver)
+        self.add_user_page = self.all_users_page.navigate_to_add_user()
+        self._create_project_manager()
+
+    def tearDown(self):
         self.global_navigation.sign_out()
+
