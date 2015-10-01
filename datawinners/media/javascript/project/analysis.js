340c5b628a46dd6c5042ec017592a31fa9c317b6
Sudharshan R <sdn2525@gmail.com>
2015-10-01 19:22:36 +0530

    Sudharshan | #4792 | Improved UI and functionality for column customization widget

--- datawinners/media/javascript/project/analysis.js
+++ datawinners/media/javascript/project/analysis.js
@@ -30,6 +30,11 @@ $(document).ready(function () {
     
     $.getJSON(headerUrl, function (columns) {
         analysisTable = new AnalysisPageDataTable(columns);
+        setTimeout(function(){
+            var custTableHeight = $(".dataTables_scroll").height();
+            $(".customization-widget").height(custTableHeight-7);
+            $(".customization-menu").height(custTableHeight-17-49);
+        }, 500);
     });
 
 
@@ -38,31 +43,140 @@ $(document).ready(function () {
 
     var customizationHeader = [
         {
-
             "data": "f6fefb725df211e5bd150800275f9736_q3",
-            "title": "level 0",
+            "title": "Data Sender",
             "visibility":true,
             "children":[
                 {
-                  "title":"level 1",
+                  "title":"Mobile Number",
                   "data":"datasender.mobile_number",
+                  "visibility":true,
+                  "children":[]
+                },
+                {
+                  "title":"Email",
+                  "data":"datasender.email",
                   "visibility":false,
                   "children":[]
                 },
                 {
-                    "title":"level 1a",
-                    "data":"datasender.email",
+                  "title":"Location Name",
+                  "data":"datasender.location_name",
+                  "visibility":true,
+                  "children":[]
+                },
+                {
+                  "title":"Location GPS Coordinates",
+                  "data":"datasender.location_gps_coordinates",
+                  "visibility":false,
+                  "children":[]
+                }
+            ]
+        },
+        {
+            "data": "f6fefb725df211e5bd150800275y9733",
+            "title": "Questionnaire",
+            "visibility":true,
+            "children":[
+                {
+                  "title":"What clinic are you reporting on?",
+                  "data":"",
+                  "visibility":true,
+                  "children":[]
+                },
+                {
+                  "title":"What date is it?",
+                  "data":"",
+                  "visibility":true,
+                  "children":[]
+                },
+                {
+                  "title":"What patient are you reporting on?",
+                  "data":"",
+                  "visibility":true,
+                  "children":[]
+                }
+            ]
+        },
+        {
+            "data": "f6fefb725df211e5bd150800275y9733",
+            "title": "Clinic",
+            "visibility":false,
+            "children":[
+                {
+                  "title":"Head of Clinic",
+                  "data":"",
+                  "visibility":false,
+                  "children":[]
+                },
+                {
+                  "title":"Room for how many patients?",
+                  "data":"",
+                  "visibility":false,
+                  "children":[]
+                },
+                {
+                  "title":"Telephone number",
+                  "data":"",
+                  "visibility":false,
+                  "children":[]
+                },
+                {
+                  "title":"Location",
+                  "data":"",
+                  "visibility":false,
+                  "children":[]
+                }
+            ]
+        },
+        {
+            "data": "f6fefb725df211e5bd150800275f97876",
+            "title": "Item - level 0",
+            "visibility":true,
+            "children":[
+                {
+                  "title":"Item 1 - level 1",
+                  "data":"",
+                  "visibility":false,
+                  "children":[
+                      {
+                          "title":"Item 1a - level 2",
+                          "data":"",
+                          "visibility":true,
+                          "children":[]
+                      }
+                  ]
+                },
+                {
+                    "title":"Item 2 - level 1",
+                    "data":"",
                     "visibility":true,
                     "children":[
                         {
-                          "title":"level 2",
-                          "data":"datasender.mobile_number",
-                          "visibility":true,
-                          "children":[]
+                            "title":"Item 2a - level 2",
+                            "data":"",
+                            "visibility":true,
+                            "children":[
+                              {
+                                "title":"Item 2aa - level 3",
+                                "data":"",
+                                "visibility":true
+                              },
+                              {
+                                "title":"Item 2ab - level 3",
+                                "data":"",
+                                "visibility":true
+                              },
+                              {
+                                "title":"Item 2ac - level 3",
+                                "data":"",
+                                "visibility":true
+                              }
+                            ]
                         },
                         {
-                            "title":"level 2a",
-                            "data":"datasender.email",
+                            "title":"Item 2b - level 2",
+                            "data":"",
                             "visibility":true
                         }
                     ]
@@ -70,9 +184,58 @@ $(document).ready(function () {
             ]
         },
         {
-            "data": "f6fefb725df211e5bd150800275f9736_q4",
-            "title": "level 0a",
-            "visibility":true
+            "data": "f6fefb725df211e5bd150800275f97886",
+            "title": "Item 1 - level 0",
+            "visibility":true,
+            "children":[
+                {
+                  "title":"Item 1 - level 1",
+                  "data":"",
+                  "visibility":false,
+                  "children":[
+                      {
+                          "title":"Item 1a - level 2",
+                          "data":"",
+                          "visibility":true,
+                          "children":[]
+                      }
+                  ]
+                },
+                {
+                    "title":"Item 2 - level 1",
+                    "data":"",
+                    "visibility":true,
+                    "children":[
+                        {
+                            "title":"Item 2a - level 2",
+                            "data":"",
+                            "visibility":true,
+                            "children":[
+                              {
+                                "title":"Item 2aa - level 3",
+                                "data":"",
+                                "visibility":true
+                              },
+                              {
+                                "title":"Item 2ab - level 3",
+                                "data":"",
+                                "visibility":true
+                              },
+                              {
+                                "title":"Item 2ac - level 3",
+                                "data":"",
+                                "visibility":true
+                              }
+                            ]
+                        },
+                        {
+                            "title":"Item 2b - level 2",
+                            "data":"",
+                            "visibility":true
+                        }
+                    ]
+                }
+            ]
         }
     ];
 
@@ -80,41 +243,71 @@ $(document).ready(function () {
     var ColCustomWidget = (function($){
         function ColCustomWidget(customizationHeader) {
             this.$dataTable = $(".analysis_tbl_wrapper");
-            this.$columnWidget = $(".customization_widget");
-            this.$custIcon = $(".cust_icon");
+            this.$columnWidget = $(".customization-widget");
+            this.$custMenu = $(".customization-menu");
+            this.$custIcon = $("#cust-icon, .customization-widget-close");
+            this.$selectAll = $(".select-all");
+            this.$selectNone = $(".select-none");
+            this.$customizationOverlay = $(".customization-overlay");
+            this.$pageHeader = $("#container_header_application");
+            this.$pageContent = $("#container_content");
 
             this.items = customizationHeader;
             this.init();
+
+            //Bind initial click events
+            this.bindEvents();
         }
 
         ColCustomWidget.prototype.init = function() {
-            var self = this;
-
             //Start constructing the widget with loaded Items
             this.constructItems(this.items);
+        };
+
+        ColCustomWidget.prototype.bindEvents = function() {
+
+            var self = this;
+            var customizationOverlayHeight;
 
             this.$custIcon.on("click", function(){
                   if(self.$dataTable.hasClass("shrink")) {
                     self.$dataTable.removeClass("shrink");
                     self.$columnWidget.removeClass("expand");
+                    self.$customizationOverlay.hide();
                   } else {
+                    customizationOverlayHeight = self.$pageHeader.outerHeight() + self.$pageContent.outerHeight() + 30;
                     self.$dataTable.addClass("shrink");
                     self.$columnWidget.addClass("expand");
+                    self.$customizationOverlay.height(customizationOverlayHeight).show();
                   }
             });
 
-            $(".customization_widget input[type=checkbox]").click(function(){
+            $(".customization-menu li").on("click", function() {
+                var $checkBox = $(this).find("input[type=checkbox]");
+                $checkBox.prop("checked", !$checkBox.prop("checked"));
+            });
+
+            this.$selectAll.on("click", function(){
+               self.$custMenu.find("input[type=checkbox]").prop('checked', true);
+            });
+
+            this.$selectNone.on("click", function(){
+               self.$custMenu.find("input[type=checkbox]").prop('checked', false);
+            });
+
+            $(".customization-menu input[type=checkbox]").click(function(){
                 if(this.checked){
                     $(this).parents('li').children('input[type=checkbox]').prop('checked',true);
                 }
                 $(this).parent().find('input[type=checkbox]').prop('checked',this.checked);
             });
+
         };
 
         ColCustomWidget.prototype.constructItems = function(customizationHeader) {
             var self = this;
             $.each(customizationHeader, function(index, value) {
-                var $newParentElement = self.createColItems("ul", value, self.$columnWidget);
+                var $newParentElement = self.createColItems("ul", value, self.$custMenu);
 
                 if(value.hasOwnProperty('children') && (value.children.length > 0)) {
                    self.iterateItems(value.children, $newParentElement);
