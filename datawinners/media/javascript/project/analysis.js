82268a887c57f8b8a61d370bdba9d127f0456755
Sudharshan R <sdn2525@gmail.com>
2015-10-07 14:54:12 +0530

    Sudharshan | #NA | Milena UI feedback changes

--- datawinners/media/javascript/project/analysis.js
+++ datawinners/media/javascript/project/analysis.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
 
+    var analysisTable,
+        colCustomization;
+
     var tableElement = $("#analysis_table");
-    var AnalysisPageDataTable = (function($,tableElement){
-    	function AnalysisPageDataTable(columns){
+    var AnalysisPageDataTable = (function ($, tableElement) {
+        function AnalysisPageDataTable(columns) {
             tableElement.DataTable({
                 "dom": '<ip<t>ipfl>',
-                "language":{
+                "language": {
                     "info": interpolate(gettext("<b>%(start)s to %(end)s</b> of %(total)s %(subject_type)s(s)"),
-                    {'start': '_START_', 'end': '_END_', 'total': '_TOTAL_', subject_type: gettext("Submission")}, true),
+                        {
+                            'start': '_START_',
+                            'end': '_END_',
+                            'total': '_TOTAL_',
+                            subject_type: gettext("Submission")
+                        }, true),
                     "lengthMenu": gettext("Show") + ' _MENU_ ' + gettext("Submission")
                 },
                 "scrollX": true,
@@ -17,18 +25,18 @@ $(document).ready(function () {
                 "ajax": {
                     url: dataUrl
                 },
-                "columns":   columns,
-                "initComplete": function(settings, json) {
-                	$('.dataTables_scrollBody thead tr').css({visibility:'collapse'});
+                "columns": columns,
+                "initComplete": function (settings, json) {
+                    $('.dataTables_scrollBody thead tr').css({visibility: 'collapse'});
                     $(".paging_dw_pagination").show();
                 },
                 "pagingType": "dw_pagination"
             });
-    	};
+        };
 
-    	return AnalysisPageDataTable;
+        return AnalysisPageDataTable;
     })($, tableElement);
-    
+
     $.getJSON(headerUrl, function (columns) {
         analysisTable = new AnalysisPageDataTable(columns);
     });
@@ -41,7 +49,8 @@ $(document).ready(function () {
             this.$dataTable = $(".analysis_tbl_wrapper");
             this.$columnWidget = $(".customization-widget");
             this.$custMenu = $(".customization-menu");
-            this.$custIcon = $("#cust-icon, .customization-widget-close");
+            this.$colWidgetActions = $("#cust-icon, .customization-widget-close");
+            this.$customizationIcon = $("#cust-icon");
             this.$selectAll = $(".select-all");
             this.$selectNone = $(".select-none");
             this.$customizationOverlay = $(".customization-overlay");
@@ -55,73 +64,75 @@ $(document).ready(function () {
             this.bindEvents();
         }
 
-        ColCustomWidget.prototype.init = function() {
+        ColCustomWidget.prototype.init = function () {
             //Start constructing the widget with loaded Items
             this.constructItems(this.items);
         };
 
-        ColCustomWidget.prototype.bindEvents = function() {
+        ColCustomWidget.prototype.bindEvents = function () {
 
             var self = this;
             var customizationOverlayHeight;
 
-            this.$custIcon.on("click", function(){
-                  if(self.$dataTable.hasClass("shrink")) {
+            this.$colWidgetActions.on("click", function () {
+                if (self.$dataTable.hasClass("shrink")) {
                     self.$dataTable.removeClass("shrink");
                     self.$columnWidget.removeClass("expand");
                     self.$customizationOverlay.hide();
-                  } else {
+                    self.$customizationIcon.removeClass("active");
+                } else {
                     self.$columnWidget.addClass("expand");
                     self.$dataTable.addClass("shrink");
                     customizationOverlayHeight = self.$pageHeader.outerHeight() + self.$pageContent.outerHeight() + 30;
                     self.$customizationOverlay.height(customizationOverlayHeight).show();
-                  }
+                    self.$customizationIcon.addClass("active");
+                }
             });
 
-            this.$customizationOverlay.on("click", function() {
+            this.$customizationOverlay.on("click", function () {
                 self.$dataTable.removeClass("shrink");
                 self.$columnWidget.removeClass("expand");
                 $(this).hide();
             });
 
 
-            $(".customization-menu li").on("click", function(event) {
+            $(".customization-menu li").on("click", function (event) {
                 var $checkBox = $(this).find("input[type=checkbox]");
                 $checkBox.prop("checked", !$checkBox.prop("checked"));
                 event.stopPropagation();
             });
 
-            this.$selectAll.on("click", function(){
-               self.$custMenu.find("input[type=checkbox]").prop('checked', true);
+            this.$selectAll.on("click", function () {
+                self.$custMenu.find("input[type=checkbox]").prop('checked', true);
             });
 
-            this.$selectNone.on("click", function(){
-               self.$custMenu.find("input[type=checkbox]").prop('checked', false);
+            this.$selectNone.on("click", function () {
+                self.$custMenu.find("input[type=checkbox]").prop('checked', false);
             });
 
-            $(".customization-menu input[type=checkbox]").click(function(event){
-                if(this.checked){
-                    $(this).parents('li').children('input[type=checkbox]').prop('checked',true);
+            $(".customization-menu input[type=checkbox]").click(function (event) {
+                if (this.checked) {
+                    $(this).parents('li').children('input[type=checkbox]').prop('checked', true);
                 }
-                $(this).parent().find('input[type=checkbox]').prop('checked',this.checked);
+                $(this).parent().find('input[type=checkbox]').prop('checked', this.checked);
                 event.stopPropagation();
             });
 
         };
 
-        ColCustomWidget.prototype.constructItems = function(customizationHeader) {
+        ColCustomWidget.prototype.constructItems = function (customizationHeader) {
             var self = this;
-            $.each(customizationHeader, function(index, value) {
+            $.each(customizationHeader, function (index, value) {
                 var $newParentElement = self.createColItems("ul", value, self.$custMenu);
 
-                if(value.hasOwnProperty('children') && (value.children.length > 0)) {
-                   self.iterateItems(value.children, $newParentElement);
+                if (value.hasOwnProperty('children') && (value.children.length > 0)) {
+                    self.iterateItems(value.children, $newParentElement);
                 }
             });
         };
 
-        ColCustomWidget.prototype.createColItems = function(element, value, parentElement) {
-            var $listElement = $('<'+ element + '/>'),
+        ColCustomWidget.prototype.createColItems = function (element, value, parentElement) {
+            var $listElement = $('<' + element + '/>'),
                 $checkBox = $("<input type='checkbox' value='" + value.title + "'>");
 
             $checkBox.prop('checked', value.visibility);
@@ -131,10 +142,10 @@ $(document).ready(function () {
             return $listElement;
         };
 
-        ColCustomWidget.prototype.iterateItems = function(items, $parentElement) {
+        ColCustomWidget.prototype.iterateItems = function (items, $parentElement) {
             var self = this;
-            $.each(items, function(index, value){
-                if(value.hasOwnProperty('children') && (value.children.length > 0)) {
+            $.each(items, function (index, value) {
+                if (value.hasOwnProperty('children') && (value.children.length > 0)) {
                     var $newParentElement = self.createColItems("ul", value, $parentElement);
                     self.constructChildNodes(value, $newParentElement);
                 } else {
@@ -143,7 +154,7 @@ $(document).ready(function () {
             });
         };
 
-        ColCustomWidget.prototype.constructChildNodes = function(value, $parentElement) {
+        ColCustomWidget.prototype.constructChildNodes = function (value, $parentElement) {
             if (value.hasOwnProperty('children') && (value.children.length > 0)) {
                 this.iterateItems(value.children, $parentElement);
             } else {
@@ -156,6 +167,32 @@ $(document).ready(function () {
 
     $.getJSON(preferenceUrl, function (customizationHeader) {
         colCustomization = new ColCustomWidget(customizationHeader);
+    });
+
+    /*Chart View*/
+    var $dataTableviews = $(".chart-view-icon, .list-view-icon"),
+        $chartViewIcon = $(".chart-view-icon"),
+        $listViewIcon = $(".list-view-icon"),
+        $chartViewWrapper = $(".chart-view-wrapper"),
+        $customizationIcon = $("#cust-icon");
+
+    $dataTableviews.on("click", function (event) {
+        if($(event.target).hasClass("active")) {
+            return;
+        }
+        if ($("#analysis_table_wrapper").css("display") == "none") {
+            $chartViewIcon.removeClass("active");
+            $listViewIcon.addClass("active");
+            $chartViewWrapper.hide();
+            $custIcon.show();
+            $("#analysis_table_wrapper").show();
+        } else {
+            $chartViewIcon.addClass("active");
+            $listViewIcon.removeClass("active");
+            $("#analysis_table_wrapper").hide();
+            $customizationIcon.hide();
+            $chartViewWrapper.show();
+        }
     });
 
 });
