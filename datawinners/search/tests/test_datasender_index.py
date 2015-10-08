61f86f00c8a9837a7cf67818a767bc106be44697
Alia Sophie <azalia@MacBook-Pro-de-Alia-2.local>
2015-10-08 13:02:17 +0300

    HNI-DEV | #4789 | Test

--- datawinners/search/tests/test_datasender_index.py
+++ datawinners/search/tests/test_datasender_index.py
@@ -44,3 +44,24 @@ class TestDatasenderIndex(unittest.TestCase):
 
             self.assertEquals(["project1", "project2"], result)
 
+    def test_shoud_update_ds_index_with_bulk(self):
+        dbm = Mock(spec=DatabaseManager)
+        dbm.database_name = 'db'
+        entity1_doc = Mock(spec=Entity)
+        entity1_doc.data.return_value = {"name": "test_name", "mobile": "234551"}
+        entity1_doc.id = 'ds1_id'
+        entity1_doc.aggregation_paths = {"_type": ["reporter"]}
+
+        with patch("datawinners.search.datasender_index.get_elasticsearch_handle") as es:
+            with patch("datawinners.search.datasender_index.get_form_model_by_code") as get_form_model:
+                with patch("datawinners.search.datasender_index._create_contact_dict") as create_ds_dict:
+                    mock_form_model = Mock(spec=FormModel)
+                    mock_ds_dict = {"name": "name"}
+                    get_form_model.return_value = mock_form_model
+                    create_ds_dict.return_value = mock_ds_dict
+                    mock_es = Mock()
+                    es.return_value = mock_es
+
+                    operation = update_datasender_index(entity1_doc, dbm, bulk=True)
+                    result = mock_es.index_op(mock_ds_dict, doc_type='reporter', index='db')
+                    self.assertEquals(result, operation)
