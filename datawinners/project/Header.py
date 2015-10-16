9fc520a8cb70ce0bd68879f2d229003987dbe5c1
natch <natchup@sahajsoft.com>
2015-10-16 11:41:04 +0530

    Natchu | #NA | Added keys for translations

--- datawinners/project/Header.py
+++ datawinners/project/Header.py
@@ -30,7 +30,6 @@ class Header(object):
     def header_type_list(self):
         return self._header_type_list
 
-
     def _select_header(self):
         return filter(lambda each: each, self._prefix()) + self._fields_header()
 
@@ -65,84 +64,89 @@ class SubmissionsPageHeader():
         header = HeaderFactory(self._form_model).create_header(self.submission_type)
         header_dict = header.get_header_field_dict()
         header_dict.pop('ds_id', None)
-        unique_question_field_names = [es_unique_id_code_field_name(es_questionnaire_field_name(field.code, self._form_model.id, field.parent_field_code)) for
+        unique_question_field_names = [es_unique_id_code_field_name(
+            es_questionnaire_field_name(field.code, self._form_model.id, field.parent_field_code)) for
                                        field in
                                        self._form_model.entity_questions]
         for field_name in unique_question_field_names:
             header_dict.pop(field_name, None)
         return header_dict.values()
 
+
 class AnalysisPageHeader():
     def __init__(self, form_model, dbm, user_id):
         self._form_model = form_model
         self._dbm = dbm
         self._user_id = user_id
-    
+
     def get_column_title(self):
         header = []
         user_questionnaire_preference = get_user_questionnaire_preference(self._dbm, self._user_id, self._form_model.id)
 
         generic_columns = OrderedDict()
-        generic_columns['date']='Submission Date'
-        generic_columns['datasender.name']='Data Sender Name'
-        generic_columns['datasender.id']='Data Sender ID Number'
-        generic_columns['datasender.mobile_number']='Data Sender Mobile Number'
-        generic_columns['datasender.email']='Data Sender Email'
-        generic_columns['datasender.groups']='Data Sender Groups'
-        generic_columns['datasender.location']='Data Sender Location'
-         
+
+        generic_columns['date'] = ugettext("Submission Date")
+        generic_columns['datasender.name'] = ugettext("Data Sender Name")
+        generic_columns['datasender.id'] = ugettext("Data Sender Id")
+        generic_columns['datasender.mobile_number'] = ugettext("Data Sender Mobile Number")
+        generic_columns['datasender.email'] = ugettext("Data Sender Email")
+        generic_columns['datasender.groups'] = ugettext("Data Sender Groups")
+        generic_columns['datasender.location'] = ugettext("Data Sender Location")
+
+
         for column_id, column_title in generic_columns.iteritems():
             header.append(self._form_column_info(
-                                                 column_id, column_title,
-                                                 detect_visibility(
-                                                                   user_questionnaire_preference,
-                                                                   column_id)))
+                column_id, column_title,
+                detect_visibility(
+                    user_questionnaire_preference,
+                    column_id)))
 
         for field in self._form_model.fields:
             prefix = self._form_model.id + "_" + field.code + "_details"
             if field.is_entity_field:
                 entity_type_info = get_entity_type_info(field.unique_id_type, self._dbm)
-                #ID Nr mandatory fields, with mandatory order of fields
-                #q2 is the name field, q6 is the code field
-                expected_mandatory_fields_with_order = ['q2','q6']
-#                 [for col in expected_mandatory_fields_with_order:
+                # ID Nr mandatory fields, with mandatory order of fields
+                # q2 is the name field, q6 is the code field
+                expected_mandatory_fields_with_order = ['q2', 'q6']
+                #                 [for col in expected_mandatory_fields_with_order:
                 for val in expected_mandatory_fields_with_order:
                     if val in entity_type_info['codes']:
                         idx = entity_type_info['codes'].index(val)
-                        column_id = prefix+"."+val
+                        column_id = prefix + "." + val
                         header.append(self._form_column_info(
-                                                             column_id, entity_type_info['labels'][idx],
-                                                             detect_visibility(
-                                                                               user_questionnaire_preference,
-                                                                               column_id)))
+                            column_id, entity_type_info['labels'][idx],
+                            detect_visibility(
+                                user_questionnaire_preference,
+                                column_id)))
 
                 for idx, val in enumerate(entity_type_info['codes']):
                     if val not in entity_type_info['codes']:
-                        column_id = prefix+"."+val
+                        column_id = prefix + "." + val
                         header.append(self._form_column_info(
-                                                             column_id, entity_type_info['labels'][idx],
-                                                             detect_visibility(
-                                                                               user_questionnaire_preference,
-                                                                               column_id)))
+                            column_id, entity_type_info['labels'][idx],
+                            detect_visibility(
+                                user_questionnaire_preference,
+                                column_id)))
 
             else:
-                column_id = self._form_model.id+'_'+field.code
+                column_id = self._form_model.id + '_' + field.code
                 header.append(self._form_column_info(
-                                                     column_id, field.label,
-                                                     detect_visibility(
-                                                                       user_questionnaire_preference,
-                                                                       column_id)))
+                    column_id, field.label,
+                    detect_visibility(
+                        user_questionnaire_preference,
+                        column_id)))
 
         return header
-    
+
     def _form_column_info(self, column_id, column_title, visibility):
         return {
-                "data": column_id,
-                "name": column_id, 
-                "title": column_title, 
-                "defaultContent": "", 
-                "visible":visibility
-               }
+            "data": column_id,
+            "name": column_id,
+            "title": column_title,
+            "defaultContent": "",
+            "visible": visibility
+        }
+
 
 class SubmissionExcelHeader():
     def __init__(self, form_model, submission_type, language='en'):
@@ -178,7 +182,7 @@ class SubmissionExcelHeader():
         for key in header_dict:
             if key != SubmissionIndexConstants.DATASENDER_ID_KEY:
                 result.update({key: {"label": header_dict[key]}})
-                if key == SubmissionIndexConstants.DATASENDER_NAME_KEY: #add key column after name
+                if key == SubmissionIndexConstants.DATASENDER_NAME_KEY:  # add key column after name
                     self.add_datasender_id_column(header_dict, result)
 
         self._update_with_field_meta(self._form_model.fields, result, header=header)
@@ -195,6 +199,6 @@ class SubmissionExcelHeader():
                 header.add_unique_id_field_in_repeat(field, col)
             if field.type == "field_set":
                 details.update({"code": field.code})
-                details.update({"fields" : self.get_sub_fields_of(field, header)})
+                details.update({"fields": self.get_sub_fields_of(field, header)})
 
         return col
