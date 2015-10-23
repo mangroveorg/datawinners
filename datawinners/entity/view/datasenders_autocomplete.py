bf6a2c90180b711f32c19ec096f3960a806fd041
Sairam Krish <haisairam@gmail.com>
2015-10-23 20:01:37 +0530

    Sairam | #4794 | Updated datasender autocomplete elastic search logic

--- datawinners/entity/view/datasenders_autocomplete.py
+++ datawinners/entity/view/datasenders_autocomplete.py
@@ -4,21 +4,32 @@ from django.http import HttpResponse
 from django.views.generic.base import View
 import elasticutils
 from datawinners.main.utils import get_database_name
-from datawinners.settings import ELASTIC_SEARCH_URL, ELASTIC_SEARCH_TIMEOUT
+from datawinners.settings import ELASTIC_SEARCH_URL, ELASTIC_SEARCH_TIMEOUT,\
+    ELASTIC_SEARCH_HOST, ELASTIC_SEARCH_PORT
+from elasticsearch.client import Elasticsearch
+from elasticsearch_dsl.search import Search
+from celery.bin.celery import result
 
 
 class AllDataSenderAutoCompleteView(View):
     def get(self, request):
-        search_text = lower(request.GET["term"] or "")
         database_name = get_database_name(request.user)
-        query = elasticutils.S().es(urls=ELASTIC_SEARCH_URL, timeout=ELASTIC_SEARCH_TIMEOUT).indexes(database_name).doctypes("reporter") \
-            .query(or_={'name__match': search_text, 'name_value': search_text, 'short_code__match': search_text,
-                        'short_code_value': search_text}) \
-            .values_dict()
-        resp = [{"id": r["short_code"], "label": self.get_label(r)} for r in query[:min(query.count(), 50)]]
+        search_text = request.GET["term"]
+        es = Elasticsearch(hosts=[{"host": ELASTIC_SEARCH_HOST, "port": ELASTIC_SEARCH_PORT}])
+        search = Search(using=es, index=database_name, doc_type="reporter")
+        search = search.extra(**{"size":"10"})
+        if search_text:
+            query_params = {
+                     "query":search_text+"*",
+                     "fields":["name","short_code"],
+                     "analyze_wildcard":True
+                     }
+        search = search.query("query_string", **query_params)
+        search_results = search.execute()
+        resp = [{"id": result.short_code, "label": self.get_label(result)} for result in search_results.hits ]
         return HttpResponse(json.dumps(resp))
 
     def get_label(self, r):
-        if r['name']:
-            return r['name']
-        return r['mobile_number']
+        if r.name:
+            return r.name
+        return r.mobile_number
