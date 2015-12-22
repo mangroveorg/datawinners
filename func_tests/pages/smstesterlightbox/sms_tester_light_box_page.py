64a7569de187b52837a886797a4ab6bc99ffd21c
herihaja <hery.imiary@gmail.com>
2015-12-22 07:54:25 +0300

    Heri | #NA | Fix issue with light box (ft)

--- func_tests/pages/smstesterlightbox/sms_tester_light_box_page.py
+++ func_tests/pages/smstesterlightbox/sms_tester_light_box_page.py
@@ -23,7 +23,7 @@ class SMSTesterLightBoxPage(LightBox):
         """
         self.driver.find_text_box(SMS_TA).enter_text(fetch_(SMS, from_(sms_data)))
         self.driver.find(SEND_SMS_BTN).click()
-        self.driver.wait_until_modal_dismissed(10)
+        self.driver.wait_until_modal_dismissed()
         return self
 
     def get_response_message(self):
--- func_tests/tests/projects/questionnairetests/project_questionnaire_tests.py
+++ func_tests/tests/projects/questionnairetests/project_questionnaire_tests.py
@@ -119,7 +119,7 @@ class TestProjectQuestionnaire(HeadlessRunnerTest):
         self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
         login_page = LoginPage(self.driver)
         login_page.do_successful_login_with(VALID_CREDENTIALS)
-        id_number = EDIT_PROJECT_QUESTIONNAIRE_DATA.get(QUESTIONS[-1]).get(NEW_UNIQUE_ID_TYPE)
+        id_number = EDIT_PROJECT_QUESTIONNAIRE_DATA.get(QUESTIONS)[-1].get(NEW_UNIQUE_ID_TYPE)
         EDIT_PROJECT_QUESTIONNAIRE_DATA.get(QUESTIONS)[-1].update({EXISTING_UNIQUE_ID_TYPE:id_number})
         project_name, questionnaire_code = self._create_project(EDIT_PROJECT_DATA, EDIT_PROJECT_QUESTIONNAIRE_DATA)
         self.questionnaire_tab_page.change_answer_type_to_choice_question()
