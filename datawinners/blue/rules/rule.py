dde8b116a46ee10c6aa2032a3d79b1d3ecdd0e2d
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2016-03-04 12:26:10 +0530

    Ram/Raj | #4933 | fixing repeat, refactoring

--- datawinners/blue/rules/rule.py
+++ datawinners/blue/rules/rule.py
@@ -2,6 +2,7 @@ import abc
 import xml.etree.ElementTree as ET
 
 from mangrove.form_model.field import FieldSet
+from mangrove.form_model.xform import get_node
 
 
 class Rule(object):
@@ -9,17 +10,18 @@ class Rule(object):
 
     def update_xform(self, old_questionnaire, new_questionnaire):
         xform = old_questionnaire.xform_model
-        self._update_xform(new_questionnaire.fields, old_questionnaire.fields, xform,  xform.get_body_node())
+        self._update_xform(new_questionnaire.fields, old_questionnaire.fields, xform.get_body_node())
 
         old_questionnaire.xform = '<?xml version="1.0" encoding="utf-8"?>%s' % ET.tostring(xform.root_node)
 
-    def _update_xform(self, new_fields, old_fields, xform, parent_node):
+    def _update_xform(self, new_fields, old_fields, parent_node):
         for old_field in old_fields:
-            node = xform.get_node(parent_node, old_field.code)
+            node = get_node(parent_node, old_field.code)
             new_field = [new_field for new_field in new_fields if new_field.code == old_field.code]
-            if new_field:
+            is_calculated_field = hasattr(old_field, 'is_calculated') and old_field.is_calculated
+            if new_field and not is_calculated_field:
                 if isinstance(old_field, FieldSet):
-                    self._update_xform(new_field[0].fields, old_field.fields, xform, node)
+                    self._update_xform(new_field[0].fields, old_field.fields, node)
                 self.edit(node, old_field, new_field[0])
             else:
                 self.remove()
