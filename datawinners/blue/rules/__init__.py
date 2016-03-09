59f6f5ab078f46b1ca5236dfe6e8423bf7ce112e
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2016-03-09 11:41:26 +0530

    Ram | #4934 | registering constraint message rule

--- datawinners/blue/rules/__init__.py
+++ datawinners/blue/rules/__init__.py
@@ -1,4 +1,4 @@
 from datawinners.blue.rules.bind_rule import ConstraintMessageRule
 from datawinners.blue.rules.edit_rule import EditLabelRule, EditHintRule
 
-REGISTERED_RULES = [EditLabelRule(), EditHintRule()]
+REGISTERED_RULES = [EditLabelRule(), EditHintRule(), ConstraintMessageRule()]
