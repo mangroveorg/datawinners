925ef206ec6276a1b95360950ff4752c4392d7c0
Sairam Krish <haisairam@gmail.com>
2016-03-24 16:12:16 +0530

    Sairam | #4969 | Adding builder to questionnaire edit flow. Made it as a feature toggle

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var gulp = require("gulp");
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+//var concat = require('gulp-concat');
+var babelify = require("babelify");
+
+var config = {
+	    paths: {
+	      dist : './datawinners/media/javascript/questionnaire_builder/dist',
+	      main_js : './datawinners/media/javascript/questionnaire_builder/src/main.js',
+	      js : './datawinners/media/javascript/questionnaire_builder/src/**/*.js',
+	      css: [
+	            'node_modules/toastr/build/toastr.min.css',
+	            './datawinners/media/css/questionnaire_builder/**/*.css'
+
+	      	]
+
+	    }
+	};
+
+gulp.task('js', function() {
+	browserify({})
+		.require(config.paths.main_js, {entry: true})
+		.transform([babelify,reactify])
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist))
+});
+
+gulp.task("default",["js"])
