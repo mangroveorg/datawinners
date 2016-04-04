fcad08c02ee81e65d8f6462d627e0e9df2de2d5b
natch <natchup@sahajsoft.com>
2016-04-04 13:30:05 +0530

    Natchu | #4967 | Update itemset attachment on update of questionnaire

--- datawinners/blue/view.py
+++ datawinners/blue/view.py
@@ -300,6 +300,7 @@ class ProjectUpdate(View):
 
             base_name, extension = os.path.splitext(tmp_file.name)
             questionnaire.update_attachments(tmp_file, 'questionnaire%s' % extension)
+            questionnaire.update_attachments(mangrove_service.itemsets_csv, "itemsets.csv")
             self._purge_submissions(manager, questionnaire)
             self._purge_feed_documents(questionnaire, request)
             self._purge_media_details_documents(manager, questionnaire)
