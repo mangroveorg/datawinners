bb0bf159adae3d87772d8674eb0238441a461ae6
Sairam Krish <haisairam@gmail.com>
2016-04-08 18:31:48 +0530

    Sairam | #4972 | Updated builder packaging to handle ES6 polyfill for old browsers

--- datawinners/media/javascript/questionnaire_builder/src/main.js
+++ datawinners/media/javascript/questionnaire_builder/src/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import "babel-polyfill";
 import QuestionnaireList from './components/questionnaire-list';
 import QuestionnaireActions from './actions/questionnaire-actions';
 import injectTapEventPlugin from 'react-tap-event-plugin';
