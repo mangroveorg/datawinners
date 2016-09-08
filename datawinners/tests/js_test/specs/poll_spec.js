036b593e0719136c330eca772a4919dbb9a15f7c
andry <andry@hni.org>
2016-09-08 10:38:25 +0300

    Andry | #after_r32 | Fixing tests on poll (javascript test)

--- datawinners/tests/js_test/specs/poll_spec.js
+++ datawinners/tests/js_test/specs/poll_spec.js
@@ -15,14 +15,14 @@ describe("Deactivate Poll", function() {
             d.resolve('{"success":true}');
             return d.promise();
         });
-        expect(poll_options.status()).toBe('Active');
+        expect(poll_options.status()).toBe('active');
 
         poll_options.deactivate_poll();
         expect($.ajax.mostRecentCall.args[0]['url']).toEqual("http://deactivate_poll_url.com");
         expect($.ajax.mostRecentCall.args[0]["type"]).toEqual("post");
         expect($.ajax.mostRecentCall.args[0]["data"]).toEqual({});
 
-        expect(poll_options.status()).toBe('Deactivated');
+        expect(poll_options.status()).toBe('deactivated');
         expect(poll_options.duration()).toBe(' inactive');
         expect(poll_options.activation()).toBe('Activate');
         expect(poll_options.deactivation()).toBe('');
@@ -53,6 +53,7 @@ describe("Activate Poll", function() {
         poll_options.number_of_days(3);
         expected_date.setDate(current_date.getDate()+3);
         var expected_end_date = expected_date.getFullYear() +"-"+(expected_date.getMonth()+1) +"-"+ expected_date.getDate();
+        poll_options.endDate(expected_date);
         poll_options.activate_poll();
 
         var calculateDays = new CalculateDays(expected_date, current_date);
@@ -61,10 +62,10 @@ describe("Activate Poll", function() {
         expect($.ajax.mostRecentCall.args[0]["type"]).toEqual("post");
         expect($.ajax.mostRecentCall.args[0]["data"]).toEqual({"end_date": expected_end_date + "T23:59:00"});
 
-        expect(poll_options.status()).toBe('Active');
+        expect(poll_options.status()).toBe('active');
         expect(poll_options.from_date_poll()).toBe(current_date);
         expect(poll_options.to_date_poll()).toBe(calculateDays.get_formatted_date_for_poll(expected_date));
-        expect(poll_options.duration()).toBe(' active From : '+ current_date +'To : '+ calculateDays.get_formatted_date_for_poll(expected_date));
+        expect(poll_options.duration()).toBe(' active from '+ current_date +' to '+ calculateDays.get_formatted_date_for_poll(expected_date));
         expect(poll_options.activation()).toBe('');
         expect(poll_options.deactivation()).toBe('Deactivate');
         expect(poll_options.number_of_days()).toBe(3);
@@ -86,12 +87,13 @@ describe("Activate Poll", function() {
             d.resolve('{"success":true}');
             return d.promise();
         });
-        expect(poll_options.status()).toBe('Active');
+        expect(poll_options.status()).toBe('active');
 
         poll_options.number_of_days(5);
         expected_date.setDate(current_date.getDate()+5);
         var expected_end_date = expected_date.getFullYear() + "-" + (expected_date.getMonth() + 1) + "-" + expected_date.getDate();
-        poll_options.activate_poll();
+        poll_options.endDate(expected_date);
+        poll_options.edit_poll();
 
         var calculateDays = new CalculateDays(expected_date, current_date);
 
@@ -99,10 +101,10 @@ describe("Activate Poll", function() {
         expect($.ajax.mostRecentCall.args[0]["type"]).toEqual("post");
         expect($.ajax.mostRecentCall.args[0]["data"]).toEqual({"end_date": expected_end_date + "T23:59:00"});
 
-        expect(poll_options.status()).toBe('Active');
+        expect(poll_options.status()).toBe('active');
         expect(poll_options.from_date_poll()).toBe(current_date);
         expect(poll_options.to_date_poll()).toBe(calculateDays.get_formatted_date_for_poll(expected_date));
-        expect(poll_options.duration()).toBe(' active From : '+ current_date +'To : '+calculateDays.get_formatted_date_for_poll(expected_date));
+        expect(poll_options.duration()).toBe(' active from '+ current_date +' to '+calculateDays.get_formatted_date_for_poll(expected_date));
         expect(poll_options.activation()).toBe('');
         expect(poll_options.deactivation()).toBe('Deactivate');
         expect(poll_options.number_of_days()).toBe(5);
