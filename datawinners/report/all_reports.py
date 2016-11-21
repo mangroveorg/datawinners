b6af5c9a199af027e3060a7923efc868bcd8c28f
natch <natchup@sahajsoft.com>
2016-11-21 12:31:03 +0530

    Raj/Natchu | #5093 | UI Enhancements - Dynamic styling

--- datawinners/report/all_reports.py
+++ datawinners/report/all_reports.py
@@ -15,16 +15,39 @@ class AllReportsView(TemplateView):
         configs = get_report_configs(dbm)
         return self.render_to_response(RequestContext(request, {
             "configs": configs,
-            "content": (configs or '') and _get_content(dbm, configs[0], request)
+            "content": (configs or '') and Template(_build_report_content(dbm, configs[0], request)).render(
+                RequestContext(request))
         }))
 
 
 def report_content(request, report_id):
     dbm = get_database_manager(request.user)
     config = get_report_config(dbm, report_id)
-    return HttpResponse(_get_content(dbm, config, request))
+    content = _build_report_content(dbm, config, request)
+    return HttpResponse(content)
+
+
+def report_stylesheet(request, report_id):
+    dbm = get_database_manager(request.user)
+    config = get_report_config(dbm, report_id)
+    style = config.stylesheet().replace("{{report_id}}", "report_"+report_id)
+    return HttpResponse(mimetype="text/css", content=style)
+
+
+def _build_report_content(dbm, config, request):
+    content = ""
+    content += _get_style_content(config)
+    content += _get_content(dbm, config, request)
+    return content
+
+
+def _get_style_content(config):
+    style = '<link rel="stylesheet" href="/reports/' + config.id + '/stylesheet/" />'
+    return style
 
 
 def _get_content(dbm, config, request):
     page_number = request.GET.get("page_number") or "1"
-    return Template(config.template()).render(RequestContext(request, {"report_data": get_report_data(dbm, config, int(page_number))}))
+    return Template(config.template()).render(
+        RequestContext(request,
+                       {"report_data": get_report_data(dbm, config, int(page_number)), "report_id": "report_"+config.id}))
--- datawinners/report/urls.py
+++ datawinners/report/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 
-from datawinners.report.all_reports import AllReportsView, report_content
+
+from datawinners.report.all_reports import AllReportsView, report_content, report_stylesheet
 
 urlpatterns = patterns('',
+                       url(r'^reports/(?P<report_id>.+?)/stylesheet/$', report_stylesheet),
                        url(r'reports/$', AllReportsView.as_view(), name='all_reports'),
                        url(r'^reports/(?P<report_id>.+?)/$', report_content)
                        )
