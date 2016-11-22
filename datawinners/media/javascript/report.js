698eef1f1ec8dfaaf18cdeb0e57657d6a70c6295
rajbharath <rbrajbharath1@gmail.com>
2016-11-22 13:19:56 +0530

    Raj/Natchu | #5093 | UI Enhancements - Loader on data loading

--- datawinners/media/javascript/report.js
+++ datawinners/media/javascript/report.js
@@ -8,16 +8,22 @@ $(function(){
     };
 
     var loadReportTabCallback = function(anchorElement) {
+        $( document ).ajaxComplete(function() {
+          $.unblockUI();
+        });
         $("#report_navigation li.active").addClass("inactive");
         $("#report_navigation li.active").removeClass("active");
         anchorElement.parent().addClass("active");
-        initPaginationWidget(anchorElement)
+        initPaginationWidget(anchorElement);
     };
 
     $("#report_navigation a").click(function(){
         var anchorElement = $(this);
         $("#report_container>.pagination_container").empty();
         $("#report_container>.report_content").load(anchorElement.attr("id"), function() {loadReportTabCallback(anchorElement);});
+        $(document).ajaxStart(function() {
+            $.blockUI({ message: '<h1><img src="/media/images/ajax-loader.gif"/><span class="loading">' + gettext("Just a moment") + '...</span></h1>', css: { width: '275px'}});
+        });
     });
 
     initPaginationWidget($('#report_navigation li.active a'));
--- datawinners/report/urls.py
+++ datawinners/report/urls.py
@@ -5,7 +5,7 @@ from datawinners.report.all_reports import AllReportsView, report_content, repor
 
 urlpatterns = patterns('',
                        url(r'^reports/(?P<report_id>.+?)/stylesheet/$', report_stylesheet),
-                       url(r'^reports/report_(?P<report_id>.+?)/fonts/(?P<font_file_name>.+?)/$', report_font_file),
+                       url(r'^reports/(?P<report_id>.+?)/fonts/(?P<font_file_name>.+?)/$', report_font_file),
                        url(r'reports/$', AllReportsView.as_view(), name='all_reports'),
                        url(r'^reports/(?P<report_id>.+?)/$', report_content)
                        )
