91685a41b21653918f44e1d37bc77e04b779d34a
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2016-12-20 16:17:40 +0530

    Ram | #5095 | fix broken range date fix

--- datawinners/report/filter.py
+++ datawinners/report/filter.py
@@ -98,7 +98,7 @@ def _filter_value(qn, filters):
     value = type_and_value and type_and_value.split(";")[2] or None
     if _type(qn, filters) == "date":
         value = value and [parse_date(date) for date in value.split("to")]
-        value = value and len(value) == 1 and value + value
+        value = value and len(value) == 1 and value + value or value
     if is_idnr_question(qn):
         value = value and {get_idnr_question(qn): value}
     return value
