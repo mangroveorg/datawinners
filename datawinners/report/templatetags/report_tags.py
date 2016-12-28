df005867d5eeb9eeeda930e437be9dabe94af909
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2016-12-28 19:06:25 +0530

    Ram/Natchu | #5100 | stopgap load previously selected filter values

--- datawinners/report/templatetags/report_tags.py
+++ datawinners/report/templatetags/report_tags.py
@@ -28,6 +28,7 @@ def count(qn):
 @register.inclusion_tag('report/filters.html', takes_context=True)
 def filters(context):
     report_filters = get_report_filters(context.get("dbm"), context.get("config"), context.get("config").questionnaires[0])
+    _stopgap_load_previous_selected_values(context.get("filters"), report_filters)
     return {
         "report_id": context.get("report_id"),
         "idnr_filters": report_filters["idnr_filters"],
@@ -51,3 +52,21 @@ class LoopNode(template.Node):
 
     def _generate_html(self, resolved_data):
         return "".join(["<tr>%s</tr>" % "".join(["<td>%s</td>" % row[val] for val in self._parse_cell_values()]) for row in resolved_data])
+
+
+def _stopgap_load_previous_selected_values(filter_values, report_filters):
+    for report_filter in report_filters["idnr_filters"]:
+        new_options = report_filter["field"].options
+        if report_filter["field"].identifier in filter_values and \
+                filter_values[report_filter["field"].identifier].split(";")[-1]:
+            new_options = []
+            for option in report_filter["field"].options:
+                new_option = option
+                if filter_values[report_filter["field"].identifier].split(";")[-1] == option[0]:
+                    new_option = option + ('selected',)
+                new_options.append(new_option)
+        setattr(report_filter["field"], "new_options", new_options)
+    for report_filter in report_filters["date_filters"]:
+        if report_filter["field"].identifier in filter_values and \
+                filter_values[report_filter["field"].identifier].split(";")[-1]:
+            setattr(report_filter["field"], "value", filter_values[report_filter["field"].identifier].split(";")[-1])
