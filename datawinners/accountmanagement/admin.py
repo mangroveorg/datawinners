28ea756dc8353d2f474d0d861198ca00fc11ba7d
Mino <mino@mino-VirtualBox.(none)>
2018-06-14 11:05:43 +0300

    Mino | #831 | Adding payment period and payment transfer type on organisation django admin page
    Conflicts:
    
    	datawinners/accountmanagement/admin.py

--- datawinners/accountmanagement/admin.py
+++ datawinners/accountmanagement/admin.py
@@ -71,6 +71,8 @@ class OrganizationSettingAdmin(DatawinnerAdmin):
 
         return "--"
 
+
+
     def type(self, obj):
         return obj.organization.account_type
 
@@ -249,7 +251,7 @@ class OrganizationChangeList(ChangeList):
 
 class OrganizationAdmin(DatawinnerAdmin):
     list_display = (
-        'name', 'org_id', 'complete_address', 'office_phone', 'website', 'paid', 'active_date', 'admin_name',
+        'name', 'org_id', 'complete_address', 'office_phone', 'website', 'paid', 'active_date', 'account_type', 'payment_period', 'admin_name',
         'admin_email', 'admin_mobile_number', 'sms_api_users', 'status')
     actions = ['deactivate_organizations', 'activate_organizations', 'delete_organizations']
     search_fields = ['name', 'address', 'addressline2', 'city', 'zipcode', 'state', 'office_phone', 'website']
