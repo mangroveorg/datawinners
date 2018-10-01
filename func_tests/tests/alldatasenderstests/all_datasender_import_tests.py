1d09569c38a6895f1f718d83c42958ee9d802277
Mariot Tsitoara <mariot.tsitoara@gmail.com>
2018-10-01 10:55:36 +0300

    Mariot | #952 | FT for Edit bulk data senders in batch using Excel Import

--- func_tests/tests/alldatasenderstests/all_datasender_import_tests.py
+++ func_tests/tests/alldatasenderstests/all_datasender_import_tests.py
@@ -25,11 +25,26 @@ workbook_row2 = {
     'ds_email': 'DsEmail' + random_number(3) + "@dw.com"
 }
 
+workbook_row3 = {
+    'ds_name': "FName" + random_number(2),
+    'ds_number': random_number(6),
+    'ds_location': "Loc" + random_number(2),
+    'gps_coordinates': '%.1f, ' % float(random_number(1)) + '%.1f' % float(random_number(1)),
+    'ds_email': 'DsEmail' + random_number(3) + "@dw.com"
+}
+
+workbook_row4 = {
+    'ds_name': "FName" + random_number(2),
+    'ds_number': random_number(6),
+    'ds_location': "Loc" + random_number(2),
+    'gps_coordinates': '%.1f, ' % float(random_number(1)) + '%.1f' % float(random_number(1)),
+    'ds_email': 'DsEmail' + random_number(3) + "@dw.com"
+}
 
 class TestAllDataSendersImport(HeadlessRunnerTest):
     @classmethod
     def setUpClass(cls):
-        HeadlessRunnerTest.setUpClass()
+        HeadlessRunnerTest.setUpClassFirefox()
         login(cls.driver)
 
     @classmethod
@@ -72,6 +87,26 @@ class TestAllDataSendersImport(HeadlessRunnerTest):
         worksheet['E3'] = workbook_row2['ds_email']
         return file_name, file_path, workbook
 
+    def _prepare_update_workbook(self):
+        file_name = 'DataWinners_ImportContacts.xlsx'
+        DIR = os.path.dirname(__file__) + '/'
+        file_path = os.path.join(DIR, file_name)
+        workbook = load_workbook(file_path)
+        worksheet = workbook.get_sheet_by_name('Import Contacts')
+        worksheet['A2'] = workbook_row3['ds_name']
+        worksheet['B2'] = workbook_row3['ds_number']
+        worksheet['C2'] = workbook_row3['ds_location']
+        worksheet['D2'] = workbook_row3['gps_coordinates']
+        worksheet['E2'] = workbook_row3['ds_email']
+
+        worksheet['A3'] = workbook_row4['ds_name']
+        worksheet['B3'] = workbook_row4['ds_number']
+        worksheet['C3'] = workbook_row4['ds_location']
+        worksheet['D3'] = workbook_row4['gps_coordinates']
+        worksheet['E3'] = workbook_row4['ds_email']
+
+        return file_name, file_path, workbook
+
     @attr('functional_test')
     def test_should_import_more_than_one_data_sender(self):
         file_name, file_path, workbook = self._prepare_workbook()
@@ -86,3 +121,22 @@ class TestAllDataSendersImport(HeadlessRunnerTest):
         self._assert_datasender_uploaded(all_datasender_page, workbook_row1)
 
         self._assert_datasender_uploaded(all_datasender_page, workbook_row2)
+
+    @attr('functional_test')
+    def test_should_import_zand_edit_data_senders(self):
+        file_name, file_path, workbook = self._prepare_workbook()
+        workbook.save(file_path)
+        global_navigation = GlobalNavigationPage(self.driver)
+        all_datasender_page = global_navigation.navigate_to_all_data_sender_page()
+        self._upload_imported_datasender(file_name, file_path)
+        self._assert_datasender_uploaded(all_datasender_page, workbook_row1)
+        self._assert_datasender_uploaded(all_datasender_page, workbook_row2)
+
+        file_name, file_path, workbook = self._prepare_update_workbook()
+        workbook.save(file_path)
+        global_navigation = GlobalNavigationPage(self.driver)
+        all_datasender_page = global_navigation.navigate_to_all_data_sender_page()
+        self._upload_imported_datasender(file_name, file_path)
+        self._assert_datasender_uploaded(all_datasender_page, workbook_row3)
+        self._assert_datasender_uploaded(all_datasender_page, workbook_row4)
+
