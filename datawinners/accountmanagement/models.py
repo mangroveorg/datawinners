5ae2036ff967b381626566b19e30b9242d264b71
Mariot Tsitoara <mariot.tsitoara@gmail.com>
2018-11-28 12:52:34 +0300

    Mariot | #1007 | Add registration counter to organization

--- datawinners/accountmanagement/models.py
+++ datawinners/accountmanagement/models.py
@@ -252,6 +252,8 @@ class Organization(models.Model):
         counter_dict['total_sp_submission'] = total_sp_submission
         counter_dict['total_web_submission'] = total_web_submission
 
+        counter_dict['sms_registration_count'] = sum_dict["sms_registration_count"]
+
         total_submission = sms_submission_count + total_web_submission + total_sp_submission
         counter_dict['combined_total_submissions'] = total_submission
 
